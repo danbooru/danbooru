857379fadf2ad9dbb59f47f0601e02cee8e5504a
albert <r888888888@gmail.com>
2010-10-27 19:23:53 -0400

    * Added post voting (partial) * Fixed jrails * Added header to comment/index * Post votes now store the score

---
+++ test/unit/post_vote_test.rb
@@ -0,0 +1,29 @@
+require_relative '../test_helper'
+
+class PostVoteTest < ActiveSupport::TestCase
+  setup do
+    user = Factory.create(:user)
+    CurrentUser.user = user
+    CurrentUser.ip_addr = "127.0.0.1"
+    MEMCACHE.flush_all
+    
+    @post = Factory.create(:post)
+  end
+  
+  context "Voting for a post" do
+    should "interpret up as +1 score" do
+      vote = PostVote.create(:post_id => @post.id, :score => "up")
+      assert_equal(1, vote.score)
+    end
+    
+    should "interpret down as -1 score" do
+      vote = PostVote.create(:post_id => @post.id, :score => "down")
+      assert_equal(-1, vote.score)
+    end
+    
+    should "not accept any other scores" do
+      vote = PostVote.create(:post_id => @post.id, :score => "xxx")
+      assert(vote.errors.any?)
+    end
+  end
+end
---
+++ vendor/plugins/jrails/init.rb
@@ -0,0 +1,3 @@
+$: << File.expand_path("..", __FILE__)
+
+require "lib/jrails"
