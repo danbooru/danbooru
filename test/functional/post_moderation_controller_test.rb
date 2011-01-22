0cfaed90f8fb5916bc87f6d263a1ac8c158ed7bb
albert <r888888888@gmail.com>
2011-01-22 17:04:57 -0500

    fixed some functional tests

--- test/functional/post_moderation_controller_test.rb
+++ test/functional/post_moderation_controller_test.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), %w(.. test_helper))
+require "test_helper"
 
 class PostModerationControllerTest < ActionController::TestCase
   context "A post moderation controller" do
