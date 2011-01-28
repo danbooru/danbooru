a86b986805ef107ce34db6aa4ba493ff984379cb
albert <r888888888@gmail.com>
2011-01-28 18:12:09 -0500

    added session controller test

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -1,8 +1,42 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "the sessions controller" do
+    setup do
+      @user = Factory.create(:user)
+    end
+    
+    context "new action" do
+      should "render" do
+        get :new
+        assert_response :success
+      end
+    end
+    
+    context "create action" do
+      should "create a new session" do
+        post :create, {:name => @user.name, :password => "password"}
+        assert_redirected_to posts_path
+        assert_equal(@user.id, session[:user_id])
+      end
+    end
+    
+    context "destroy action" do
+      setup do
+        CurrentUser.user = @user
+        CurrentUser.ip_addr = "127.0.0.1"
+      end
+      
+      teardown do
+        CurrentUser.user = nil
+        CurrentUser.ip_addr = nil
+      end
+      
+      should "clear the session" do
+        post :destroy, {}, {:user_id => @user.id}
+        assert_redirected_to posts_path
+        assert_nil(session[:user_id])
+      end
+    end
   end
 end
