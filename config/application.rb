1c964b5189f38a23a59fd3313f245cb10c08168b
albert <r888888888@gmail.com>
2011-05-24 18:04:25 -0400

    upgraded to rails 3.1.0.rc1

--- config/application.rb
+++ config/application.rb
@@ -1,37 +1,40 @@
 require File.expand_path('../boot', __FILE__)
+
 require 'rails/all'
+
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Danbooru
   class Application < Rails::Application
+    config.encoding = "utf-8"
+    config.filter_parameters += [:password]
+    config.assets.enabled = true
     config.autoload_paths += %W(#{config.root}/app/presenters #{config.root}/app/logical)
     config.plugins = [:all]
     config.time_zone = 'Eastern Time (US & Canada)'
-    config.encoding = "utf-8"    
     config.active_record.schema_format = :sql
-    config.filter_parameters << :password
-    config.action_view.javascript_expansions[:defaults] = [
-      "src/lib/jquery-1.6.0.min.js",
-      "src/lib/jquery-ui-1.8.9.custom.min.js",
-      "src/lib/jquery.timeout.js",
-      "src/lib/rails.js",
-      "src/app/common.js",
-      "src/app/cookie.js",
-      "src/app/utility.js",
-      "src/app/posts.js",
-      "src/app/comments.js",
-      "src/app/uploads.js",
-      "src/app/users.js",
-      "src/app/favorites.js",
-      "src/app/post_flags.js",
-      "src/app/post_appeals.js",
-      "src/app/post_moderation.js",
-      "src/app/pools.js",
-      "src/app/wiki_pages.js",
-      "src/app/forum_posts.js",
-      "src/app/notes.js"
-    ]
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+    # config.action_view.javascript_expansions[:defaults] = [
+    #   "src/lib/jquery-1.6.0.min.js",
+    #   "src/lib/jquery-ui-1.8.9.custom.min.js",
+    #   "src/lib/jquery.timeout.js",
+    #   "src/lib/rails.js",
+    #   "src/app/common.js",
+    #   "src/app/cookie.js",
+    #   "src/app/utility.js",
+    #   "src/app/posts.js",
+    #   "src/app/comments.js",
+    #   "src/app/uploads.js",
+    #   "src/app/users.js",
+    #   "src/app/favorites.js",
+    #   "src/app/post_flags.js",
+    #   "src/app/post_appeals.js",
+    #   "src/app/post_moderation.js",
+    #   "src/app/pools.js",
+    #   "src/app/wiki_pages.js",
+    #   "src/app/forum_posts.js",
+    #   "src/app/notes.js"
+    # ]
   end
 end
-
--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,3 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Add new inflection rules using the following format
+# (all these examples are active by default):
 # ActiveSupport::Inflector.inflections do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
