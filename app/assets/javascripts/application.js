ca68b2def15e419add94619ba09c39a75a539158
albert <r888888888@gmail.com>
2011-05-24 18:24:45 -0400

    moved stylesheets and javascripts to assets

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,7 @@
+//= require jquery-1.6.2.min.js
+//= require jquery-ui-1.8.12.custom.min.js
+//= require jquery.hotkeys.js
+//= require jquery.timeout.js
+//= require rails.js
+//= require common.js
+//= require_tree .
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,7 +3,7 @@ class Tag < ActiveRecord::Base
   after_save :update_category_cache
   has_one :wiki_page, :foreign_key => "name", :primary_key => "title"
   scope :name_matches, lambda {|name| where(["name LIKE ? ESCAPE E'\\\\'", name.to_escaped_for_sql_like])}
-  search_method :name_matches
+  search_methods :name_matches
 
   class CategoryMapping
     Danbooru.config.reverse_tag_category_mapping.each do |value, category|
