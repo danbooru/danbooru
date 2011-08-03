09d7bba90a067033b318c43d236a3283708e1760
albert <r888888888@gmail.com>
2011-08-03 18:40:37 -0400

    added moderator/tag

--- app/controllers/moderator/tags_controller.rb
+++ app/controllers/moderator/tags_controller.rb
@@ -1,6 +1,7 @@
 module Moderator
   class TagsController < ApplicationController
     before_filter :moderator_only
+    rescue_from TagBatchChange::Error, :with => :error
     
     def edit
     end
@@ -9,6 +10,10 @@ module Moderator
       tag_batch_change = TagBatchChange.new(params[:tag][:predicate], params[:tag][:consequent])
       tag_batch_change.execute
       redirect_to edit_moderator_tag_path, :notice => "Posts updated"
+    end
+    
+    def error
+      redirect_to edit_moderator_tag_path, :notice => "Error"
     end
   end
 end
