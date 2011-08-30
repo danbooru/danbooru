f74b6d76a9c8067044c41d64023ec6313e167bdb
albert <r888888888@gmail.com>
2011-08-30 10:25:17 -0400

    added maintenance file

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,12 +64,14 @@ namespace :deploy do
   namespace :web do
     desc "Present a maintenance page to visitors."
     task :disable do
-      run "mv #{current_path}/public/maintenance.html.bak #{current_path}/public/maintenance.html"
+      maintenance_html_path = "#{current_path}/public/maintenance.html.bak"
+      run "if [ -e #{maintenance_html_path} ] ; then mv #{maintenance_html_path} #{current_path}/public/maintenance.html ; fi"
     end
     
     desc "Makes the application web-accessible again."
     task :enable do
-      run "mv #{current_path}/public/maintenance.html #{current_path}/public/maintenance.html.bak"
+      maintenance_html_path = "#{current_path}/public/maintenance.html"
+      run "if [ -e #{maintenance_html_path} ] ; then mv #{maintenance_html_path} #{current_path}/public/maintenance.html.bak ; fi"
     end
   end
 end
