c37766c4c1b45d585011e8bad23c452b50a28ee1
Robin Liao <rliao@riotgames.com>
2011-09-03 15:13:56 -0700

    keyed pagination in post#index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,27 @@
 //= require jquery-1.6.2.min.js
 //= require jquery-ui-1.8.12.custom.min.js
+//= require keymaster.min.js
 //= require jquery.hotkeys.js
 //= require jquery.timeout.js
 //= require rails.js
 //= require common.js
+//= require_self
 //= require_tree .
+
+(function() {
+  Danbooru.Paginator = {};
+  Danbooru.Paginator.next_page = function() {
+    if($('.paginator li span').parent().next().length != 0)
+    {
+      window.location = $('.paginator li span').parent().next().find('a').attr('href');
+    }
+  }
+
+  Danbooru.Paginator.prev_page = function() {
+    if($('.paginator li span').parent().prev().length != 0)
+    {
+      window.location = $('.paginator li span').parent().prev().find('a').attr('href');
+      console.log('logged')
+    }
+  }
+})();
