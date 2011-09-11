b92c31260578d8cf8ab8f53257af1d67c61230e7
albert <r888888888@gmail.com>
2011-09-11 17:25:18 -0400

    removed outdated js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,13 +1,2 @@
 $(function() {
-  $("div#c-users div#a-new footer.nav-links a").click(function(event) {
-    $("div#c-users div#a-new > div").hide();
-    $(event.target.hash).show();
-  });
-  
-  if (Danbooru.meta("errors")) {
-    // $("#p1").hide();
-    // $("#notice").hide();
-  } else {
-    // $("#p2").hide();
-  }
 });
