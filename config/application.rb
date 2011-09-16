713ee07b55cafb2d46b3bdf2f89afc7dffc98f84
albert <r888888888@gmail.com>
2011-09-16 19:04:33 -0400

    removed img dir initializer

--- config/application.rb
+++ config/application.rb
@@ -19,27 +19,5 @@ module Danbooru
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {:enable_starttls_auto => false}
     config.action_mailer.perform_deliveries = true
-    
-    # config.action_view.javascript_expansions[:defaults] = [
-    #   "src/lib/jquery-1.6.0.min.js",
-    #   "src/lib/jquery-ui-1.8.9.custom.min.js",
-    #   "src/lib/jquery.timeout.js",
-    #   "src/lib/rails.js",
-    #   "src/app/common.js",
-    #   "src/app/cookie.js",
-    #   "src/app/utility.js",
-    #   "src/app/posts.js",
-    #   "src/app/comments.js",
-    #   "src/app/uploads.js",
-    #   "src/app/users.js",
-    #   "src/app/favorites.js",
-    #   "src/app/post_flags.js",
-    #   "src/app/post_appeals.js",
-    #   "src/app/post_moderation.js",
-    #   "src/app/pools.js",
-    #   "src/app/wiki_pages.js",
-    #   "src/app/forum_posts.js",
-    #   "src/app/notes.js"
-    # ]
   end
 end
--- config/initializers/create_empty_directories.rb
+++
@@ -1,6 +0,0 @@
-require 'fileutils'
-
-FileUtils.mkdir_p("#{Rails.root}/public/data/preview")
-FileUtils.mkdir_p("#{Rails.root}/public/data/medium")
-FileUtils.mkdir_p("#{Rails.root}/public/data/large")
-FileUtils.mkdir_p("#{Rails.root}/public/data/original")
