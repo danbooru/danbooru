adbbcf34425a44fb4826474167062fea7cc983c3
albert <r888888888@gmail.com>
2011-09-18 16:39:00 -0400

    removed uniqueness constraint for emails for password resets

--- app/models/user_password_reset_nonce.rb
+++ app/models/user_password_reset_nonce.rb
@@ -1,5 +1,4 @@
 class UserPasswordResetNonce < ActiveRecord::Base
-  validates_uniqueness_of :email
   validates_presence_of :email, :key
   validate :validate_existence_of_email
   before_validation :initialize_key, :on => :create
