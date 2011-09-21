50142897853a5523938e60b93448116c6e75acff
albert <r888888888@gmail.com>
2011-09-21 16:03:25 -0400

    bug fix with catch exception

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -74,7 +74,7 @@ class Upload < ActiveRecord::Base
         end
       end
     rescue Exception => x
-      update_attribute(:status, "error: #{x.class} - #{x.message}", :backtrace => x.backtrace.join("\n"))
+      update_attributes(:status => "error: #{x.class} - #{x.message}", :backtrace => x.backtrace.join("\n"))
     ensure
       delete_temp_file
     end
