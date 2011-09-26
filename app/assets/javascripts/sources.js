11b623c66bc46ee9426043e908a0beeb77630db0
albert <r888888888@gmail.com>
2011-09-26 13:04:12 -0400

    refactored main links

--- app/assets/javascripts/sources.js
+++ app/assets/javascripts/sources.js
@@ -2,7 +2,13 @@
   Danbooru.Sources = {};
   
   Danbooru.Sources.get = function(url) {
-    
+    $.get("/sources.json", {
+      url: url
+    }).success(function(data) {
+      console.log(data);
+    }).error(function(data) {
+      console.log(data);
+    });
   }
 })();
 
