81dc20c58f47c07cc316587956e10e005411b089
albert <r888888888@gmail.com>
2011-10-15 22:46:26 -0400

    dtext tweaks

--- app/logical/d_text.rb
+++ app/logical/d_text.rb
@@ -199,7 +199,7 @@ class DText
   def self.sanitize(text)
     Sanitize.clean(
       text,
-      :elements => %w(tn h1 h2 h3 h4 h5 h6 a span div blockquote br p ul li ol em strong small big b i font),
+      :elements => %w(tn h1 h2 h3 h4 h5 h6 a span div blockquote br p ul li ol em strong small b i),
       :attributes => {
         "a" => %w(href title style),
         "span" => %w(class style),
