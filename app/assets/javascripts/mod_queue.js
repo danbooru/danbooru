89636d9ba3931a21cd4cc49573a2e355e2716394
albert <r888888888@gmail.com>
2011-10-26 18:19:00 -0400

    added confirmation step for approve all/hide all

--- app/assets/javascripts/mod_queue.js
+++ app/assets/javascripts/mod_queue.js
@@ -1,20 +1,26 @@
 (function() {
   Danbooru.ModQueue = {};
   
-  Danbooru.ModQueue.initialize_approve_all_button = function(e) {
-    $("#c-moderator-post-queues #approve-all-button").click(function() {
+  Danbooru.ModQueue.initialize_approve_all_button = function() {
+    $("#c-moderator-post-queues #approve-all-button").click(function(e) {
+      if (!confirm("Are you sure you want to approve every post on this page?")) {
+        return;
+      }
+
       $(".approve-link").trigger("click");
+      e.preventDefault();
     });
-    
-    e.preventDefault();
   }
   
-  Danbooru.ModQueue.initialize_hide_all_button = function(e) {
-    $("#c-moderator-post-queues #hide-all-button").click(function() {
+  Danbooru.ModQueue.initialize_hide_all_button = function() {
+    $("#c-moderator-post-queues #hide-all-button").click(function(e) {
+      if (!confirm("Are you sure you want to hide every post on this page?")) {
+        return;
+      }
+
       $(".disapprove-link").trigger("click");
+      e.preventDefault();
     });
-    
-    e.preventDefault();
   }
 })();
 
