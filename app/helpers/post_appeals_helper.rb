1ad033f08b85feb3e9f448edcce48e151751ee6c
albert <r888888888@gmail.com>
2011-12-13 17:20:50 -0500

    better formatting for appeal/flag info

--- app/helpers/post_appeals_helper.rb
+++ app/helpers/post_appeals_helper.rb
@@ -1,9 +1,13 @@
 module PostAppealsHelper
   def post_appeal_reasons(post)
-    post.appeals.map do |appeal|
-      content_tag("span", :class => "flag-and-reason-count") do
-        (appeal.reason + " (" + link_to(appeal.creator.name, user_path(appeal.creator_id)) + ")").html_safe
-      end
-    end.join("; ").html_safe
+    html = []
+    html << '<ul>'
+    
+    post.appeals.each do |appeal|
+      html << '<li>' + appeal.reason + ' - ' + link_to(appeal.creator.name, user_path(appeal.creator)) + ' ' + time_ago_in_words(appeal.created_at) + ' ago</li>'
+    end
+    
+    html << '</ul>'
+    html.join("\n").html_safe
   end
 end
--- app/helpers/post_flags_helper.rb
+++ app/helpers/post_flags_helper.rb
@@ -1,9 +1,13 @@
 module PostFlagsHelper
   def post_flag_reasons(post)
-    post.flags.map do |flag|
-      content_tag("span") do
-        (flag.reason + " (" + link_to(flag.creator.name, user_path(flag.creator_id)) + ")").html_safe
-      end
-    end.join("; ").html_safe
+    html = []
+    html << '<ul>'
+    
+    post.flags.each do |flag|
+      html << '<li>' + flag.reason + ' - ' + link_to(flag.creator.name, user_path(flag.creator)) + ' ' + time_ago_in_words(flag.created_at) + ' ago</li>'
+    end
+    
+    html << '</ul>'
+    html.join("\n").html_safe
   end
 end
