30fb0b660c9947a89ae2bc15595292633cc6e980
albert <r888888888@gmail.com>
2011-12-20 11:57:33 -0500

    added promote link to user page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,6 +9,7 @@ module Admin
     def update
       @user = User.find(params[:id])
       @user.level = params[:user][:level]
+      @user.inviter_id = CurrentUser.id unless @user.inviter_id.present?
       @user.save
       redirect_to edit_admin_user_path(@user, :notice => "User updated"), :notice => "User updated"
     end
