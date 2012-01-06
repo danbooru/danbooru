19c6c8932f283ba215f32ac12dcdf4998d167129
albert <r888888888@gmail.com>
2012-01-06 18:27:46 -0500

    fixes #199

--- app/helpers/post_versions_helper.rb
+++ app/helpers/post_versions_helper.rb
@@ -3,13 +3,13 @@ module PostVersionsHelper
     diff = post_version.diff(post_version.previous)
     html = []
     diff[:added_tags].each do |tag|
-      html << '<ins>' + tag + '</ins>'
+      html << '<ins>' + link_to(tag, posts_path(:tags => tag)) + '</ins>'
     end
     diff[:removed_tags].each do |tag|
-      html << '<del>' + tag + '</del>'
+      html << '<del>' + link_to(tag, posts_path(:tags => tag)) + '</del>'
     end
     diff[:unchanged_tags].each do |tag|
-      html << '<span>' + tag + '</span>' unless tag =~ /^(?:rating|source):/
+      html << '<span>' + link_to(tag, posts_path(:tags => tag)) + '</span>' unless tag =~ /^(?:rating|source):/
     end
     return html.join(" ").html_safe
   end
