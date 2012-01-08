d06ec6560dfa3ca2224f5bdf81713bfaf93d7a46
albert <r888888888@gmail.com>
2012-01-07 20:27:32 -0500

    pool verison fix

--- app/helpers/pool_versions_helper.rb
+++ app/helpers/pool_versions_helper.rb
@@ -3,12 +3,12 @@ module PoolVersionsHelper
     prev = PoolVersion.where(["pool_id = ? and id < ?", current.pool_id, current.id]).order("id desc").first
 
     if prev.nil?
-      return current.post_id_array.map {|x| content_tag("ins", "+#{x}")}.join(" ").html_safe
+      return current.post_id_array.map {|x| '<ins><a href="/posts/' + x.to_s + '">' + x.to_s + '</a></ins>'}.join(" ").html_safe
     end
 
     added = current.post_id_array - prev.post_id_array
     removed = prev.post_id_array - current.post_id_array
 
-    (added.map {|x| '<ins>+<a href="/posts/' + x.to_s + '">' + x.to_s + '</a></ins>'}.join(" ") + removed.map {|x| '<del>&ndash;<a href="/posts/' + x.to_s + '">' + x.to_s + '</a></del>'}.join(" ")).html_safe
+    (added.map {|x| '<ins><a href="/posts/' + x.to_s + '">' + x.to_s + '</a></ins>'}.join(" ") + removed.map {|x| '<del><a href="/posts/' + x.to_s + '">' + x.to_s + '</a></del>'}.join(" ")).html_safe
   end
 end
