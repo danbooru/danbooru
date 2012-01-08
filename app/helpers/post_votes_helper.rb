4feb352f33c4e52954c4db59435e9e4b96373934
albert <r888888888@gmail.com>
2012-01-07 20:14:56 -0500

    fix

--- app/helpers/post_votes_helper.rb
+++ app/helpers/post_votes_helper.rb
@@ -1,14 +1,2 @@
 module PostVotesHelper
-  def pool_version_diff(current)
-    prev = PoolVersion.where(["pool_id = ? and id < ?", current.pool_id, current.id]).order("id desc").first
-
-    if prev.nil?
-      return current.post_id_array.map {|x| content_tag("ins", "+#{x}")}.join(" ").html_safe
-    end
-
-    added = current.post_id_array - prev.post_id_array
-    removed = prev.post_id_array - current.post_id_array
-
-    added.map {|x| '<ins>+<a href="/posts/' + x.to_s + '">' + x.to_s + '</a></ins>'}.join(" ") + removed.map {|x| '<del>&ndash;<a href="/posts/' + x.to_s + '">' + x.to_s + '</a></del>'}.join(" ")
-  end
 end
