15ef261ffb3ecdb77bd7c416c68280e708bf18bb
albert <r888888888@gmail.com>
2012-01-13 19:19:55 -0500

    fixes #269

--- app/controllers/pool_elements_controller.rb
+++ app/controllers/pool_elements_controller.rb
@@ -3,12 +3,7 @@ class PoolElementsController < ApplicationController
   before_filter :member_only
   
   def create
-    @pool = Pool.find_by_name(params[:pool_name]) || Pool.find_by_id(params[:pool_id])
-    
-    if @pool.nil?
-      return
-    end
-    
+    @pool = Pool.find_by_name(params[:pool_name]) || Pool.find_by_id(params[:pool_id]) || Pool.create(:name => params[:pool_name], :description => "This pool was automatically generated")
     @post = Post.find(params[:post_id])
     @pool.add!(@post)
     append_pool_to_session(@pool)
