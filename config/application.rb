4cc04d395a4fc00b18d4e4ff4f469ba1b8f63d6e
albert <r888888888@gmail.com>
2012-02-10 17:51:34 -0500

    fix

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,10 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 if defined?(Bundler)
-  Bundler.require(:default, :assets, Rails.env)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
 end
 
 module Danbooru
@@ -21,3 +24,4 @@ module Danbooru
     config.action_mailer.perform_deliveries = true
   end
 end
+
--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
-
 # Initialize the rails application
 Danbooru::Application.initialize!
