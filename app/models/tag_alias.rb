af8c939f7b6194346670619ea94d292666eb711f
albert <r888888888@gmail.com>
2012-02-20 14:55:09 -0500

    fixes #317

--- app/models/tag_alias.rb
+++ app/models/tag_alias.rb
@@ -7,7 +7,7 @@ class TagAlias < ActiveRecord::Base
   validates_uniqueness_of :antecedent_name
   validate :absence_of_transitive_relation
   belongs_to :creator, :class_name => "User"
-  scope :name_matches, lambda {|name| where("(antecedent_name = ? or consequent_name = ?)", name, name)}
+  scope :name_matches, lambda {|name| where("(antecedent_name = ? or consequent_name = ?)", name.downcase, name.downcase)}
   search_method :name_matches
   
   def self.to_aliased(names)
