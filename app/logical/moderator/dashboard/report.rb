1515b6822ffbd7ce26421053f5a90097a35c9aa0
albert <r888888888@gmail.com>
2012-02-24 17:18:32 -0500

    fixes #328

--- app/logical/moderator/dashboard/report.rb
+++ app/logical/moderator/dashboard/report.rb
@@ -4,8 +4,8 @@ module Moderator
       attr_reader :min_date, :max_level
       
       def initialize(min_date, max_level)
-        @min_date = min_date
-        @max_level = max_level
+        @min_date = min_date.present? ? min_date.to_date : 1.week.ago
+        @max_level = max_level.present? ? User::Levels::MEMBER : max_level.to_i
       end
       
       def artists
