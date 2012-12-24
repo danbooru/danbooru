29bad46cb5fd1b09d77e61a943dc2dcca2238e21
albert <r888888888@gmail.com>
2012-12-24 16:58:10 -0500

    fix

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -27,7 +27,7 @@ module PaginationHelper
     window = 3
     
     if records.current_page >= 2
-      html << link_to("<<", params.merge(:page => records.current_page - 1))
+      html << "<li><span>" + link_to("<<", params.merge(:page => records.current_page - 1)) + "</span></li>"
     end
     
     if records.total_pages <= (window * 2) + 5
@@ -57,7 +57,7 @@ module PaginationHelper
     end
     
     if records.current_page < records.total_pages
-      html << link_to(">>", params.merge(:page => records.current_page + 1))
+      html << "<li><span>" + link_to(">>", params.merge(:page => records.current_page + 1)) + "</span></li>"
     end
     
     html << "</menu></div>"
