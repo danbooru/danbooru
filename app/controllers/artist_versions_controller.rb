d6ae894097ed8a36768ab9b4af7d6bfe2cdbe5f4
albert <r888888888@gmail.com>
2013-01-07 15:58:42 -0500

    remove artist version blank search

--- app/controllers/artist_versions_controller.rb
+++ app/controllers/artist_versions_controller.rb
@@ -2,8 +2,7 @@ class ArtistVersionsController < ApplicationController
   respond_to :html, :xml, :json
 
   def index
-    @search = ArtistVersion.search(params[:search])
-    @artist_versions = @search.paginate(params[:page])
+    @artist_versions = ArtistVersion.search(params[:search]).paginate(params[:page])
     respond_with(@artist_versions)
   end
 end
