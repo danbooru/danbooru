5fcd80224921b4cd8ebc0260c491c4ee371a1cb6
albert <r888888888@gmail.com>
2013-01-14 12:08:57 -0500

    fix post versions controller

--- app/controllers/post_versions_controller.rb
+++ app/controllers/post_versions_controller.rb
@@ -1,7 +1,10 @@
 class PostVersionsController < ApplicationController
+  respond_to :html, :xml, :json
+
   def index
     @search = PostVersion.search(params[:search])
     @post_versions = @search.order("id desc").paginate(params[:page])
+    respond_with(@post_versions)
   end
   
   def search
