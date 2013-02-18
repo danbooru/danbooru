c9a0a3b6c3848ca5416f6476084d845e4028a287
albert <r888888888@gmail.com>
2013-02-17 20:13:22 -0500

    use gem

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'rmagick', :require => "RMagick"
 gem 'daemons'
 gem 'net-ssh'
 gem 'net-sftp'
+gem 'sys-proctable', :require => "sys/proctable"
 
 group :development do
   gem 'newrelic_rpm'
--- app/logical/memorylogic.rb
+++ app/logical/memorylogic.rb
@@ -4,6 +4,11 @@ module Memorylogic
   end
 
   def self.memory_usage
-    number_to_human_size(`ps -o rss= -p #{Process.pid}`.to_i * 1.kilobyte)
+    ps = Sys::ProcTable.ps(Process.pid)
+    if ps.respond_to?(:rss)
+      number_to_human_size(ps.rss.to_i * 1.kilobyte)
+    else
+      0
+    end
   end
 end
