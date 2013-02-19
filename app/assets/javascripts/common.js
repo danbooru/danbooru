68161f7b85f88c0fde7296a33c6fe0cae6c11fc3
albert <r888888888@gmail.com>
2013-02-19 12:59:59 -0500

    revert nested navlinks

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -3,26 +3,6 @@ $(function() {
   $(".striped tbody tr:even").addClass("even");
   $(".striped tbody tr:odd").addClass("odd");
   
-  // More link
-  if ($("#site-map-link").length > 0) {
-    $("#site-map-link").click(function(e) {
-      $("#more-links").toggle();
-      e.preventDefault();
-      e.stopPropagation();
-    });
-
-    $("#more-links").show();
-    $("#more-links").position({
-      of: $("#site-map-link"),
-      my: "left top",
-      at: "left top"
-    }).hide();
-
-    $(document).click(function(e) {
-      $("#more-links").hide();
-    });
-  }
-  
   // Account notices
   $("#hide-sign-up-notice").click(function(e) {
     $("#sign-up-notice").hide();
