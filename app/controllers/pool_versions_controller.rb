d7fb091bd1246d514686f83c31ec7b336bbcae02
albert <r888888888@gmail.com>
2013-02-21 16:48:46 -0500

    fix pool version ordering

--- app/controllers/pool_versions_controller.rb
+++ app/controllers/pool_versions_controller.rb
@@ -1,6 +1,6 @@
 class PoolVersionsController < ApplicationController
   def index
     @search = PoolVersion.search(params[:search])
-    @pool_versions = @search.order("id desc").paginate(params[:page])
+    @pool_versions = @search.order("updated_at desc").paginate(params[:page])
   end
 end
---
+++ db/migrate/20130221214811_add_index_updated_at_on_pool_versions.rb
@@ -0,0 +1,11 @@
+class AddIndexUpdatedAtOnPoolVersions < ActiveRecord::Migration
+  def up
+    execute "set statement_timeout = 0"
+    add_index :post_versions, :updated_at
+  end
+
+  def down
+    execute "set statement_timeout = 0"
+    remove_index :post_versions, :updated_at
+  end
+end
