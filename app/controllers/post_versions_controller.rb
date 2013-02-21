b8d93c301a1458ccbda3493d7c4532bcd7cbfa93
albert <r888888888@gmail.com>
2013-02-21 13:37:56 -0500

    fix for post version pagination

--- app/controllers/post_versions_controller.rb
+++ app/controllers/post_versions_controller.rb
@@ -2,7 +2,7 @@ class PostVersionsController < ApplicationController
   respond_to :html, :xml, :json
 
   def index
-    @post_versions = PostVersion.search(params[:search]).order("updated_at desc").paginate(params[:page], :count => 1_000_000)
+    @post_versions = PostVersion.search(params[:search]).order("updated_at desc").paginate(params[:page], :count => (params[:search].present? ? nil : 1_000_000))
     respond_with(@post_versions)
   end
   
