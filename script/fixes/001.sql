e0dfd148042894b80a56b33ccb4d1b67cb6de8e1
albert <r888888888@gmail.com>
2013-02-22 16:58:05 -0500

    add fix script

---
+++ script/fixes/001.sql
@@ -0,0 +1,3 @@
+set statement_timeout = 0;
+update posts set is_pending = false where is_deleted = true;
+update posts set last_commented_at = null where last_commented_at is not null and not exists (select 1 from comments _ where _.post_id = posts.id limit 1);
