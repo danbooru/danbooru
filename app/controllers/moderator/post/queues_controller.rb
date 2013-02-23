38f75961f6150d9e63315682c00042a68bac0652
albert <r888888888@gmail.com>
2013-02-22 21:33:11 -0500

    improve mod queue

--- app/controllers/moderator/post/queues_controller.rb
+++ app/controllers/moderator/post/queues_controller.rb
@@ -6,7 +6,8 @@ module Moderator
       
       def show
         ::Post.without_timeout do
-          @posts = ::Post.order("id asc").pending_or_flagged.available_for_moderation(params[:hidden]).search(:tag_match => "#{params[:query]} status:any").paginate(params[:page])
+          @posts = ::Post.order("posts.id asc").pending_or_flagged.available_for_moderation(params[:hidden]).search(:tag_match => "#{params[:query]} status:any").paginate(params[:page], :limit => 100)
+          @posts.all # cache the data
         end
         respond_with(@posts)
       end
