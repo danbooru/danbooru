97a5579d0d7d5db9e4599d50df69934e4fd21bd8
小太 <nospam.kotarou.dono@gmail.com>
2013-02-23 14:50:01 +1100

    Remove trailing whitespace in files I'm about to edit

--- app/assets/javascripts/utility.js
+++ app/assets/javascripts/utility.js
@@ -2,27 +2,27 @@
   Danbooru.meta = function(key) {
     return $("meta[name=" + key + "]").attr("content");
   }
-  
+
   Danbooru.notice = function(msg) {
     $('#notice').html(msg).addClass("ui-state-highlight").removeClass("ui-state-error").fadeIn("fast");
   }
-  
+
   Danbooru.error = function(msg) {
     $('#notice').html(msg).removeClass("ui-state-highlight").addClass("ui-state-error").fadeIn("fast");
   }
-  
+
   Danbooru.is_subset = function(array, subarray) {
     var all = true;
-    
+
     $.each(subarray, function(i, val) {
       if ($.inArray(val, array) === -1) {
         all = false;
       }
     });
-    
+
     return all;
   }
-  
+
   Danbooru.without = function(array, element) {
     var temp = [];
     $.each(array, function(i, v) {
@@ -32,7 +32,7 @@
     });
     return temp;
   }
-  
+
   Danbooru.reject = function(array, f) {
     var filtered = [];
     $.each(array, function(i, x) {
