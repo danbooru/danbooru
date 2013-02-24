1d0fc3f39b89efb77b6fc3af041c280a6d5ca40f
albert <r888888888@gmail.com>
2013-02-24 13:54:12 -0500

    fix

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -15,16 +15,6 @@ $(function() {
     Danbooru.Cookie.put('hide_upgrade_account_notice', '1', 7);
     e.preventDefault();
   });
-
-  // TOS link
-  if (!location.href.match(/terms_of_service/) && Danbooru.Cookie.get("tos") !== "1") {
-    var domain = location.href.match(/^(http:\/\/[^\/]+)/)[0];
-    location.href = domain + "/static/terms_of_service?url=" + location.href;
-  }
-  
-  $("#tos-agree-link").click(function() {
-    Danbooru.Cookie.put("tos", "1");
-  })
 });
 
 var Danbooru = {};
