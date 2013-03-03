7c4e933beaa609c7cad00a5e7c51a4883295a2fc
albert <r888888888@gmail.com>
2013-03-03 15:51:57 -0500

    potential fix for #737

--- app/assets/javascripts/uploads.js
+++ app/assets/javascripts/uploads.js
@@ -15,7 +15,7 @@
   
   Danbooru.Upload.initialize_enter_on_tags = function() {
     $("#upload_tag_string,#post_tag_string").bind("keydown.return", function(e) {
-      $("#form").submit();
+      $("#form").trigger("submit");
       e.preventDefault();
     });
   }
