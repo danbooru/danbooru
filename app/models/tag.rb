1da9e24bd0b9ba1caa1dfa18a06e38d21508b76f
albert <r888888888@gmail.com>
2013-03-09 01:56:59 -0500

    fixes #804

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -87,11 +87,18 @@ class Tag < ActiveRecord::Base
     def update_category_cache_for_all(force = false)
       if category_changed? || force
         update_category_cache
+        update_category_post_counts if category_changed?
         
         Danbooru.config.other_server_hosts.each do |host|
           delay(:queue => host).update_category_cache
         end
       end
+    end
+    
+    def update_category_post_counts
+      old_field = "tag_count_#{Danbooru.config.reverse_tag_category_mapping[category_was]}".downcase
+      new_field = "tag_count_#{category_name}".downcase
+      Post.raw_tag_match(name).update_all("#{old_field} = #{old_field} - 1, #{new_field} = #{new_field} + 1")
     end
     
     def update_category_cache
