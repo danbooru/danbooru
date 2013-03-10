27926df522768e7a5910dda04dfc0f7c802bc3b3
albert <r888888888@gmail.com>
2013-03-09 20:48:37 -0500

    fixes #805

---
+++ app/assets/javascripts/post_popular.js
@@ -0,0 +1,41 @@
+(function() {
+  Danbooru.PostPopular = {};
+
+  Danbooru.PostPopular.nav_prev = function() {
+    if ($("#popular-nav-links").length) {
+      var href = $("#popular-nav-links a[rel=prev]").attr("href");
+      if (href) {
+        location.href = href;
+      }
+    }
+  }
+  
+  Danbooru.PostPopular.nav_next = function() {
+    if ($("#popular-nav-links").length) {
+      var href = $("#popular-nav-links a[rel=next]").attr("href");
+      if (href) {
+        location.href = href;
+      }
+    }
+  }
+
+  Danbooru.PostPopular.initialize_all = function() {
+    if ($("#c-explore-posts").length) {
+      if (Danbooru.meta("enable-js-navigation") === "true") {
+        $(document).bind("keydown.a", function(e) {
+          Danbooru.PostPopular.nav_prev();
+          e.preventDefault();
+        });
+
+        $(document).bind("keydown.d", function(e) {
+          Danbooru.PostPopular.nav_next();
+          e.preventDefault();
+        });
+      }
+    }
+  }
+})();
+
+$(document).ready(function() {
+  Danbooru.PostPopular.initialize_all();
+});
--- app/presenters/post_set_presenters/popular.rb
+++ app/presenters/post_set_presenters/popular.rb
@@ -31,6 +31,83 @@ module PostSetPresenters
       1.month.since(date)
     end
     
+    def link_rel_for_scale?(template, scale)
+      (template.params[:scale].blank? && scale == "day") || template.params[:scale].to_s.include?(scale)
+    end
+    
+    def next_date_for_scale(scale)
+      case scale
+      when "Day"
+        next_day
+        
+      when "Week"
+        next_week
+        
+      when "Month"
+        next_month
+        
+      else
+        nil
+      end
+    end
+
+    def prev_date_for_scale(scale)
+      case scale
+      when "Day"
+        prev_day
+        
+      when "Week"
+        prev_week
+        
+      when "Month"
+        prev_month
+        
+      else
+        nil
+      end
+    end
+    
+    def nav_links_for_scale(template, scale)
+      html = []
+      html << '<span class="period">'
+      html << template.link_to(
+        "&laquo;prev".html_safe, 
+        template.popular_explore_posts_path(
+          :date => prev_date_for_scale(scale), 
+          :scale => scale.downcase
+        ),
+        :rel => (link_rel_for_scale?(template, scale.downcase) ? "prev" : nil)
+      )
+      html << template.link_to(
+        scale, 
+        template.popular_explore_posts_path(
+          :date => date, 
+          :scale => scale.downcase
+        ), 
+        :class => "desc"
+      )
+      html << template.link_to(
+        "next&raquo;".html_safe, 
+        template.popular_explore_posts_path(
+          :date => next_date_for_scale(scale), 
+          :scale => scale.downcase
+        ),
+        :rel => (link_rel_for_scale?(template, scale.downcase) ? "next" : nil)
+      )
+      html << '</span>'
+      html.join("\n").html_safe
+    end
+    
+    def nav_links(template)
+      html =  []
+      html << '<p id="popular-nav-links">'
+      html << nav_links_for_scale(template, "Day")
+      html << nav_links_for_scale(template, "Week")
+      html << nav_links_for_scale(template, "Month")
+      html << '</p>'
+      html.join("\n").html_safe
+    end
+    
     def range_text
       if min_date == max_date
         date.strftime("%B %d, %Y")
