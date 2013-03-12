698c08f0588306e113a06495e978c9b7de0d82e4
albert <r888888888@gmail.com>
2013-03-12 13:00:01 -0400

    fixes #835

--- app/presenters/post_set_presenters/wiki_page.rb
+++ app/presenters/post_set_presenters/wiki_page.rb
@@ -3,5 +3,13 @@ module PostSetPresenters
     def posts
       @post_set.posts
     end
+    
+    def post_previews_html(template)
+      result = super(template)
+      if result =~ /Nobody here but us chickens/
+        result = ""
+      end
+      result.html_safe
+    end
   end
 end
