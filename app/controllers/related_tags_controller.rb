ada7b2626042e621be5b080c8447dac6b0b71c53
albert <r888888888@gmail.com>
2013-03-14 17:08:16 -0400

    fixes #869

--- app/controllers/related_tags_controller.rb
+++ app/controllers/related_tags_controller.rb
@@ -2,7 +2,7 @@ class RelatedTagsController < ApplicationController
   respond_to :json
   
   def show
-    @query = RelatedTagQuery.new(params[:query], params[:category])
+    @query = RelatedTagQuery.new(params[:query].to_s.downcase, params[:category])
     respond_with(@query) do |format|
       format.json do
         render :json => @query.to_json
