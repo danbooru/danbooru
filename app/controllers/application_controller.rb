854467891f64cefdc971ec8ee5732035161c2d74
albert <r888888888@gmail.com>
2013-03-15 15:32:55 -0400

    fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,8 +20,8 @@ protected
       render :template => "static/error", :status => 500
     elsif exception.is_a?(::ActiveRecord::RecordNotFound)
       @exception = nil
-      @error_message = "That record was not found", :status => 404
-      render :template => "static/error", :status => 500
+      @error_message = "That record was not found"
+      render :template => "static/error", :status => 404
     else
       render :template => "static/error", :status => 500
     end
