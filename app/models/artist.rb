a530383500999347977b75c99c89a6cb7c4ae5a9
albert <r888888888@gmail.com>
2013-03-15 13:39:18 -0400

    fixes #876

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -27,6 +27,7 @@ class Artist < ActiveRecord::Base
           u = u.to_escaped_for_sql_like.gsub(/\*/, '%') + '%'
           artists += Artist.joins(:urls).where(["artists.is_active = TRUE AND artist_urls.normalized_url LIKE ? ESCAPE E'\\\\'", u]).all(:order => "artists.name")
           url = File.dirname(url) + "/"
+          break if url =~ /\.(?:net|com|org)\/$/
         end
 
         artists.uniq_by {|x| x.name}.slice(0, 20)
