fe3f8647008653027df4383c2d5520d05f778f33
albert <r888888888@gmail.com>
2013-03-18 15:11:59 -0700

    fixes #960

--- app/models/pool_version.rb
+++ app/models/pool_version.rb
@@ -33,6 +33,10 @@ class PoolVersion < ActiveRecord::Base
   
   extend SearchMethods
   
+  def updater_name
+    User.id_to_name(updater_id)
+  end
+  
   def initialize_updater
     self.updater_id = CurrentUser.id
     self.updater_ip_addr = CurrentUser.ip_addr
