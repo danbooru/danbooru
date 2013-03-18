6cb85fcfdfffa124665b47b66c9bf5d93d316dc4
albert <r888888888@gmail.com>
2013-03-17 22:36:14 -0400

    fixes #883

--- app/controllers/pool_versions_controller.rb
+++ app/controllers/pool_versions_controller.rb
@@ -1,5 +1,9 @@
 class PoolVersionsController < ApplicationController
   def index
+    if params[:search] && params[:search][:pool_id]
+      @pool = Pool.find(params[:search][:pool_id])
+    end
+    
     @pool_versions = PoolVersion.search(params[:search]).order("updated_at desc").paginate(params[:page], :search_count => params[:search])
   end
 end
