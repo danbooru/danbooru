bd6c8a5e90dd9c456121b511059d64a9595d419a
albert <r888888888@gmail.com>
2013-03-18 16:41:09 -0700

    fixes #957

---
+++ script/fixes/008.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'config', 'environment'))
+
+ActiveRecord::Base.connection.execute("set statement_timeout = 0")
+Tag.find_each do |tag|
+  tag.fix_post_count
+end
