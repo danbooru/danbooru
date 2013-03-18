51d1f541027fff2318669ee178ff13c0174a1188
albert <r888888888@gmail.com>
2013-03-17 21:28:24 -0400

    update fix

--- script/fixes/007.rb
+++ script/fixes/007.rb
@@ -4,3 +4,4 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'config',
 
 ActiveRecord::Base.connection.execute("set statement_timeout = 0")
 ActiveRecord::Base.connection.execute("update comments set updater_id = creator_id")
+ActiveRecord::Base.connection.execute("update tags set post_count = 0 where post_count < 0")
