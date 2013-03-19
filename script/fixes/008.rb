95ac6c776269bff7e48ab51bdaaa176aad26473b
albert <r888888888@gmail.com>
2013-03-18 22:18:32 -0700

    fix ordering of fix method

--- script/fixes/008.rb
+++ script/fixes/008.rb
@@ -3,8 +3,9 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'config', 'environment'))
 
 ActiveRecord::Base.connection.execute("set statement_timeout = 0")
+
+ActiveRecord::Base.connection.execute("update comments set updater_id = creator_id where updater_id <> creator_id")
+
 Tag.find_each do |tag|
   tag.fix_post_count
 end
-
-ActiveRecord::Base.connection.execute("update comments set updater_id = creator_id where updater_id <> creator_id")
