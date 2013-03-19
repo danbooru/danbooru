17530f13b395228e5fdb65b3c1039c1c29f5ec22
Lightforger <hi-hi-hi8@hotmail.com>
2013-03-19 14:51:49 +0100

    removal of absolute pos.
    Has been fixed with css.
    I'm not sure if i'll break something by leaving an empty file, thus i'll just leave it at that.

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,18 +1,1 @@
-$(function() {
-  var $sidebar = $("#sidebar");
-  var $content = $("#content");
-
-  if (!$sidebar.length || !$content.length) {
-    return;
-  }
-
-  var sidebar_offset = $sidebar.offset().top + $sidebar.height();
-  var content_offset = $content.offset().top + $content.height();
-  var offset = null;
-  if (sidebar_offset > content_offset) {
-    offset = sidebar_offset;
-  } else {
-    offset = content_offset;
-  }
-  $("#page-footer").css({"position": "absolute", "top": offset, "width": "100%", "height": "3em"});
-});
+$(function() {});
