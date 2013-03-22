eed3772cdbaca4386ce8da638778e36a94b9941c
albert <r888888888@gmail.com>
2013-03-22 09:53:03 -0700

    fixes #989

--- script/fixes/009.rb
+++ script/fixes/009.rb
@@ -4,4 +4,4 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'config',
 
 ActiveRecord::Base.connection.execute("set statement_timeout = 0")
 
-ActiveRecord::Base.connection.execute("update wiki_pages set updater_id = creator_id")
+ActiveRecord::Base.connection.execute("update wiki_pages set updater_id = (select _.updater_id from wiki_page_versions _ where _.wiki_page_id = wiki_pages.id order by _.updated_at desc limit 1)")
