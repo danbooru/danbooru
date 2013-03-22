56b9d0dfd9daf2d213d8c71681dbbc04c869e3c0
albert <r888888888@gmail.com>
2013-03-22 19:28:19 -0400

    fixes #1040

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -1,5 +1,5 @@
 class ForumPost < ActiveRecord::Base
-  attr_accessible :body, :topic_id, :as => [:member, :builder, :privileged, :platinum, :contributor, :admin, :moderator, :default]
+  attr_accessible :body, :topic_id, :as => [:member, :builder, :janitor, :privileged, :platinum, :contributor, :admin, :moderator, :default]
   attr_accessible :is_locked, :is_sticky, :is_deleted, :as => [:admin, :moderator, :janitor]
   belongs_to :creator, :class_name => "User"
   belongs_to :topic, :class_name => "ForumTopic"
