10292f3544e8b5e315fb37479ac05bc416f8da73
ToksT <tttoksss@gmail.com>
2013-03-28 14:03:12 -0300

    fixes #900 (both + and -)

--- app/helpers/post_versions_helper.rb
+++ app/helpers/post_versions_helper.rb
@@ -3,10 +3,10 @@ module PostVersionsHelper
     diff = post_version.diff(post_version.previous)
     html = []
     diff[:added_tags].each do |tag|
-      html << '<ins>' + link_to(tag, posts_path(:tags => tag)) + '</ins>'
+      html << '<ins>+' + link_to(tag, posts_path(:tags => tag)) + '</ins>'
     end
     diff[:removed_tags].each do |tag|
-      html << '<del>' + link_to(tag, posts_path(:tags => tag)) + '</del>'
+      html << '<del>-' + link_to(tag, posts_path(:tags => tag)) + '</del>'
     end
     diff[:unchanged_tags].each do |tag|
       html << '<span>' + link_to(tag, posts_path(:tags => tag)) + '</span>'
