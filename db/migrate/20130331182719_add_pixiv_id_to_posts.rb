b751aaf7cfe1bad950950af2207f165f59af3a0c
albert <r888888888@gmail.com>
2013-03-31 15:08:54 -0400

    fix index

--- db/migrate/20130331182719_add_pixiv_id_to_posts.rb
+++ db/migrate/20130331182719_add_pixiv_id_to_posts.rb
@@ -1,7 +1,7 @@
 class AddPixivIdToPosts < ActiveRecord::Migration
   def up
     execute "set statement_timeout = 0"
-    remove_index :posts, :pixiv_id
+    # remove_index :posts, :pixiv_id
     add_column :posts, :pixiv_id, :integer
     execute "create index index_posts_on_pixiv_id on posts (pixiv_id) where pixiv_id is not null"
   end
