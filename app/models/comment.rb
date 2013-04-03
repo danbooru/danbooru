3d7ebd0108e245b9deac144f8572600892176e6e
Toks <tttoksss@gmail.com>
2013-04-02 20:34:41 -0400

    adds post_id search to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -49,6 +49,10 @@ class Comment < ActiveRecord::Base
         q = q.body_matches(params[:body_matches])
       end
 
+      if params[:post_id].present?
+        q = q.where("post_id = ?", params[:post_id].to_i)
+      end
+
       if params[:post_tags_match].present?
         q = q.post_tags_match(params[:post_tags_match])
       end
