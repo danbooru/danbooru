0d864e65d20415d5aae4e6b17ce77c30155203a4
albert <r888888888@gmail.com>
2013-04-09 18:08:01 -0400

    fixes #1246

--- app/logical/daily_maintenance.rb
+++ app/logical/daily_maintenance.rb
@@ -6,6 +6,7 @@ class DailyMaintenance
     Upload.delete_all(['created_at < ?', 1.day.ago])
     ModAction.delete_all(['created_at < ?', 3.days.ago])
     Delayed::Job.delete_all(['created_at < ?'], 1.day.ago)
+    PostVote.delete_all(['created_at < ?', 1.month.ago])
     TagSubscription.process_all
     prune_ad_hits
   end
