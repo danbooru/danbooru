c5bf7b6c476b1c9045ae731cd7bc9dfdded5c854
albert <r888888888@gmail.com>
2013-04-09 18:15:04 -0400

    fixes #1241

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,11 @@
 class StaticController < ApplicationController
   def terms_of_service
   end
+  
+  def accept_terms_of_service
+    cookies.permanent[:accepted_tos] = "1"
+    redirect_to(params[:url] || posts_path)
+  end
 
   def error
   end
--- config/routes.rb
+++ config/routes.rb
@@ -311,6 +311,7 @@ Danbooru::Application.routes.draw do
   match "/static/bookmarklet" => "static#bookmarklet", :as => "bookmarklet"
   match "/static/site_map" => "static#site_map", :as => "site_map"
   match "/static/terms_of_service" => "static#terms_of_service", :as => "terms_of_service"
+  match "/static/accept_terms_of_service" => "static#accept_terms_of_service", :as => "accept_terms_of_service"
   match "/static/mrtg" => "static#mrtg", :as => "mrtg"
   match "/static/contact" => "static#contact", :as => "contact"
   match "/static/benchmark" => "static#benchmark"
