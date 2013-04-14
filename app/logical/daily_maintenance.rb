592741da7bfe2df403ed0ab374b9365aa67c774a
Toks <tttoksss@gmail.com>
2013-04-13 23:26:52 -0400

    possible fix for #1248

--- app/logical/daily_maintenance.rb
+++ app/logical/daily_maintenance.rb
@@ -5,7 +5,7 @@ class DailyMaintenance
     TagPruner.new.prune!
     Upload.delete_all(['created_at < ?', 1.day.ago])
     ModAction.delete_all(['created_at < ?', 3.days.ago])
-    Delayed::Job.delete_all(['created_at < ?'], 1.day.ago)
+    Delayed::Job.delete_all(['created_at < ?', 1.day.ago])
     PostVote.delete_all(['created_at < ?', 1.month.ago])
     TagSubscription.process_all
     prune_ad_hits
