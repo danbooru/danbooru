79f4fb4d9f673686ddcd0ada38627be17b4450f0
Toks <tttoksss@gmail.com>
2013-04-14 14:55:05 -0400

    stop tag pruner

--- app/logical/daily_maintenance.rb
+++ app/logical/daily_maintenance.rb
@@ -2,7 +2,7 @@ class DailyMaintenance
   def run
     ActiveRecord::Base.connection.execute("set statement_timeout = 0")
     PostPruner.new.prune!
-    TagPruner.new.prune!
+    # TagPruner.new.prune!
     Upload.delete_all(['created_at < ?', 1.day.ago])
     ModAction.delete_all(['created_at < ?', 3.days.ago])
     Delayed::Job.delete_all(['created_at < ?', 1.day.ago])
