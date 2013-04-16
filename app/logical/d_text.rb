458f4110210155e2a01899a86b483c389357b98f
Toks <tttoksss@gmail.com>
2013-04-16 13:49:39 -0400

    fixes #1311

--- app/logical/d_text.rb
+++ app/logical/d_text.rb
@@ -80,6 +80,8 @@ class DText
     str = str.gsub(/\bpool #(\d+)/i, %{<a href="/pools/\\1">pool #\\1</a>})
     str = str.gsub(/\buser #(\d+)/i, %{<a href="/users/\\1">user #\\1</a>})
     str = str.gsub(/\bartist #(\d+)/i, %{<a href="/artists/\\1">artist #\\1</a>})
+    str = str.gsub(/\bpixiv #(\d+)(?!\/p\d|\d)/i, %{<a href="http://www.pixiv.net/member_illust.php?mode=medium&illust_id=\\1">pixiv #\\1</a>})
+    str = str.gsub(/\bpixiv #(\d+)\/p(\d+)/i, %{<a href="http://www.pixiv.net/member_illust.php?mode=manga_big&illust_id=\\1&page=\\2">pixiv #\\1/p\\2</a>})
   end
 
   def self.parse_list(str, options = {})
