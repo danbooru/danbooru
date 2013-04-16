fd93741c65f24330996b1d52362d3a1afe6e75e3
r888888888 <r888888888@gmail.com>
2013-04-15 22:37:40 -0700

    disable prefork/postfork code

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -32,19 +32,19 @@ stdout_path "log/unicorn.log"
 # Set master PID location
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
-before_fork do |server, worker|
-  ActiveRecord::Base.connection.disconnect!
+# before_fork do |server, worker|
+#   ActiveRecord::Base.connection.disconnect!
 
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
-end
+#   old_pid = "#{server.config[:pid]}.oldbin"
+#   if File.exists?(old_pid) && server.pid != old_pid
+#     begin
+#       Process.kill("QUIT", File.read(old_pid).to_i)
+#     rescue Errno::ENOENT, Errno::ESRCH
+#       # someone else did our job for us
+#     end
+#   end
+# end
 
-after_fork do |server, worker|
-  ActiveRecord::Base.establish_connection
-end
+# after_fork do |server, worker|
+#   ActiveRecord::Base.establish_connection
+# end
