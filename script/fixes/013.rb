8ad46b13520107823c3d191a285f8af473a9284b
r888888888 <r888888888@gmail.com>
2013-04-19 16:41:20 -0700

    update version

--- script/fixes/013.rb
+++ script/fixes/013.rb
@@ -6,14 +6,10 @@ ActiveRecord::Base.connection.execute("set statement_timeout = 0")
 
 Post.select("id, score, up_score, down_score, fav_count").find_each do |post|
   post.update_column(:score, post.up_score + post.down_score + post.fav_count)
-end
+end ; true
 
 Comment.find_each do |comment|
   if !Post.exists?("id = #{comment.post_id}")
     comment.destroy
   end
-end
-
-User.where("name like ? or name like ?", "\\_%", "%\\_").each do |user|
-  puts "#{user.id}\t#{user.name}\t#{user.level}\t#{user.email}\t#{user.last_logged_in_at}"
 end ; true
