caa335d80fd0054f8c823b1a6045c30441b38da3
r888888888 <r888888888@gmail.com>
2013-04-22 21:43:10 -0700

    changes to fix

--- script/fixes/013.rb
+++ script/fixes/013.rb
@@ -5,7 +5,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'config',
 ActiveRecord::Base.connection.execute("set statement_timeout = 0")
 
 Comment.find_each do |comment|
-  if !Post.exists?("id = #{comment.post_id}")
+  if !Post.exists?(comment.post_id)
     puts "deleting comment #{comment.id}"
     comment.destroy
   end
@@ -18,11 +18,14 @@ end ; true
 
 ArtistVersion.update_all "is_banned = false"
 
-Artist.find_each do |artist|
-  if artist.is_banned?
-    puts "updating artist #{artist.id}"
-    artist.versions.last.update_column(:is_banned, true)
-  end
+Artist.where("is_banned = true").find_each do |artist|
+  puts "updating artist #{artist.id}"
+  artist.versions.last.update_column(:is_banned, true)
+end ; true
+
+User.find_each do |user|
+  puts "updating user #{user.id}"
+  user.update_column(:favorite_count, Favorite.for_user(user).where("user_id = ?", user.id).count)
 end ; true
 
 Post.find_each do |post|
@@ -30,10 +33,15 @@ Post.find_each do |post|
   post.update_column(:fav_count, Favorite.where("post_id = #{post.id}").count)
 end ; true
 
-User.find_each do |user|
-  puts "updating user #{user.id}"
-  user.update_column(:favorite_count, Favorite.for_user(user).where("user_id = ?", user.id).count)
-end ; true
+danbooru_conn = PGconn.connect(dbname: 'danbooru')
+danbooru2_conn = PGconn.connect(dbname: "danbooru2")
+danbooru_conn.exec("set statement_timeout = 0")
+danbooru_conn.exec( "SELECT * FROM comments WHERE id < 29130" ) do |result|
+  result.each do |row|
+    # puts row["id"], row["created_at"], row["post_id"], row["user_id"], row["body"], row["ip_addr"], row["score"]
+    danbooru2_conn.exec "insert into comments (id, created_at, updated_at, post_id, creator_id, body, ip_addr, score, updater_id, updater_ip_addr) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [row["id"], row["created_at"], row["created_at"], row["post_id"], row["user_id"], row["body"], row["ip_addr"], row["score"], row["user_id"], row["ip_addr"]] 
+  end
+end
 
 
 # Post.select("id, score, up_score, down_score, fav_count").find_each do |post|
