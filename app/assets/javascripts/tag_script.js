d737eab661e1362aa69899f6a763dbef1880ad25
Toks <tttoksss@gmail.com>
2013-04-26 13:05:56 -0400

    fixes #1442

--- app/assets/javascripts/tag_script.js
+++ app/assets/javascripts/tag_script.js
@@ -34,7 +34,7 @@
       }
     } else if (command === "[reset]") {
       return [];
-    } else if (command[0] === "-") {
+    } else if (command[0] === "-" && !command.match(/^-pool:/i)) {
       return Danbooru.reject(tags, function(x) {return x === command.substr(1, 100)});
     } else {
       tags.push(command);
