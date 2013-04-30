a80a71ffaf57b1d6d8bb754371fa90ad86c8c904
Toks <tttoksss@gmail.com>
2013-04-29 20:06:13 -0400

    order children previews by id_asc (#1481)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,10 +22,13 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post_flag = PostFlag.new(:post_id => @post.id)
     @post_appeal = PostAppeal.new(:post_id => @post.id)
+
     @children_post_set = PostSets::Post.new("parent:#{@post.id} -id:#{@post.id}", 1, 200)
+    @children_post_set.posts.reverse!
     @parent_post_set = PostSets::Post.new("id:#{@post.parent_id} status:any")
     @siblings_post_set = PostSets::Post.new("parent:#{@post.parent_id} -id:#{@post.parent_id}", 1, 200)
     @siblings_post_set.posts.reverse!
+
     respond_with(@post)
   end
 
