9b7d887c48aae09ccaa6fd4d0a4dedcc609cdfcb
r888888888 <r888888888@gmail.com>
2013-06-18 20:44:34 -0700

    add statement timeout for migration

--- db/migrate/20130618230158_add_enable_auto_complete_to_users.rb
+++ db/migrate/20130618230158_add_enable_auto_complete_to_users.rb
@@ -1,5 +1,6 @@
 class AddEnableAutoCompleteToUsers < ActiveRecord::Migration
   def change
+    execute("set statement_timeout = 0")
     add_column :users, :enable_auto_complete, :boolean, :null => false, :default => :true
   end
 end
