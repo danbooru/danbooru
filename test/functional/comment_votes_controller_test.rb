f6510703e0891b516a783c9ee51059cac2156d58
r888888888 <r888888888@gmail.com>
2013-06-21 17:29:48 -0700

    fixes #1792

--- test/functional/comment_votes_controller_test.rb
+++ test/functional/comment_votes_controller_test.rb
@@ -14,18 +14,39 @@ class CommentVotesControllerTest < ActionController::TestCase
       CurrentUser.ip_addr = nil
     end
 
-    should "create a vote" do
-      assert_difference("CommentVote.count", 1) do
-        post :create, {:format => "js", :comment_id => @comment.id, :score => 1}, {:user_id => @user.id}
-        assert_response :success
+    context "#create.json" do
+      should "create a vote" do
+        assert_difference("CommentVote.count", 1) do
+          post :create, {:format => "json", :comment_id => @comment.id, :score => 1}, {:user_id => @user.id}
+          assert_response :success
+          assert_equal("{\"success\": true}", @response.body.strip)
+        end
+      end
+
+      should "fail silently on errors" do
+        FactoryGirl.create(:comment_vote, :comment => @comment)
+        assert_difference("CommentVote.count", 0) do
+          post :create, {:format => "json", :comment_id => @comment.id, :score => 1}, {:user_id => @user.id}
+          assert_response 422
+          assert_equal("{\"errors\":{\"user_id\":[\"have already voted for this comment\"]}}", @response.body.strip)
+        end
       end
     end
 
-    should "fail silently on errors" do
-      FactoryGirl.create(:comment_vote, :comment => @comment)
-      assert_difference("CommentVote.count", 0) do
-        post :create, {:format => "js", :comment_id => @comment.id, :score => 1}, {:user_id => @user.id}
-        assert_response :success
+    context "#create.js" do
+      should "create a vote" do
+        assert_difference("CommentVote.count", 1) do
+          post :create, {:format => "js", :comment_id => @comment.id, :score => 1}, {:user_id => @user.id}
+          assert_response :success
+        end
+      end
+
+      should "fail silently on errors" do
+        FactoryGirl.create(:comment_vote, :comment => @comment)
+        assert_difference("CommentVote.count", 0) do
+          post :create, {:format => "js", :comment_id => @comment.id, :score => 1}, {:user_id => @user.id}
+          assert_response :success
+        end
       end
     end
   end
