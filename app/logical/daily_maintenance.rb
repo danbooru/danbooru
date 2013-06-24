cc60f794d828959d398da59517efbc421875dbb8
r888888888 <r888888888@gmail.com>
2013-06-24 12:46:10 -0700

    extend delayed job prune period to 1 week

--- app/logical/daily_maintenance.rb
+++ app/logical/daily_maintenance.rb
@@ -5,14 +5,9 @@ class DailyMaintenance
     TagPruner.new.prune!
     Upload.delete_all(['created_at < ?', 1.day.ago])
     ModAction.delete_all(['created_at < ?', 3.days.ago])
-    Delayed::Job.delete_all(['created_at < ?', 1.day.ago])
+    Delayed::Job.delete_all(['created_at < ?', 7.days.ago])
     PostVote.delete_all(['created_at < ?', 1.month.ago])
     TagSubscription.process_all
     ApiCacheGenerator.new.generate_tag_cache
-    prune_ad_hits
-  end
-  
-  def prune_ad_hits
-    AdvertisementHit.delete_all(["created_at < ?", 1.month.ago])
   end
 end
