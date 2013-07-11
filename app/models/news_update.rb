045cfc795b65634d1be6e3979147f75b2b3e55a6
r888888888 <r888888888@gmail.com>
2013-07-11 16:20:29 -0700

    limit news updates to ones created in past 2 weeks

--- app/models/news_update.rb
+++ app/models/news_update.rb
@@ -1,7 +1,7 @@
 class NewsUpdate < ActiveRecord::Base
   belongs_to :creator, :class_name => "User"
   belongs_to :updater, :class_name => "User"
-  scope :recent, order("created_at desc").limit(5)
+  scope :recent, lambda {where("created_at >= ?", 2.weeks.ago).order("created_at desc").limit(5)}
   before_validation :initialize_creator, :on => :create
   before_validation :initialize_updater
 
