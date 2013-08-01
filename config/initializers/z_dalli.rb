e3ad8d93d47816426042089f26867185c7014605
r888888888 <r888888888@gmail.com>
2013-08-01 15:06:56 -0700

    fix for production config

--- config/initializers/z_dalli.rb
+++ config/initializers/z_dalli.rb
@@ -1,3 +1,9 @@
 unless defined?(MEMCACHE)
   MEMCACHE = Dalli::Client.new(Danbooru.config.memcached_servers, :namespace => Danbooru.config.app_name.gsub(/[^A-Za-z0-9]/, "_"))
 end
+
+if Rails.env.production?
+  Danbooru::Application.configure do
+    config.cache_store = :dalli_store, Danbooru.config.memcached_servers
+  end
+end
