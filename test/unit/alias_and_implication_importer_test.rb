09b6fd41f0c53a8ecab715f038d64a422f1cfab9
Toks <tttoksss@gmail.com>
2013-09-09 20:47:04 -0400

    Update test with change from 0845c61908086372353852aa63dc4a56e126ca18

--- test/unit/alias_and_implication_importer_test.rb
+++ test/unit/alias_and_implication_importer_test.rb
@@ -20,7 +20,7 @@ class AliasAndImplicationImporterTest < ActiveSupport::TestCase
       end
 
       should "process it" do
-        assert_difference("Delayed::Job.count", 4) do
+        assert_difference("Delayed::Job.count", 2) do
           @importer.process!
         end
       end
