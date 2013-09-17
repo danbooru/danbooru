12d63a7d8895d245c059ab3108d4258949df93d5
Toks <tttoksss@gmail.com>
2013-09-17 11:31:11 -0400

    fixes #1096

--- app/helpers/delayed_jobs_helper.rb
+++ app/helpers/delayed_jobs_helper.rb
@@ -1,29 +1,60 @@
 module DelayedJobsHelper
+  def print_name(job)
+    case job.name
+    when "Class#expire_cache"
+      "<strong>expire post count cache</strong>"
+
+    when "Upload#process!"
+      "<strong>upload post</strong>"
+
+    when "Tag#update_related"
+      "<strong>update related tags</strong>"
+
+    when "TagAlias#process!"
+      "<strong>alias</strong>"
+
+    when "TagImplication#process!"
+      "<strong>implication</strong>"
+
+    when "Class#clear_cache_for"
+      "<strong>expire tag alias cache</strong>"
+
+    when "Tag#update_category_cache"
+      "<strong>update tag category cache</strong>"
+
+    when "Tag#update_category_post_counts"
+      "<strong>update category post counts</strong>"
+
+    else
+      h(job.name)
+    end
+  end
+
   def print_handler(job)
     case job.name
     when "Class#expire_cache"
-      "<strong>expire post count cache</strong>: " + h(job.payload_object.args.flatten.join(" "))
+      h(job.payload_object.args.flatten.join(" "))
 
     when "Upload#process!"
-      '<strong>upload post</strong>: <a href="/uploads/' + job.payload_object.object.id.to_s + '">record</a>'
+      %{<a href="/uploads/#{job.payload_object.object.id}">record</a>}
 
     when "Tag#update_related"
-      "<strong>update related tags</strong>: " + h(job.payload_object.name)
+      h(job.payload_object.name)
 
     when "TagAlias#process!"
-      '<strong>alias</strong>: ' + h(job.payload_object.antecedent_name) + " -&gt; " + h(job.payload_object.consequent_name)
+      h(job.payload_object.antecedent_name) + " -&gt; " + h(job.payload_object.consequent_name)
 
     when "TagImplication#process!"
-      '<strong>implication</strong>: ' + h(job.payload_object.antecedent_name) + " -&gt; " + h(job.payload_object.consequent_name)
+      h(job.payload_object.antecedent_name) + " -&gt; " + h(job.payload_object.consequent_name)
 
     when "Class#clear_cache_for"
-      "<strong>expire tag alias cache</strong>: " + h(job.payload_object.args.flatten.join(" "))
+      h(job.payload_object.args.flatten.join(" "))
 
     when "Tag#update_category_cache"
-      "<strong>update tag category cache</strong>: " + h(job.payload_object.name)
+      h(job.payload_object.name)
 
     when "Tag#update_category_post_counts"
-      "<strong>update category post counts</strong>: " + h(job.payload_object.name)
+      h(job.payload_object.name)
 
     else
       h(job.handler)
