f78de67de9acb66003e68a040d59478a7b241f09
Toks <tttoksss@gmail.com>
2013-10-06 17:06:44 -0400

    #1952: Update tests

--- test/unit/tag_subscription_test.rb
+++ test/unit/tag_subscription_test.rb
@@ -21,7 +21,7 @@ class TagSubscriptionTest < ActiveSupport::TestCase
       end
 
       should "fail" do
-        sub = FactoryGirl.build(:tag_subscription, :tag_query => "aaa bbb", :creator => @user, :name => "zzz")
+        sub = FactoryGirl.build(:tag_subscription, :tag_query => "aaa\nbbb", :creator => @user, :name => "zzz")
         sub.save
         assert_equal(["You can create up to 0 tag subscriptions"], sub.errors.full_messages)
       end
@@ -35,7 +35,7 @@ class TagSubscriptionTest < ActiveSupport::TestCase
       posts << FactoryGirl.create(:post, :tag_string => "ccc")
       posts << FactoryGirl.create(:post, :tag_string => "ddd")
       CurrentUser.scoped(user, "127.0.0.1") do
-        sub_1 = FactoryGirl.create(:tag_subscription, :tag_query => "aaa bbb", :name => "zzz")
+        sub_1 = FactoryGirl.create(:tag_subscription, :tag_query => "aaa\nbbb", :name => "zzz")
         sub_2 = FactoryGirl.create(:tag_subscription, :tag_query => "ccc", :name => "yyy")
         assert_equal([posts[1].id, posts[0].id], TagSubscription.find_posts(user.id, "zzz").map(&:id))
         assert_equal([posts[2].id, posts[1].id, posts[0].id], TagSubscription.find_posts(user.id).map(&:id))
@@ -49,7 +49,7 @@ class TagSubscriptionTest < ActiveSupport::TestCase
       posts << FactoryGirl.create(:post, :tag_string => "bbb")
       posts << FactoryGirl.create(:post, :tag_string => "ccc")
       CurrentUser.scoped(user, "127.0.0.1") do
-        sub = FactoryGirl.create(:tag_subscription, :tag_query => "aaa bbb", :name => "zzz")
+        sub = FactoryGirl.create(:tag_subscription, :tag_query => "aaa\nbbb", :name => "zzz")
         assert_equal("#{posts[1].id},#{posts[0].id}", sub.post_ids)
       end
     end
