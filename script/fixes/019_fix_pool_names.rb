ab7f97c5aef1a1529959513736c346a568ae2500
Toks <tttoksss@gmail.com>
2013-10-07 11:43:13 -0400

    #1984: Fix script

--- script/fixes/019_fix_pool_names.rb
+++ script/fixes/019_fix_pool_names.rb
@@ -4,7 +4,17 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'config',
 
 ActiveRecord::Base.connection.execute("set statement_timeout = 0")
 
-Pool.where("name like ?", "%,%").find_each do |pool|
-  pool.update_attribute(:name, pool.name.gsub(/,/, ""))
-  pool.create_version(true)
+admin = User.admins.first
+
+CurrentUser.scoped(admin, "127.0.0.1") do
+  Pool.where("name like ?", "%,%").find_each do |pool|
+    pool.update_attribute(:name, pool.name.gsub(/,/, ""))
+    pool.create_version(true)
+  end
+end
+
+CurrentUser.scoped(admin, "127.0.0.1") do
+  Pool.joins(:versions).where("pool_versions.name like ?", "%,%").uniq.find_each do |pool|
+    pool.create_version(true)
+  end
 end
