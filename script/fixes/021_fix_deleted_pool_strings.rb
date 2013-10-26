7b5399956622b8be7d60a3bc4eb2f37276b53d5f
Toks <tttoksss@gmail.com>
2013-10-26 18:42:36 -0400

    #2016: Update script

--- script/fixes/021_fix_deleted_pool_strings.rb
+++ script/fixes/021_fix_deleted_pool_strings.rb
@@ -7,8 +7,10 @@ ActiveRecord::Base.connection.execute("set statement_timeout = 0")
 CurrentUser.user = User.admins.first
 CurrentUser.ip_addr = "127.0.0.1"
 
-Pool.deleted.where("post_count > 0").find_each do |pool|
-  Post.where("id in (?)", pool.post_id_array).tag_match("-pool:#{pool.id}").find_each do |post|
-    post.add_pool!(pool, true)
+CurrentUser.without_safe_mode do
+  Pool.deleted.where("post_count > 0").find_each do |pool|
+    Post.where("id in (?)", pool.post_id_array).tag_match("-pool:#{pool.id}").find_each do |post|
+      post.add_pool!(pool, true)
+    end
   end
 end
