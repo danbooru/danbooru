94c0318d035e0abc4ffc3864cbc8627746559ee0
Toks <tttoksss@gmail.com>
2013-12-16 22:03:06 -0500

    fixes #2065

---
+++ db/migrate/20131217025233_add_updated_at_and_id_index_to_post_versions.rb
@@ -0,0 +1,13 @@
+class AddUpdatedAtAndIdIndexToPostVersions < ActiveRecord::Migration
+  def self.up
+    execute "set statement_timeout = 0"
+    remove_index :post_versions, :updated_at
+    add_index :post_versions, [:updated_at, :id]
+  end
+
+  def self.down
+    execute "set statement_timeout = 0"
+    remove_index :post_versions, [:updated_at, :id]
+    add_index :post_versions, :updated_at
+  end
+end
