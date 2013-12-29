c89a4907f9b2830c6780a6600f8599122a488075
Toks <tttoksss@gmail.com>
2013-12-28 19:37:15 -0500

    Update migration

--- db/migrate/20131228230219_update_aliased_implication_antecedents.rb
+++ db/migrate/20131228230219_update_aliased_implication_antecedents.rb
@@ -4,13 +4,15 @@ class UpdateAliasedImplicationAntecedents < ActiveRecord::Migration
     TagImplication.find_each do |ti|
       ta = TagAlias.where("antecedent_name = ? AND status != ?", ti.antecedent_name, "pending").first
       if ta
+        existing_ti = TagImplication.where("antecedent_name = ? AND consequent_name = ?", ta.consequent_name, ti.consequent_name).first
+        existing_ti.destroy if existing_ti
+
         if ta.consequent_name == ti.consequent_name
-          ti.destroy
-        elsif TagImplication.where("antecedent_name = ? AND consequent_name = ?", ta.consequent_name, ti.consequent_name).exists?
           ti.destroy
         else
           ti.antecedent_name = ta.consequent_name
           ti.save
+          ti.update_posts
         end
         ta.update_posts
       end
