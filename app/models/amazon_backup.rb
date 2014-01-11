ca26bf0f3bf1b6c6469bcdfc93e88e2309af0b9d
r888888888 <r888888888@gmail.com>
2014-01-10 16:50:56 -0800

    hard code s3 server

--- app/models/amazon_backup.rb
+++ app/models/amazon_backup.rb
@@ -16,7 +16,8 @@ class AmazonBackup < ActiveRecord::Base
     Post.where("id > ?", last_id).limit(200).order("id").each do |post|
       AWS::S3::Base.establish_connection!(
         :access_key_id => Danbooru.config.amazon_s3_access_key_id,
-        :secret_access_key => Danbooru.config.amazon_s3_secret_access_key
+        :secret_access_key => Danbooru.config.amazon_s3_secret_access_key,
+        :server => "s3-us-east-1.amazonaws.com"
       )
 
       if File.exists?(post.file_path)
