3748f50b6dac6e0f8c56cc0906f656e9261855ce
Toks <tttoksss@gmail.com>
2014-01-12 20:03:35 -0500

    fixes #2090

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,7 +1,7 @@
 module TagsHelper
   def tag_post_count_style(tag)
     @highest_post_count ||= Tag.highest_post_count
-    width_percent = Math.log(tag.post_count, @highest_post_count) * 100
+    width_percent = Math.log([tag.post_count, 1].max, @highest_post_count) * 100
     "background: linear-gradient(to left, #DDD #{width_percent}%, white #{width_percent}%)"
   end
 end
