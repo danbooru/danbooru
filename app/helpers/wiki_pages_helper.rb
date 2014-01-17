200ce6e150a05e41508a6ed4bea072cc563b41f4
Toks <tttoksss@gmail.com>
2014-01-17 18:58:47 -0500

    Add redundant link to post search on wiki pages
    This should make it more clear that the ~6 posts displayed are just a
    sample, not all of them.

--- app/helpers/wiki_pages_helper.rb
+++ app/helpers/wiki_pages_helper.rb
@@ -38,7 +38,8 @@ module WikiPagesHelper
     html = '<div id="wiki-page-posts">'
 
     if Post.fast_count(wiki_page.title) > 0
-      html << "<h2>Posts</h2>"
+      full_link = link_to("view all", posts_path(:tags => wiki_page.title))
+      html << "<h2>Posts (#{full_link})</h2>"
       html << wiki_page.post_set.presenter.post_previews_html(self)
     end
     
