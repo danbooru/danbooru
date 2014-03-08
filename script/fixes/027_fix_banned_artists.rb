26be011bf4038712b7c65c62637073228229f835
r888888888 <r888888888@gmail.com>
2014-03-07 18:10:40 -0800

    fixes #1520

---
+++ script/fixes/027_fix_banned_artists.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'config', 'environment'))
+
+ActiveRecord::Base.connection.execute("set statement_timeout = 0")
+
+CurrentUser.user = User.admins.first
+CurrentUser.ip_addr = "127.0.0.1"
+
+CurrentUser.without_safe_mode do
+  Artist.where(:is_banned => true).find_each do |artist|
+    Post.tag_match(artist.name).where(:is_banned => true, :is_deleted => true).find_each do |post|
+      unless post.post_flags.any?
+        post.undelete!
+      end
+    end
+  end
+end
