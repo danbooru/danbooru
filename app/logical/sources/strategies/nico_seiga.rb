ce2bcc4570c5e6b0db44b5b7cb55b3c11a07e885
Toks <tttoksss@gmail.com>
2014-04-30 12:31:41 -0400

    Seiga: support alternate type of direct link url

--- app/logical/sources/strategies/nico_seiga.rb
+++ app/logical/sources/strategies/nico_seiga.rb
@@ -62,6 +62,8 @@ module Sources
             "http://seiga.nicovideo.jp/seiga/im#{$1}"
           elsif url =~ %r{\Ahttp://lohas\.nicoseiga\.jp/priv/[a-f0-9]+/\d+/(\d+)}i
             "http://seiga.nicovideo.jp/seiga/im#{$1}"
+          elsif url =~ %r{\Ahttp://lohas\.nicoseiga\.jp/priv/(\d+)}i
+            "http://seiga.nicovideo.jp/seiga/im#{$1}"
           elsif url =~ %r{/seiga/im\d+}
             url
           else
