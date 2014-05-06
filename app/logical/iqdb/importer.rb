6c4cd425652ef87cb0cbe9c94daf51e9c3c3ebf8
r888888888 <r888888888@gmail.com>
2014-05-05 17:21:18 -0700

    iqdb command fixes

--- app/logical/iqdb/importer.rb
+++ app/logical/iqdb/importer.rb
@@ -1,14 +1,15 @@
 module Iqdb
   class Importer
     def import!
-      Post.find_each do |post|
-        IO.popen("iqdb command #{Danbooru.config.iqdb_file}", "w") do |io|
+      Post.where("created_at >= ?", 3.months.ago).find_each do |post|
+        IO.popen("iqdb command #{Danbooru.config.iqdb_file}", "w+") do |io|
           if File.exists?(post.preview_file_path)
             puts post.id
             hex = post.id.to_s(16)
             io.puts "add 0 #{hex}:#{post.preview_file_path}"
           end
           io.puts "quit"
+          io.read
         end
       end
     end
