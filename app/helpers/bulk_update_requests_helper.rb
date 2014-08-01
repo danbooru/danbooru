76f31bd6b56220b5cb5eade525326dcff3ec7a0e
Toks <tttoksss@gmail.com>
2014-07-31 21:19:43 -0400

    Bulk reqs: add line breaks
    Scripts longer than 1 line are completely unreadable without line breaks

--- app/helpers/bulk_update_requests_helper.rb
+++ app/helpers/bulk_update_requests_helper.rb
@@ -1,2 +1,6 @@
 module BulkUpdateRequestsHelper
+  def script_with_line_breaks(script)
+    escaped_script = script.gsub(/&/, "&amp;").gsub(/</, "&lt;").gsub(/>/, "&gt;")
+    escaped_script.gsub(/\n/m, "<br>").html_safe
+  end
 end
