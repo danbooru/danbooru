87d3cc31b325dbf9bd106a53f0382a9812cdec80
Toks <tttoksss@gmail.com>
2014-09-20 14:50:52 -0400

    fixes #2257

--- app/models/post.rb
+++ app/models/post.rb
@@ -536,9 +536,11 @@ class Post < ActiveRecord::Base
         end
 
         if image_width >= 1024 && image_width.to_f / image_height >= 4
-          tags << "wide_image long_image"
+          tags << "wide_image"
+          tags << "long_image"
         elsif image_height >= 1024 && image_height.to_f / image_width >= 4
-          tags << "tall_image long_image"
+          tags << "tall_image"
+          tags << "long_image"
         end
       end
 
