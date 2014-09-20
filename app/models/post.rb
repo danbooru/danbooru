507f07e648ad209668d28e769f06f9fdf4d33d47
evazion <noizave@gmail.com>
2014-09-20 12:02:04 -0500

    Fix issue #2257.
    Remove any long_image/wide_image/tall_image tags before adding them
    automatically so they're not added twice.

--- app/models/post.rb
+++ app/models/post.rb
@@ -519,7 +519,7 @@ class Post < ActiveRecord::Base
     def add_automatic_tags(tags)
       return tags if !Danbooru.config.enable_dimension_autotagging
 
-      tags -= %w(incredibly_absurdres absurdres highres lowres huge_filesize)
+      tags -= %w(incredibly_absurdres absurdres highres lowres huge_filesize wide_image tall_image long_image)
 
       if has_dimensions?
         if image_width >= 10_000 || image_height >= 10_000
