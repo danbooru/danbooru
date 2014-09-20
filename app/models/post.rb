6d523153fa979a2f84aadcb3bcff5ee15497e85f
Toks <tttoksss@gmail.com>
2014-09-20 15:10:49 -0400

    Change automatic tags to be added before aliases/implications
    This allows aliases and implications to work on tags added automatically

--- app/models/post.rb
+++ app/models/post.rb
@@ -502,10 +502,10 @@ class Post < ActiveRecord::Base
       normalized_tags = normalized_tags.map{|tag| tag.downcase}
       normalized_tags = remove_negated_tags(normalized_tags)
       normalized_tags = normalized_tags.map {|x| Tag.find_or_create_by_name(x).name}
-      normalized_tags = TagAlias.to_aliased(normalized_tags)
-      normalized_tags = TagImplication.with_descendants(normalized_tags)
       normalized_tags = %w(tagme) if normalized_tags.empty?
       normalized_tags = add_automatic_tags(normalized_tags)
+      normalized_tags = TagAlias.to_aliased(normalized_tags)
+      normalized_tags = TagImplication.with_descendants(normalized_tags)
       normalized_tags.sort!
       set_tag_string(normalized_tags.uniq.sort.join(" "))
     end
