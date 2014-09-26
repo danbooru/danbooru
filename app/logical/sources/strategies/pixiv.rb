4e1f6c3a90e5109eb25b1a08e74a0be0fc86a274
evazion <noizave@gmail.com>
2014-09-25 20:59:33 -0500

    Fetch metadata from Pixiv's smartphone API instead of scraping the HTML.
    Also, normalize all URLs into old-style full-size URLs (e.g.
    http://i2.pixiv.net/img78/img/demekyon/46187950.jpg).
    
    Fetching the metadata from the API is necessary because for old-style
    Pixiv URLs we need to know the image directory (e.g. "img04"), and that
    information isn't available in the HTML.

--- app/logical/sources/strategies/pixiv.rb
+++ app/logical/sources/strategies/pixiv.rb
@@ -1,5 +1,7 @@
 ﻿# encoding: UTF-8
 
+require 'csv'
+
 module Sources
   module Strategies
     class Pixiv < Base
@@ -20,97 +22,41 @@ module Sources
       end
 
       def unique_id
-        image_url =~ /\/img\/([^\/]+)/
-        $1
+        @pixiv_moniker
       end
 
       def get
-        agent.get(URI.parse(normalized_url).request_uri) do |page|
-          @artist_name, @profile_url = get_profile_from_page(page)
-          @image_url = get_image_url_from_page(page)
-          @tags = get_tags_from_page(page)
-          @page_count = get_page_count_from_page(page)
-        end
-      end
+        agent.get(spapi_url) do |page|
+          rows = CSV.parse(page.content.force_encoding("UTF-8"))
+          metadata = rows[0]
 
-    protected
+          illust_id       = metadata[0]
+          pixiv_artist_id = metadata[1]
+          file_ext        = metadata[2]
 
-      def get_profile_from_page(page)
-        profile_url = page.search("a.user-link").first
-        if profile_url
-          profile_url = "http://www.pixiv.net" + profile_url["href"]
-        end
+          # We want "img04", not "img4"
+          image_directory = "img" + metadata[4].rjust(2, "0")
 
-        artist_name = page.search("h1.user").first
-        if artist_name
-          artist_name = artist_name.inner_text
-        end
+          @artist_name = metadata[5]
+          @profile_url = "http://www.pixiv.net/member.php?id=#{pixiv_artist_id}"
+          @pixiv_moniker = metadata[24]
 
-        return [artist_name, profile_url]
-      end
-
-      def get_image_url_from_page(page)
-        elements = page.search("div.works_display a img").find_all do |node|
-          node["src"] !~ /source\.pixiv\.net/
-        end
-
-        if elements.any?
-          elements.first.attr("src").sub(/_[ms]\./, ".")
-        else
-          nil
-        end
-      end
-
-      def get_tags_from_page(page)
-        # puts page.root.to_xhtml
-
-        links = page.search("ul.tags a.text").find_all do |node|
-          node["href"] =~ /search\.php/
-        end
-
-        original_flag = page.search("a.original-works")
-
-        if links.any?
-          links.map! do |node|
-            [node.inner_text, "http://www.pixiv.net" + node.attr("href")]
+          @tags = metadata[13].split(/\s+/).map do |tag|
+              [tag, "http://www.pixiv.net/search.php?s_mode=s_tag_full&word=#{tag}"]
           end
 
-          if original_flag.any?
-            links << ["オリジナル", "http://www.pixiv.net/search.php?s_mode=s_tag_full&word=%E3%82%AA%E3%83%AA%E3%82%B8%E3%83%8A%E3%83%AB"]
-          end
-
-          links
-        else
-          []
-        end
-      end
-
-      def get_page_count_from_page(page)
-        elements = page.search("ul.meta li").find_all do |node|
-          node.text =~ /Manga|漫画/
-        end
-
-        if elements.any?
-          elements[0].text =~ /(?:Manga|漫画) (\d+)P/
-          $1.to_i
-        else
-          1
-        end
-      end
-
-      def normalized_url
-        @normalized_url ||= begin
-          if url =~ /\/(\d+)(?:_big)?(?:_m|_p\d+)?\.(?:jpg|jpeg|png|gif)/i
-            "http://www.pixiv.net/member_illust.php?mode=medium&illust_id=#{$1}"
-          elsif url =~ /mode=big/
-            url.sub(/mode=big/, "mode=medium")
-          elsif url =~ /member_illust\.php/ && url =~ /illust_id=/
-            url
+          # Is this image in a gallery?
+          if not metadata[19].nil?
+            @page_count = metadata[19]
+            @image_url  = "http://i1.pixiv.net/#{image_directory}/img/#{@pixiv_moniker}/#{illust_id}_big_p#{gallery_index_from_url}.#{file_ext}"
           else
-            nil
+            @page_count = 1
+            @image_url  = "http://i1.pixiv.net/#{image_directory}/img/#{@pixiv_moniker}/#{illust_id}.#{file_ext}"
           end
         end
       end
+
+      protected
 
       def agent
         @agent ||= begin
@@ -124,6 +70,66 @@ module Sources
           end
 
           mech
+        end
+      end
+
+      # Pixiv's API requires the PHPSESSID to be passed as a query parameter
+      # instead of in a cookie. If PHPSESSID isn't passed then the API
+      # won't return results for R-18 images.
+      def spapi_url
+        "http://spapi.pixiv.net/iphone/illust.php?illust_id=#{illust_id_from_url}&PHPSESSID=#{phpsessid}"
+      end
+
+      def phpsessid
+        agent.cookies.select do |cookie| cookie.name == "PHPSESSID" end.first.value
+      end
+
+      def illust_id_from_url
+        # http://i2.pixiv.net/img04/img/syounen_no_uta/46170939_p0.jpg
+        # http://i2.pixiv.net/img04/img/syounen_no_uta/46170939_big_p0.jpg
+        # http://i2.pixiv.net/img04/img/syounen_no_uta/mobile/46170939_480mw.jpg
+        # http://i2.pixiv.net/img04/img/syounen_no_uta/mobile/46170939_480mw_p0.jpg
+        # http://i2.pixiv.net/img78/img/demekyon/46187950.jpg?1411668966
+        # http://img04.pixiv.net/img/syounen_no_uta/46170939_p0.jpg
+        # http://img04.pixiv.net/img/syounen_no_uta/46170939_m.jpg
+        # http://i2.pixiv.net/img-inf/img/2014/09/25/00/48/37/46170739_s.jpg
+        # http://i1.pixiv.net/c/600x600/img-master/img/2014/09/24/23/25/08/46168376_p0_master1200.jpg
+        # http://i1.pixiv.net/c/150x150/img-master/img/2014/09/24/23/25/08/46168376_p0_master1200.jpg
+        # http://i1.pixiv.net/img-original/img/2014/09/25/23/09/29/46183440_p0.jpg
+        # http://i2.pixiv.net/img-inf/img/2014/09/25/00/57/24/46170939_128x128.jpg
+        if url =~ /\/(\d+)(?:_big|_480mw)?(?:_s|_m|_p\d+|_\d+x\d+)?(?:_master\d+)?\.(?:jpg|jpeg|png|gif)/i
+          $1
+
+        # http://www.pixiv.net/member_illust.php?mode=medium&illust_id=46170939
+        # http://www.pixiv.net/member_illust.php?mode=big&illust_id=46168376
+        # http://www.pixiv.net/member_illust.php?mode=manga&illust_id=46170939
+        # http://www.pixiv.net/member_illust.php?mode=manga_big&illust_id=46170939&page=0
+        elsif url =~ /illust_id=(\d+)/i
+          $1
+
+        else
+          raise "Invalid URL"
+        end
+      end
+
+      def gallery_index_from_url
+        # Get the page number ("p0") from the URL for images that are part of a gallery:
+        # * http://i2.pixiv.net/img04/img/syounen_no_uta/46170939_p0.jpg
+        # * http://i2.pixiv.net/img04/img/syounen_no_uta/46170939_big_p0.jpg
+        # * http://i2.pixiv.net/img04/img/syounen_no_uta/mobile/46170939_480mw_p0.jpg
+        # * http://img04.pixiv.net/img/syounen_no_uta/46170939_p0.jpg
+        # ...but not for images that aren't part of a gallery:
+        # * http://i1.pixiv.net/c/600x600/img-master/img/2014/09/24/23/25/08/46168376_p0_master1200.jpg
+        # * http://i1.pixiv.net/img-original/img/2014/09/25/23/09/29/46183440_p0.jpg
+        if url =~ %r!pixiv.net/img\d+/img/\w+/(?:mobile/)?\d+(?:_big|_480mw)?_p(\d+)\.(?:jpg|jpeg|png|gif)!i
+          $1
+
+        # http://www.pixiv.net/member_illust.php?mode=manga_big&illust_id=46170939&page=0
+        elsif url =~ /page=(\d+)/i
+          $1
+
+        else
+          0
         end
       end
     end
