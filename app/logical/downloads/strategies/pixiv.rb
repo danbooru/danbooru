a1dbe70624063639a9d79c925011c755c114bfc0
evazion <noizave@gmail.com>
2014-09-27 01:00:40 -0500

    Don't rewrite old-style full-size image URLs.
    This avoids making unnecessary HTTP requests for old-style full-size
    image URLs that don't need to be normalized.
    
    Also, fix the regex for small manga pages so it doesn't attempt to
    rewrite new-style URLs.

--- app/logical/downloads/strategies/pixiv.rb
+++ app/logical/downloads/strategies/pixiv.rb
@@ -5,9 +5,9 @@ module Downloads
         if url =~ /https?:\/\/(?:\w+\.)?pixiv\.net/
           url, headers = rewrite_headers(url, headers)
           url, headers = rewrite_cdn(url, headers)
-          url, headers = rewrite_urls(url, headers)
-          #url, headers = rewrite_small_and_medium_images(url, headers)
-          #url, headers = rewrite_small_manga_pages(url, headers)
+          url, headers = rewrite_small_and_medium_images(url, headers)
+          url, headers = rewrite_small_manga_pages(url, headers)
+          url, headers = rewrite_unnormalized_urls(url, headers)
         end
 
         return [url, headers]
@@ -19,10 +19,19 @@ module Downloads
         return [url, headers]
       end
 
-      def rewrite_urls(url, headers)
-        source = ::Sources::Strategies::Pixiv.new(url)
-        source.get
-        return [source.image_url, headers]
+      # Rewrite anything not of this form:
+      # * http://i2.pixiv.net/img78/img/demekyon/46187950.jpg?1411668966
+      # * http://img78.pixiv.net/img/demekyon/46187950.jpg?1411668966
+      # * http://i2.pixiv.net/img04/img/syounen_no_uta/46170939_big_p0.jpg
+      # * http://img04.pixiv.net/img/syounen_no_uta/46170939_big_p0.jpg
+      def rewrite_unnormalized_urls(url, headers)
+        if url !~ %r!/img/[^/]+/\d+(?:_big_p\d+)?\.(?:jpg|jpeg|png|gif)!i
+          source = ::Sources::Strategies::Pixiv.new(url)
+          source.get
+          url = source.image_url
+        end
+
+        return [url, headers]
       end
 
       def rewrite_small_and_medium_images(url, headers)
@@ -37,8 +46,15 @@ module Downloads
         return [url, headers]
       end
 
+      # Rewrite these:
+      # * http://i2.pixiv.net/img04/img/syounen_no_uta/46170939_p0.jpg
+      # * http://img04.pixiv.net/img/syounen_no_uta/46170939_p0.jpg
+      # ...but not these:
+      # * http://i2.pixiv.net/img04/img/syounen_no_uta/46170939_big_p0.jpg
+      # * http://i1.pixiv.net/c/600x600/img-master/img/2014/09/24/23/25/08/46168376_p0_master1200.jpg
+      # * http://i1.pixiv.net/img-original/img/2014/09/25/23/09/29/46183440_p0.jpg
       def rewrite_small_manga_pages(url, headers)
-        if url =~ %r!(\d+_p\d+)\.!
+        if url =~ %r!/img/[^/]+/(\d+_p\d+)\.(?:jpg|jpeg|png|gif)!i
           match = $1
           repl = match.sub(/_p/, "_big_p")
           big_url = url.sub(match, repl)
