6d420f722ff19a7b9b5f02825859e1eb5f099067
Toks <tttoksss@gmail.com>
2014-11-26 19:10:28 -0500

    saved search api

--- app/controllers/saved_searches_controller.rb
+++ app/controllers/saved_searches_controller.rb
@@ -1,10 +1,16 @@
 class SavedSearchesController < ApplicationController
-  respond_to :html, :js
+  respond_to :html, :xml, :json, :js
   
   def index
     @saved_searches = saved_searches.order("tag_query")
     @categories = @saved_searches.group_by{|saved_search| saved_search.category.to_s}
     @categories = @categories.sort_by{|category, saved_searches| category.to_s}
+
+    respond_with(@saved_searches) do |format|
+      format.xml do
+        render :xml => @saved_searches.to_xml(:root => "saved-searches")
+      end
+    end
   end
 
   def create
