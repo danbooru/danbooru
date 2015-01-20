74d636bafdfeb4960f1bbb97105c76b940b695f8
r888888888 <r888888888@gmail.com>
2015-01-19 16:58:54 -0800

    fixes #2333

---
+++ db/migrate/20150120005624_remove_unused_indexes.rb
@@ -0,0 +1,7 @@
+class RemoveUnusedIndexes < ActiveRecord::Migration
+  def change
+    execute "set statement_timeout = 0"
+    remove_index :posts, :source
+    remove_index :posts, :uploader_ip_addr
+  end
+end
