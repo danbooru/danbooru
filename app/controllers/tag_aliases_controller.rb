99f55c32a4872c1f0c343ad48160e907699c7e36
Toks <tttoksss@gmail.com>
2015-06-09 23:54:22 -0400

    fixes #2379

--- app/controllers/tag_aliases_controller.rb
+++ app/controllers/tag_aliases_controller.rb
@@ -44,6 +44,7 @@ class TagAliasesController < ApplicationController
   def approve
     @tag_alias = TagAlias.find(params[:id])
     @tag_alias.update_column(:status, "queued")
+    @tag_alias.rename_wiki_and_artist
     @tag_alias.delay(:queue => "default").process!
     respond_with(@tag_alias, :location => tag_alias_path(@tag_alias))
   end
