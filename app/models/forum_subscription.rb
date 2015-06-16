01189017c8c3a9b712d7da8a11230b9913ed51c8
r888888888 <r888888888@gmail.com>
2015-06-16 16:21:22 -0700

    forum subscription ignores last post when processing

--- app/models/forum_subscription.rb
+++ app/models/forum_subscription.rb
@@ -12,7 +12,7 @@ class ForumSubscription < ActiveRecord::Base
       forum_topic = subscription.forum_topic
       if forum_topic.updated_at > subscription.last_read_at
         CurrentUser.scoped(subscription.user, "127.0.0.1") do
-          forum_posts = forum_topic.posts.where("created_at >= ?", subscription.last_read_at).order("id desc")
+          forum_posts = forum_topic.posts.where("created_at > ?", subscription.last_read_at).order("id desc")
           begin
             UserMailer.forum_notice(subscription.user, forum_topic, forum_posts).deliver
           rescue Net::SMTPSyntaxError
