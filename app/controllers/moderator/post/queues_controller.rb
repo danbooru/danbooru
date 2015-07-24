d361be4ccd5b7c2c925ee1f95d831d02636567ae
r888888888 <r888888888@gmail.com>
2015-07-24 13:47:24 -0700

    fixes #2453: Customize per page pagination limit for mod queue

--- app/controllers/moderator/post/queues_controller.rb
+++ app/controllers/moderator/post/queues_controller.rb
@@ -5,11 +5,21 @@ module Moderator
       before_filter :post_approvers_only
 
       def show
+        if params[:per_page]
+          session["mq_per_page"] = params[:per_page]
+        end
+
         ::Post.without_timeout do
-          @posts = ::Post.order("posts.id asc").pending_or_flagged.available_for_moderation(params[:hidden]).search(:tag_match => "#{params[:query]} status:any").paginate(params[:page], :limit => 25)
+          @posts = ::Post.order("posts.id asc").pending_or_flagged.available_for_moderation(params[:hidden]).search(:tag_match => "#{params[:query]} status:any").paginate(params[:page], :limit => per_page)
           @posts.each # hack to force rails to eager load
         end
         respond_with(@posts)
+      end
+
+    protected
+
+      def per_page
+        session["mq_per_page"] || params[:per_page] || 25
       end
     end
   end
