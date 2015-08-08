23dfc455d5bdda802a795197b6f8994bd615e7f4
r888888888 <r888888888@gmail.com>
2015-08-07 17:29:33 -0700

    simplify contributor report

--- app/logical/reports/contributors.rb
+++ app/logical/reports/contributors.rb
@@ -3,7 +3,7 @@ require 'statistics2'
 module Reports
   class Contributors < User
     def users
-      ::User.where("users.level >= ? and users.post_upload_count >= 250", ::User::Levels::CONTRIBUTOR).order("created_at desc").limit(50).map {|x| Reports::UserPromotions::User.new(x)}
+      ::User.where("users.level >= ? and users.post_upload_count >= 250", ::User::Levels::CONTRIBUTOR).order("created_at desc").map {|x| Reports::UserPromotions::User.new(x)}
     end
   end
 end
