767b11d4b57dc57fd586b0a38e81daa24aac6592
r888888888 <r888888888@gmail.com>
2015-08-17 13:30:16 -0700

    fixes #2495: Post approvers can't delete/undelete/ban posts

--- app/controllers/moderator/post/posts_controller.rb
+++ app/controllers/moderator/post/posts_controller.rb
@@ -1,7 +1,8 @@
 module Moderator
   module Post
     class PostsController < ApplicationController
-      before_filter :moderator_only, :only => [:delete, :undelete, :move_favorites, :ban, :unban, :confirm_delete, :confirm_move_favorites, :confirm_ban]
+      before_filter :post_approvers_only, :only => [:delete, :undelete, :ban, :unban, :confirm_ban, :confirm_delete]
+      before_filter :moderator_only, :only => [:move_favorites, :confirm_move_favorites]
       before_filter :admin_only, :only => [:expunge]
       rescue_from ::PostFlag::Error, ::Post::ApprovalError, :with => :rescue_exception
 
