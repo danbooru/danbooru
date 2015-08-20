d44dd09a8862fb4f9a1d4ae015d6ff0e18d9193d
r888888888 <r888888888@gmail.com>
2015-08-20 14:11:46 -0700

    revise janitor trial report

--- app/logical/reports/janitor_trials.rb
+++ app/logical/reports/janitor_trials.rb
@@ -19,10 +19,6 @@ module Reports
         @approval_count ||= Post.where("approver_id = ? and created_at >= ?", user.id, since).count
       end
 
-      def deleted_count
-        Post.where("approver_id = ? and created_at >= ? and is_deleted = true", user.id, since).count
-      end
-
       def percentile_25_score
         ActiveRecord::Base.select_value_sql("select percentile_cont(0.25) within group (order by score) from posts where created_at >= ? and approver_id = ?", since, user.id).to_i
       end
@@ -31,10 +27,10 @@ module Reports
         ActiveRecord::Base.select_value_sql("select percentile_cont(0.50) within group (order by score) from posts where created_at >= ? and approver_id = ?", since, user.id).to_i
       end
 
-      def confidence_interval
-        hits = Post.where("approver_id = ? and created_at >= ? and score >= 3", user.id, since).count
+      def deletion_confidence
+        hits = Post.where("approver_id = ? and created_at >= ? and is_deleted = true", user.id, since).count
         total = Post.where("approver_id = ? and created_at >= ?", user.id, since).count
-        Reports::UserPromotions.ci_lower_bound(hits, total, 0.95)
+        Reports::UserPromotions.ci_lower_bound(hits, total, 0.95).to_i
       end
     end
 
