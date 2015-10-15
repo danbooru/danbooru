d8e84a90dc96af47e221a1d7effc251d873f2978
r888888888 <r888888888@gmail.com>
2015-10-15 12:12:21 -0700

    Revert "fixes #2530: improved ip addr search"
    This reverts commit 1eb2d515fdcac4b71aee57e2a86e7874af88f309.
    
    Conflicts:
    	app/views/moderator/ip_addrs/index.html.erb

--- app/logical/moderator/ip_addr_search.rb
+++ app/logical/moderator/ip_addr_search.rb
@@ -19,44 +19,9 @@ module Moderator
       end
     end
 
-    def find_common_users
-      user = User.find_by_name(params[:user_name])
-      ip_addrs = Set.new
-      user_ids = {}
-
-      ip_addrs.merge(find_distinct_ip_addrs("comments", "ip_addr", "creator_id", user.id))
-      ip_addrs.merge(find_distinct_ip_addrs("post_versions", "updater_ip_addr", "updater_id", user.id))
-      ip_addrs.merge(find_distinct_ip_addrs("note_versions", "updater_ip_addr", "updater_id", user.id))
-      ip_addrs.merge(find_distinct_ip_addrs("pool_versions", "updater_ip_addr", "updater_id", user.id))
-      ip_addrs.merge(find_distinct_ip_addrs("wiki_page_versions", "updater_ip_addr", "updater_id", user.id))
-      ip_addrs.merge(find_distinct_ip_addrs("dmails", "creator_ip_addr", "from_id", user.id))
-
-      ip_addrs.each do |ip_addr|
-        find_distinct_user_ids(user_ids, "comments", "ip_addr", "creator_id", ip_addrs.to_a)
-        find_distinct_user_ids(user_ids, "post_versions", "updater_ip_addr", "updater_id", ip_addrs.to_a)
-        find_distinct_user_ids(user_ids, "note_versions", "updater_ip_addr", "updater_id", ip_addrs.to_a)
-        find_distinct_user_ids(user_ids, "pool_versions", "updater_ip_addr", "updater_id", ip_addrs.to_a)
-        find_distinct_user_ids(user_ids, "wiki_page_versions", "updater_ip_addr", "updater_id", ip_addrs.to_a)
-        find_distinct_user_ids(user_ids, "dmails", "creator_ip_addr", "from_id", ip_addrs.to_a)
-      end
-
-      user_ids
-    end
-
   private
     def select_all_sql(sql, *params)
       ActiveRecord::Base.select_all_sql(sql, *params)
-    end
-
-    def find_distinct_ip_addrs(table_name, ip_field_name, user_field_name, user_id)
-      select_all_sql("select #{ip_field_name} from #{table_name} where updated_at >= ? and #{user_field_name} = ? group by #{ip_field_name}", 3.months.ago, user_id).rows.flatten
-    end
-
-    def find_distinct_user_ids(user_ids, table_name, ip_field_name, user_field_name, ip_addrs)
-      select_all_sql("select #{user_field_name}, count(*) from #{table_name} where updated_at >= ? and #{ip_field_name} in (?) group by #{user_field_name}", 3.months.ago, ip_addrs).rows.each do |user_id, count|
-        user_ids[user_id] ||= 0
-        user_ids[user_id] += count.to_i
-      end
     end
 
     def search_by_ip_addr(ip_addrs)
