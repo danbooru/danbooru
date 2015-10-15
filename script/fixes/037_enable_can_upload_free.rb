0b9aebddb67185624b2e144fadef320078281ce7
r888888888 <r888888888@gmail.com>
2015-10-15 16:27:18 -0700

    fix bug #2469

--- script/fixes/037_enable_can_upload_free.rb
+++ script/fixes/037_enable_can_upload_free.rb
@@ -7,8 +7,8 @@ ActiveRecord::Base.connection.execute("set statement_timeout = 0")
 CurrentUser.user = User.admins.first
 CurrentUser.ip_addr = "127.0.0.1"
 
-User.where("level >= ?", User::Levels::CONTRIBUTOR).find_each do |user|
+User.where("level >= ?", 33).find_each do |user|
   user.can_upload_free = true
-  user.level = User::Levels::BUILDER if user.level == User::Levels::CONTRIBUTOR
+  user.level = User::Levels::BUILDER if user.level == 33
   user.save
 end
