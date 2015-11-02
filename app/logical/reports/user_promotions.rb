4cf7effc4a02b471eac0412b7e7c92380c2aa92c
r888888888 <r888888888@gmail.com>
2015-11-02 12:45:48 -0800

    fixes #2542: User promotion report shows wrong users

--- app/logical/reports/user_promotions.rb
+++ app/logical/reports/user_promotions.rb
@@ -66,7 +66,7 @@ module Reports
     end
 
     def users
-      ::User.where("users.bit_prefs & ? > 0 and users.post_upload_count >= 250", ::User.flag_value_for("can_upload_free")).order("created_at desc").map {|x| Reports::UserPromotions::User.new(x)}
+      ::User.where("users.bit_prefs & ? = 0 and users.post_upload_count >= 250", ::User.flag_value_for("can_upload_free")).order("created_at desc").map {|x| Reports::UserPromotions::User.new(x)}
     end
   end
 end
