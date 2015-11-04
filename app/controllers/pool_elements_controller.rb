bf182cb65243ba97eddb6134ca1bacc1aee3985e
Toks <tttoksss@gmail.com>
2015-11-03 23:31:19 -0500

    Better error when trying to add to nonexistent pool
    fixes #2546

--- app/controllers/pool_elements_controller.rb
+++ app/controllers/pool_elements_controller.rb
@@ -5,13 +5,13 @@ class PoolElementsController < ApplicationController
   def create
     @pool = Pool.find_by_name(params[:pool_name]) || Pool.find_by_id(params[:pool_id])
 
-    if @pool.present?
+    if @pool.present? && !@pool.is_deleted?
       @post = Post.find(params[:post_id])
       @pool.add!(@post)
       append_pool_to_session(@pool)
+    else
+      @error = "That pool does not exist"
     end
-
-    respond_with(@pool, :location => post_path(@post))
   end
 
   def destroy
