7576377fd8d9f1bd0ff7f575d529e4a5c6d99198
r888888888 <r888888888@gmail.com>
2016-03-24 17:43:52 -0700

    add unicorn logic to cap

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -23,3 +23,25 @@ stdout_path "log/unicorn.log"
 
 # Set master PID location
 pid "#{app_path}/tmp/pids/unicorn.pid"
+
+before_fork do |server, worker|
+  old_pid = "#{server.config[:pid]}.oldbin"
+  
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
