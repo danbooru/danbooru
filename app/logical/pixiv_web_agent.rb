84857e7a3e61f81cc851742674ac2d108a7f1e71
r888888888 <r888888888@gmail.com>
2016-05-12 14:01:35 -0700

    fix pixiv web agent

--- app/logical/pixiv_web_agent.rb
+++ app/logical/pixiv_web_agent.rb
@@ -5,23 +5,23 @@ class PixivWebAgent
 
   def self.build
     mech = Mechanize.new
-    # phpsessid = Cache.get("pixiv-phpsessid")
+    phpsessid = Cache.get("pixiv-phpsessid")
 
-    # if phpsessid
-    #   cookie = Mechanize::Cookie.new("PHPSESSID", phpsessid)
-    #   cookie.domain = ".pixiv.net"
-    #   cookie.path = "/"
-    #   mech.cookie_jar.add(cookie)
-    # else
-    #   mech.get("https://www.pixiv.net/login.php") do |page|
-    #     page.form_with(:action => "/login.php") do |form|
-    #       form['pixiv_id'] = Danbooru.config.pixiv_login
-    #       form['pass'] = Danbooru.config.pixiv_password
-    #     end.click_button
-    #   end
-    #   phpsessid = mech.cookie_jar.cookies.select{|c| c.name == "PHPSESSID"}.first
-    #   Cache.put("pixiv-phpsessid", phpsessid.value, 1.month) if phpsessid
-    # end
+    if phpsessid
+      cookie = Mechanize::Cookie.new("PHPSESSID", phpsessid)
+      cookie.domain = ".pixiv.net"
+      cookie.path = "/"
+      mech.cookie_jar.add(cookie)
+    else
+      mech.get("https://www.pixiv.net") do |page|
+        page.form_with(:action => "https://www.pixiv.net/login.php") do |form|
+          form['pixiv_id'] = Danbooru.config.pixiv_login
+          form['pass'] = Danbooru.config.pixiv_password
+        end.click_button
+      end
+      phpsessid = mech.cookie_jar.cookies.select{|c| c.name == "PHPSESSID"}.first
+      Cache.put("pixiv-phpsessid", phpsessid.value, 1.month) if phpsessid
+    end
 
     mech
   end
