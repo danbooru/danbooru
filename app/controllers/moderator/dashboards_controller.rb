73d62bbc011c658549d43836f300a20884d51672
r888888888 <r888888888@gmail.com>
2016-08-30 15:14:46 -0700

    expose mod dashboard to all users

--- app/controllers/moderator/dashboards_controller.rb
+++ app/controllers/moderator/dashboards_controller.rb
@@ -1,6 +1,6 @@
 module Moderator
   class DashboardsController < ApplicationController
-    before_filter :moderator_only
+    before_filter :member_only
     helper :post_flags, :post_appeals
 
     def show
