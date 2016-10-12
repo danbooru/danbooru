8d8511dc08735b5d60780b45fb57ece3d933b7fc
Albert Yi <ayi@desk.com>
2016-10-12 16:46:29 -0700

    update rbenv ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,5 +4,5 @@ set :application, "danbooru"
 set :repo_url,  "git://github.com/r888888888/danbooru.git"
 set :scm, :git
 set :deploy_to, "/var/www/danbooru2"
-set :rbenv_ruby, "2.1.5"
+set :rbenv_ruby, "2.3.1"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
