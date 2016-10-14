danbooru/danbooru

commit 6d6d00b190dd76aabcfaa54cb10c8252d4e844a1

expose voting to non-gold supervoters

app/controllers/post_votes_controller.rb
@@ -1,10 +1,16 @@
 class PostVotesController < ApplicationController
-  before_filter :gold_only
+  before_filter :voter_only
 
   def create
     @post = Post.find(params[:post_id])
     @post.vote!(params[:score])
   rescue PostVote::Error => x
     @error = x
+  end
+
+protected
+
+  def voter_only
+    CurrentUser.is_voter?
   end
 end

app/models/user.rb
@@ -401,6 +401,10 @@ class User < ActiveRecord::Base
       level >= Levels::ADMIN
     end
 
+    def is_voter?
+      is_gold? || is_super_voter?
+    end
+
     def create_mod_action
       if level_changed?
         ModAction.create(:description => %{"#{name}":/users/#{id} level changed #{level_string_was} -> #{level_string}})

app/logical/anonymous_user.rb
@@ -241,6 +241,14 @@ class AnonymousUser
     false
   end
 
+  def is_voter?
+    false
+  end
+
+  def is_super_voter?
+    false
+  end
+
   %w(member banned gold builder platinum janitor moderator admin).each do |name|
     define_method("is_#{name}?") do
       false

app/views/comments/partials/index/_header.html.erb
@@ -16,7 +16,7 @@
       <strong>Score</strong>
       <span>
         <span id="score-for-post-<%= post.id %>"><%= post.score %></span>
-        <% if CurrentUser.is_gold? %>
+        <% if CurrentUser.is_voter? %>
           (vote <%= link_to("up", post_votes_path(:score => "up", :post_id => post.id), :remote => true, :method => :post) %>/<%= link_to("down", post_votes_path(:score => "down", :post_id => post.id), :remote => true, :method => :post) %>)
         <% end %>
       </span>

app/views/posts/partials/show/_information.html.erb
@@ -16,7 +16,7 @@
   </li>
   <li>Source: <%= post_source_tag(post) %></li>
   <li>Rating: <%= post.pretty_rating %></li>
-  <li>Score: <span id="score-for-post-<%= post.id %>"><%= post.score %></span> <% if CurrentUser.is_gold? %>(<span id="vote-links-for-post-<%= post.id %>">vote <%= link_to "up", post_votes_path(:post_id => post.id, :score => "up"), :remote => true, :method => :post %>/<%= link_to "down", post_votes_path(:post_id => post.id, :score => "down"), :remote => true, :method => :post %></span><%= link_to "undo vote", unvote_post_path(post), :remote => true, :method => :put, :id => "unvote-link-for-post-#{post.id}", :class => "unvote-post-link" %>)<% end %></li>
+  <li>Score: <span id="score-for-post-<%= post.id %>"><%= post.score %></span> <% if CurrentUser.is_voter? %>(<span id="vote-links-for-post-<%= post.id %>">vote <%= link_to "up", post_votes_path(:post_id => post.id, :score => "up"), :remote => true, :method => :post %>/<%= link_to "down", post_votes_path(:post_id => post.id, :score => "down"), :remote => true, :method => :post %></span><%= link_to "undo vote", unvote_post_path(post), :remote => true, :method => :put, :id => "unvote-link-for-post-#{post.id}", :class => "unvote-post-link" %>)<% end %></li>
   <li>Favorites: <span id="favcount-for-post-<%= post.id %>"><%= post.fav_count %></span>
   <% if CurrentUser.is_gold? %>
     <%= link_to "Show &raquo;".html_safe, "#", :id => "show-favlist-link" %>

script/fixes/040_fix_post_scores.rb
@@ -1,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'config', 'environment'))
+
+ActiveRecord::Base.connection.execute("set statement_timeout = 0")
+
+CurrentUser.user = User.admins.first
+CurrentUser.ip_addr = "127.0.0.1"
+
+PostVote.find_each do |vote|
+  vote.destroy unless User.find(vote.user_id).is_gold?
+end

commit d84184b5f10ea337c7217b00c5dc9fd2a998d7b5

Prevent anon/banned/member users from voting (fix #2719).
There was a regression in 6d6d00b; `before_filter :voter_only` was a
no-op in the post vote controller because it merely returned false,
which does not halt the request. The fix is to arrange for a voter_only
method to be defined that properly redirects to the access denied page.

app/controllers/application_controller.rb
@@ -133,9 +133,9 @@ protected
     end
   end
 
-  %w(member banned builder gold platinum janitor moderator admin).each do |level|
-    define_method("#{level}_only") do
-      if !CurrentUser.user.is_banned_or_ip_banned? && CurrentUser.user.__send__("is_#{level}?")
+  User::Roles.each do |role|
+    define_method("#{role}_only") do
+      if !CurrentUser.user.is_banned_or_ip_banned? && CurrentUser.user.__send__("is_#{role}?")
         true
       else
         access_denied()

app/controllers/post_votes_controller.rb
@@ -14,10 +14,4 @@ class PostVotesController < ApplicationController
   rescue PostVote::Error => x
     @error = x
   end
-
-protected
-
-  def voter_only
-    CurrentUser.is_voter?
-  end
 end

app/models/user.rb
@@ -16,6 +16,16 @@ class User < ActiveRecord::Base
     ADMIN = 50
   end
 
+  # Used for `before_filter :<role>_only`. Must have a corresponding `is_<role>?` method.
+  Roles = Levels.constants.map(&:downcase) + [
+    :anonymous,
+    :banned,
+    :approver,
+    :voter,
+    :super_voter,
+    :verified,
+  ]
+
   BOOLEAN_ATTRIBUTES = %w(
     is_banned
     has_mail
@@ -382,6 +392,10 @@ class User < ActiveRecord::Base
 
     def is_member?
       true
+    end
+
+    def is_blocked?
+      is_banned?
     end
 
     def is_builder?