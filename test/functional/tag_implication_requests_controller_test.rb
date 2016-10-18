5743166e25f46361ded6b774596147413eaf0480
evazion <noizave@gmail.com>
2016-10-18 05:18:43 -0500

    Add a test case for implication requests.

--- test/functional/tag_implication_requests_controller_test.rb
+++ test/functional/tag_implication_requests_controller_test.rb
@@ -25,11 +25,31 @@ class TagImplicationRequestsControllerTest < ActionController::TestCase
     end
 
     context "create action" do
-      should "render" do
+      should "create forum post" do
         assert_difference("ForumTopic.count", 1) do
           post :create, {:tag_implication_request => {:antecedent_name => "aaa", :consequent_name => "bbb", :reason => "ccc", :skip_secondary_validations => true}}, {:user_id => @user.id}
         end
         assert_redirected_to(forum_topic_path(ForumTopic.last))
+      end
+
+      should "create a pending implication" do
+        params = {
+          :tag_implication_request => {
+            :antecedent_name => "foo",
+            :consequent_name => "bar",
+            :reason => "blah blah",
+            :skip_secondary_validations => true
+          }
+        }
+
+        post :create, params, {:user_id => @user.id}
+
+        tir = assigns(:tag_implication_request)
+        assert_redirected_to(forum_topic_path(tir.forum_topic))
+
+        assert("foo", tir.tag_implication.antecedent_name)
+        assert("bar", tir.tag_implication.consequent_name)
+        assert("pending", tir.tag_implication.status)
       end
     end
   end
