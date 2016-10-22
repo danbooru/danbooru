danbooru/danbooru

commit 0006b76c4d9772e574f7af26aeec7b2692871696

Always obey rating locks; make rerating locked posts an error.
Currently rating locks are only obeyed when using the rating: metatag.
They aren't obeyed when:

* Changing the rating via the API.
* Changing the rating via 'Rate Safe' in the mode menu (uses the API).
* Reverting to previous versions.

Also, the current behavior is to ignore the rating: metatag if the post
is locked. This patch instead makes the update fail completely (note that
this could affect trying to mass revert posts that may be rating locked).

Note: the check for `!is_rating_locked_changed?` is so that

  PUT /posts/1.json?post[rating]=s&post[is_rating_locked]=true

works (ie., locking and changing the rating at the same time is okay).

app/models/post.rb
@@ -47,6 +47,7 @@ class Post < ActiveRecord::Base
   validates_uniqueness_of :md5
   validates_inclusion_of :rating, in: %w(s q e), message: "rating must be s, q, or e"
   validate :post_is_not_its_own_parent
+  validate :updater_can_change_rating
   attr_accessible :source, :rating, :tag_string, :old_tag_string, :old_parent_id, :old_source, :old_rating, :parent_id, :has_embedded_notes, :as => [:member, :builder, :gold, :platinum, :janitor, :moderator, :admin, :default]
   attr_accessible :is_rating_locked, :is_note_locked, :as => [:builder, :janitor, :moderator, :admin]
   attr_accessible :is_status_locked, :as => [:admin]
@@ -769,9 +770,7 @@ class Post < ActiveRecord::Base
           self.source = $1
 
         when /^rating:([qse])/i
-          unless is_rating_locked?
-            self.rating = $1.downcase
-          end
+          self.rating = $1.downcase
         end
       end
     end
@@ -1760,6 +1759,15 @@ class Post < ActiveRecord::Base
 
     self.tag_string = tags.join(" ")
     save
+  end
+
+  def updater_can_change_rating
+    if rating_changed? && is_rating_locked?
+      # Don't forbid changes if the rating lock was just now set in the same update.
+      if !is_rating_locked_changed?
+        errors.add(:rating, "is locked and cannot be changed. Unlock the post first.")
+      end
+    end
   end
 
   def update_column(name, value)

commit 07921d2c88f4e464ecf73be7f4a1cfcec981dc1d

Make rating:s obey rate locks; make source:blah update pixiv id.
Move normalize_tags (which processes metatags) from before_save to
before_validation. This is so that it runs as early as possible, before
strip_source / parse_pixiv_ids / updater_can_change_rating, so these
callbacks can handle source/rating changes from metatags.

Fixes a couple bugs:

* Ratings locks were ignored when using rating:s metatag (regression in 0006b76)
* Pixiv ids weren't updated when using source:blah metatag.

Note: this means that `post.update_attribute(:tag_string => "art:bkub)`
is now wrong. This is because update_attribute runs callbacks but not
validations, so it doesn't process metatags from the tag string.
`update` or `update_attributes` must be used instead.

app/models/post.rb
@@ -7,8 +7,10 @@ class Post < ActiveRecord::Base
   class RevertError < Exception ; end
   class SearchError < Exception ; end
 
-  before_validation :strip_source
   before_validation :initialize_uploader, :on => :create
+  before_validation :merge_old_changes
+  before_validation :normalize_tags
+  before_validation :strip_source
   before_validation :parse_pixiv_id
   before_validation :blank_out_nonexistent_parents
   before_validation :remove_parent_loops
@@ -16,8 +18,6 @@ class Post < ActiveRecord::Base
   validates_inclusion_of :rating, in: %w(s q e), message: "rating must be s, q, or e"
   validate :post_is_not_its_own_parent
   validate :updater_can_change_rating
-  before_save :merge_old_changes
-  before_save :normalize_tags
   before_save :update_tag_post_counts
   before_save :set_tag_counts
   before_save :set_pool_category_pseudo_tags