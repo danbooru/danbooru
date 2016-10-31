00da01ea3ef33c6d76da45ce6902807c96b0c6ea
evazion <noizave@gmail.com>
2016-10-31 01:37:49 +0000

    Remove unused NotesController#pass_html_id.
    Dead code as far as I can tell. According to `git log -p -G "X-Html-Id"`
    was added in 2011 and never used. HTML id is passed to javascript in the
    JSON response to `POST /notes.json` instead.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,6 @@
 class NotesController < ApplicationController
   respond_to :html, :xml, :json, :js
   before_filter :member_only, :except => [:index, :show]
-  before_filter :pass_html_id, :only => [:create]
 
   def search
   end
@@ -66,12 +65,6 @@ private
 
   def create_params
     params.require(:note).permit(:x, :y, :width, :height, :body, :post_id, :html_id)
-  end
-
-  def pass_html_id
-    if params[:note] && params[:note][:html_id]
-      response.headers["X-Html-Id"] = params[:note][:html_id]
-    end
   end
 
   def index_by_post
