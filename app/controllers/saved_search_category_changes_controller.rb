00ddd2f5d4bd4ab7df42ab8c588050808b98c6a7
evazion <noizave@gmail.com>
2016-12-22 21:44:35 -0600

    Fix #2795: "check_availability" bug with saved searches.

--- app/controllers/saved_search_category_changes_controller.rb
+++ app/controllers/saved_search_category_changes_controller.rb
@@ -1,6 +1,7 @@
 class SavedSearchCategoryChangesController < ApplicationController
+  include SavedSearches::CheckAvailability
+
   before_filter :member_only
-  before_filter :check_availabililty
   respond_to :html
 
   def new
@@ -11,23 +12,5 @@ class SavedSearchCategoryChangesController < ApplicationController
     SavedSearch.rename(CurrentUser.user.id, params[:old], params[:new])
     flash[:notice] = "Saved searches will be renamed"
     redirect_to saved_searches_path
-  end
-
-private
-  
-  def check_availabililty
-    if !SavedSearch.enabled?
-      respond_to do |format|
-        format.html do
-          flash[:notice] = "Listbooru service is not configured. Saved searches are not available."
-          redirect_to :back
-        end
-        format.json do
-          render json: {success: false, reason: "Listbooru service is not configured"}.to_json, status: 501
-        end
-      end
-
-      return false
-    end
   end
 end
