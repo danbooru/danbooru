811fa7508b48356ab57b71391d62d796759c2050
Albert Yi <ayi@desk.com>
2016-12-28 15:53:04 -0800

    add statement tiemout to migration

--- db/migrate/20161227003428_add_sticky_to_comments.rb
+++ db/migrate/20161227003428_add_sticky_to_comments.rb
@@ -1,5 +1,6 @@
 class AddStickyToComments < ActiveRecord::Migration
   def change
+    execute "set statement_timeout = 0"
     add_column :comments, :is_sticky, :boolean, null: false, default: false
   end
 end
