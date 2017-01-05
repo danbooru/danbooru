5f0d6d23df798ff82337136cd663db5c6c3cbb48
evazion <noizave@gmail.com>
2017-01-05 17:11:06 -0600

    Fix wrong API limit for users without API key.
    Fixes an issue with tag scripting failures caused by the user hitting
    API limits when they didn't have an API key.

--- app/models/user.rb
+++ app/models/user.rb
@@ -599,9 +599,9 @@ class User < ActiveRecord::Base
     end
 
     def api_hourly_limit(idempotent = true)
-      base = if is_platinum? && api_key.present?
+      base = if is_platinum?
         5000
-      elsif is_gold? && api_key.present?
+      elsif is_gold?
         1000
       else
         300
