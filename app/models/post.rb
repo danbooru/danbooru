danbooru/danbooru

commit 6e62a4cc33e82f1dca56ffb52d634b0d2eef61f5

Optimize Post#favorited_users.
Load users in one query rather than one query per user.

app/models/post.rb
@@ -965,9 +965,7 @@ class Post < ActiveRecord::Base
     end
 
     def favorited_users
-      favorited_user_ids.map {|id| User.find(id)}.select do |x|
-        !x.hide_favorites?
-      end
+      User.find(favorited_user_ids).reject(&:hide_favorites?)
     end
 
     def favorite_groups(active_id=nil)

commit 2763f7e1377e9f961ea5f3b65671d9535391f5aa

Fix ordering of favlists (sort by users who favorited first).
Fix bug introduced in 6e62a4c.

app/models/post.rb
@@ -960,12 +960,12 @@ class Post < ActiveRecord::Base
     rescue PostVote::Error
     end
 
-    def favorited_user_ids
-      fav_string.scan(/\d+/)
-    end
-
+    # users who favorited this post, ordered by users who favorited it first
     def favorited_users
-      User.find(favorited_user_ids).reject(&:hide_favorites?)
+      favorited_user_ids = fav_string.scan(/\d+/).map(&:to_i)
+      visible_users = User.find(favorited_user_ids).reject(&:hide_favorites?)
+      ordered_users = visible_users.index_by(&:id).slice(*favorited_user_ids).values
+      ordered_users
     end
 
     def favorite_groups(active_id=nil)