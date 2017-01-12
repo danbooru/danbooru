cf046eecbfb3c2ca38d76b2940fb253f3f02f691
evazion <noizave@gmail.com>
2017-01-12 07:43:59 +0000

    Fix ip addr search tests.

--- app/logical/moderator/ip_addr_search.rb
+++ app/logical/moderator/ip_addr_search.rb
@@ -58,7 +58,7 @@ module Moderator
       add_row(sums, PostAppeal.where(creator: users).where.not(creator_ip_addr: nil).group(:creator_ip_addr).count)
       add_row(sums, PostFlag.where(creator: users).group(:creator_ip_addr).count)
       add_row(sums, Upload.where(uploader: users).group(:uploader_ip_addr).count)
-      add_row(sums, User.where(id: users).group(:last_ip_addr).count)
+      add_row(sums, User.where(id: users).where.not(last_ip_addr: nil).group(:last_ip_addr).count)
 
       sums
     end
