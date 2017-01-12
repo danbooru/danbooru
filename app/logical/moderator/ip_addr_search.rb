e8f71739becaea61ee6a2ea8ae12bbc5c32bd3db
evazion <noizave@gmail.com>
2017-01-12 05:34:48 +0000

    IpAddrSearch: search flags/appeals/artcomms/artists for IPs.

--- app/logical/moderator/ip_addr_search.rb
+++ app/logical/moderator/ip_addr_search.rb
@@ -22,12 +22,17 @@ module Moderator
     def search_by_ip_addr(ip_addrs)
       sums = Hash.new {|h, k| h[k] = 0}
 
+      add_row(sums, ArtistCommentaryVersion.where(updater_ip_addr: ip_addrs).group(:updater).count)
+      add_row(sums, ArtistVersion.where(updater_ip_addr: ip_addrs).group(:updater).count)
       add_row(sums, NoteVersion.where(updater_ip_addr: ip_addrs).group(:updater).count)
       add_row(sums, PoolArchive.where(updater_ip_addr: ip_addrs).group(:updater).count) if PoolArchive.enabled?
       add_row(sums, PostVersion.where(updater_ip_addr: ip_addrs).group(:updater).count)
       add_row(sums, WikiPageVersion.where(updater_ip_addr: ip_addrs).group(:updater).count)
       add_row(sums, Comment.where(ip_addr: ip_addrs).group(:creator).count)
       add_row(sums, Dmail.where(creator_ip_addr: ip_addrs).group(:from).count)
+      add_row(sums, PostAppeal.where(creator_ip_addr: ip_addrs).group(:creator).count)
+      add_row(sums, PostFlag.where(creator_ip_addr: ip_addrs).group(:creator).count)
+      add_row(sums, Upload.where(uploader_ip_addr: ip_addrs).group(:uploader).count)
       add_row(sums, Hash[User.where(last_ip_addr: ip_addrs).collect { |user| [user, 1] }])
 
       sums
@@ -42,12 +47,18 @@ module Moderator
       sums = Hash.new {|h, k| h[k] = 0}
       users = User.find(user_ids)
 
+      add_row(sums, ArtistCommentaryVersion.where(updater: users).group(:updater_ip_addr).count)
+      add_row(sums, ArtistVersion.where(updater: users).group(:updater_ip_addr).count)
       add_row(sums, NoteVersion.where(updater: users).group(:updater_ip_addr).count)
       add_row(sums, PoolArchive.where(updater: users).group(:updater_ip_addr).count) if PoolArchive.enabled?
       add_row(sums, PostVersion.where(updater: users).group(:updater_ip_addr).count)
       add_row(sums, WikiPageVersion.where(updater: users).group(:updater_ip_addr).count)
       add_row(sums, Comment.where(creator: users).group(:ip_addr).count)
       add_row(sums, Dmail.where(from: users).group(:creator_ip_addr).count)
+      add_row(sums, PostAppeal.where(creator: users).group(:creator_ip_addr).count)
+      add_row(sums, PostFlag.where(creator: users).group(:creator_ip_addr).count)
+      add_row(sums, Upload.where(uploader: users).group(:uploader_ip_addr).count)
+      add_row(sums, User.where(id: users).group(:last_ip_addr).count)
 
       sums
     end
