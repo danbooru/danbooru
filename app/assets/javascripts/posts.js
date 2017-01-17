a53341d112d407128797e8d07c1f8708631e1430
r888888888 <r888888888@gmail.com>
2017-01-17 14:55:53 -0800

    fixes #2846: revert postvote js change

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -480,6 +480,14 @@
     Danbooru.Post.initialize_title_for($post);
   }
 
+  Danbooru.Post.vote = function(score, id) {
+    Danbooru.notice("Voting...");
+
+    $.post("/posts/" + id + "/votes.js", {
+       score: score
+    });
+  }
+
   Danbooru.Post.update = function(post_id, params) {
     Danbooru.Post.notice_update("inc");
 
