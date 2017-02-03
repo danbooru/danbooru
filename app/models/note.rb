e98e7f1ea7bf3600175834ca0a8f8bb01a9e38a6
evazion <noizave@gmail.com>
2017-02-03 01:49:30 -0600

    Fix note_update_count increment test.
      5) Failure:
    NoteTest#test_: In all cases updating a note should increment the updater's note_update_count. [/home/danbooru/src/danbooru/test/unit/note_test.rb:113]:
    "CurrentUser.note_update_count" didn't change by 1.
    Expected: 1
      Actual: 0

--- app/models/note.rb
+++ app/models/note.rb
@@ -148,6 +148,7 @@ class Note < ActiveRecord::Base
 
   def create_version
     User.where(id: CurrentUser.id).update_all("note_update_count = note_update_count + 1")
+    CurrentUser.reload
 
     if merge_version?
       merge_version
