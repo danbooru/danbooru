2d62fff89bf6b62f07014e14220bdae8d9a2b574
evazion <noizave@gmail.com>
2017-02-06 02:17:37 -0600

    tests: add more wiki page versions controller tests.

--- test/functional/wiki_page_versions_controller_test.rb
+++ test/functional/wiki_page_versions_controller_test.rb
@@ -6,6 +6,10 @@ class WikiPageVersionsControllerTest < ActionController::TestCase
       @user = FactoryGirl.create(:user)
       CurrentUser.user = @user
       CurrentUser.ip_addr = "127.0.0.1"
+
+      @wiki_page = FactoryGirl.create(:wiki_page)
+      @wiki_page.update_attributes(:body => "1 2")
+      @wiki_page.update_attributes(:body => "2 3")
     end
 
     teardown do
@@ -14,12 +18,6 @@ class WikiPageVersionsControllerTest < ActionController::TestCase
     end
 
     context "index action" do
-      setup do
-        @wiki_page = FactoryGirl.create(:wiki_page)
-        @wiki_page.update_attributes(:body => "1 2")
-        @wiki_page.update_attributes(:body => "2 3")
-      end
-
       should "list all versions" do
         get :index
         assert_response :success
@@ -30,6 +28,20 @@ class WikiPageVersionsControllerTest < ActionController::TestCase
         get :index, {:search => {:wiki_page_id => @wiki_page.id}}
         assert_response :success
         assert_not_nil(assigns(:wiki_page_versions))
+      end
+    end
+
+    context "show action" do
+      should "render" do
+        get :show, { id: @wiki_page.versions.first.id }
+        assert_response :success
+      end
+    end
+
+    context "diff action" do
+      should "render" do
+        get :diff, { thispage: @wiki_page.versions.first.id, otherpage: @wiki_page.versions.last.id }
+        assert_response :success
       end
     end
   end
