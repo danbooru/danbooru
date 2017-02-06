6bf050c607d26bc29f7b397ada7184419d81ae30
r888888888 <r888888888@gmail.com>
2017-02-06 11:59:27 -0800

    add download strategy for artstation

---
+++ app/logical/downloads/rewrite_strategies/art_station.rb
@@ -0,0 +1,21 @@
+module Downloads
+  module RewriteStrategies
+    class ArtStation < Base
+      def rewrite(url, headers, data = {})
+        # example: https://cdnb3.artstation.com/p/assets/images/images/003/716/071/large/aoi-ogata-hate-city.jpg?1476754974
+        if url =~ %r!^https?://cdn\w*\.artstation\.com/p/assets/images/images/\d+/\d+/\d+/large/!
+          url, headers = rewrite_large_url(url, headers)
+        end
+
+        return [url, headers, data]
+      end
+
+    protected
+      def rewrite_large_url(url, headers)
+        # example: https://cdnb3.artstation.com/p/assets/images/images/003/716/071/original/aoi-ogata-hate-city.jpg?1476754974
+        url = url.sub(%r!/large/!, "/original/")
+        return [url, headers]
+      end
+    end
+  end
+end
