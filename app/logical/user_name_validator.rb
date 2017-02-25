6966b74e18cb2f620636b5a39d4ff89a5902b7af
evazion <noizave@gmail.com>
2017-02-25 01:47:57 -0600

    Disallow unicode whitespace in usernames (#2894).

---
+++ app/logical/user_name_validator.rb
@@ -0,0 +1,10 @@
+class UserNameValidator < ActiveModel::EachValidator
+  def validate_each(rec, attr, value)
+    name = User.normalize_name(value)
+
+    rec.errors[attr] << "already exists" if User.find_by_name(name).present?
+    rec.errors[attr] << "must be 2 to 100 characters long" if !name.length.between?(2, 100)
+    rec.errors[attr] << "cannot have whitespace or colons" if name =~ /[[:space:]]|:/
+    rec.errors[attr] << "cannot begin or end with an underscore" if name =~ /\A_|_\z/
+  end
+end
