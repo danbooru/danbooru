79c0dc36477c43405adabe3d921fc8c6de8ca0a9
r888888888 <r888888888@gmail.com>
2017-03-13 17:22:21 -0700

    fix double escape in gbq

--- app/logical/google_big_query/post_version.rb
+++ app/logical/google_big_query/post_version.rb
@@ -1,13 +1,11 @@
 module GoogleBigQuery
   class PostVersion < Base
     def find_removed(tag, limit = 1_000)
-      tag = escape(tag)
       limit = limit.to_i
       query("select id, post_id, updated_at, updater_id, updater_ip_addr, tags, added_tags, removed_tags, parent_id, rating, source from [#{data_set}.post_versions] where #{remove_tag_condition(tag)} order by updated_at desc limit #{limit}")
     end
 
     def find_added(tag, limit = 1_000)
-      tag = escape(tag)
       limit = limit.to_i
       query("select id, post_id, updated_at, updater_id, updater_ip_addr, tags, added_tags, removed_tags, parent_id, rating, source from [#{data_set}.post_versions] where #{add_tag_condition(tag)} order by updated_at desc limit #{limit}")
     end
