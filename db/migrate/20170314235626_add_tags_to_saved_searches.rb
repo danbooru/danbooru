78fdc79db9a939fa1d61c403aa3283912f824627
r888888888 <r888888888@gmail.com>
2017-03-16 15:32:25 -0700

    add statement timeout to migration

--- db/migrate/20170314235626_add_tags_to_saved_searches.rb
+++ db/migrate/20170314235626_add_tags_to_saved_searches.rb
@@ -1,5 +1,6 @@
 class AddTagsToSavedSearches < ActiveRecord::Migration
   def change
+  	execute "set statement_timeout = 0"
   	add_column :saved_searches, :labels, "text", array: true, null: false, default: []
   	add_index :saved_searches, :labels, using: :gin
   	rename_column :saved_searches, :tag_query, :query
