396d466a9b4fcdad2194052e6771e74c43cdd2c9
evazion <noizave@gmail.com>
2017-03-19 23:21:59 -0500

    /posts/:id - make flagger/appealer ip addresses visible to mods.

--- app/helpers/post_appeals_helper.rb
+++ app/helpers/post_appeals_helper.rb
@@ -4,7 +4,12 @@ module PostAppealsHelper
     html << '<ul>'
 
     post.appeals.each do |appeal|
-      html << '<li>' + DText.parse_inline(appeal.reason).html_safe + ' - ' + link_to_user(appeal.creator) + ' ' + time_ago_in_words_tagged(appeal.created_at) + '</li>'
+      reason = DText.parse_inline(appeal.reason).html_safe
+      user = link_to_user(appeal.creator)
+      ip = link_to_ip(appeal.creator_ip_addr)
+      time = time_ago_in_words_tagged(appeal.created_at)
+
+      html << "<li>#{reason} - #{user} (#{ip}) #{time}</li>"
     end
 
     html << '</ul>'
--- app/helpers/post_flags_helper.rb
+++ app/helpers/post_flags_helper.rb
@@ -8,7 +8,7 @@ module PostFlagsHelper
       html << DText.parse_inline(flag.reason).html_safe
 
       if CurrentUser.is_moderator?
-        html << ' - ' + link_to_user(flag.creator)
+        html << " - #{link_to_user(flag.creator)} (#{link_to_ip(flag.creator_ip_addr)})"
       end
 
       html << ' - ' + time_ago_in_words_tagged(flag.created_at)
