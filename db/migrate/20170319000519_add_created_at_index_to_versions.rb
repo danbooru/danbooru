6d2d381af697558f26bda4fe4ccdb2b7035662f4
r888888888 <r888888888@gmail.com>
2017-03-27 16:53:12 -0700

    revert migration change

--- db/migrate/20170319000519_add_created_at_index_to_versions.rb
+++ db/migrate/20170319000519_add_created_at_index_to_versions.rb
@@ -4,7 +4,7 @@ class AddCreatedAtIndexToVersions < ActiveRecord::Migration
       add_index :note_versions, :created_at
       add_index :artist_versions, :created_at
       add_index :wiki_page_versions, :created_at
-      # add_index :post_appeals, :created_at
+      add_index :post_appeals, :created_at
     end
   end
 end
