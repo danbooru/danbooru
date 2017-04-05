086b520dcc5b2ae4f021694781fd2623879aa12e
evazion <noizave@gmail.com>
2017-04-05 00:52:59 -0500

    archives: raise exception if not configured.

--- app/controllers/pool_versions_controller.rb
+++ app/controllers/pool_versions_controller.rb
@@ -29,17 +29,7 @@ private
 
   def check_availabililty
     if !PoolArchive.enabled?
-      respond_to do |format|
-        format.html do
-          flash[:notice] = "Archive service is not configured. Pool versions are not saved."
-          redirect_to :back
-        end
-        format.json do
-          render json: {success: false, reason: "Archive service is not configured"}.to_json, status: 501
-        end
-      end
-
-      return false
+      raise NotImplementedError.new("Archive service is not configured. Pool versions are not saved.")
     end
   end
 end
