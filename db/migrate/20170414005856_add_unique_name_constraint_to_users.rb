38a2298de92a0862d690bdeabb0c8688c6209a3a
r888888888 <r888888888@gmail.com>
2017-04-14 13:18:16 -0700

    disable statement timeout

--- db/migrate/20170414005856_add_unique_name_constraint_to_users.rb
+++ db/migrate/20170414005856_add_unique_name_constraint_to_users.rb
@@ -1,6 +1,8 @@
 class AddUniqueNameConstraintToUsers < ActiveRecord::Migration
   def up
-  	remove_index :users, :name
-  	execute "create unique index index_users_on_name on users(lower(name))"
+  	ActiveRecord::Base.without_timeout do
+	  	remove_index :users, :name
+  		execute "create unique index index_users_on_name on users(lower(name))"
+  	end
   end
 end
