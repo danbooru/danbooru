76eefd0ffee736a60db7a8b9d726e93645c2eac4
r888888888 <r888888888@gmail.com>
2017-04-22 01:16:47 -0700

    add support for deploying specific branch or commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,4 @@ set :scm, :git
 set :deploy_to, "/var/www/danbooru2"
 set :rbenv_ruby, "2.3.1"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
+set :branch, ENV.fetch("branch", "master")
