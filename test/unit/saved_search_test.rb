8e5491a5367efa7f380e5381e0aa9ac3616d6ffa
evazion <noizave@gmail.com>
2017-04-28 16:22:37 -0500

    saved searches: add query normalization tests.

--- test/unit/saved_search_test.rb
+++ test/unit/saved_search_test.rb
@@ -30,13 +30,18 @@ class SavedSearchTest < ActiveSupport::TestCase
   context ".queries_for" do
     setup do
       @user = FactoryGirl.create(:user)
+      FactoryGirl.create(:tag_alias, antecedent_name: "bbb", consequent_name: "ccc", creator: @user)
       FactoryGirl.create(:saved_search, user: @user, label_string: "blah", query: "aaa")
-      FactoryGirl.create(:saved_search, user: @user, label_string: "zah", query: "bbb")
+      FactoryGirl.create(:saved_search, user: @user, label_string: "zah", query: "CCC BBB AAA")
+      FactoryGirl.create(:saved_search, user: @user, label_string: "qux", query: " aaa  bbb  ccc ")
     end
 
     should "fetch the queries used by a user for a label" do
       assert_equal(%w(aaa), SavedSearch.queries_for(@user.id, "blah"))
-      assert_equal(%w(aaa bbb), SavedSearch.queries_for(@user.id))
+    end
+
+    should "return fully normalized queries" do
+      assert_equal(["aaa", "aaa ccc"], SavedSearch.queries_for(@user.id))
     end
   end
 
@@ -84,7 +89,8 @@ class SavedSearchTest < ActiveSupport::TestCase
   context "Creating a saved search" do
     setup do
       @user = FactoryGirl.create(:gold_user)
-      @saved_search = @user.saved_searches.create(:query => " xxx ")
+      FactoryGirl.create(:tag_alias, antecedent_name: "zzz", consequent_name: "yyy", creator: @user)
+      @saved_search = @user.saved_searches.create(:query => " ZZZ xxx ")
     end
 
     should "update the bitpref on the user" do
@@ -92,8 +98,8 @@ class SavedSearchTest < ActiveSupport::TestCase
       assert(@user.has_saved_searches?, "should have saved_searches bitpref set")
     end
 
-    should "normalize whitespace" do
-      assert_equal("xxx", @saved_search.query)
+    should "normalize the query aside from the order" do
+      assert_equal("yyy xxx", @saved_search.query)
     end
 
     should "normalize the label string" do
