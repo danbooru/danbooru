ff5586cb01d0b79b3abba2f559e26b252ed699a4
r888888888 <r888888888@gmail.com>
2017-05-09 12:46:40 -0700

    refactor twitter service to handle cards (fixes #3031)

--- app/logical/twitter_service.rb
+++ app/logical/twitter_service.rb
@@ -17,25 +17,59 @@ class TwitterService
     end
   end
 
+  def extract_urls_for_status(tweet)
+    tweet.media.map do |obj|
+      if obj.is_a?(Twitter::Media::Photo)
+        obj.media_url.to_s + ":orig"
+      elsif obj.is_a?(Twitter::Media::Video)
+        video = obj.video_info.variants.select do |x|
+          x.content_type == "video/mp4"
+        end.max_by {|y| y.bitrate}
+        if video
+          video.url.to_s
+        end
+      end
+    end.compact
+  end
+
+  def extract_og_image_from_page(url, n = 5)
+    raise "too many redirects" if n == 0
+
+    Net::HTTP.start(url.host, url.port, :use_ssl => (url.normalized_scheme == "https")) do |http|
+      resp = http.request_get(url.request_uri)
+      if resp.is_a?(Net::HTTPMovedPermanently) && resp["Location"]
+        redirect_url = Addressable::URI.parse(resp["Location"])
+        redirect_url.host = url.host if redirect_url.host.nil?
+        redirect_url.scheme = url.scheme if redirect_url.scheme.nil?
+        redirect_url.port = url.port if redirect_url.port.nil?
+        return extract_og_image_from_page(redirect_url, n - 1)
+      elsif resp.is_a?(Net::HTTPSuccess)
+        doc = Nokogiri::HTML(resp.body)
+        images = doc.css("meta[property='og:image']")
+        return images.first.attr("content")
+      end
+    end
+  end
+
+  def extract_urls_for_card(attrs)
+    url = attrs.urls.map {|x| x.expanded_url}.reject {|x| x.host == "twitter.com"}.first
+    [extract_og_image_from_page(url)].compact
+  end
+
   def image_urls(tweet_url)
     tweet_url =~ %r{/status/(\d+)}
     twitter_id = $1
-    attrs = client.status(twitter_id).attrs
+    tweet = client.status(twitter_id)
     urls = []
-    attrs[:entities][:media].each do |obj|
-      urls << obj[:media_url] + ":orig"
+
+    if tweet.media.any?
+      urls = extract_urls_for_status(tweet)
+    elsif tweet.urls.any?
+      urls = extract_urls_for_card(tweet)
     end
-    attrs[:extended_entities][:media].each do |obj|
-      if obj[:video_info]
-        largest = obj[:video_info][:variants].select {|x| x[:url] =~ /\.mp4$/}.max_by {|x| x[:bitrate]}
-        urls.clear
-        urls << largest[:url] if largest
-      else
-        urls << obj[:media_url] + ":orig"
-      end
-    end
+
     urls.uniq
-  rescue => e
-    []
+  # rescue => e
+  #   []
   end
 end
--- test/unit/sources/twitter_test.rb
+++ test/unit/sources/twitter_test.rb
@@ -2,6 +2,39 @@ require 'test_helper'
 
 module Sources
   class TwitterTest < ActiveSupport::TestCase
+    context "A video" do
+      setup do
+        @site = Sources::Site.new("https://twitter.com/CincinnatiZoo/status/859073537713328129")
+        @site.get
+      end
+
+      should "get the image url" do
+        assert_equal("https://video.twimg.com/ext_tw_video/859073467769126913/pu/vid/1280x720/cPGgVROXHy3yrK6u.mp4", @site.image_url)
+      end
+    end
+
+    context "An animated gif" do
+      setup do
+        @site = Sources::Site.new("https://twitter.com/DaniStrawberry1/status/859435334765088769")
+        @site.get
+      end
+
+      should "get the image url" do
+        assert_equal("https://video.twimg.com/tweet_video/C-1Tns7WsAAqvqn.mp4", @site.image_url)
+      end
+    end
+
+    context "A twitter summary card" do
+      setup do
+        @site = Sources::Site.new("https://twitter.com/NatGeo/status/787654447937847296")
+        @site.get
+      end
+
+      should "get the image url" do
+        assert_equal("http://yourshot.nationalgeographic.com/u/fQYSUbVfts-T7odkrFJckdiFeHvab0GWOfzhj7tYdC0uglagsDcUxj3Tf7HBF3kZEj7S5m-zeDmZP6DBxBJlyJX_1mFp-hGf4JPt97xp0QJkwf4po1MmnZH73WC3a2Pa1Ky62C-v0cYXTur3-QwD3Pz5UI_cKIi81GABTXII8VwKUopxlNW2MYAR8kPYU2IoUhOjlvVefNcLYI74J-0IpI4tHDXE/", @site.image_url)
+      end
+    end
+
     context "The source site for a restricted twitter" do
       setup do
         @site = Sources::Site.new("https://mobile.twitter.com/Strangestone/status/556440271961858051")
