71950597e8e3a1575432ff2c5211c850422cdc2a
evazion <noizave@gmail.com>
2017-06-05 13:43:57 -0500

    Add source tests for `https://$artist.artstation.com/projects/$id`.

--- test/unit/sources/art_station_test.rb
+++ test/unit/sources/art_station_test.rb
@@ -4,7 +4,7 @@ module Sources
   class ArtStationTest < ActiveSupport::TestCase
     context "The source site for an art station artwork page" do
       setup do
-        @site = Sources::Site.new("https://jeyrain.artstation.com/artwork/04XA4")
+        @site = Sources::Site.new("https://www.artstation.com/artwork/04XA4")
         @site.get
       end
 
@@ -27,6 +27,35 @@ module Sources
       should "get the artist commentary" do
         assert_equal("pink", @site.artist_commentary_title)
         assert_equal("", @site.artist_commentary_desc)
+      end
+    end
+
+    context "The source site for an art station projects page" do
+      setup do
+        @site = Sources::Site.new("https://dantewontdie.artstation.com/projects/YZK5q")
+        @site.get
+      end
+
+      should "get the image url" do
+        url = "https://cdna.artstation.com/p/assets/images/images/006/066/534/original/yinan-cui-reika.jpg?1495781565"
+        assert_equal(url, @site.image_url)
+      end
+
+      should "get the profile" do
+        assert_equal("https://www.artstation.com/artist/dantewontdie", @site.profile_url)
+      end
+
+      should "get the artist name" do
+        assert_equal("dantewontdie", @site.artist_name)
+      end
+
+      should "get the tags" do
+        assert_equal(%w[gantz reika], @site.tags.map(&:first))
+      end
+
+      should "get the artist commentary" do
+        assert_equal("Reika ", @site.artist_commentary_title)
+        assert_equal("From Gantz.", @site.artist_commentary_desc)
       end
     end
   end
