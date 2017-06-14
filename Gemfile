d691c1de36e4b41f703fbdcda97d249687331b78
r888888888 <r888888888@gmail.com>
2017-06-14 16:35:16 -0700

    fixes #3066: add bootsnap support

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem 'memcache_mock'
 gem 'memoist'
 gem 'daemons'
 gem 'oauth2'
+gem 'bootsnap'
 
 # needed for looser jpeg header compat
 gem 'ruby-imagespec', :require => "image_spec", :git => "https://github.com/r888888888/ruby-imagespec.git", :branch => "exif-fixes"
--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,14 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bootsnap'
+require 'tmpdir'
+Bootsnap.setup(
+  cache_dir: File.join(Dir.tmpdir, 'bootsnap-cache'), # Path to your cache
+  development_mode: ENV['MY_ENV'] == 'development',
+  load_path_cache: true, # Should we optimize the LOAD_PATH with a cache?
+  autoload_paths_cache: true, # Should we optimize ActiveSupport autoloads with cache?
+  disable_trace: false, # Sets `RubyVM::InstructionSequence.compile_option = { trace_instruction: false }`
+  compile_cache_iseq: true, # Should compile Ruby code into ISeq cache?
+  compile_cache_yaml: true # Should compile YAML into a cache?
+)
