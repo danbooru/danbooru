1a998cb3fbc593fd39a0ddc582cbbb0532dea684
evazion <noizave@gmail.com>
2017-06-16 13:23:04 -0500

    Move API methods from initializer to ApplicationRecord.

--- config/initializers/active_record_api_extensions.rb
+++ config/initializers/active_record_api_extensions.rb
@@ -1,54 +1,5 @@
-module Danbooru
-  module Extensions
-    module ActiveRecordApi
-      extend ActiveSupport::Concern
-
-      def as_json(options = {})
-        options ||= {}
-        options[:except] ||= []
-        options[:except] += hidden_attributes
-
-        options[:methods] ||= []
-        options[:methods] += method_attributes
-
-        super(options)
-      end
-
-      def to_xml(options = {}, &block)
-        options ||= {}
-
-        options[:except] ||= []
-        options[:except] += hidden_attributes
-
-        options[:methods] ||= []
-        options[:methods] += method_attributes
-
-        super(options, &block)
-      end
-
-      def serializable_hash(*args)
-        hash = super(*args)
-        hash.transform_keys { |key| key.delete("?") }
-      end
-
-    protected
-      def hidden_attributes
-        [:uploader_ip_addr, :updater_ip_addr, :creator_ip_addr, :ip_addr]
-      end
-
-      def method_attributes
-        []
-      end
-    end
-  end
-end
-
 class Delayed::Job
   def hidden_attributes
     [:handler]
   end
-end
-
-class ActiveRecord::Base
-  include Danbooru::Extensions::ActiveRecordApi
 end
