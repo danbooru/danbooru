d5ce6c98ef95a40b46c67ea4c802a05ab7f249d9
evazion <noizave@gmail.com>
2017-06-22 23:20:04 -0500

    tumblr: add tumblr api client.

---
+++ app/logical/tumblr_api_client.rb
@@ -0,0 +1,9 @@
+class TumblrApiClient < Struct.new(:api_key)
+  include HTTParty
+  base_uri "https://api.tumblr.com/v2/blog/"
+
+  def posts(blog_name, post_id)
+    response = self.class.get("/#{blog_name}/posts", query: { id: post_id, api_key: api_key })
+    response.parsed_response.with_indifferent_access[:response]
+  end
+end
