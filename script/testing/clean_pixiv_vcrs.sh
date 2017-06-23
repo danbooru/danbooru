a9f15cd8620c252c9c428fc04cf915dd86dadad7
r888888888 <r888888888@gmail.com>
2017-06-23 14:10:19 -0700

    remove unused pixiv test script

--- script/testing/clean_pixiv_vcrs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find test/fixtures/vcr_cassettes -name '*pixiv*' -delete
-find test/fixtures/vcr_cassettes -name '*ugoira*' -delete
-find test/fixtures/vcr_cassettes -name '*seiga*' -delete
-find test/fixtures/vcr_cassettes -name '*twitter*' -delete
-find test/fixtures/vcr_cassettes -name '*artist*' -delete
