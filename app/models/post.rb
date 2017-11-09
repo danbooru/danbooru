d571efd7030b2fdd638da871c96e4ff2e938d6dc
evazion <noizave@gmail.com>
2017-11-08 19:37:09 -0600

    posts: warn when adding artist tag with no artist entry.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1724,11 +1724,18 @@ class Post < ApplicationRecord
     def added_tags_are_valid
       new_tags = added_tags.select { |t| t.post_count <= 1 }
       new_general_tags = new_tags.select { |t| t.category == Tag.categories.general }
+      new_artist_tags = new_tags.select { |t| t.category == Tag.categories.artist }
 
       if new_general_tags.present?
         n = new_general_tags.size
         tag_wiki_links = new_general_tags.map { |tag| "[[#{tag.name}]]" }
         self.warnings[:base] << "Created #{n} new #{n == 1 ? "tag" : "tags"}: #{tag_wiki_links.join(", ")}"
+      end
+
+      new_artist_tags.each do |tag|
+        if tag.artist.blank?
+          self.warnings[:base] << "Artist [[#{tag.name}]] requires an artist entry. \"Create new artist entry\":[/artists/new?name=#{CGI::escape(tag.name)}]"
+        end
       end
     end
   end
