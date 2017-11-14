dc5fdf6893111d5b0865bc7178e4bb9bb75445be
BrokenEagle <BrokenEagle98@gmail.com>
2017-11-14 14:22:00 -0600

    TagCategory.header_mapping: fix incorrect comment.

--- app/logical/tag_category.rb
+++ app/logical/tag_category.rb
@@ -29,7 +29,7 @@ class TagCategory
       @@humanized_mapping ||= Hash[Danbooru.config.full_tag_config_info.map {|k,v| [k,v["humanized"]]}]
     end
 
-    # Returns a hash mapping for humanized_essential_tag_string (models/post.rb)
+    # Returns a hash mapping for split_tag_list_html (presenters/tag_set_presenter.rb)
     def header_mapping
       @@header_mapping ||= Hash[Danbooru.config.full_tag_config_info.map {|k,v| [k,v["header"]]}]
     end
