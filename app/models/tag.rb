e5cc6a794955412df7fb8b53474369c321fb8b6d
r888888888 <r888888888@gmail.com>
2017-11-15 14:44:36 -0800

    use cache for fetching categories

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -141,7 +141,7 @@ class Tag < ApplicationRecord
       Post.with_timeout(30_000, nil, {:tags => name}) do
         Post.raw_tag_match(name).where("true /* Tag#update_category_post_counts */").find_each do |post|
           post.reload
-          post.set_tag_counts
+          post.set_tag_counts(false)
           args = TagCategory.categories.map {|x| ["tag_count_#{x}",post.send("tag_count_#{x}")]}.to_h.update(:tag_count => post.tag_count)
           Post.where(:id => post.id).update_all(args)
         end
