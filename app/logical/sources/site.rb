danbooru/danbooru

commit 3c524aa5e630a1e976b3ddccb73a42aab8a6d595

fix bug with source::site

app/logical/sources/site.rb
@@ -17,16 +17,14 @@ module Sources
     end
 
     def initialize(url, referer_url: nil)
-      @url = url
-
       Site.strategies.each do |strategy|
         if strategy.url_match?(url) || strategy.url_match?(referer_url)
           @strategy = strategy.new(url, referer_url)
-          break
+          return
         end
-
-        raise NoStrategyError.new
       end
+
+      raise NoStrategyError.new
     end
 
     def referer_url

app/assets/stylesheets/specific/forum.scss
@@ -6,7 +6,7 @@ div.list-of-forum-posts {
   }
 
   article {
-    border: 1px solid #999;
+    border: 1px solid #AAA;
     margin: 1em 0em;
     word-wrap: break-word;
     box-shadow: 1px 1px 2px #AAA;

app/logical/downloads/rewrite_strategies/art_station.rb
@@ -18,7 +18,6 @@ module Downloads
     protected
       def rewrite_html_url(url, headers)
         return [url, headers] unless Sources::Strategies::ArtStation.url_match?(url)
-
         source = Sources::Site.new(url)
         source.get
         [source.image_url, headers]

app/logical/sources/strategies/art_station.rb
@@ -6,9 +6,9 @@ module Sources::Strategies
       self.project_id(url).present?
     end
 
-    # https://www.artstation.com/artwork/04XA4"
-    # https://dantewontdie.artstation.com/projects/YZK5q"
-    # https://www.artstation.com/artwork/cody-from-sf"
+    # https://www.artstation.com/artwork/04XA4
+    # https://dantewontdie.artstation.com/projects/YZK5q
+    # https://www.artstation.com/artwork/cody-from-sf
     def self.project_id(url)
       if url =~ %r!\Ahttps?://\w+\.artstation\.com/(?:artwork|projects)/(?<project_id>[a-z0-9-]+)\z!i
         $~[:project_id]

test/unit/downloads/art_station_test.rb
@@ -11,7 +11,7 @@ module Downloads
       end
 
       should "download the large image instead" do
-        assert_equal("https://cdnb3.artstation.com/p/assets/images/images/003/716/071/original/aoi-ogata-hate-city.jpg?1476754974", @download.source)
+        assert_equal("https://cdnb3.artstation.com/p/assets/images/images/003/716/071/large/aoi-ogata-hate-city.jpg?1476754974", @download.source)
       end
     end
 
@@ -37,7 +37,7 @@ module Downloads
       end
 
       should "download the original image instead" do
-        assert_equal("https://cdna.artstation.com/p/assets/images/images/006/066/534/original/yinan-cui-reika.jpg?1495781565", @download.source)
+        assert_equal("https://cdna.artstation.com/p/assets/images/images/006/066/534/large/yinan-cui-reika.jpg?1495781565", @download.source)
       end
     end
   end

test/unit/downloads/deviant_art_test.rb
@@ -11,7 +11,7 @@ module Downloads
       end
 
       should "set the html page as the source" do
-        assert_equal("http://orig00.deviantart.net/82ef/f/2016/271/7/1/aaaaaa_by_starbitt-daj8b46.gif", @download.source)
+        assert_equal("https://orig00.deviantart.net/82ef/f/2016/271/7/1/aaaaaa_by_starbitt-daj8b46.gif", @download.source)
       end
 
       should "work" do

commit 5837b74de42253229772bc3f4339e0b155396f9c

tumblr: fix exception when fetching source data.
Fixes an `undefined method `gsub' for nil:NilClass` exception when fetching source
data for this url:

* /uploads/new?url=https%3A%2F%2Fuiti-ukumo.tumblr.com%2Fpost%2F156157443158%2F2016-12

Bug introduced in 3c524aa5: `url` wasn't initialized due to `@url = url`
being removed from Sources::Site#initialize.

app/logical/sources/site.rb
@@ -4,8 +4,8 @@ module Sources
   class Site
     class NoStrategyError < RuntimeError ; end
 
-    attr_reader :url, :strategy
-    delegate :get, :get_size, :site_name, :artist_name,
+    attr_reader :strategy
+    delegate :url, :get, :get_size, :site_name, :artist_name,
       :profile_url, :image_url, :tags, :artists, :unique_id,
       :file_url, :ugoira_frame_data, :ugoira_content_type, :image_urls,
       :artist_commentary_title, :artist_commentary_desc,