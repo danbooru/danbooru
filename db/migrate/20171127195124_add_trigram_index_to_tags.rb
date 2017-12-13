41d4c33093276d46cf13b8efc0d52b0ac21dc7ff
r888888888 <r888888888@gmail.com>
2017-12-13 15:06:25 -0800

    add no statement timeout block to migration

--- db/migrate/20171127195124_add_trigram_index_to_tags.rb
+++ db/migrate/20171127195124_add_trigram_index_to_tags.rb
@@ -1,9 +1,13 @@
 class AddTrigramIndexToTags < ActiveRecord::Migration
   def up
-    execute "create index index_tags_on_name_trgm on tags using gin (name gin_trgm_ops)"
+  	Tag.without_timeout do
+	    execute "create index index_tags_on_name_trgm on tags using gin (name gin_trgm_ops)"
+	  end
   end
 
   def down
-    execute "drop index index_tags_on_name_trgm"
+  	Tag.without_timeout do
+	    execute "drop index index_tags_on_name_trgm"
+	  end
   end
 end
