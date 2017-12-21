danbooru/danbooru

commit e2eb45a5a3bb7bc5ac46846e8352b9af2cda7e7c

Auto-promote DanbooruBot to Mod. Auto-create DanbooruBot if it doesn't exist.

config/danbooru_default_config.rb
@@ -37,8 +37,10 @@ module Danbooru
     end
 
     # System actions, such as sending automated dmails, will be performed with this account.
+    # This account will be created automatically if it doesn't exist. It will
+    # be promoted to Moderator if it isn't already a Moderator.
     def system_user
-      User.find_by_name("DanbooruBot") || User.admins.first
+      "DanbooruBot"
     end
 
     def upload_feedback_topic

config/initializers/system_user.rb
@@ -1,0 +1,10 @@
+require "securerandom"
+
+system = User.find_or_create_by!(name: Danbooru.config.system_user) do |user|
+  user.password = SecureRandom.base64(32)
+end
+
+unless system.is_moderator?
+  system.level = User::Levels::MODERATOR
+  system.save
+end

app/models/dmail.rb
@@ -86,7 +86,7 @@ class Dmail < ApplicationRecord
       end
 
       def create_automated(params)
-        dmail = Dmail.new(from: Danbooru.config.system_user, **params)
+        dmail = Dmail.new(from: User.system, **params)
         dmail.owner = dmail.to
         dmail.save
         dmail
@@ -234,7 +234,7 @@ class Dmail < ApplicationRecord
   end
 
   def is_automated?
-    from == Danbooru.config.system_user
+    from == User.system
   end
 
   def filtered?

app/models/user.rb
@@ -316,7 +316,7 @@ class User < ApplicationRecord
 
     module ClassMethods
       def system
-        Danbooru.config.system_user
+        User.find_by!(name: Danbooru.config.system_user)
       end
 
       def level_hash
@@ -366,7 +366,7 @@ class User < ApplicationRecord
     def promote_to_admin_if_first_user
       return if Rails.env.test?
 
-      if User.count == 0
+      if User.admins.count == 0
         self.level = Levels::ADMIN
         self.can_approve_posts = true
         self.can_upload_free = true

test/unit/dmail_test.rb
@@ -177,7 +177,7 @@ class DmailTest < ActiveSupport::TestCase
     context "that is automated" do
       setup do
         @bot = FactoryGirl.create(:user)
-        Danbooru.config.stubs(:system_user).returns(@bot)
+        User.stubs(:system).returns(@bot)
       end
 
       should "only create a copy for the recipient" do

test/unit/post_disapproval_test.rb
@@ -85,7 +85,7 @@ class PostDisapprovalTest < ActiveSupport::TestCase
 
         should "dmail the uploaders" do
           bot = FactoryGirl.create(:user)
-          Danbooru.config.stubs(:system_user).returns(bot)
+          User.stubs(:system).returns(bot)
 
           assert_difference(["@uploaders[0].dmails.count", "@uploaders[1].dmails.count"], 1) do
             PostDisapproval.dmail_messages!

test/unit/post_replacement_test.rb
@@ -20,7 +20,7 @@ class PostReplacementTest < ActiveSupport::TestCase
     Delayed::Worker.delay_jobs = true # don't delete the old images right away
 
     @system = FactoryGirl.create(:user, created_at: 2.weeks.ago)
-    Danbooru.config.stubs(:system_user).returns(@system)
+    User.stubs(:system).returns(@system)
 
     @uploader = FactoryGirl.create(:user, created_at: 2.weeks.ago, can_upload_free: true)
     @replacer = FactoryGirl.create(:user, created_at: 2.weeks.ago, can_approve_posts: true)

test/unit/user_test.rb
@@ -28,7 +28,7 @@ class UserTest < ActiveSupport::TestCase
 
       should "send an automated dmail to the user" do
         bot = FactoryGirl.create(:user)
-        Danbooru.config.stubs(:system_user).returns(bot)
+        User.stubs(:system).returns(bot)
 
         assert_difference("Dmail.count", 1) do
           @user.promote_to!(User::Levels::GOLD)

commit fcec99405caf4162464254cd0e7a12f111e4f623

Fix failure in `rake db:migrate` on fresh install (#3443).
Create the DanbooruBot account in db/seeds.rb instead of in an
initializer. Move the old db/seeds.rb to db/populate.rb.

Fixes a bug introduced in e2eb45a. Creating DanbooruBot in an
initializer was wrong because in a fresh install the users table doesn't
exist yet. This caused `rake db:migrate` to fail.

config/danbooru_default_config.rb
@@ -36,9 +36,10 @@ module Danbooru
       "webmaster@#{server_host}"
     end
 
-    # System actions, such as sending automated dmails, will be performed with this account.
-    # This account will be created automatically if it doesn't exist. It will
-    # be promoted to Moderator if it isn't already a Moderator.
+    # System actions, such as sending automated dmails, will be performed with
+    # this account. This account must have Moderator privileges.
+    #
+    # Run `rake db:seed` to create this account if it doesn't already exist in your install.
     def system_user
       "DanbooruBot"
     end

config/initializers/system_user.rb
@@ -1,10 +1,0 @@
-require "securerandom"
-
-system = User.find_or_create_by!(name: Danbooru.config.system_user) do |user|
-  user.password = SecureRandom.base64(32)
-end
-
-unless system.is_moderator?
-  system.level = User::Levels::MODERATOR
-  system.save
-end

db/populate.rb
@@ -1,0 +1,218 @@
+require 'set'
+require 'timecop'
+
+CurrentUser.ip_addr = "127.0.0.1"
+Delayed::Worker.delay_jobs = false
+$used_names = Set.new([""])
+
+def rand_string(n, unique = false)
+  string = ""
+
+  n = rand(n) + 1
+
+  while $used_names.include?(string)
+    consonants = "bcdfghjklmnpqrstvwxz".scan(/./)
+    vowels = "aeiouy".scan(/./)
+    string = ""
+    n.times do
+      string << consonants[rand(consonants.size)]
+      string << vowels[rand(vowels.size)]
+    end
+    return string unless unique
+  end
+
+  $used_names.add(string)
+  string
+end
+
+def rand_sentence(n, unique = false)
+  (0..n).map {rand_string(n, unique)}.join(" ") + "."
+end
+
+def rand_paragraph(n, unique = false)
+  (0..n).map {rand_sentence(n, unique)}.join(" ")
+end
+
+def rand_document(n, unique = false)
+  (0..n).map {rand_paragraph(n, unique)}.join("\n\n")
+end
+
+if User.count == 0
+  puts "Creating users"
+
+  Timecop.travel(1.month.ago) do
+    user = User.create(
+      :name => "admin",
+      :password => "password1",
+      :password_confirmation => "password1"
+    )
+
+    CurrentUser.user = user
+    User::Levels.constants.reject{|x| [:ADMIN, :BLOCKED].include?(x)}.each do |level|
+      newuser = User.create(
+      :name => level.to_s.downcase,
+      :password => "password1",
+      :password_confirmation => "password1"
+      )
+      newuser.promote_to!(User::Levels.const_get(level), {:skip_feedback => true, :skip_dmail => true})
+    end
+
+    newuser = User.create(
+      :name => "banned",
+      :password => "password1",
+      :password_confirmation => "password1"
+      )
+    Ban.create(:user_id => newuser.id, :reason => "from the start", :duration => 99999)
+
+    newuser = User.create(
+      :name => "uploader",
+      :password => "password1",
+      :password_confirmation => "password1"
+      )
+    newuser.promote_to!(User::Levels::BUILDER, {:can_upload_free => true, :skip_feedback => true, :skip_dmail => true})
+
+    newuser = User.create(
+      :name => "approver",
+      :password => "password1",
+      :password_confirmation => "password1"
+      )
+    newuser.promote_to!(User::Levels::BUILDER, {:can_approve_posts => true, :skip_feedback => true, :skip_dmail => true})
+
+  end
+
+  0.upto(10) do |i|
+    User.create(
+      :name => rand_string(8, true),
+      :password => "password1",
+      :password_confirmation => "password1"
+    )
+  end
+  $used_names = Set.new([""])
+else
+  puts "Skipping users"
+  user = User.find_by_name("albert")
+end
+
+CurrentUser.user = User.admins.first
+
+if Upload.count == 0
+  puts "Creating uploads"
+  1.upto(100) do |i|
+    color1 = rand(4096).to_s(16)
+    color2 = rand(4096).to_s(16)
+    width = rand(2000) + 100
+    height = rand(2000) + 100
+    url = "http://ipsumimage.appspot.com/#{width}x#{height}"
+    tags = rand_sentence(12).scan(/[a-z]+/).join(" ")
+
+    Upload.create!(:source => url, :content_type => "image/gif", :rating => "q", :tag_string => tags, :server => Socket.gethostname)
+  end
+else
+  puts "Skipping uploads"
+end
+
+if Post.count == 0
+  puts "Creating posts"
+  Upload.all.each do |upload|
+    upload.process!
+  end
+  if Post.count == 0
+    raise "Uploads failed conversion"
+  end
+else
+  puts "Skipping posts"
+end
+
+if Comment.count == 0
+  puts "Creating comments"
+  Post.all.each do |post|
+    rand(10).times do
+      Comment.create(:post_id => post.id, :body => rand_paragraph(6))
+    end
+  end
+else
+  puts "Skipping comments"
+end
+
+if Note.count == 0
+  puts "Creating notes"
+  Post.order("random()").limit(10).each do |post|
+    rand(5).times do
+      note = Note.create(:post_id => post.id, :x => rand(post.image_width), :y => rand(post.image_height), :width => 100, :height => 100, :body => Time.now.to_f.to_s)
+
+      rand(10).times do |i|
+        note.update_attributes(:body => rand_sentence(6))
+      end
+    end
+  end
+else
+  puts "Skipping notes"
+end
+
+if Artist.count == 0
+  puts "Creating artists"
+  20.times do |i|
+    Artist.create(:name => rand_string(9, true))
+  end
+  $used_names = Set.new([""])
+else
+  puts "Skipping artists"
+end
+
+if TagAlias.count == 0
+  puts "Creating tag aliases"
+
+  20.times do |i|
+    TagAlias.create(:antecedent_name => rand_string(9, true), :consequent_name => rand_string(9, true))
+  end
+  $used_names = Set.new([""])
+else
+  puts "Skipping tag aliases"
+end
+
+if TagImplication.count == 0
+  puts "Creating tag implictions"
+
+  20.times do |i|
+    TagImplication.create(:antecedent_name => rand_string(9, true), :consequent_name => rand_string(9, true))
+  end
+  $used_names = Set.new([""])
+else
+  puts "Skipping tag implications"
+end
+
+if Pool.count == 0
+  puts "Creating pools"
+
+  1.upto(20) do |i|
+    pool = Pool.create(:name => rand_string(9, true))
+    rand(33).times do |j|
+      pool.add!(Post.order("random()").first)
+    end
+  end
+  $used_names = Set.new([""])
+end
+
+if Favorite.count == 0
+  puts "Creating favorites"
+
+  Post.order("random()").limit(50).each do |post|
+    user = User.order("random()").first
+    post.add_favorite!(user)
+    post.add_favorite!(CurrentUser.user)
+  end
+else
+  puts "Skipping favorites"
+end
+
+if ForumTopic.count == 0
+  puts "Creating forum posts"
+
+  20.times do |i|
+    topic = ForumTopic.create(:title => rand_sentence(6))
+
+    rand(100).times do |j|
+      post = ForumPost.create(:topic_id => topic.id, :body => rand_document(6))
+    end
+  end
+end

db/seeds.rb
@@ -1,218 +1,10 @@
-require 'set'
-require 'timecop'
+require "securerandom"
 
-CurrentUser.ip_addr = "127.0.0.1"
-Delayed::Worker.delay_jobs = false
-$used_names = Set.new([""])
-
-def rand_string(n, unique = false)
-  string = ""
-
-  n = rand(n) + 1
-
-  while $used_names.include?(string)
-    consonants = "bcdfghjklmnpqrstvwxz".scan(/./)
-    vowels = "aeiouy".scan(/./)
-    string = ""
-    n.times do
-      string << consonants[rand(consonants.size)]
-      string << vowels[rand(vowels.size)]
-    end
-    return string unless unique
-  end
-
-  $used_names.add(string)
-  string
+system = User.find_or_create_by!(name: Danbooru.config.system_user) do |user|
+  user.password = SecureRandom.base64(32)
 end
 
-def rand_sentence(n, unique = false)
-  (0..n).map {rand_string(n, unique)}.join(" ") + "."
-end
-
-def rand_paragraph(n, unique = false)
-  (0..n).map {rand_sentence(n, unique)}.join(" ")
-end
-
-def rand_document(n, unique = false)
-  (0..n).map {rand_paragraph(n, unique)}.join("\n\n")
-end
-
-if User.count == 0
-  puts "Creating users"
-
-  Timecop.travel(1.month.ago) do
-    user = User.create(
-      :name => "admin",
-      :password => "password1",
-      :password_confirmation => "password1"
-    )
-
-    CurrentUser.user = user
-    User::Levels.constants.reject{|x| [:ADMIN, :BLOCKED].include?(x)}.each do |level|
-      newuser = User.create(
-      :name => level.to_s.downcase,
-      :password => "password1",
-      :password_confirmation => "password1"
-      )
-      newuser.promote_to!(User::Levels.const_get(level), {:skip_feedback => true, :skip_dmail => true})
-    end
-
-    newuser = User.create(
-      :name => "banned",
-      :password => "password1",
-      :password_confirmation => "password1"
-      )
-    Ban.create(:user_id => newuser.id, :reason => "from the start", :duration => 99999)
-
-    newuser = User.create(
-      :name => "uploader",
-      :password => "password1",
-      :password_confirmation => "password1"
-      )
-    newuser.promote_to!(User::Levels::BUILDER, {:can_upload_free => true, :skip_feedback => true, :skip_dmail => true})
-
-    newuser = User.create(
-      :name => "approver",
-      :password => "password1",
-      :password_confirmation => "password1"
-      )
-    newuser.promote_to!(User::Levels::BUILDER, {:can_approve_posts => true, :skip_feedback => true, :skip_dmail => true})
-
-  end
-
-  0.upto(10) do |i|
-    User.create(
-      :name => rand_string(8, true),
-      :password => "password1",
-      :password_confirmation => "password1"
-    )
-  end
-  $used_names = Set.new([""])
-else
-  puts "Skipping users"
-  user = User.find_by_name("albert")
-end
-
-CurrentUser.user = User.admins.first
-
-if Upload.count == 0
-  puts "Creating uploads"
-  1.upto(100) do |i|
-    color1 = rand(4096).to_s(16)
-    color2 = rand(4096).to_s(16)
-    width = rand(2000) + 100
-    height = rand(2000) + 100
-    url = "http://ipsumimage.appspot.com/#{width}x#{height}"
-    tags = rand_sentence(12).scan(/[a-z]+/).join(" ")
-
-    Upload.create!(:source => url, :content_type => "image/gif", :rating => "q", :tag_string => tags, :server => Socket.gethostname)
-  end
-else
-  puts "Skipping uploads"
-end
-
-if Post.count == 0
-  puts "Creating posts"
-  Upload.all.each do |upload|
-    upload.process!
-  end
-  if Post.count == 0
-    raise "Uploads failed conversion"
-  end
-else
-  puts "Skipping posts"
-end
-
-if Comment.count == 0
-  puts "Creating comments"
-  Post.all.each do |post|
-    rand(10).times do
-      Comment.create(:post_id => post.id, :body => rand_paragraph(6))
-    end
-  end
-else
-  puts "Skipping comments"
-end
-
-if Note.count == 0
-  puts "Creating notes"
-  Post.order("random()").limit(10).each do |post|
-    rand(5).times do
-      note = Note.create(:post_id => post.id, :x => rand(post.image_width), :y => rand(post.image_height), :width => 100, :height => 100, :body => Time.now.to_f.to_s)
-
-      rand(10).times do |i|
-        note.update_attributes(:body => rand_sentence(6))
-      end
-    end
-  end
-else
-  puts "Skipping notes"
-end
-
-if Artist.count == 0
-  puts "Creating artists"
-  20.times do |i|
-    Artist.create(:name => rand_string(9, true))
-  end
-  $used_names = Set.new([""])
-else
-  puts "Skipping artists"
-end
-
-if TagAlias.count == 0
-  puts "Creating tag aliases"
-
-  20.times do |i|
-    TagAlias.create(:antecedent_name => rand_string(9, true), :consequent_name => rand_string(9, true))
-  end
-  $used_names = Set.new([""])
-else
-  puts "Skipping tag aliases"
-end
-
-if TagImplication.count == 0
-  puts "Creating tag implictions"
-
-  20.times do |i|
-    TagImplication.create(:antecedent_name => rand_string(9, true), :consequent_name => rand_string(9, true))
-  end
-  $used_names = Set.new([""])
-else
-  puts "Skipping tag implications"
-end
-
-if Pool.count == 0
-  puts "Creating pools"
-
-  1.upto(20) do |i|
-    pool = Pool.create(:name => rand_string(9, true))
-    rand(33).times do |j|
-      pool.add!(Post.order("random()").first)
-    end
-  end
-  $used_names = Set.new([""])
-end
-
-if Favorite.count == 0
-  puts "Creating favorites"
-
-  Post.order("random()").limit(50).each do |post|
-    user = User.order("random()").first
-    post.add_favorite!(user)
-    post.add_favorite!(CurrentUser.user)
-  end
-else
-  puts "Skipping favorites"
-end
-
-if ForumTopic.count == 0
-  puts "Creating forum posts"
-
-  20.times do |i|
-    topic = ForumTopic.create(:title => rand_sentence(6))
-
-    rand(100).times do |j|
-      post = ForumPost.create(:topic_id => topic.id, :body => rand_document(6))
-    end
-  end
+unless system.is_moderator?
+  system.level = User::Levels::MODERATOR
+  system.save
 end

test/test_helper.rb
@@ -64,3 +64,5 @@ end
 
 Delayed::Worker.delay_jobs = false
 TestAfterCommit.enabled = false
+
+Rails.application.load_seed