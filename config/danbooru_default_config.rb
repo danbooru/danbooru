97fe82e74bfeb75eecb633b4ebb284f6c4d62ab6
r888888888 <r888888888@gmail.com>
2018-01-18 12:52:17 -0800

    fix default naming scheme for large file urls

--- config/danbooru_default_config.rb
+++ config/danbooru_default_config.rb
@@ -222,7 +222,7 @@ module Danbooru
     end
 
     def build_large_file_url(post)
-      "/data/#{post.file_path_prefix}#{Danbooru.config.large_image_prefix}/#{post.md5}.#{post.large_file_ext}"
+      "/data/sample/#{post.file_path_prefix}#{Danbooru.config.large_image_prefix}#{post.md5}.#{post.large_file_ext}"
     end
 
 #TAG CONFIGURATION
