f83480cc8ad8688a50fe08d6cc32187d1eb00c42
r888888888 <r888888888@gmail.com>
2018-01-22 18:04:55 -0800

    hard core path to rbenv root

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,3 +4,4 @@ server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user =
 server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
+set :rbenv_path, "/home/albert/.rbenv"
