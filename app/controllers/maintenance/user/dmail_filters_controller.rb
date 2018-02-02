1db24cd18f1e2551ef6f4e138c3ec11725d0a5e1
evazion <noizave@gmail.com>
2018-02-02 02:12:12 -0600

    dmail filters: convert to strong params.

--- app/controllers/maintenance/user/dmail_filters_controller.rb
+++ app/controllers/maintenance/user/dmail_filters_controller.rb
@@ -10,12 +10,12 @@ module Maintenance
 
       def update
         @dmail_filter = CurrentUser.dmail_filter || DmailFilter.new
-        @dmail_filter.update(params.require(:dmail_filter).permit(:words), :as => CurrentUser.role)
+        @dmail_filter.update(dmail_filter_params)
         flash[:notice] = "Filter updated"
         respond_with(@dmail)
       end
 
-    private
+      private
 
       def ensure_ownership
         @dmail = Dmail.find(params[:dmail_id])
@@ -23,6 +23,10 @@ module Maintenance
         if @dmail.owner_id != CurrentUser.user.id
           raise User::PrivilegeError.new
         end
+      end
+
+      def dmail_filter_params
+        params.require(:dmail_filter).permit(:words)
       end
     end
   end
