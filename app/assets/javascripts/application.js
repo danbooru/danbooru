cf27de2ec9c3cff48c31d53ce13c861f6a175c02
evazion <noizave@gmail.com>
2018-02-06 19:11:01 -0600

    Fix #3532: Make Enter key accept current tag during autocomplete.
    Previously we patched the jqueryui-autocomplete library in order to
    customize how the Tab and Enter keys behaved. Specifically, we wanted to
    prevent the Tab key from moving the focus out of the tag input box, and
    we wanted to prevent the Enter key from submitting the page when editing tags.
    
    These things can achieved without patching the library by using
    `event.preventDefault` and `event.stopImmediatePropagation` to prevent
    other event handlers from running after these keys trigger the
    `autocompleteselect` event.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,7 @@
 //= require jquery-ui-1.11.2.min.js
 //= require jquery.hotkeys.js
 //= require jquery.timeout.js
-//= require jquery-ui-autocomplete-custom.js
+//= require jquery-ui-autocomplete-1.11.2.js
 //= require jquery.storageapi.js
 //= require jquery.dropdown.min.js
 //= require jquery.hammer.js
