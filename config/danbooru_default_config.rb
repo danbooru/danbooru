feaad0bbd06c4d0b25870763faf13cc6e4e59df9
evazion <noizave@gmail.com>
2018-02-17 16:07:56 -0600

    nginx.conf: fix inconsistent client_max_body_size.

--- config/danbooru_default_config.rb
+++ config/danbooru_default_config.rb
@@ -187,7 +187,8 @@ module Danbooru
       1_000
     end
 
-    # Maximum size of an upload.
+    # Maximum size of an upload. If you change this, you must also change
+    # `client_max_body_size` in your nginx.conf.
     def max_file_size
       35.megabytes
     end
