611dacfd22f53f237e311999cad86a63a172f169
evazion <noizave@gmail.com>
2018-04-12 21:38:42 -0500

    modqueue: remove dead "approve all" / "hide all" code (disabled since #2446).

--- app/assets/javascripts/mod_queue.js
+++ app/assets/javascripts/mod_queue.js
@@ -13,28 +13,6 @@
     }
   }
 
-  Danbooru.ModQueue.initialize_approve_all_button = function() {
-    $("#approve-all-button").click(function(e) {
-      if (!confirm("Are you sure you want to approve every post on this page?")) {
-        return;
-      }
-
-      $(".approve-link").trigger("click");
-      e.preventDefault();
-    });
-  }
-
-  Danbooru.ModQueue.initialize_hide_all_button = function() {
-    $("#hide-all-button").click(function(e) {
-      if (!confirm("Are you sure you want to hide every post on this page?")) {
-        return;
-      }
-
-      $(".disapprove-link").trigger("click");
-      e.preventDefault();
-    });
-  }
-
   Danbooru.ModQueue.initialize_hilights = function() {
     $.each($("div.post"), function(i, v) {
       var $post = $(v);
@@ -77,8 +55,6 @@
 
 $(function() {
   if ($("#c-moderator-post-queues").length) {
-    Danbooru.ModQueue.initialize_approve_all_button();
-    Danbooru.ModQueue.initialize_hide_all_button();
     Danbooru.ModQueue.initialize_hilights();
     Danbooru.ModQueue.initialize_detailed_rejection_links();
   }
