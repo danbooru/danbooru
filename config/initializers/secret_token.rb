d047d04de68747705e0432ad20d1cd29b7a4312a
Albert Yi <albert@carbonfive.com>
2018-05-15 15:34:53 -0700

    switch from secret_token to secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -6,4 +6,4 @@ Rails.application.config.action_dispatch.session = {
   :key    => '_danbooru2_session',
   :secret => StateChecker.instance.session_secret_key
 }
-Rails.application.config.secret_token = StateChecker.instance.secret_token
+Rails.application.config.secret_key_base = StateChecker.instance.secret_token
