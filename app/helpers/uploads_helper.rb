acf327712a687cd187f96a5a52922f4d2af8c40c
Albert Yi <albert@carbonfive.com>
2018-05-15 14:37:37 -0700

    remove unused ccs sig helper

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -1,9 +1,2 @@
 module UploadsHelper
-  def ccs_build_sig(url)
-    return nil unless Danbooru.config.ccs_server.present?
-
-    ref = ImageProxy.fake_referer_for(url)
-    verifier = ActiveSupport::MessageVerifier.new(Danbooru.config.ccs_key, serializer: JSON, digest: "SHA256")
-    verifier.generate("#{url},#{ref}")
-  end
 end
