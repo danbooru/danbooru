977779801deadcdd5419d7b7720e87c6dff7db2f
Albert Yi <albert@carbonfive.com>
2018-05-25 11:31:59 -0700

    remove uniqueness constraint on urls (#3272)

--- app/models/artist_url.rb
+++ app/models/artist_url.rb
@@ -2,7 +2,7 @@ class ArtistUrl < ApplicationRecord
   before_validation :parse_prefix
   before_save :initialize_normalized_url, on: [ :create ]
   before_save :normalize
-  validates :url, presence: true, uniqueness: { scope: :artist_id }
+  validates :url, presence: true
   validate :validate_url_format
   belongs_to :artist, :touch => true
 
