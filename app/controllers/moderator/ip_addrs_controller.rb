4739c45da0520630e8ea49aaa37cc17107efe3d7
r888888888 <r888888888@gmail.com>
2018-05-26 13:13:07 -0700

    fixes #3729

--- app/controllers/moderator/ip_addrs_controller.rb
+++ app/controllers/moderator/ip_addrs_controller.rb
@@ -1,10 +1,12 @@
 module Moderator
   class IpAddrsController < ApplicationController
     before_action :moderator_only
+    respond_to :html, :json
 
     def index
       @search = IpAddrSearch.new(params[:search])
       @results = @search.execute
+      respond_with(@results)
     end
 
     def search
