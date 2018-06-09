99286409c5df8f98410c5efe69f586aa93ca37cd
Albert Yi <albert@carbonfive.com>
2018-06-09 13:38:06 -0700

    update tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1384,7 +1384,8 @@ class Post < ApplicationRecord
     def replace!(params)
       transaction do
         replacement = replacements.create(params)
-        replacement.process!
+        processor = UploadService::Replacer.new(post: self, replacement: replacement)
+        processor.process!
         replacement
       end
     end
