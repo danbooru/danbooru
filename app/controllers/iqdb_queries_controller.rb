fffbc708b27fad4b25e6a903d9f3ab4fd93483db
Albert Yi <albert@carbonfive.com>
2018-06-19 16:46:22 -0700

    support url/post_id params on iqdb queries endpoint

--- app/controllers/iqdb_queries_controller.rb
+++ app/controllers/iqdb_queries_controller.rb
@@ -2,6 +2,19 @@ class IqdbQueriesController < ApplicationController
   respond_to :html, :json, :xml
 
   def show
+    if params[:url]
+      url = URI::HTTP.build(host: Danbooru.config.iqdb_server, path: "/similar", query: URI.encode_www_form({callback: iqdb_queries_url, url: params[:url]}))
+      redirect_to url.to_s
+      return
+    end
+
+    if params[:post_id]
+      post = Post.find(params[:post_id])
+      url = URI::HTTP.build(host: Danbooru.config.iqdb_server, path: "/similar", query: URI.encode_www_form({callback: iqdb_queries_url, url: post.preview_file_url}))
+      redirect_to url.to_s
+      return      
+    end
+
     if params[:matches]
       @matches = JSON.parse(params[:matches])
       @matches = @matches.map {|x| [Post.find(x[0]), x[1]]}
