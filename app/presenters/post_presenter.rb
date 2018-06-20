8c0792f4cae33b6e1ad39f9b96b1a841c83540e0
Albert Yi <albert@carbonfive.com>
2018-06-20 16:08:06 -0700

    add srcset to image previews

--- app/presenters/post_presenter.rb
+++ app/presenters/post_presenter.rb
@@ -35,13 +35,14 @@ class PostPresenter < Presenter
     html << %{<a href="#{path}/#{post.id}#{tag_param}">}
 
     if options[:show_cropped] && post.has_cropped? && !CurrentUser.user.disable_cropped_thumbnails?
-      src = post.crop_file_url
+      srcset = ["#{post.crop_file_url} 1x", "#{post.preview_file_url} 2x"].join(", ")
     else
-      src = post.preview_file_url
+      srcset = ["#{post.preview_file_url} 1x"].join(", ")
     end
 
+    src = post.preview_file_url
     tooltip = "#{post.tag_string} rating:#{post.rating} score:#{post.score}"
-    html << %{<img itemprop="thumbnailUrl" src="#{src}" title="#{h(tooltip)}" alt="#{h(post.tag_string)}">}
+    html << %{<img itemprop="thumbnailUrl" srcset="#{srcset}" src="#{src}" title="#{h(tooltip)}" alt="#{h(post.tag_string)}">}
     html << %{</a>}
 
     if options[:pool]
