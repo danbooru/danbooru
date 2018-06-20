e262295cf1921215e85668bbd7274cec325e334e
Albert Yi <albert@carbonfive.com>
2018-06-20 11:53:44 -0700

    support json for iqdb query actions

--- app/controllers/iqdb_queries_controller.rb
+++ app/controllers/iqdb_queries_controller.rb
@@ -1,11 +1,11 @@
 class IqdbQueriesController < ApplicationController
-  respond_to :html, :json, :xml
+  respond_to :html, :json
 
   def show
     if params[:url]
       url = URI.parse(Danbooru.config.iqdbs_server)
       url.path = "/similar"
-      url.query = {callback: iqdb_queries_url, url: params[:url]}.to_query
+      url.query = {callback: iqdb_queries_url(format: params[:format]), url: params[:url]}.to_query
       redirect_to url.to_s
       return
     end
@@ -14,7 +14,7 @@ class IqdbQueriesController < ApplicationController
       post = Post.find(params[:post_id])
       url = URI.parse(Danbooru.config.iqdbs_server)
       url.path = "/similar"
-      url.query = {callback: iqdb_queries_url, url: post.preview_file_url}.to_query
+      url.query = {callback: iqdb_queries_url(format: params[:format]), url: post.preview_file_url}.to_query
       redirect_to url.to_s
       return      
     end
