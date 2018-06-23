5aae4b649fb69070a581cb8242def3124107d605
r888888888 <r888888888@gmail.com>
2018-06-23 12:01:44 -0700

    add xhr variant detection

--- app/controllers/iqdb_queries_controller.rb
+++ app/controllers/iqdb_queries_controller.rb
@@ -1,5 +1,6 @@
 class IqdbQueriesController < ApplicationController
   respond_to :html, :json
+  before_action :detect_xhr
 
   def show
     if params[:url]
@@ -15,15 +16,21 @@ class IqdbQueriesController < ApplicationController
     end
 
     respond_with(@matches) do |fmt|
-      fmt.html
-
-      fmt.html.xhr do
-        render layout: false
+      fmt.html do |html|
+        html.xhr { render layout: false}
       end
       
       fmt.json do
         render json: @matches
       end
+    end
+  end
+
+private
+  
+  def detect_xhr
+    if request.xhr?
+      request.variant = :xhr
     end
   end
 end
