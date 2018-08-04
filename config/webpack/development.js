752557e813dd73b762d10af94f1ec048d606a17b
evazion <noizave@gmail.com>
2018-08-04 11:27:10 -0500

    Add eslint support.
    Run with `yarn run lint` or `bin/webpack`.

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,8 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+const eslint = require('./loaders/eslint')
+
+environment.loaders.append('eslint', eslint);
 
 module.exports = environment.toWebpackConfig()
