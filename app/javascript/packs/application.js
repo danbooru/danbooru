107d19c6b371d7327b06ee777fd79a0a046cd1a2
evazion <noizave@gmail.com>
2018-08-05 13:42:42 -0500

    Tag scripts: drop [if ...] and [reset] syntax (fix #3807, #3773)
    Fixes #3807. Drops the TagScript module entirely and replaces it with
    a simple call to Post.tag.
    
    Simultaneously fixes #3773 as a consequence of removing TagScript.run.
    Post.update already updates data-tags by itself, so there was no need
    for TagScript.run to update data-tags as well.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -38,4 +38,3 @@ export { default as Note } from '../src/javascripts/notes.js';
 export { default as PostModeMenu } from '../src/javascripts/post_mode_menu.js';
 export { default as Utility } from '../src/javascripts/utility.js';
 export { default as Ugoira } from '../src/javascripts/ugoira.js';
-export { default as TagScript } from '../src/javascripts/tag_script.js';
