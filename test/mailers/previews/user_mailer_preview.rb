604bfb09234c3d01c6177c81cc96641cc61ba2bc
evazion <noizave@gmail.com>
2018-08-27 22:04:37 -0500

    Fix #3846: Subscribed forum posts link to invalid URL in email updates.
    Test: http://localhost/rails/mailers/user_mailer/forum_notice

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,4 +3,12 @@ class UserMailerPreview < ActionMailer::Preview
     dmail = User.admins.first.dmails.first
     UserMailer.dmail_notice(dmail)
   end
+
+  def forum_notice
+    topic = ForumTopic.first
+    posts = topic.posts
+    user = topic.creator
+
+    UserMailer.forum_notice(user, topic, posts)
+  end
 end
