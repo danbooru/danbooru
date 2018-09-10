c9300cc54e7e558977181d5b8ce67985c612c53e
evazion <noizave@gmail.com>
2018-09-10 18:55:38 -0500

    sta.sh: add tests + docs.

--- app/logical/sources/strategies/stash.rb
+++ app/logical/sources/strategies/stash.rb
@@ -1,3 +1,15 @@
+# Page URLs:
+# * https://sta.sh/0wxs31o7nn2 (single image)
+# * https://sta.sh/21leo8mz87ue (folder)
+#
+# Image URLs:
+# * https://orig00.deviantart.net/0fd2/f/2018/252/9/c/a_pepe_by_noizave-dcmga0s.png
+#
+# Ref:
+# * https://github.com/r888888888/danbooru/issues/3877
+# * https://www.deviantartsupport.com/en/article/what-is-stash-3391708
+# * https://www.deviantart.com/developers/http/v1/20160316/stash_item/4662dd8b10e336486ea9a0b14da62b74
+#
 module Sources
   module Strategies
     class Stash < DeviantArt
