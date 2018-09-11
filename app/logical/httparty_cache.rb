20f4dd6d2e8cf3c87b239ac95acbc15b59e196f6
Albert Yi <albert@carbonfive.com>
2018-09-11 14:07:53 -0700

    fix cache key (#3886)

--- app/logical/httparty_cache.rb
+++ app/logical/httparty_cache.rb
@@ -2,7 +2,8 @@ module HttpartyCache
   extend self
 
   def get(url, headers: {}, params: {}, base_uri: nil, format: :html, expiry: 60)
-    Cache.get("cachedget:#{Cache.hash(url)}", expiry) do
+    key = Cache.hash({url: url, headers: headers, params: params, base_uri: base_uri, format: format}.to_s)
+    Cache.get("cachedget:#{key}", expiry) do
       resp = HTTParty.get(url, Danbooru.config.httparty_options.deep_merge(query: params, headers: headers, base_uri: base_uri, format: format))
       body = resp.body.force_encoding("utf-8")
       [body, resp.code]
