c100600875450eb63a9f45697e67d8b1e0e35fea
Albert Yi <albert@carbonfive.com>
2018-09-12 15:19:27 -0700

    fix migration

--- db/migrate/20180912185624_add_prefixes_to_tags.rb
+++ db/migrate/20180912185624_add_prefixes_to_tags.rb
@@ -1,6 +1,6 @@
 class AddPrefixesToTags < ActiveRecord::Migration[5.2]
   def change
     execute "set statement_timeout = 0"
-    execute "CREATE INDEX index_tags_on_name_prefix ON tags USING gin (REGEXP_REPLACE(name, '([a-z0-9])[a-z0-9'']*($|[^a-z0-9'']+)', '\1', 'g') gin_trgm_ops) WHERE post_count > 0"
+    execute "CREATE INDEX index_tags_on_name_prefix ON tags USING gin (REGEXP_REPLACE(name, '([a-z0-9])[a-z0-9'']*($|[^a-z0-9'']+)', '\\1', 'g') gin_trgm_ops) WHERE post_count > 0"
   end
 end
