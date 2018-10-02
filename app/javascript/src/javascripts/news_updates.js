danbooru/danbooru

commit f72b32b27b93b69e99330c1dfefebae9280dff00

Fix eslint warnings.

app/javascript/src/javascripts/news_updates.js
@@ -5,7 +5,7 @@ let NewsUpdate = {};
 NewsUpdate.initialize = function() {
   var key = $("#news-updates").data("id");
 
-  if (Cookie.get("news-ticker") == key) {
+  if (Cookie.get("news-ticker") === key) {
     $("#news-updates").hide();
   } else {
     $("#news-updates").show();

app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 /* eslint no-console:0 */
+/* global require */
 
 function importAll(r) {
   r.keys().forEach(r);

app/javascript/src/javascripts/artist_commentaries.js
@@ -63,15 +63,16 @@ ArtistCommentary.initialize_edit_commentary_dialog = function() {
 }
 
 ArtistCommentary.fetch_commentary = function() {
+  var commentary = "";
   Utility.notice("Fetching artist commentary...");
 
   var type = $('#fetch-commentary select[name="commentary_source_type"]').val();
   if (type === "Source") {
     var source = $('#fetch-commentary input[name="commentary_source"]').val();
-    var commentary = ArtistCommentary.from_source(source);
+    commentary = ArtistCommentary.from_source(source);
   } else if (type === "Post") {
     var id = $('#fetch-commentary input[name="commentary_post_id"]').val();
-    var commentary = ArtistCommentary.from_post_id(id);
+    commentary = ArtistCommentary.from_post_id(id);
   }
 
   commentary.then(ArtistCommentary.fill_commentary).then(function (success) {
@@ -120,13 +121,13 @@ ArtistCommentary.merge_commentaries = function(description, commentary) {
   var normalized_source = $("#image-container").data().normalizedSource;
 
   if ((commentary.original_description && description) &&
-      (commentary.original_description != description)) {
+      (commentary.original_description !== description)) {
     return description
       + "\n\n[tn]\nSource: " + normalized_source + "\n[/tn]"
       + "\n\nh6. " + (commentary.original_title || "Untitled")
       + "\n\n" + commentary.original_description
       + "\n\n[tn]\nSource: " + commentary.source + "\n[/tn]";
-  } else if (commentary.source != post_source) {
+  } else if (commentary.source !== post_source) {
     return commentary.original_description + "\n\n[tn]\nSource: " + commentary.source + "\n[/tn]";
   } else {
     return commentary.original_description || description;

app/javascript/src/javascripts/artists.js
@@ -14,10 +14,10 @@ Artist.initialize_check_name = function() {
     if ($("#artist_name").val().length > 0) {
       $("#check-name-result").html("");
 
-      $.getJSON("/artists?search[name]=" + escape($("#artist_name").val()), function(data) {
-        if (data.length === 0) {
-          $.getJSON("/wiki_pages/" + escape($("#artist_name").val()), function(data) {
-            if (data !== null) {
+      $.getJSON("/artists?search[name]=" + escape($("#artist_name").val()), function(artists) {
+        if (artists.length === 0) {
+          $.getJSON("/wiki_pages/" + escape($("#artist_name").val()), function(wiki_pages) {
+            if (wiki_pages !== null) {
               $("#check-name-result").html("<a href='/wiki_pages/" + escape($("#artist_name").val()) + "'>A wiki page with this name already exists</a>. You must either move the wiki page or pick another artist name.")
             }
           });

app/javascript/src/javascripts/autocomplete.js.erb
@@ -3,9 +3,12 @@ import SavedSearch from './saved_searches'
 
 let Autocomplete = {};
 
-Autocomplete.AUTOCOMPLETE_VERSION = 1;
-Autocomplete.PREFIXES = /^(-|~|<%= TagCategory.mapping.keys.map {|category| category + ':'}.join('|') %>)(.*)$/i;
-Autocomplete.METATAGS = /^(<%= Tag::METATAGS %>):(.*)$/i;
+Autocomplete.METATAGS = <%= Tag::METATAGS.to_json.html_safe %>;
+Autocomplete.TAG_CATEGORIES = <%= TagCategory.mapping.to_json.html_safe %>;
+
+Autocomplete.TAG_PREFIXES = "-|~|" + Object.keys(Autocomplete.TAG_CATEGORIES).map(category => category + ":").join("|");
+Autocomplete.TAG_PREFIXES_REGEX = new RegExp("^(" + Autocomplete.TAG_PREFIXES + ")(.*)$", "i");
+Autocomplete.METATAGS_REGEX = new RegExp("^(" + Autocomplete.METATAGS + "):(.*)$", "i");
 
 Autocomplete.initialize_all = function() {
   if (Utility.meta("enable-auto-complete") === "true") {
@@ -42,16 +45,15 @@ Autocomplete.initialize_mention_autocomplete = function($fields) {
     },
     source: function(req, resp) {
       var cursor = this.element.get(0).selectionStart;
-      var i;
       var name = null;
 
-      for (i=cursor; i>=1; --i) {
-        if (req.term[i-1] === " ") {
+      for (var i = cursor; i >= 1; --i) {
+        if (req.term[i - 1] === " ") {
           return;
         }
 
-        if (req.term[i-1] === "@") {
-          if (i == 1 || /[ \r\n]/.test(req.term[i-2])) {
+        if (req.term[i - 1] === "@") {
+          if (i === 1 || /[ \r\n]/.test(req.term[i - 2])) {
             name = req.term.substring(i, cursor);
             break;
           } else {
@@ -63,8 +65,6 @@ Autocomplete.initialize_mention_autocomplete = function($fields) {
       if (name) {
         Autocomplete.user_source(name, resp, "@");
       }
-
-      return;
     }
   });
 }
@@ -99,7 +99,7 @@ Autocomplete.initialize_tag_autocomplete = function() {
         return;
       }
 
-      switch(metatag) {
+      switch (metatag) {
       case "md5":
       case "width":
       case "height":
@@ -117,7 +117,7 @@ Autocomplete.initialize_tag_autocomplete = function() {
       case "pixiv_id":
       case "pixiv":
       <% TagCategory.short_name_list.each do |category| %>
-        case "<%= category %>tags":
+        case "<%= category %>tags": // eslint-disable-line
       <% end %>
         resp([]);
         return;
@@ -194,7 +194,7 @@ Autocomplete.initialize_artist_autocomplete = function($fields) {
               type: "tag",
               label: artist.name.replace(/_/g, " "),
               value: artist.name,
-              category: <%= Tag.categories.artist %>,
+              category: Autocomplete.TAG_CATEGORIES.artist,
             };
           }));
         }
@@ -250,9 +250,7 @@ Autocomplete.normal_source = function(term, resp) {
     return window.Danbooru.Autocomplete.normal_source(term, resp);
   }
 
-  var key = "ac-" + term.replace(/\./g,'\uFFFF');
-
-  $.ajax({
+  return $.ajax({
     url: "/tags/autocomplete.json",
     data: {
       "search[name_matches]": term,
@@ -288,12 +286,14 @@ Autocomplete.parse_query = function(text, caret) {
     return {};
   }
 
-  if (match = term.match(Autocomplete.PREFIXES)) {
+  match = term.match(Autocomplete.TAG_PREFIXES_REGEX);
+  if (match) {
     metatag = match[1].toLowerCase();
     term = match[2];
   }
-  
-  if (match = term.match(Autocomplete.METATAGS)) {
+
+  match = term.match(Autocomplete.METATAGS_REGEX);
+  if (match) {
     metatag = match[1].toLowerCase();
     term = match[2];
   }
@@ -307,8 +307,7 @@ Autocomplete.insert_completion = function(input, completion) {
   var before_caret_text = input.value.substring(0, input.selectionStart).trim();
   var after_caret_text = input.value.substring(input.selectionStart).trim();
 
-  var prefixes = "-|~|" + "<%= TagCategory.mapping.keys.map {|category| category + ':'}.join('|') %>";
-  var regexp = new RegExp("(" + prefixes + ")?\\S+$", "g");
+  var regexp = new RegExp("(" + Autocomplete.TAG_PREFIXES + ")?\\S+$", "g");
   before_caret_text = before_caret_text.replace(regexp, "$1") + completion + " ";
 
   input.value = before_caret_text + after_caret_text;
@@ -357,12 +356,12 @@ Autocomplete.render_item = function(list, item) {
   }
 
   if (item.post_count !== undefined) {
-    var count;
-    if (item.post_count >= 1000) {
-      count = Math.floor(item.post_count / 1000) + "k";
-    } else {
-      count = item.post_count;
+    var count = item.post_count;
+
+    if (count >= 1000) {
+      count = Math.floor(count / 1000) + "k";
     }
+
     var $post_count = $("<span/>").addClass("post-count").css("float", "right").text(count);
     $link.append($post_count);
   }
@@ -440,7 +439,7 @@ Autocomplete.user_source = function(term, resp, metatag) {
     return window.Danbooru.Autocomplete.user_source(term, resp, metatag);
   }
 
-  $.ajax({
+  return $.ajax({
     url: "/users.json",
     data: {
       "search[order]": "post_upload_count",
@@ -450,8 +449,8 @@ Autocomplete.user_source = function(term, resp, metatag) {
     },
     method: "get",
     success: function(data) {
-      var prefix;
-      var display_name;
+      var prefix = "";
+      var display_name = null;
 
       if (metatag === "@") {
         prefix = "@";
@@ -478,7 +477,7 @@ Autocomplete.pool_source = function(term, resp, metatag) {
     return window.Danbooru.Autocomplete.pool_source(term, resp, metatag);
   }
 
-  $.ajax({
+  return $.ajax({
     url: "/pools.json",
     data: {
       "search[order]": "post_count",
@@ -505,7 +504,7 @@ Autocomplete.favorite_group_source = function(term, resp, metatag) {
     return window.Danbooru.Autocomplete.favorite_group_source(term, resp, metatag);
   }
 
-  $.ajax({
+  return $.ajax({
     url: "/favorite_groups.json",
     data: {
       "search[name_matches]": term,

app/javascript/src/javascripts/blacklists.js
@@ -112,7 +112,7 @@ Blacklist.apply = function() {
 
   $.each(this.posts(), function(i, post) {
     var post_count = 0;
-    $.each(Blacklist.entries, function(i, entry) {
+    $.each(Blacklist.entries, function(j, entry) {
       if (Blacklist.post_match(post, entry)) {
         entry.hits += 1;
         count += 1;
@@ -192,7 +192,7 @@ Blacklist.initialize_all = function() {
 }
 
 $(document).ready(function() {
-  if ($("#blacklist-box").length == 0) {
+  if ($("#blacklist-box").length === 0) {
     return;
   }
 

app/javascript/src/javascripts/comments.js
@@ -39,7 +39,7 @@ Comment.quote = function(e) {
       var $link = $(e.target);
       var $div = $link.closest("div.comments-for-post").find(".new-comment");
       var $textarea = $div.find("textarea");
-      var msg = data["quoted_response"];
+      var msg = data.quoted_response;
       if ($textarea.val().length > 0) {
         msg = $textarea.val() + "\n\n" + msg;
       }

app/javascript/src/javascripts/common.js
@@ -45,6 +45,6 @@ $(function() {
   });
 });
 
-global.submitInvisibleRecaptchaForm = function () {
+window.submitInvisibleRecaptchaForm = function () {
   document.getElementById("signup-form").submit();
 }

commit 4fd949ca8d2b153af37148643a128dca9ca507ed

news updates: fix news updates not staying hidden.
ref: https://danbooru.donmai.us/forum_posts/151011

Bug: `Cookie.get("news-ticker") === key` always returned false because
the cookie was a string but the key was an integer.

Regressed in f72b32b27b after switching from `==` to `===`.

app/javascript/src/javascripts/news_updates.js
@@ -3,7 +3,7 @@ import Cookie from './cookie'
 let NewsUpdate = {};
 
 NewsUpdate.initialize = function() {
-  var key = $("#news-updates").data("id");
+  var key = $("#news-updates").data("id").toString();
 
   if (Cookie.get("news-ticker") === key) {
     $("#news-updates").hide();