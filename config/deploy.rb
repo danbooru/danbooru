25c0c8884cfc9ce655932cee17e207b69720eddf
r888888888 <r888888888@gmail.com>
2018-10-04 01:12:25 -0700

    move production2 configs to production

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
-set :stages, %w(production production2 development staging)
+set :stages, %w(production development staging)
 set :default_stage, "staging"
 set :application, "danbooru"
 set :repo_url,  "git://github.com/r888888888/danbooru.git"
 set :scm, :git
-set :deploy_to, "/var/www/danbooru2"
-set :rbenv_ruby, "2.5.0"
+set :deploy_to, "/var/www/danbooru"
+set :rbenv_ruby, "2.5.1"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 set :branch, ENV.fetch("branch", "master")
