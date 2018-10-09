1b6da3da71bdf1d3d440b56b84993f130e47b040
Albert Yi <albert@carbonfive.com>
2018-10-08 17:43:15 -0700

    remove oogaki from app role

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ set :user, "danbooru"
 set :rails_env, "production"
 server "kagamihara", :roles => %w(web app db), :primary => true
 server "shima", :roles => %w(web app)
-server "oogaki", :roles => %w(app worker)
+server "oogaki", :roles => %w(worker)
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
 set :rbenv_path, "/home/danbooru/.rbenv"
