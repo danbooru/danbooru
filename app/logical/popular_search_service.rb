5c9f69a9e5a65e9366118c85c3a70b3924120ae4
r888888888 <r888888888@gmail.com>
2018-10-20 19:46:07 -0700

    go back one day on popular search service if no results are found

--- app/logical/popular_search_service.rb
+++ app/logical/popular_search_service.rb
@@ -22,12 +22,12 @@ class PopularSearchService
     JSON.parse(fetch_data.to_s).map {|x| x[0]}
   end
 
-  def fetch_data
+  def fetch_data()
     return [] unless self.class.enabled?
     
     dates = date.strftime("%Y-%m-%d")
 
-    Cache.get("ps-day-#{dates}", 1.minute) do
+    data = Cache.get("ps-day-#{dates}", 1.minute) do
       url = "#{Danbooru.config.reportbooru_server}/post_searches/rank?date=#{dates}"
       response = HTTParty.get(url, Danbooru.config.httparty_options.reverse_merge(timeout: 3))
       if response.success?
@@ -37,6 +37,21 @@ class PopularSearchService
       end
       response
     end.to_s.force_encoding("utf-8")
+
+    if data.empty?
+      dates = date.yesterday.strftime("%Y-%m-%d")
+
+      data = Cache.get("ps-day-#{dates}", 1.minute) do
+        url = "#{Danbooru.config.reportbooru_server}/post_searches/rank?date=#{dates}"
+        response = HTTParty.get(url, Danbooru.config.httparty_options.reverse_merge(timeout: 3))
+        if response.success?
+          response = response.body
+        else
+          response = ""
+        end
+        response
+      end.to_s.force_encoding("utf-8")
+    end
 
   rescue => e
     Rails.logger.error(e.to_s)
