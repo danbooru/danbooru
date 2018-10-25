04c741739d250d4eaa999dd9044e422be391c7ae
Albert Yi <albert@carbonfive.com>
2018-10-25 14:06:44 -0700

    ignore md5 dups in upload error checker

--- app/logical/upload_error_checker.rb
+++ app/logical/upload_error_checker.rb
@@ -4,7 +4,7 @@ require 'mail'
 
 class UploadErrorChecker
   def check!
-    uploads = Upload.where("status like 'error%' and status not like 'error: Upload::Error - Post with MD5%' and status not like 'error: RuntimeError - duplicate%' and status not like 'error: ActiveRecord::RecordInvalid - Validation failed: Md5 has already been taken' and created_at >= ?", 1.hour.ago)
+    uploads = Upload.where("status like 'error%' and status not like 'error: Upload::Error - Post with MD5%' and status not like 'error: ActiveRecord::RecordInvalid - Validation failed: Md5 duplicate%' and created_at >= ?", 1.hour.ago)
     if uploads.size > 5
       mail = Mail.new do
         from Danbooru.config.contact_email
