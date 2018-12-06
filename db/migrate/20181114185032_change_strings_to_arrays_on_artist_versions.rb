79dfa6c28fe67a8d6cb02624c744afe238f6d5fb
Albert Yi <albert@carbonfive.com>
2018-12-06 10:31:24 -0800

    fix migration for circleci

--- db/migrate/20181114185032_change_strings_to_arrays_on_artist_versions.rb
+++ db/migrate/20181114185032_change_strings_to_arrays_on_artist_versions.rb
@@ -1,11 +1,14 @@
 class ChangeStringsToArraysOnArtistVersions < ActiveRecord::Migration[5.2]
   def up
     ArtistVersion.without_timeout do
-      change_column_default :artist_versions, :other_names, from: '', to: nil
-      change_column :artist_versions, :other_names, "text[]", using: "array_remove(regexp_split_to_array(other_names, '\\s+'), '')", default: "{}"
+      add_column :artist_versions, :other_names_array, "text[]", default: "{}"
+      execute "update artist_versions set other_names_array = array_remove(regexp_split_to_array(other_names, '\\s+'), '')"
+      remove_column :artist_versions, :other_names
+      rename_column :artist_versions, :other_names_array, :other_names
 
-      change_column :artist_versions, :url_string, "text[]", using: "array_remove(regexp_split_to_array(url_string, '\\s+'), '')", default: "{}"
-      rename_column :artist_versions, :url_string, :urls
+      add_column :artist_versions, :urls, "text[]", default: "{}"
+      execute "update artist_versions set urls = array_remove(regexp_split_to_array(url_string, '\\s+'), '')"
+      remove_column :artist_versions, :url_string
     end
   end
 
