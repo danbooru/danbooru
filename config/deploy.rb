dfe6ccd80776a6aa898c2820cebe95e94714dce9
GlassedSilver <falk993@gmx.net>
2019-05-16 01:41:22 +0200

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :stages, %w(production development staging)
 set :default_stage, "staging"
 set :application, "danbooru"
-set :repo_url,  "git://github.com/r888888888/danbooru.git"
+set :repo_url,  "git://github.com/GlassedSilver/danbooru.git"
 set :scm, :git
 set :deploy_to, "/var/www/danbooru"
 set :rbenv_ruby, "2.5.1"
