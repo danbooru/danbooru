f82cfe3c4e01d1241866202845cf78f0f849c582
GlassedSilver <falk993@gmx.net>
2019-06-12 08:30:45 +0200

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
 set :user, "danbooru"
 set :rails_env, "production"
-server "kagamihara", :roles => %w(web app db), :primary => true
-server "shima", :roles => %w(web app)
-server "saitou", :roles => %w(web app)
-server "oogaki", :roles => %w(worker)
+server "kagamihara", :roles => %w(192.168.2.176), :primary => true
+server "shima", :roles => %w(192.168.2.176)
+server "saitou", :roles => %w(192.168.2.176)
+server "oogaki", :roles => %w(192.168.2.176)
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
 set :rbenv_path, "/home/danbooru/.rbenv"
