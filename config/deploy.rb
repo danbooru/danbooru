cbf678921d8bf385fed95100e122287574d25ed6
GlassedSilver <falk993@gmx.net>
2019-07-02 02:42:57 +0200

    Update deploy.rb
    https://github.com/kdoshere/booru/blob/b913158202b0b5c85b4e13f39de3d6ae3f32cb7b/config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :stages, %w(production development staging)
 set :default_stage, "staging"
 set :application, "danbooru"
 set :repo_url,  "git://github.com/GlassedSilver/danbooru.git"
-set :scm, :git
+
 set :deploy_to, "/var/www/danbooru"
 set :rbenv_ruby, "2.5.1"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
