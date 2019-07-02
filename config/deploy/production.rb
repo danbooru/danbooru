6fd25d9dc15247f9f0c1d0585c51dfebfe6e1575
GlassedSilver <falk993@gmx.net>
2019-07-02 03:00:14 +0200

    Update production.rb
    https://github.com/kdoshere/booru/blob/b913158202b0b5c85b4e13f39de3d6ae3f32cb7b/config/deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
 set :user, "danbooru"
 set :rails_env, "production"
-server "192.168.2.176", :roles => %w(web app db), :primary => true
-server "192.168.2.176", :roles => %w(web app)
-server "192.168.2.176", :roles => %w(web app)
-server "192.168.2.176", :roles => %w(worker)
+server "192.168.2.192", :roles => %w(web app db), :primary => true
+# server "", :roles => %w(web app)
+# server "", :roles => %w(web app)
+server "192.168.2.192", :roles => %w(worker)
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
 set :rbenv_path, "/home/danbooru/.rbenv"
