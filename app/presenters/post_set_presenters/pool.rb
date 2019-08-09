6d171f44c47ba1262b24abf1be38c950f437d603
evazion <noizave@gmail.com>
2019-08-08 22:34:19 -0500

    pools/show: remove unused related tags calculation.

--- app/presenters/post_set_presenters/pool.rb
+++ app/presenters/post_set_presenters/pool.rb
@@ -1,15 +1,10 @@
 module PostSetPresenters
   class Pool < Base
-    attr_reader :tag_set_presenter, :post_set
+    attr_reader :post_set
     delegate :posts, :to => :post_set
 
     def initialize(post_set)
       @post_set = post_set
-      @tag_set_presenter = TagSetPresenter.new(
-        RelatedTagCalculator.calculate_from_sample_to_array(
-          post_set.tag_string
-        ).map {|x| x[0]}
-      )
     end
 
     def post_previews_html(template)
