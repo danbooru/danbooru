danbooru/danbooru

commit 98b31aafa6b87530c81784512d21643461c32f13

fix session comparison

app/views/layouts/default.html.erb
@@ -43,7 +43,7 @@
   </header>
 
   <div id="page">
-    <% if CurrentUser.is_anonymous? && session[:started_at] && session[:started_at] < 1.minute.ago && cookies[:hide_sign_up_notice].blank? %>
+    <% if CurrentUser.is_anonymous? && session[:started_at].is_a?(ActiveSupport::TimeWithZone) && session[:started_at] < 1.minute.ago && cookies[:hide_sign_up_notice].blank? %>
       <%= render "users/sign_up_notice" %>
     <% end %>
 

commit 38ab13f0b7882e379e710d01cbb434ce4c2b3b3d

Remove unused sign up banner.
No longer used, probably broken in 98b31aafa6.

app/javascript/src/javascripts/common.js
@@ -2,13 +2,6 @@ import Cookie from './cookie'
 import Utility from './utility'
 
 $(function() {
-  // Account notices
-  $("#hide-sign-up-notice").on("click.danbooru", function(e) {
-    $("#sign-up-notice").hide();
-    Cookie.put("hide_sign_up_notice", "1", 7);
-    e.preventDefault();
-  });
-
   $("#hide-upgrade-account-notice").on("click.danbooru", function(e) {
     $("#upgrade-account-notice").hide();
     Cookie.put('hide_upgrade_account_notice', '1', 7);

app/javascript/src/styles/common/main_layout.scss
@@ -20,7 +20,7 @@ div#page {
   margin: 0 20px;
   padding: 0 10px;
 
-  div#upgrade-account-notice, div#sign-up-notice, div#ban-notice, div#dmail-notice, div#mod-notice {
+  div#upgrade-account-notice, div#ban-notice, div#dmail-notice, div#mod-notice {
     margin: 1em 0;
     padding: 1em;
     text-align: center;

app/views/layouts/default.html.erb
@@ -115,10 +115,6 @@
   </header>
 
   <div id="page">
-    <% if CurrentUser.is_anonymous? && session[:started_at].is_a?(ActiveSupport::TimeWithZone) && session[:started_at] < 1.minute.ago && cookies[:hide_sign_up_notice].blank? %>
-      <%= render "users/sign_up_notice" %>
-    <% end %>
-
     <% if !CurrentUser.is_anonymous? && !CurrentUser.is_gold? && cookies[:hide_upgrade_account_notice].blank? && params[:action] != "upgrade_information" %>
       <%= render "users/upgrade_notice" %>
     <% end %>

app/views/users/_sign_up_notice.html.erb
@@ -1,19 +1,0 @@
-<div class="ui-corner-all ui-state-highlight" id="sign-up-notice">
-  <h1>Like the site? <%= link_to "Sign up for a free basic account!", new_user_path, id: "sign-up-link" %></h1>
-  <p><%= link_to "No thanks", "#", :id => "hide-sign-up-notice" %></p>
-</div>
-
-<% content_for(:html_header) do %>
-  <script>
-    $(function() {
-      $("#sign-up-link").click(function() {
-        window._paq = window._paq || [];
-        window._paq.push(['trackEvent', 'accounts', 'clickedSignUpLink']);
-      });
-      $("#hide-sign-up-notice").click(function() {
-        window._paq = window._paq || [];
-        window._paq.push(['trackEvent', 'accounts', 'clickedHideSignUpLink']);
-      });
-    });
-  </script>
-<% end %>