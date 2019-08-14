90fa67d4c15f6c9f22abe90441c17c883f659431
evazion <noizave@gmail.com>
2019-08-14 01:46:44 -0500

    Remove unused terms of service banner.
    No longer used, inadvertently hidden in a6233f297e.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,12 +1,6 @@
 class StaticController < ApplicationController
   def terms_of_service
   end
-  
-  def accept_terms_of_service
-    cookies.permanent[:accepted_tos] = "1"
-    url = params[:url] if params[:url] && params[:url].start_with?("/")
-    redirect_to(url || posts_path)
-  end
 
   def not_found
     render plain: "not found", status: :not_found
--- config/routes.rb
+++ config/routes.rb
@@ -423,8 +423,6 @@ Rails.application.routes.draw do
   get "/static/bookmarklet" => "static#bookmarklet", :as => "bookmarklet"
   get "/static/site_map" => "static#site_map", :as => "site_map"
   get "/static/terms_of_service" => "static#terms_of_service", :as => "terms_of_service"
-  post "/static/accept_terms_of_service" => "static#accept_terms_of_service", :as => "accept_terms_of_service"
-  get "/static/mrtg" => "static#mrtg", :as => "mrtg"
   get "/static/contact" => "static#contact", :as => "contact"
   get "/meta_searches/tags" => "meta_searches#tags", :as => "meta_searches_tags"
 
