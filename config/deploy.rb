danbooru/danbooru

commit 223e5f1e81d97748312d2838c9cd2bcd12318578

capistrano: enable conditional migrations.

config/deploy.rb
@@ -4,5 +4,11 @@ set :application, "danbooru"
 set :repo_url,  "git://github.com/r888888888/danbooru.git"
 set :deploy_to, "/var/www/danbooru2"
 set :rbenv_ruby, "2.5.1"
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle"
 set :branch, ENV.fetch("branch", "master")
+
+# skip migrations if files in db/migrate weren't modified
+set :conditionally_migrate, true
+
+# run migrations on the primary app server
+set :migration_role, :app

config/deploy/production.rb
@@ -1,11 +1,11 @@
 set :user, "danbooru"
 set :rails_env, "production"
-server "kagamihara", :roles => %w(web app db), :primary => true
+set :rbenv_path, "/home/danbooru/.rbenv"
+append :linked_files, ".env.production"
+
+server "kagamihara", :roles => %w(web app), :primary => true
 server "shima", :roles => %w(web app)
 server "saitou", :roles => %w(web app)
 server "oogaki", :roles => %w(worker)
-
-set :linked_files, fetch(:linked_files, []).push(".env.production")
-set :rbenv_path, "/home/danbooru/.rbenv"
 
 after "deploy:finished", "newrelic:notice_deployment"

commit 804ccd4fccae9136b1ac234141818998ecfcc447

capistrano: fix cronjobs not updating.
Bug: capistrano didn't update the crontab during deployment. As a result
the crontab still referenced an old deployment. Eventually that
deployment was deleted and cronjobs started silently failing.

Broken in 223e5f1e8. Turns out that `whenever` defaults to running on
servers with the `db` role. Removing kagamihara from the db role meant
that whenever was no longer configured to run anywhere.

config/deploy.rb
@@ -12,5 +12,7 @@ set :conditionally_migrate, true
 # run migrations on the primary app server
 set :migration_role, :app
 
+set :whenever_roles, :cron
+
 # how long unicorn:legacy_restart (used by deploy:rolling) waits until killing the old unicorn.
 set :unicorn_restart_sleep_time, 10

config/deploy/production.rb
@@ -2,7 +2,7 @@ set :user, "danbooru"
 set :rails_env, "production"
 append :linked_files, ".env.production"
 
-server "kagamihara", :roles => %w(web app), :primary => true
+server "kagamihara", :roles => %w(web app cron), :primary => true
 server "shima", :roles => %w(web app)
 server "saitou", :roles => %w(web app)
 server "oogaki", :roles => %w(worker)