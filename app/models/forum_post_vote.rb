4f5d80bb60c21a56ba9dbad765de54c11c53fac7
evazion <noizave@gmail.com>
2019-08-22 20:51:45 -0500

    forum post votes: fix conflicting scope name.
    Fixes a conflict with `Enumerable#excluding` in Rails 6.
    
        Rename Array#without and Enumerable#without to Array#excluding and
        Enumerable#excluding. Old method names are retained as aliases.

--- app/models/forum_post_vote.rb
+++ app/models/forum_post_vote.rb
@@ -6,7 +6,7 @@ class ForumPostVote < ApplicationRecord
   scope :up, -> {where(score: 1)}
   scope :down, -> {where(score: -1)}
   scope :by, ->(user_id) {where(creator_id: user_id)}
-  scope :excluding, ->(user_id) {where("creator_id <> ?", user_id)}
+  scope :excluding_user, ->(user_id) {where("creator_id <> ?", user_id)}
 
   def up?
     score == 1
