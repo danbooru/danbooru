danbooru/danbooru

commit dc4d2e54b23c56b296e280bd978b7657e33d8742

pools: stop using the pool_string field (#4160).
Stop using the pool_string field internally, but keep maintaining it
until we can drop it later.

* Stop using the pool_string for `pool:<name>` metatag searches.
* Stop using the pool_string in the `Post#pools` method. This is used to
  get the list of pools on post show pages.

app/models/pool.rb
@@ -112,14 +112,18 @@ class Pool < ApplicationRecord
     normalize_name(name).mb_chars.downcase
   end
 
-  def self.find_by_name(name)
+  def self.named(name)
     if name =~ /^\d+$/
-      where("pools.id = ?", name.to_i).first
+      where("pools.id = ?", name.to_i)
     elsif name
-      where_ilike(:name, normalize_name_for_search(name)).first
+      where_ilike(:name, normalize_name_for_search(name))
     else
       nil
     end
+  end
+
+  def self.find_by_name(name)
+    named(name).try(:first)
   end
 
   def versions

app/models/post.rb
@@ -1020,11 +1020,7 @@ class Post < ApplicationRecord
 
   module PoolMethods
     def pools
-      @pools ||= begin
-        return Pool.none if pool_string.blank?
-        pool_ids = pool_string.scan(/\d+/)
-        Pool.where(id: pool_ids).series_first
-      end
+      Pool.where("pools.post_ids && array[?]", id).series_first
     end
 
     def has_active_pools?

test/unit/post_test.rb
@@ -1948,17 +1948,28 @@ class PostTest < ActiveSupport::TestCase
     should "return posts for the pool:<name> metatag" do
       SqsService.any_instance.stubs(:send_message)
 
-      FactoryBot.create(:pool, name: "test_a", category: "series")
-      FactoryBot.create(:pool, name: "test_b", category: "collection")
-      post1 = FactoryBot.create(:post, tag_string: "pool:test_a")
-      post2 = FactoryBot.create(:post, tag_string: "pool:test_b")
+      pool1 = create(:pool, name: "test_a", category: "series")
+      pool2 = create(:pool, name: "test_b", category: "collection")
+      post1 = create(:post, tag_string: "pool:test_a")
+      post2 = create(:post, tag_string: "pool:test_b")
+
+      assert_tag_match([post1], "pool:#{pool1.id}")
+      assert_tag_match([post2], "pool:#{pool2.id}")
+
+      assert_tag_match([post1], "pool:TEST_A")
+      assert_tag_match([post2], "pool:Test_B")
 
       assert_tag_match([post1], "pool:test_a")
       assert_tag_match([post2], "-pool:test_a")
+
+      assert_tag_match([], "pool:test_a pool:test_b")
       assert_tag_match([], "-pool:test_a -pool:test_b")
+
       assert_tag_match([post2, post1], "pool:test*")
 
       assert_tag_match([post2, post1], "pool:any")
+      assert_tag_match([post2, post1], "-pool:none")
+      assert_tag_match([], "-pool:any")
       assert_tag_match([], "pool:none")
 
       assert_tag_match([post1], "pool:series")

app/logical/post_query_builder.rb
@@ -211,10 +211,38 @@ class PostQueryBuilder
       end
     end
 
-    if q[:pool] == "none"
-      relation = relation.where("posts.pool_string = ''")
-    elsif q[:pool] == "any"
-      relation = relation.where("posts.pool_string != ''")
+    q[:pool].to_a.each do |pool_name|
+      case pool_name
+      when "none"
+        relation = relation.where.not(id: Pool.select("unnest(post_ids)"))
+      when "any"
+        relation = relation.where(id: Pool.select("unnest(post_ids)"))
+      when "series"
+        relation = relation.where(id: Pool.series.select("unnest(post_ids)"))
+      when "collection"
+        relation = relation.where(id: Pool.collection.select("unnest(post_ids)"))
+      when /\*/
+        relation = relation.where(id: Pool.name_matches(pool_name).select("unnest(post_ids)"))
+      else
+        relation = relation.where(id: Pool.named(pool_name).select("unnest(post_ids)"))
+      end
+    end
+
+    q[:pool_neg].to_a.each do |pool_name|
+      case pool_name
+      when "none"
+        relation = relation.where(id: Pool.select("unnest(post_ids)"))
+      when "any"
+        relation = relation.where.not(id: Pool.select("unnest(post_ids)"))
+      when "series"
+        relation = relation.where.not(id: Pool.series.select("unnest(post_ids)"))
+      when "collection"
+        relation = relation.where.not(id: Pool.collection.select("unnest(post_ids)"))
+      when /\*/
+        relation = relation.where.not(id: Pool.name_matches(pool_name).select("unnest(post_ids)"))
+      else
+        relation = relation.where.not(id: Pool.named(pool_name).select("unnest(post_ids)"))
+      end
     end
 
     if q[:saved_searches]

app/models/tag.rb
@@ -619,37 +619,15 @@ class Tag < ApplicationRecord
             q[:disapproval_neg] << g2
 
           when "-pool"
-            if g2.downcase == "none"
-              q[:pool] = "any"
-            elsif g2.downcase == "any"
-              q[:pool] = "none"
-            elsif g2.downcase == "series"
-              q[:tags][:exclude] << "pool:series"
-            elsif g2.downcase == "collection"
-              q[:tags][:exclude] << "pool:collection"
-            else
-              q[:tags][:exclude] << "pool:#{Pool.name_to_id(g2)}"
-            end
+            q[:pool_neg] ||= []
+            q[:pool_neg] << g2
 
           when "pool"
-            if g2.downcase == "none"
-              q[:pool] = "none"
-            elsif g2.downcase == "any"
-              q[:pool] = "any"
-            elsif g2.downcase == "series"
-              q[:tags][:related] << "pool:series"
-            elsif g2.downcase == "collection"
-              q[:tags][:related] << "pool:collection"
-            elsif g2.include?("*")
-              pool_ids = Pool.search(name_matches: g2, order: "post_count").limit(Danbooru.config.tag_query_limit).pluck(:id)
-              q[:tags][:include] += pool_ids.map { |id| "pool:#{id}" }
-            else
-              q[:tags][:related] << "pool:#{Pool.name_to_id(g2)}"
-            end
+            q[:pool] ||= []
+            q[:pool] << g2
 
           when "ordpool"
             pool_id = Pool.name_to_id(g2)
-            q[:tags][:related] << "pool:#{pool_id}"
             q[:ordpool] = pool_id
 
           when "-favgroup"

test/functional/posts_controller_test.rb
@@ -143,6 +143,16 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
         assert_response :success
       end
 
+      context "with pools" do
+        should "render the pool list" do
+          as(@user) { @post.update(tag_string: "newpool:comic") }
+          get post_path(@post)
+
+          assert_response :success
+          assert_select "#pool-nav .pool-name", /Pool: comic/
+        end
+      end
+
       context "with only deleted comments" do
         setup do
           as(@user) { create(:comment, post: @post, is_deleted: true) }

commit 763ac1a7e04303d176fd18db187ced133f6dfd5e

pools: stop maintaining pool category pseudotags in pool strings (#4160)
Stop maintaining pool category pseudo tags (pool:series, pool:collection)
in pool strings. They're no longer used and the changes to the
`Post#pools` method in dc4d2e54b caused issues with this.

Also allow Members to change the category of large pools again. This was
only restricted because maintaining these pseudotags forced us to update
every post in the pool whenever a pool's category was changed.

app/jobs/update_pool_pseudo_tags_job.rb
@@ -1,8 +1,0 @@
-class UpdatePoolPseudoTagsJob < ApplicationJob
-  queue_as :default
-  queue_with_priority 20
-
-  def perform(pool)
-    pool.update_category_pseudo_tags_for_posts
-  end
-end

app/models/pool.rb
@@ -8,12 +8,10 @@ class Pool < ApplicationRecord
   validates_uniqueness_of :name, case_sensitive: false, if: :name_changed?
   validate :validate_name, if: :name_changed?
   validates_inclusion_of :category, :in => %w(series collection)
-  validate :updater_can_change_category
   validate :updater_can_remove_posts
   validate :updater_can_edit_deleted
   before_validation :normalize_post_ids
   before_validation :normalize_name
-  after_save :update_category_pseudo_tags_for_posts_async
   after_save :create_version
   after_create :synchronize!
 
@@ -292,30 +290,6 @@ class Pool < ApplicationRecord
 
   def creator_name
     creator.name
-  end
-
-  def update_category_pseudo_tags_for_posts_async
-    if saved_change_to_category?
-      UpdatePoolPseudoTagsJob.perform_later(self)
-    end
-  end
-
-  def update_category_pseudo_tags_for_posts
-    Post.where(id: post_ids).find_each do |post|
-      post.reload
-      post.set_pool_category_pseudo_tags
-      Post.where(:id => post.id).update_all(:pool_string => post.pool_string)
-    end
-  end
-
-  def category_changeable_by?(user)
-    user.is_builder? || (user.is_member? && post_count <= Danbooru.config.pool_category_change_limit)
-  end
-
-  def updater_can_change_category
-    if category_changed? && !category_changeable_by?(CurrentUser.user)
-      errors[:base] << "You cannot change the category of pools with greater than #{Danbooru.config.pool_category_change_limit} posts"
-    end
   end
 
   def validate_name

app/models/post.rb
@@ -30,7 +30,6 @@ class Post < ApplicationRecord
   validate :updater_can_change_rating
   before_save :update_tag_post_counts
   before_save :set_tag_counts
-  after_save :set_pool_category_pseudo_tags
   before_create :autoban
   after_save :create_version
   after_save :update_parent_on_save
@@ -1041,7 +1040,6 @@ class Post < ApplicationRecord
 
       with_lock do
         self.pool_string = "#{pool_string} pool:#{pool.id}".strip
-        set_pool_category_pseudo_tags
         update_column(:pool_string, pool_string) unless new_record?
         pool.add!(self)
       end
@@ -1053,7 +1051,6 @@ class Post < ApplicationRecord
 
       with_lock do
         self.pool_string = pool_string.gsub(/(?:\A| )pool:#{pool.id}(?:\Z| )/, " ").strip
-        set_pool_category_pseudo_tags
         update_column(:pool_string, pool_string) unless new_record?
         pool.remove!(self)
       end
@@ -1062,18 +1059,6 @@ class Post < ApplicationRecord
     def remove_from_all_pools
       pools.find_each do |pool|
         pool.remove!(self)
-      end
-    end
-
-    def set_pool_category_pseudo_tags
-      self.pool_string = (pool_string.split - ["pool:series", "pool:collection"]).join(" ")
-
-      pool_categories = pools.undeleted.pluck(:category)
-      if pool_categories.include?("series")
-        self.pool_string = "#{pool_string} pool:series".strip
-      end
-      if pool_categories.include?("collection")
-        self.pool_string = "#{pool_string} pool:collection".strip
       end
     end
   end

config/danbooru_default_config.rb
@@ -103,11 +103,6 @@ module Danbooru
       2
     end
 
-    # Members cannot change the category of pools with more than this many posts.
-    def pool_category_change_limit
-      100
-    end
-
     # Users cannot search for more than X regular tags at a time.
     def base_tag_query_limit
       6

test/unit/pool_test.rb
@@ -106,7 +106,7 @@ class PoolTest < ActiveSupport::TestCase
       assert_equal(@posts.map(&:id), @pool.post_ids)
 
       @posts.each(&:reload)
-      assert_equal(["pool:#{@pool.id} pool:series"] * @posts.size, @posts.map(&:pool_string))
+      assert_equal(["pool:#{@pool.id}"] * @posts.size, @posts.map(&:pool_string))
     end
   end
 
@@ -161,7 +161,7 @@ class PoolTest < ActiveSupport::TestCase
 
     should "update any new posts that were added" do
       @p1.reload
-      assert_equal("pool:#{@pool.id} pool:series", @p1.pool_string)
+      assert_equal("pool:#{@pool.id}", @p1.pool_string)
     end
   end
 
@@ -194,7 +194,7 @@ class PoolTest < ActiveSupport::TestCase
       end
 
       should "add the pool to the post" do
-        assert_equal("pool:#{@pool.id} pool:series", @p1.pool_string)
+        assert_equal("pool:#{@pool.id}", @p1.pool_string)
       end
 
       should "increment the post count" do
@@ -211,7 +211,7 @@ class PoolTest < ActiveSupport::TestCase
         end
 
         should "not double add the pool to the post" do
-          assert_equal("pool:#{@pool.id} pool:series", @p1.pool_string)
+          assert_equal("pool:#{@pool.id}", @p1.pool_string)
         end
 
         should "not double increment the post count" do
@@ -279,43 +279,12 @@ class PoolTest < ActiveSupport::TestCase
         end
 
         should "not affect the post" do
-          assert_equal("pool:#{@pool.id} pool:series", @p1.pool_string)
+          assert_equal("pool:#{@pool.id}", @p1.pool_string)
         end
 
         should "not affect the post count" do
           assert_equal(1, @pool.post_count)
         end
-      end
-    end
-
-    context "by changing the category" do
-      setup do
-        Danbooru.config.stubs(:pool_category_change_limit).returns(1)
-        @pool.add!(@p1)
-        @pool.add!(@p2)
-      end
-
-      teardown do
-        Danbooru.config.unstub(:pool_category_change_limit)
-      end
-
-      should "not allow Members to change the category of large pools" do
-        @member = FactoryBot.create(:member_user)
-        as(@member) { @pool.update(category: "collection") }
-
-        assert_equal(["You cannot change the category of pools with greater than 1 posts"], @pool.errors[:base])
-      end
-
-      should "allow Builders to change the category of large pools" do
-        perform_enqueued_jobs do
-          @builder = create(:builder_user)
-          as(@builder) { @pool.update(category: "collection") }
-        end
-
-        assert_equal(true, @pool.valid?)
-        assert_equal("collection", @pool.category)
-        assert_equal("pool:#{@pool.id} pool:collection", @p1.reload.pool_string)
-        assert_equal("pool:#{@pool.id} pool:collection", @p2.reload.pool_string)
       end
     end
 
@@ -406,7 +375,7 @@ class PoolTest < ActiveSupport::TestCase
         @p2.reload
         @p3.reload
         assert_equal("", @p1.pool_string)
-        assert_equal("pool:#{@pool.id} pool:series", @p2.pool_string)
+        assert_equal("pool:#{@pool.id}", @p2.pool_string)
         assert_equal("", @p3.pool_string)
       end
     end

test/unit/post_archive_test.rb
@@ -79,7 +79,7 @@ class PostArchiveTest < ActiveSupport::TestCase
 
       should "create a version" do
         assert_equal("tagme", @post.tag_string)
-        assert_equal("pool:#{@pool.id} pool:series", @post.pool_string)
+        assert_equal("pool:#{@pool.id}", @post.pool_string)
 
         assert_equal(1, @post.versions.size)
         assert_equal("tagme", @post.versions.last.tags)

test/unit/post_test.rb
@@ -863,7 +863,7 @@ class PostTest < ActiveSupport::TestCase
               @post.reload
               @pool.reload
               assert_equal([@post.id], @pool.post_ids)
-              assert_equal("pool:#{@pool.id} pool:series", @post.pool_string)
+              assert_equal("pool:#{@pool.id}", @post.pool_string)
             end
           end
 
@@ -894,7 +894,7 @@ class PostTest < ActiveSupport::TestCase
               @post.reload
               @pool.reload
               assert_equal([@post.id], @pool.post_ids)
-              assert_equal("pool:#{@pool.id} pool:series", @post.pool_string)
+              assert_equal("pool:#{@pool.id}", @post.pool_string)
             end
           end
 
@@ -909,7 +909,7 @@ class PostTest < ActiveSupport::TestCase
                 @post.reload
                 @pool.reload
                 assert_equal([@post.id], @pool.post_ids)
-                assert_equal("pool:#{@pool.id} pool:series", @post.pool_string)
+                assert_equal("pool:#{@pool.id}", @post.pool_string)
               end
             end
 
@@ -920,7 +920,7 @@ class PostTest < ActiveSupport::TestCase
                 @post.reload
                 assert_not_nil(@pool)
                 assert_equal([@post.id], @pool.post_ids)
-                assert_equal("pool:#{@pool.id} pool:series", @post.pool_string)
+                assert_equal("pool:#{@pool.id}", @post.pool_string)
               end
             end
 
@@ -1768,10 +1768,10 @@ class PostTest < ActiveSupport::TestCase
         pool = FactoryBot.create(:pool)
         post.add_pool!(pool)
         post.reload
-        assert_equal("pool:#{pool.id} pool:series", post.pool_string)
+        assert_equal("pool:#{pool.id}", post.pool_string)
         post.add_pool!(pool)
         post.reload
-        assert_equal("pool:#{pool.id} pool:series", post.pool_string)
+        assert_equal("pool:#{pool.id}", post.pool_string)
         post.remove_pool!(pool)
         post.reload
         assert_equal("", post.pool_string)