312606bf98818363627c1efa2efae5f478ee7075
evazion <noizave@gmail.com>
2019-09-09 11:51:01 -0500

    Fix #4137: Share cookies across subdomains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_danbooru_session'
+Rails.application.config.session_store :cookie_store, key: '_danbooru2_session', domain: :all, tld_length: 2
