a5043245b6de6d44bf37f5eadb77c90fe9f3e893
evazion <noizave@gmail.com>
2019-09-10 21:51:14 -0500

    views: migrate related tags, mass edits to simple form.

--- app/controllers/related_tags_controller.rb
+++ app/controllers/related_tags_controller.rb
@@ -2,7 +2,10 @@ class RelatedTagsController < ApplicationController
   respond_to :json, :xml, :js, :html
 
   def show
-    @query = RelatedTagQuery.new(query: params[:query], category: params[:category], user: CurrentUser.user)
+    query = params[:query] || search_params[:query]
+    category = params[:category] || search_params[:category]
+
+    @query = RelatedTagQuery.new(query: query, category: category, user: CurrentUser.user)
     respond_with(@query)
   end
 end
