31ebfa0a8288755ee4024b01cbf036f186c55fa5
evazion <noizave@gmail.com>
2019-09-17 00:28:41 -0500

    css: standardize styling of fineprint text.
    Fineprint text was variously styled with `.info`, `.tn`, `.hint`, or
    `.cost-footnote` css classes. Standardize on `.fineprint` instead. Use
    `.hint` only for form hints and `.tn` only for <tn> tags in translation
    notes.
    
    Incidentally changes the font size of form hints to 0.8em (was 0.7em)
    and the color of fineprint to #888 (was #AAA or #666).

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -5,32 +5,32 @@ module TagsHelper
     html = ""
 
     if tag.antecedent_alias
-      html << "<p class='hint'>This tag has been aliased to "
+      html << "<p class='fineprint'>This tag has been aliased to "
       html << link_to(tag.antecedent_alias.consequent_name, show_or_new_wiki_pages_path(:title => tag.antecedent_alias.consequent_name))
       html << " (#{link_to "learn more", wiki_pages_path(title: "help:tag_aliases")}).</p>"
     end
 
     if tag.consequent_aliases.present?
-      html << "<p class='hint'>The following tags are aliased to this tag: "
+      html << "<p class='fineprint'>The following tags are aliased to this tag: "
       html << raw(tag.consequent_aliases.map {|x| link_to(x.antecedent_name, show_or_new_wiki_pages_path(:title => x.antecedent_name))}.join(", "))
       html << " (#{link_to "learn more", wiki_pages_path(title: "help:tag_aliases")}).</p>"
     end
 
     automatic_tags = TagImplication.automatic_tags_for([tag.name])
     if automatic_tags.present?
-      html << "<p class='hint'>This tag automatically adds "
+      html << "<p class='fineprint'>This tag automatically adds "
       html << raw(automatic_tags.map {|x| link_to(x, show_or_new_wiki_pages_path(:title => x))}.join(", "))
       html << " (#{link_to "learn more", wiki_pages_path(title: "help:autotags")}).</p>"
     end
 
     if tag.antecedent_implications.present?
-      html << "<p class='hint'>This tag implicates "
+      html << "<p class='fineprint'>This tag implicates "
       html << raw(tag.antecedent_implications.map {|x| link_to(x.consequent_name, show_or_new_wiki_pages_path(:title => x.consequent_name))}.join(", "))
       html << " (#{link_to "learn more", wiki_pages_path(title: "help:tag_implications")}).</p>"
     end
 
     if tag.consequent_implications.present?
-      html << "<p class='hint'>The following tags implicate this tag: "
+      html << "<p class='fineprint'>The following tags implicate this tag: "
       html << raw(tag.consequent_implications.map {|x| link_to(x.antecedent_name, show_or_new_wiki_pages_path(:title => x.antecedent_name))}.join(", "))
       html << " (#{link_to "learn more", wiki_pages_path(title: "help:tag_implications")}).</p>"
     end
