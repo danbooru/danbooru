312adb772d0b7a18d35d262b2846563548a73d85
evazion <noizave@gmail.com>
2019-09-18 03:20:26 -0500

    views: refactor post flag and appeal reasons.
    * Prefer partials over helpers.
    * Add css classes to flag/appeal reason lists.
    * Wrap dtext in span.prose container.

--- app/controllers/moderator/dashboards_controller.rb
+++ app/controllers/moderator/dashboards_controller.rb
@@ -1,7 +1,6 @@
 module Moderator
   class DashboardsController < ApplicationController
     before_action :member_only
-    helper :post_flags, :post_appeals
 
     def show
       @dashboard = Moderator::Dashboard::Report.new(**search_params.to_h.symbolize_keys)
