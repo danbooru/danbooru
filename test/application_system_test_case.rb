9ba46105ddbe79e3e56a9f9423fe04c2bd2a862a
evazion <noizave@gmail.com>
2019-09-29 14:29:28 -0500

    tests: skip system tests when firefox isn't installed.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,4 +3,8 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include SystemTestHelper
   driven_by :selenium, using: :headless_firefox, screen_size: [1400, 1400]
+
+  setup do
+    skip "Firefox not installed" unless system("firefox --version > /dev/null")
+  end
 end
