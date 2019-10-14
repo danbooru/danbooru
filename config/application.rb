3e378697689098b959bd0d111ce54c9c937f895d
evazion <noizave@gmail.com>
2019-10-14 13:50:31 -0500

    rails: disable asset pipeline.
    Disable the assets pipeline (Sprockets). Sprockets errors out now after
    upgrading to Sprockets 4 because of missing config files. We don't use
    it any more after switching to Webpack, so we can disable it entirely.
    
    Also disable a few more Rails features that we don't use (ActiveStorage,
    ActionCable, ActionMailbox, ActionText).

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,16 @@
 require_relative 'boot'
-require 'rails/all'
+require "rails"
+require "active_record/railtie"
+#require "active_storage/engine"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+#require "action_cable/engine"
+#require "action_mailbox/engine"
+#require "action_text/engine"
+require "rails/test_unit/railtie"
+#require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
 
@@ -16,8 +27,8 @@ module Danbooru
     config.active_record.schema_format = :sql
     config.encoding = "utf-8"
     config.filter_parameters += [:password]
-    config.assets.enabled = true
-    config.assets.version = '1.0'
+    #config.assets.enabled = true
+    #config.assets.version = '1.0'
     config.autoload_paths += %W(#{config.root}/app/presenters #{config.root}/app/logical #{config.root}/app/mailers)
     config.plugins = [:all]
     config.time_zone = 'Eastern Time (US & Canada)'
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -29,11 +29,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = true
+  # config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
