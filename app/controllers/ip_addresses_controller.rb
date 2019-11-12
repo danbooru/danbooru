68e1140b0d75f9a808344ec2842fe14a6aa2db24
evazion <noizave@gmail.com>
2019-11-11 19:41:09 -0600

    /ip_addresses: allow grouping IPs by subnet.

--- app/controllers/ip_addresses_controller.rb
+++ app/controllers/ip_addresses_controller.rb
@@ -4,10 +4,10 @@ class IpAddressesController < ApplicationController
 
   def index
     if search_params[:group_by] == "ip_addr"
-      @ip_addresses = IpAddress.search(search_params).group_by_ip_addr
+      @ip_addresses = IpAddress.search(search_params).group_by_ip_addr(search_params[:ipv4_masklen], search_params[:ipv6_masklen]).paginate(params[:page], limit: params[:limit] || 1000)
       respond_with(@ip_addresses)
     elsif search_params[:group_by] == "user"
-      @ip_addresses = IpAddress.search(search_params).group_by_user
+      @ip_addresses = IpAddress.includes(:user).search(search_params).group_by_user.paginate(params[:page], limit: params[:limit] || 1000)
       respond_with(@ip_addresses)
     else
       @ip_addresses = IpAddress.includes(:user, :model).paginated_search(params)
--- app/models/ip_address.rb
+++ app/models/ip_address.rb
@@ -14,12 +14,29 @@ class IpAddress < ApplicationRecord
     q.order(created_at: :desc)
   end
 
-  def self.group_by_ip_addr
-    group(:ip_addr).select("ip_addr, COUNT(*) AS count_all").reorder("count_all DESC, ip_addr")
+  def self.group_by_ip_addr(ipv4_masklen = nil, ipv6_masklen = nil)
+    ipv4_masklen ||= 32
+    ipv6_masklen ||= 128
+
+    q = select(sanitize_sql([<<~SQL, ipv4_masklen, ipv6_masklen]))
+      CASE
+        WHEN family(ip_addr) = 4
+        THEN network(set_masklen(ip_addr, ?))
+        ELSE network(set_masklen(ip_addr, ?))
+      END AS ip_addr,
+      COUNT(*) AS count_all
+    SQL
+
+    q.group("1").reorder("count_all DESC, ip_addr")
   end
 
   def self.group_by_user
     group(:user_id).select("user_id, COUNT(*) AS count_all").reorder("count_all DESC, user_id")
+  end
+
+  def to_s
+    # include the subnet mask only when the IP denotes a subnet.
+    ip_addr.size > 1 ? ip_addr.to_string : ip_addr.to_s
   end
 
   def readonly?
