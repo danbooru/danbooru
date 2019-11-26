f8e109f6dcd39e1d17df2b76a394debeb3c92108
evazion <noizave@gmail.com>
2019-11-26 01:48:19 -0600

    Update gems and yarn packages.
    Remove *.scss.erb loader because it's now supported natively by
    webpacker.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,18 +2,6 @@ const { environment } = require('@rails/webpacker')
 const erb =  require('./loaders/erb')
 const webpack = require('webpack');
 
-environment.loaders.append('scss.erb', {
-  test: /\.scss\.erb$/,
-  enforce: 'pre',
-  exclude: /node_modules/,
-  use: [
-    'style-loader',
-    'postcss-loader',
-    'sass-loader',
-    'rails-erb-loader'
-  ]
-});
-
 environment.loaders.append('erb', erb);
 
 environment.config.output.library = ["Danbooru"];
