danbooru/danbooru

commit 3d3f615591ebb3c69606222870853567d0070d6f

config: refactor restricted tag settings.

app/models/post.rb
@@ -1794,11 +1794,11 @@ class Post < ApplicationRecord
   has_bit_flags BOOLEAN_ATTRIBUTES
 
   def safeblocked?
-    CurrentUser.safe_mode? && (rating != "s" || has_tag?("toddlercon|toddler|diaper|tentacle|rape|bestiality|beastiality|lolita|loli|nude|shota|pussy|penis"))
+    CurrentUser.safe_mode? && (rating != "s" || Danbooru.config.safe_mode_restricted_tags.any? { |tag| tag.in?(tag_array) })
   end
 
   def levelblocked?
-    !Danbooru.config.can_user_see_post?(CurrentUser.user, self)
+    !CurrentUser.is_gold? && Danbooru.config.restricted_tags.any? { |tag| tag.in?(tag_array) }
   end
 
   def banblocked?
@@ -1806,10 +1806,7 @@ class Post < ApplicationRecord
   end
 
   def visible?
-    return false if safeblocked?
-    return false if levelblocked?
-    return false if banblocked?
-    return true
+    !safeblocked? && !levelblocked? && !banblocked?
   end
 
   def reload(options = nil)

config/danbooru_default_config.rb
@@ -350,20 +350,14 @@ module Danbooru
       20
     end
 
-    def is_post_restricted?(post)
-      false
+    # Tags that are not visible in safe mode.
+    def safe_mode_restricted_tags
+      restricted_tags + %w[censored condom nipples nude penis pussy sexually_suggestive]
     end
 
-    def is_user_restricted?(user)
-      !user.is_gold?
-    end
-
-    def can_user_see_post?(user, post)
-      if is_user_restricted?(user) && is_post_restricted?(post)
-        false
-      else
-        true
-      end
+    # Tags that are only visible to Gold+ users.
+    def restricted_tags
+      []
     end
 
     def max_appeals_per_day

commit fc06a2a0f76e212ac5dc1c311cea3229a3b1f613

posts: fix members not being able to tag posts.
Fix regression caused by 3d3f61559. This is what happened:

* The posts controller calls post.visible? before post.update.
* post.visible? calls post.tag_array.
* The call to tag_array causes the current value of tag_string to be
  cached in @tag_array.
* post.update calls post.merge_old_changes.
* post.merge_old_changes accesses tag_array, which contains the old
  cached version of tag_string rather than the new version from the
  update.
* post.merge_old_changes detects no changes, so the update does nothing.
* This only happens for Members because for Gold+ users the call to
  post.visible? short circuits before accessing tag_array.

Moral of the story: cache invalidation is hard. Don't cache unless you have to.

app/models/post.rb
@@ -589,6 +589,7 @@ class Post < ApplicationRecord
     end
 
     def merge_old_changes
+      reset_tag_array_cache
       @removed_tags = []
 
       if old_tag_string