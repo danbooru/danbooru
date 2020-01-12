abacc8ebb1823546680fa54ce0b1b22fa0d9a7a7
evazion <noizave@gmail.com>
2020-01-11 20:01:14 -0600

    views: convert helpers to partials.

--- app/helpers/wiki_pages_helper.rb
+++ app/helpers/wiki_pages_helper.rb
@@ -1,22 +1,4 @@
 module WikiPagesHelper
-  def wiki_page_alias_and_implication_list(wiki_page)
-    alias_and_implication_list(wiki_page.tag)
-  end
-
-  def wiki_page_post_previews(wiki_page)
-    html = '<div id="wiki-page-posts">'
-
-    if Post.fast_count(wiki_page.title) > 0
-      full_link = link_to("view all", posts_path(:tags => wiki_page.title))
-      html << "<h2>Posts (#{full_link})</h2>"
-      html << wiki_page.post_set.presenter.post_previews_html(self)
-    end
-
-    html << "</div>"
-
-    html.html_safe
-  end
-
   def wiki_page_other_names_list(wiki_page)
     names_html = wiki_page.other_names.map {|name| link_to(name, "http://www.pixiv.net/search.php?s_mode=s_tag_full&word=#{u(name)}", :class => "wiki-other-name")}
     names_html.join(" ").html_safe
