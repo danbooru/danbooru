b2d12e2e20fe71743d12dd04e13548d19ac71c83
BrokenEagle <brokeneagle98@yahoo.com>
2020-01-17 06:08:13 +0000

    Remove posts not shown for blank message check

--- app/presenters/post_set_presenters/base.rb
+++ app/presenters/post_set_presenters/base.rb
@@ -6,8 +6,7 @@ module PostSetPresenters
 
     def post_previews_html(template, options = {})
       html = ""
-
-      if posts.empty?
+      if none_shown(options)
         return template.render("post_sets/blank")
       end
 
@@ -17,6 +16,14 @@ module PostSetPresenters
       end
 
       html.html_safe
+    end
+
+    def not_shown(post, options)
+      !options[:show_deleted] && post.is_deleted? && @post_set.tag_string !~ /status:(?:all|any|deleted|banned)/ && !@post_set.raw
+    end
+
+    def none_shown(options)
+      posts.reject {|post| not_shown(post, options) }.empty?
     end
   end
 end
