danbooru/danbooru

commit b4ce2d83a66c993ff71f307816d6b54ddd5039ee

models: remove belongs_to_creator macro.
The belongs_to_creator macro was used to initialize the creator_id field
to the CurrentUser. This made tests complicated because it meant you had
to create and set the current user every time you wanted to create an
object, when lead to the current user being set over and over again. It
also meant you had to constantly be aware of what the CurrentUser was in
many different contexts, which was often confusing. Setting creators
explicitly simplifies everything greatly.

app/models/bulk_update_request.rb
@@ -15,7 +15,7 @@ class BulkUpdateRequest < ApplicationRecord
   validate :validate_script, :on => :create
   before_validation :initialize_attributes, :on => :create
   before_validation :normalize_text
-  after_create :create_forum_topic
+  before_create :create_forum_topic
   after_save :update_notice
 
   scope :pending_first, -> { order(Arel.sql("(case status when 'pending' then 0 when 'approved' then 1 else 2 end)")) }
@@ -94,12 +94,9 @@ class BulkUpdateRequest < ApplicationRecord
     end
 
     def create_forum_topic
-      if forum_topic_id
-        forum_post = forum_topic.posts.create(body: reason_with_link)
-        update(forum_post_id: forum_post.id)
-      else
-        forum_topic = ForumTopic.create(title: title, category_id: 1, original_post_attributes: {body: reason_with_link})
-        update(forum_topic_id: forum_topic.id, forum_post_id: forum_topic.posts.first.id)
+      CurrentUser.as(user) do
+        self.forum_topic = ForumTopic.create(title: title, category_id: 1, creator: user) unless forum_topic.present?
+        self.forum_post = forum_topic.posts.create(body: reason_with_link, creator: user) unless forum_post.present?
       end
     end
 

test/unit/bulk_update_request_test.rb
@@ -37,7 +37,7 @@ class BulkUpdateRequestTest < ActiveSupport::TestCase
 
     context "#update_notice" do
       setup do
-        @forum_topic = FactoryBot.create(:forum_topic)
+        @forum_topic = create(:forum_topic, creator: @admin)
       end
 
       should "update the cache" do
@@ -70,7 +70,7 @@ class BulkUpdateRequestTest < ActiveSupport::TestCase
           mass update aaa -> bbb
         '
 
-        @bur = FactoryBot.create(:bulk_update_request, :script => @script)
+        @bur = create(:bulk_update_request, script: @script, user: @admin)
         @bur.approve!(@admin)
 
         assert_enqueued_jobs(3)
@@ -111,7 +111,7 @@ class BulkUpdateRequestTest < ActiveSupport::TestCase
 
     context "that has an invalid alias" do
       setup do
-        @alias1 = FactoryBot.create(:tag_alias)
+        @alias1 = create(:tag_alias, creator: @admin)
         @req = FactoryBot.build(:bulk_update_request, :script => "create alias bbb -> aaa")
       end
 
@@ -172,8 +172,8 @@ class BulkUpdateRequestTest < ActiveSupport::TestCase
 
     context "with an associated forum topic" do
       setup do
-        @topic = FactoryBot.create(:forum_topic, :title => "[bulk] hoge")
-        @post = FactoryBot.create(:forum_post, :topic_id => @topic.id)
+        @topic = create(:forum_topic, title: "[bulk] hoge", creator: @admin)
+        @post = create(:forum_post, topic: @topic, creator: @admin)
         @req = FactoryBot.create(:bulk_update_request, :script => "create alias AAA -> BBB", :forum_topic_id => @topic.id, :forum_post_id => @post.id, :title => "[bulk] hoge")
       end
 

test/factories/bulk_update_request.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory(:bulk_update_request) do |f|
+    user
     title {"xxx"}
     script {"create alias aaa -> bbb"}
     skip_secondary_validations {true}

commit 1aadd1966306c8ac577ab7f412c42309ba48d844

BURs: fix broken [bur:<id>] links in forum posts.
Fix bulk update requests generating invalid [bur:<id>] links in forum
posts. The id was missing because the BUR created the forum topic in a
before_create hook, which created the post before the BUR was saved so
the BUR didn't have an id yet. Fix regression caused by b4ce2d83.

app/models/bulk_update_request.rb
@@ -15,7 +15,7 @@ class BulkUpdateRequest < ApplicationRecord
   validate :validate_script, :on => :create
   before_validation :initialize_attributes, :on => :create
   before_validation :normalize_text
-  before_create :create_forum_topic
+  after_create :create_forum_topic
   after_save :update_notice
 
   scope :pending_first, -> { order(Arel.sql("(case status when 'pending' then 0 when 'approved' then 1 else 2 end)")) }
@@ -97,6 +97,7 @@ class BulkUpdateRequest < ApplicationRecord
       CurrentUser.as(user) do
         self.forum_topic = ForumTopic.create(title: title, category_id: 1, creator: user) unless forum_topic.present?
         self.forum_post = forum_topic.posts.create(body: reason_with_link, creator: user) unless forum_post.present?
+        save
       end
     end
 

test/unit/bulk_update_request_test.rb
@@ -104,9 +104,11 @@ class BulkUpdateRequestTest < ActiveSupport::TestCase
     end
 
     should "create a forum topic" do
-      assert_difference("ForumTopic.count", 1) do
-        BulkUpdateRequest.create(:title => "abc", :reason => "zzz", :script => "create alias aaa -> bbb", :skip_secondary_validations => true)
-      end
+      bur = create(:bulk_update_request, reason: "zzz", script: "create alias aaa -> bbb")
+
+      assert_equal(true, bur.forum_post.present?)
+      assert_match(/\[bur:#{bur.id}\]/, bur.forum_post.body)
+      assert_match(/zzz/, bur.forum_post.body)
     end
 
     context "that has an invalid alias" do