24ae35aa96a3eec7bd5bc1a4a881877e8c64438a
BrokenEagle <brokeneagle98@yahoo.com>
2020-02-15 22:04:47 +0000

    Add BUR counters to forum topics index

--- app/helpers/forum_topics_helper.rb
+++ app/helpers/forum_topics_helper.rb
@@ -10,4 +10,12 @@ module ForumTopicsHelper
   def new_forum_topic?(topic, read_forum_topics)
     !read_forum_topics.map(&:id).include?(topic.id)
   end
+
+  def bulk_update_request_counts(topic)
+    requests = []
+    requests << %(<span class="topic-pending-burs">Pending: #{topic.pending_bur_count}</span>) if topic.pending_bur_count.positive?
+    requests << %(<span class="topic-approved-burs">Approved: #{topic.approved_bur_count}</span>) if topic.approved_bur_count.positive?
+    requests << %(<span class="topic-rejected-burs">Rejected: #{topic.rejected_bur_count}</span>) if topic.rejected_bur_count.positive?
+    requests.join(" | ").html_safe
+  end
 end
