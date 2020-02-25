71611f28a93050e47b528fc86b8cfc1cb1d89d16
BrokenEagle <brokeneagle98@yahoo.com>
2020-02-25 17:33:58 +0000

    Fix exception on feedback caused by the visible function

--- app/controllers/user_feedbacks_controller.rb
+++ app/controllers/user_feedbacks_controller.rb
@@ -8,18 +8,18 @@ class UserFeedbacksController < ApplicationController
   end
 
   def edit
-    @user_feedback = UserFeedback.visible.find(params[:id])
+    @user_feedback = UserFeedback.visible(CurrentUser.user).find(params[:id])
     check_privilege(@user_feedback)
     respond_with(@user_feedback)
   end
 
   def show
-    @user_feedback = UserFeedback.visible.find(params[:id])
+    @user_feedback = UserFeedback.visible(CurrentUser.user).find(params[:id])
     respond_with(@user_feedback)
   end
 
   def index
-    @user_feedbacks = UserFeedback.visible(CurrentUser.user).paginated_search(params, count_pages: true)
+    @user_feedbacks = UserFeedback.visible.paginated_search(params, count_pages: true)
     @user_feedbacks = @user_feedbacks.includes(:user, :creator) if request.format.html?
 
     respond_with(@user_feedbacks)
@@ -31,7 +31,7 @@ class UserFeedbacksController < ApplicationController
   end
 
   def update
-    @user_feedback = UserFeedback.visible.find(params[:id])
+    @user_feedback = UserFeedback.visible(CurrentUser.user).find(params[:id])
     check_privilege(@user_feedback)
     @user_feedback.update(user_feedback_params(:update, @user_feedback))
     respond_with(@user_feedback)
