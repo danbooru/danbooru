danbooru/danbooru

commit 57ba5c59a43b6b2bf32a182d97bd23fc2933d2e8

tag_aliases/index: fix tag category search options excluding empty tags.
Fix Tag.search to not filter out empty tags by default. Set the default
in the tags controller instead.

Fixes various search pages, namely aliases/implications/wiki pages, that
rely on Tag.search and that don't expect it to filter tags out by default.

app/models/tag.rb
@@ -847,8 +847,8 @@ class Tag < ApplicationRecord
         q = q.name_or_alias_matches(params[:name_or_alias_matches])
       end
 
-      if params[:hide_empty].blank? || params[:hide_empty].to_s.truthy?
-        q = q.where("post_count > 0")
+      if params[:hide_empty].to_s.truthy?
+        q = q.nonempty
       end
 
       if params[:has_wiki].to_s.truthy?

app/controllers/tags_controller.rb
@@ -9,7 +9,7 @@ class TagsController < ApplicationController
   end
 
   def index
-    @tags = Tag.paginated_search(params)
+    @tags = Tag.paginated_search(params, hide_empty: true)
     @tags = @tags.includes(:consequent_aliases) if request.format.html?
     respond_with(@tags)
   end

commit ea96295940f00a660351052112114d0561e92e15

autocomplete: fix autocomplete returning empty tags.
Fix regression caused by 57ba5c59. Autocomplete implicitly relied on
Tag.search filtering out nonempty tags by default.

app/models/tag.rb
@@ -349,8 +349,11 @@ class Tag < ApplicationRecord
 
       query1 =
         Tag
+        .nonempty
         .select("tags.name, tags.post_count, tags.category, null AS antecedent_name")
-        .search(:name_matches => wildcard_name, :order => "count").limit(limit)
+        .name_matches(wildcard_name)
+        .order(post_count: :desc)
+        .limit(limit)
 
       query2 =
         TagAlias