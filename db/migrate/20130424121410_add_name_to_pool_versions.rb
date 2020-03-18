96085f17dd1b8ec8a683218a5232271ec7d30ed0
evazion <noizave@gmail.com>
2020-03-18 03:32:40 -0500

    Fix #4334: db:migrate hangs.
    Possible fix for #4334. Running fix scripts inside migrations is bad practice.

--- db/migrate/20130424121410_add_name_to_pool_versions.rb
+++ db/migrate/20130424121410_add_name_to_pool_versions.rb
@@ -2,8 +2,5 @@ class AddNameToPoolVersions < ActiveRecord::Migration[4.2]
   def change
     execute("set statement_timeout = 0")
     add_column :pool_versions, :name, :string
-    PoolVersion.find_each do |pool_version|
-      pool_version.update_column(:name, pool_version.pool.name)
-    end
   end
 end
