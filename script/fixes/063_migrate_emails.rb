f7175d3b125ee891158fab4fc1ea9f3659dccaf6
evazion <noizave@gmail.com>
2020-03-18 15:11:32 -0500

    emails: fix migrate emails fix script.

--- script/fixes/063_migrate_emails.rb
+++ script/fixes/063_migrate_emails.rb
@@ -2,8 +2,11 @@
 
 require_relative "../../config/environment"
 
-User.where.not(email: nil).find_each.with_index do |user, n|
-  email = EmailAddress.new(user: user, address: user.email, is_verified: true)
-  email.save(validate: false)
-  puts "n=#{n} id=#{user.id} name=#{user.name} email=#{user.email} normalized_address=#{email.normalized_address}"
+EmailAddress.transaction do
+  User.where.not(email: nil).find_each.with_index do |user, n|
+    email = EmailAddress.new(user: user, address: user.email, is_verified: true)
+    email.normalized_address = email.normalized_address.to_s
+    email.save(validate: false)
+    puts "n=#{n} id=#{user.id} name=#{user.name} email=#{user.email} normalized_address=#{email.normalized_address}"
+  end
 end
