92aeb8728f34ead6d20fcbb8893c828d91fe7818
evazion <noizave@gmail.com>
2020-03-18 01:36:54 -0500

    pundit: convert ip bans to pundit.

--- test/functional/ip_bans_controller_test.rb
+++ test/functional/ip_bans_controller_test.rb
@@ -4,6 +4,7 @@ class IpBansControllerTest < ActionDispatch::IntegrationTest
   context "The ip bans controller" do
     setup do
       @admin = create(:admin_user)
+      @ip_ban = create(:ip_ban)
     end
 
     context "new action" do
@@ -17,17 +18,12 @@ class IpBansControllerTest < ActionDispatch::IntegrationTest
       should "create a new ip ban" do
         assert_difference("IpBan.count", 1) do
           post_auth ip_bans_path, @admin, params: {:ip_ban => {:ip_addr => "1.2.3.4", :reason => "xyz"}}
+          assert_response :redirect
         end
       end
     end
 
     context "index action" do
-      setup do
-        as(@admin) do
-          create(:ip_ban)
-        end
-      end
-
       should "render" do
         get_auth ip_bans_path, @admin
         assert_response :success
@@ -42,15 +38,10 @@ class IpBansControllerTest < ActionDispatch::IntegrationTest
     end
 
     context "destroy action" do
-      setup do
-        as(@admin) do
-          @ip_ban = create(:ip_ban)
-        end
-      end
-
       should "destroy an ip ban" do
         assert_difference("IpBan.count", -1) do
           delete_auth ip_ban_path(@ip_ban), @admin, params: {:format => "js"}
+          assert_response :success
         end
       end
     end
