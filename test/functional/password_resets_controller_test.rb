8aac108e6bb9d5dd2089bb157779ba516e04ffe2
evazion <noizave@gmail.com>
2020-03-20 15:40:51 -0500

    tests: fix password reset tests.

--- test/functional/password_resets_controller_test.rb
+++ test/functional/password_resets_controller_test.rb
@@ -2,10 +2,6 @@ require 'test_helper'
 
 class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
   context "The passwords resets controller" do
-    setup do
-      @user = create(:user)
-    end
-
     context "show action" do
       should "work" do
         get password_reset_path
@@ -14,11 +10,20 @@ class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
     end
 
     context "create action" do
-      should "work" do
+      should "should send a password reset email if the user has a verified email address" do
+        @user = create(:user, email_address: build(:email_address))
         post password_reset_path, params: { user: { name: @user.name } }
 
         assert_redirected_to new_session_path
         assert_enqueued_email_with UserMailer, :password_reset, args: [@user]
+      end
+
+      should "should fail if the user doesn't have a verified email address" do
+        @user = create(:user)
+        post password_reset_path, params: { user: { name: @user.name } }
+
+        assert_redirected_to @user
+        assert_no_enqueued_emails
       end
     end
   end
