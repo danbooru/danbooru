15ba2f6cd7c10181b1c75f700f3186553353a5e3
evazion <noizave@gmail.com>
2020-03-19 19:06:29 -0500

    tests: fix email delivery tests.
    Setting deliver_later_queue_name inside config/application.rb broke
    tests because assert_enqueued_email_with assumes that the deliver_later
    queue is called `mailers`.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.deliver_later_queue_name = :default
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = [I18n.default_locale]
