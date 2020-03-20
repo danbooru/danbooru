ab5432d149a5661471ad28b2fc4411033b24bb52
evazion <noizave@gmail.com>
2020-03-19 19:31:57 -0500

    pundit: convert pools to pundit.

--- app/controllers/pool_orders_controller.rb
+++ app/controllers/pool_orders_controller.rb
@@ -1,9 +1,8 @@
 class PoolOrdersController < ApplicationController
   respond_to :html, :xml, :json, :js
-  before_action :member_only
 
   def edit
-    @pool = Pool.find(params[:pool_id])
+    @pool = authorize Pool.find(params[:pool_id])
     respond_with(@pool)
   end
 end
