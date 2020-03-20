1344d4c161b6c9523b6b2ca60499512cd55445ff
evazion <noizave@gmail.com>
2020-03-20 02:01:15 -0500

    pundit: convert moderator/dashboard to pundit.
    Make it visible to everyone since there's no non-public information here.

--- app/controllers/moderator/dashboards_controller.rb
+++ app/controllers/moderator/dashboards_controller.rb
@@ -1,7 +1,5 @@
 module Moderator
   class DashboardsController < ApplicationController
-    before_action :member_only
-
     def show
       @dashboard = Moderator::Dashboard::Report.new(**search_params.to_h.symbolize_keys)
     end
