99e7e359af451f82d91fcf7ab6157ebc714a8d32
evazion <noizave@gmail.com>
2020-04-17 00:13:06 -0500

    flags/appeals: limit flag/appeal reasons to 140 characters.

--- app/models/post_appeal.rb
+++ app/models/post_appeal.rb
@@ -6,6 +6,7 @@ class PostAppeal < ApplicationRecord
   belongs_to :creator, :class_name => "User"
   belongs_to :post
   validates_presence_of :reason
+  validates :reason, presence: true, length: { in: 1..140 }
   validate :validate_post_is_inactive
   validate :validate_creator_is_not_limited
   validates_uniqueness_of :creator_id, :scope => :post_id, :message => "have already appealed this post"
--- app/models/post_flag.rb
+++ app/models/post_flag.rb
@@ -10,7 +10,7 @@ class PostFlag < ApplicationRecord
 
   belongs_to :creator, class_name: "User"
   belongs_to :post
-  validates_presence_of :reason
+  validates :reason, presence: true, length: { in: 1..140 }
   validate :validate_creator_is_not_limited, on: :create
   validate :validate_post
   validates_uniqueness_of :creator_id, :scope => :post_id, :on => :create, :unless => :is_deletion, :message => "have already flagged this post"
