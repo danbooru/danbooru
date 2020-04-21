danbooru/danbooru

commit 6ca42947bd2cf63828ba68e96c41d987d5ee6b94

search: support quoted values for all metatags.
Support using quoted values with all metatags. For example: user:"blah blah",
pool:"blah blah", commentary:"blah blah", etc. Things like rating:"safe",
id:"42" also work. Both single and double quotes are supported.

Also make the status: and rating: metatags fully free. Before only
status:deleted and rating:s were free.

app/logical/post_query_builder.rb
@@ -1,3 +1,5 @@
+require "strscan"
+
 class PostQueryBuilder
   COUNT_METATAGS = %w[
     comment_count deleted_comment_count active_comment_count
@@ -752,16 +754,44 @@ class PostQueryBuilder
 
   concerning :ParseMethods do
     class_methods do
-      def scan_query(query, strip_metatags: false)
-        tagstr = query.to_s.gsub(/\u3000/, " ").strip
-        list = tagstr.scan(/-?(?:source|commentary):".*?"/) || []
-        list += tagstr.gsub(/-?(?:source|commentary):".*?"/, "").scan(/[^[:space:]]+/).uniq
-        list = list.map { |tag| tag.sub(/^[-~]/, "") } if strip_metatags
-        list
+      def scan_query(query)
+        terms = []
+        query = query.gsub(/[[:space:]]/, " ")
+        scanner = StringScanner.new(query)
+
+        until scanner.eos?
+          scanner.skip(/ +/)
+
+          if scanner.scan(/(#{METATAGS.join("|")}):/i)
+            metatag = scanner.captures.first
+
+            if scanner.scan(/"(.+)"/)
+              value = scanner.captures.first
+            elsif scanner.scan(/'(.+)'/)
+              value = scanner.captures.first
+            else
+              value = scanner.scan(/[^ ]*/)
+            end
+
+            terms << OpenStruct.new({ type: :metatag, name: metatag.downcase, value: value })
+          elsif scanner.scan(/[^ ]+/)
+            terms << OpenStruct.new({ type: :tag, value: scanner.matched.downcase })
+          end
+        end
+
+        terms
       end
 
       def split_query(query)
-        scan_query(query)
+        scan_query(query).map do |term|
+          if term.type == :metatag && term.value.include?(" ")
+            "#{term.name}:\"#{term.value}\""
+          elsif term.type == :metatag
+            "#{term.name}:#{term.value}"
+          elsif term.type == :tag
+            term.value
+          end
+        end
       end
 
       def normalize_query(query, normalize_aliases: true, sort: true)
@@ -784,12 +814,13 @@ class PostQueryBuilder
           :exclude => []
         }
 
-        scan_query(query).each do |token|
-          q[:tag_count] += 1 unless Danbooru.config.is_unlimited_tag?(token)
+        scan_query(query).each do |term|
+          q[:tag_count] += 1 unless Danbooru.config.is_unlimited_tag?(term)
 
-          if token =~ /\A(#{METATAGS.join("|")}):(.+)\z/i
-            g1 = $1.downcase
-            g2 = $2
+          if term.type == :metatag
+            g1 = term.name
+            g2 = term.value
+
             case g1
             when "-user"
               q[:user_neg] ||= []
@@ -896,11 +927,11 @@ class PostQueryBuilder
 
             when "-commentary"
               q[:commentary_neg] ||= []
-              q[:commentary_neg] << g2.gsub(/\A"(.*)"\Z/, '\1')
+              q[:commentary_neg] << g2
 
             when "commentary"
               q[:commentary] ||= []
-              q[:commentary] << g2.gsub(/\A"(.*)"\Z/, '\1')
+              q[:commentary] << g2
 
             when "search"
               q[:saved_searches] ||= []
@@ -951,10 +982,10 @@ class PostQueryBuilder
               q[:filesize] = parse_helper(g2, :filesize)
 
             when "source"
-              q[:source] = g2.gsub(/\A"(.*)"\Z/, '\1')
+              q[:source] = g2
 
             when "-source"
-              q[:source_neg] = g2.gsub(/\A"(.*)"\Z/, '\1')
+              q[:source_neg] = g2
 
             when "date"
               q[:date] = parse_helper(g2, :date)
@@ -1041,7 +1072,7 @@ class PostQueryBuilder
             end
 
           else
-            parse_tag(token, q[:tags])
+            parse_tag(term.value, q[:tags])
           end
         end
 

config/danbooru_default_config.rb
@@ -108,8 +108,8 @@ module Danbooru
     end
 
     # Return true if the given tag shouldn't count against the user's tag search limit.
-    def is_unlimited_tag?(tag)
-      tag.match?(/\A(-?status:deleted|rating:s.*|limit:.+)\z/i)
+    def is_unlimited_tag?(term)
+      term.type == :metatag && term.name.in?(%w[status rating limit])
     end
 
     # After this many pages, the paginator will switch to sequential mode.

test/unit/post_query_builder_test.rb
@@ -186,6 +186,8 @@ class PostQueryBuilderTest < ActiveSupport::TestCase
 
       assert_tag_match([post1], "pool:TEST_A")
       assert_tag_match([post2], "pool:Test_B")
+      assert_tag_match([post2], 'pool:"Test B"')
+      assert_tag_match([post2], "pool:'Test B'")
 
       assert_tag_match([post1], "pool:test_a")
       assert_tag_match([post2], "-pool:test_a")
@@ -481,11 +483,13 @@ class PostQueryBuilderTest < ActiveSupport::TestCase
       assert_tag_match([post1], "md5:abcd")
     end
 
-    should "return posts for a source search" do
-      post1 = create(:post, source: "abcd")
+    should "return posts for a source:<text> search" do
+      post1 = create(:post, source: "abc def")
       post2 = create(:post, source: "abcdefg")
       post3 = create(:post, source: "")
 
+      assert_tag_match([post1], 'source:"abc def"')
+      assert_tag_match([post1], "source:'abc def'")
       assert_tag_match([post2], "source:abcde")
       assert_tag_match([post3, post1], "-source:abcde")
 
@@ -748,9 +752,9 @@ class PostQueryBuilderTest < ActiveSupport::TestCase
     should "not count free tags against the user's search limit" do
       post1 = create(:post, tag_string: "aaa bbb rating:s")
 
-      Danbooru.config.expects(:is_unlimited_tag?).with("rating:s").once.returns(true)
-      Danbooru.config.expects(:is_unlimited_tag?).with(anything).twice.returns(false)
       assert_tag_match([post1], "aaa bbb rating:s")
+      assert_tag_match([post1], "aaa bbb status:active")
+      assert_tag_match([post1], "aaa bbb limit:20")
     end
 
     should "succeed for exclusive tag searches with no other tag" do

commit 626c2723d750e5cd933d94a06daa727a41716270

search: fix scan_query performance regression.
Fix a severe performance regression on the posts/index page introduced
by 6ca42947.

Short answer: scan_query dynamically allocated a regex inside an
inner loop that was called thousands of times per pageload.

Long answer:

* The post index page checks each post to see if they're tagged loli/shota,
* This triggers a call to Post#tag_array for every post.
* Post#tag_array called scan_query to split the tag string.
* scan_query loops over the tag string, checking if each tag matches the
  regex /#{METATAGS.join("|")}:/.
* This regex uses string interpolation, which makes Ruby treat as a
  dynamic value rather than a static value. Ruby doesn't know the
  interpolation is static here. This causes the regex to be reallocated
  on every iteration of the loop, or in other words, for every tag in
  the tag string.
* This caused us to do thousands of regex allocations per pageload. On
  average, a posts/index pageload contains 20 posts with ~35 tags per
  post, or 7000+ total tags. Doing this many allocations killed performance.

The fix:

* Don't use scan_query for Post#tag_array. We don't have to fully parse
  the tag_string here, we can use a simple split.
* Use the /o regex flag to tell Ruby to treat the regex as static and
  only evaluate the interpolation once.

app/logical/post_query_builder.rb
@@ -713,7 +713,7 @@ class PostQueryBuilder
         until scanner.eos?
           scanner.skip(/ +/)
 
-          if scanner.scan(/(#{METATAGS.join("|")}):/i)
+          if scanner.scan(/(#{METATAGS.join("|")}):/io)
             metatag = scanner.captures.first
 
             if scanner.scan(/"(.+)"/)
@@ -752,6 +752,10 @@ class PostQueryBuilder
         tags = tags.sort if sort
         tags = tags.uniq
         tags.join(" ")
+      end
+
+      def parse_tag_edit(tag_string)
+        split_query(tag_string)
       end
 
       def parse_query(query, options = {})

app/models/post.rb
@@ -529,11 +529,11 @@ class Post < ApplicationRecord
 
   module TagMethods
     def tag_array
-      @tag_array ||= PostQueryBuilder.split_query(tag_string)
+      @tag_array ||= tag_string.split
     end
 
     def tag_array_was
-      @tag_array_was ||= PostQueryBuilder.split_query(tag_string_in_database.presence || tag_string_before_last_save || "")
+      @tag_array_was ||= (tag_string_in_database.presence || tag_string_before_last_save || "").split
     end
 
     def tags
@@ -595,9 +595,9 @@ class Post < ApplicationRecord
       if old_tag_string
         # If someone else committed changes to this post before we did,
         # then try to merge the tag changes together.
-        current_tags = tag_array_was
-        new_tags = tag_array
-        old_tags = PostQueryBuilder.split_query(old_tag_string)
+        current_tags = tag_string_was.split
+        new_tags = PostQueryBuilder.parse_tag_edit(tag_string)
+        old_tags = old_tag_string.split
 
         kept_tags = current_tags & new_tags
         @removed_tags = old_tags - kept_tags