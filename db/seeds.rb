88dfc463a19d62790bb43fbf51493425ac062a8b
evazion <noizave@gmail.com>
2020-06-10 22:26:59 -0500

    db/seeds: fix deprecation warning in `find_or_create_by!`.
    `User.find_or_create_by!` calls `User.name_matches` during username
    validation, which triggers a deprecation warning for some reason:
    
        DEPRECATION WARNING: Class level methods will no longer inherit scoping
        from `create!` in Rails 6.1. To continue using the scoped relation,
        pass it into the block directly. To instead access the full set of
        models, as Rails 6.1 will, use `User.default_scoped`.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
 require "securerandom"
 
-system = User.find_or_create_by!(name: Danbooru.config.system_user) do |user|
-  user.password = SecureRandom.base64(32)
-end
-
-unless system.is_moderator?
-  system.level = User::Levels::MODERATOR
-  system.save
-end
+User.create(
+  name: Danbooru.config.system_user,
+  password: SecureRandom.base64(32),
+  level: User::Levels::MODERATOR
+)
