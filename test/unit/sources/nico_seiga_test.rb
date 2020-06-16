b583b3c8109d888a05f647f463b67b09a3205694
evazion <noizave@gmail.com>
2020-06-16 00:04:31 -0500

    tests: fix nicoseiga download tests.

--- test/unit/sources/nico_seiga_test.rb
+++ test/unit/sources/nico_seiga_test.rb
@@ -147,5 +147,32 @@ module Sources
         assert_equal(bad_source, Sources::Strategies.normalize_source(bad_source))
       end
     end
+
+    context "downloading a 'http://seiga.nicovideo.jp/seiga/:id' url" do
+      should "download the original file" do
+        @source = "http://seiga.nicovideo.jp/seiga/im4937663"
+        @rewrite = %r{https://lohas.nicoseiga.jp/priv/\h{40}/\d+/4937663}
+        assert_rewritten(@rewrite, @source)
+        assert_downloaded(2_032, @source)
+      end
+    end
+
+    context "downloading a 'http://lohas.nicoseiga.jp/o/:hash/:id' url" do
+      should "download the original file" do
+        @source = "http://lohas.nicoseiga.jp/o/910aecf08e542285862954017f8a33a8c32a8aec/1433298801/4937663"
+        @rewrite = %r{https://lohas.nicoseiga.jp/priv/\h{40}/\d+/4937663}
+        assert_rewritten(@rewrite, @source)
+        assert_downloaded(2_032, @source)
+      end
+    end
+
+    context "downloading a 'https://lohas.nicoseiga.jp/thumb/:id' url" do
+      should "download the original file" do
+        @source = "https://lohas.nicoseiga.jp/thumb/4937663i"
+        @rewrite = %r{https://lohas.nicoseiga.jp/priv/\h{40}/\d+/4937663}
+        assert_rewritten(@rewrite, @source)
+        assert_downloaded(2_032, @source)
+      end
+    end
   end
 end
