b358714790bb3f56dbb94fa2881450c06a138838
evazion <noizave@gmail.com>
2020-12-05 12:48:45 -0600

    tests: add test for #4613.

--- test/unit/post_query_builder_test.rb
+++ test/unit/post_query_builder_test.rb
@@ -207,6 +207,7 @@ class PostQueryBuilderTest < ActiveSupport::TestCase
       post2 = create(:post, tag_string: "fav:#{CurrentUser.name}")
 
       assert_tag_match([post2, post1], "ordfav:#{CurrentUser.name}")
+      assert_tag_match([], "ordfav:does_not_exist")
     end
 
     should "return posts for the pool:<name> metatag" do
