9b48c98c61f2557de6e055ec300c3a5c01320f24
evazion <noizave@gmail.com>
2020-12-05 13:10:11 -0600

    Fix #4614: Counts endpoint responds with invalid JSON.
    Caused by the search timing out and returning nil for the count. Nil got
    serialized as the empty string instead of as null.

--- app/controllers/counts_controller.rb
+++ app/controllers/counts_controller.rb
@@ -1,9 +1,15 @@
 class CountsController < ApplicationController
-  respond_to :xml, :json
+  respond_to :html, :xml, :json
 
   def posts
     estimate_count = params.fetch(:estimate_count, "true").truthy?
     skip_cache = params.fetch(:skip_cache, "false").truthy?
     @count = PostQueryBuilder.new(params[:tags], CurrentUser.user).normalized_query.fast_count(timeout: CurrentUser.statement_timeout, estimate_count: estimate_count, skip_cache: skip_cache)
+
+    if request.format.xml?
+      respond_with({ posts: @count }, root: "counts")
+    else
+      respond_with({ counts: { posts: @count }})
+    end
   end
 end
--- test/functional/counts_controller_test.rb
+++ test/functional/counts_controller_test.rb
@@ -7,6 +7,16 @@ class CountsControllerTest < ActionDispatch::IntegrationTest
         get posts_counts_path
         assert_response :success
       end
+
+      should "render for json" do
+        get posts_counts_path(format: :json)
+        assert_response :success
+      end
+
+      should "render for xml" do
+        get posts_counts_path(format: :xml)
+        assert_response :success
+      end
     end
   end
 end
