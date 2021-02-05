danbooru/danbooru

commit 6d867de20f122687247c57e201aa41f8e912ccb4

Add way for controllers with different models to use the only parameter
The /emails endpoint was passing in the "Email" model because that's
how the emails controller classifies. This was to fix that, and to
allow any other such cases in the future.

app/controllers/application_controller.rb
@@ -35,8 +35,10 @@ class ApplicationController < ActionController::Base
       return
     end
 
+    model = options[0]&.delete(:model)
     if subject.respond_to?(:includes) && (request.format.json? || request.format.xml?)
-      associations = ParameterBuilder.includes_parameters(params[:only], model_name)
+      model ||= model_name
+      associations = ParameterBuilder.includes_parameters(params[:only], model)
       subject = subject.includes(associations)
     end
 

commit 327deb9cbb6950e785ebdcc9f3489657c6bc302a

tests: fix regression in 6d867de20.
6d867de20 caused an exception in the ApiKeysController, which calls
respond_with with two arguments: `respond_with(CurrentUser.user, @api_key)`.
`options[0]` referred to the second argument, which was incorrect.

app/controllers/application_controller.rb
@@ -29,15 +29,13 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def respond_with(subject, *options, &block)
+  def respond_with(subject, *args, model: model_name, **options, &block)
     if params[:action] == "index" && is_redirect?(subject)
       redirect_to_show(subject)
       return
     end
 
-    model = options[0]&.delete(:model)
     if subject.respond_to?(:includes) && (request.format.json? || request.format.xml?)
-      model ||= model_name
       associations = ParameterBuilder.includes_parameters(params[:only], model)
       subject = subject.includes(associations)
     end