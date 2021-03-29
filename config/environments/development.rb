6404aa9aa9bb0803af420c1c5f263d6a2fe600cc
evazion <noizave@gmail.com>
2021-03-29 02:27:59 -0500

    Log to stdout in development and production.
    Always log to stdout instead of logging to files in `log/{development,production}.log`.
    
    For development, logging to files wasn't really useful, and could
    generate multi-gigabyte log files if you weren't paying attention. For
    production, most systems these days (such as Docker and Systemd) prefer
    that you write your logs to stdout so they can manage them.
    
    Fixes the Docker image writing logs inside the container, which never
    got rotated and could fill up the container.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,6 +64,10 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
+
   BetterErrors::Middleware.allow_ip!(IPAddr.new("0.0.0.0/0"))
   BetterErrors::Middleware.allow_ip!(IPAddr.new("::/0"))
 
