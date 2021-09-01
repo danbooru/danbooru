danbooru/danbooru

commit 38c9559fe81bb0792e1ced292fed1c75e7b16ea9

nokogiri: switch to the nokogumbo-based html5 parser.
https://github.com/sparklemotion/nokogiri/blob/main/CHANGELOG.md#1120--2021-08-02

app/logical/d_text.rb
@@ -49,7 +49,7 @@ class DText
   # @param artists [Array<Artist>]
   # @return [String] the HTML output
   def self.postprocess(html, wiki_pages, tags, artists)
-    fragment = Nokogiri::HTML.fragment(html)
+    fragment = Nokogiri::HTML5.fragment(html)
 
     fragment.css("a.dtext-wiki-link").each do |node|
       path = Addressable::URI.parse(node["href"]).path
@@ -174,7 +174,7 @@ class DText
   # @return [Array<String>] the list of wiki page names
   def self.parse_wiki_titles(text)
     html = DTextRagel.parse(text)
-    fragment = Nokogiri::HTML.fragment(html)
+    fragment = Nokogiri::HTML5.fragment(html)
 
     titles = fragment.css("a.dtext-wiki-link").map do |node|
       title = node["href"][%r{\A/wiki_pages/(.*)\z}i, 1]
@@ -191,7 +191,7 @@ class DText
   # @return [Array<String>] the list of external URLs
   def self.parse_external_links(text)
     html = DTextRagel.parse(text)
-    fragment = Nokogiri::HTML.fragment(html)
+    fragment = Nokogiri::HTML5.fragment(html)
 
     links = fragment.css("a.dtext-external-link").map { |node| node["href"] }
     links.uniq
@@ -326,7 +326,7 @@ class DText
   # @param html [String] the HTML input
   # @return [String] the Markdown output
   def self.html_to_markdown(html)
-    html = Nokogiri::HTML.fragment(html)
+    html = Nokogiri::HTML5.fragment(html)
 
     html.children.map do |node|
       case node.name
@@ -349,7 +349,7 @@ class DText
   # @param inline [Boolean] if true, convert <img> tags to plaintext
   # @return [String] the DText output
   def self.from_html(text, inline: false, &block)
-    html = Nokogiri::HTML.fragment(text)
+    html = Nokogiri::HTML5.fragment(text)
 
     dtext = html.children.map do |element|
       block.call(element) if block.present?

app/logical/sources/strategies/deviant_art.rb
@@ -269,7 +269,7 @@ module Sources
         resp = http.cache(1.minute).get(page_url_from_image_url, follow: {max_hops: 1})
 
         if resp.status.success?
-          Nokogiri::HTML(resp.body.to_s)
+          resp.parse
         # the work was deleted
         elsif resp.code == 404
           nil

app/logical/sources/strategies/tumblr.rb
@@ -183,7 +183,7 @@ module Sources::Strategies
     end
 
     def inline_images
-      html = Nokogiri::HTML.fragment(artist_commentary_desc)
+      html = Nokogiri::HTML5.fragment(artist_commentary_desc)
       html.css("img").map { |node| node["src"] }
     end
 

commit 19c0027d1ff1f754e6007933938b9c984f28c440

hentai foundry: fix 'Document tree depth exceeded' when parsing commentaries.
Fix a regression in 38c9559fe that caused #4657 to fail again.

app/logical/d_text.rb
@@ -49,7 +49,7 @@ class DText
   # @param artists [Array<Artist>]
   # @return [String] the HTML output
   def self.postprocess(html, wiki_pages, tags, artists)
-    fragment = Nokogiri::HTML5.fragment(html)
+    fragment = parse_html(html)
 
     fragment.css("a.dtext-wiki-link").each do |node|
       path = Addressable::URI.parse(node["href"]).path
@@ -174,7 +174,7 @@ class DText
   # @return [Array<String>] the list of wiki page names
   def self.parse_wiki_titles(text)
     html = DTextRagel.parse(text)
-    fragment = Nokogiri::HTML5.fragment(html)
+    fragment = parse_html(html)
 
     titles = fragment.css("a.dtext-wiki-link").map do |node|
       title = node["href"][%r{\A/wiki_pages/(.*)\z}i, 1]
@@ -191,7 +191,7 @@ class DText
   # @return [Array<String>] the list of external URLs
   def self.parse_external_links(text)
     html = DTextRagel.parse(text)
-    fragment = Nokogiri::HTML5.fragment(html)
+    fragment = parse_html(html)
 
     links = fragment.css("a.dtext-external-link").map { |node| node["href"] }
     links.uniq
@@ -326,7 +326,7 @@ class DText
   # @param html [String] the HTML input
   # @return [String] the Markdown output
   def self.html_to_markdown(html)
-    html = Nokogiri::HTML5.fragment(html)
+    html = parse_html(html)
 
     html.children.map do |node|
       case node.name
@@ -349,7 +349,7 @@ class DText
   # @param inline [Boolean] if true, convert <img> tags to plaintext
   # @return [String] the DText output
   def self.from_html(text, inline: false, &block)
-    html = Nokogiri::HTML5.fragment(text)
+    html = parse_html(text)
 
     dtext = html.children.map do |element|
       block.call(element) if block.present?
@@ -426,5 +426,12 @@ class DText
   # @return [String] a plain text string
   def self.excerpt(text, length: 160)
     strip_dtext(text).split(/\r\n|\r|\n/).first.to_s.truncate(length)
+  end
+
+  # Parse a string of HTML to a document object.
+  # @param html [String]
+  # @return [Nokogiri::HTML5::DocumentFragment]
+  def self.parse_html(html)
+    Nokogiri::HTML5.fragment(html, max_tree_depth: -1)
   end
 end