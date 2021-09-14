9552b41c0a5fe1615b711bc8cd92bd7ca07d4dc6
evazion <noizave@gmail.com>
2021-09-14 04:08:16 -0500

    Update Ruby gems and Yarn packages.

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -1,2 +1,2 @@
 import "../../../node_modules/ruffle-mirror/ruffle.js";
-import "../../../node_modules/ruffle-mirror/054626d51434c6b2e151.wasm";
+import "../../../node_modules/ruffle-mirror/fcf929249022847b08a7.wasm";
