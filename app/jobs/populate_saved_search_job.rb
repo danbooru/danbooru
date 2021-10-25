6d2ce5c8c1714d2dda376c6e76abdafc871088d6
evazion <noizave@gmail.com>
2021-10-24 22:16:34 -0500

    saved searches: lower job priority.
    Lower the priority of the populate saved search job. This is so that
    large numbers of saved searches don't overwhelm the job queue and
    prevent higher priority jobs from running.

--- app/jobs/populate_saved_search_job.rb
+++ app/jobs/populate_saved_search_job.rb
@@ -2,6 +2,7 @@
 # `search:all` or `search:<label>`.
 class PopulateSavedSearchJob < ApplicationJob
   queue_as :default
+  queue_with_priority 20
 
   def perform(query)
     SavedSearch.populate(query)
