6c9b49c194cb76310876f8fd2939667e2a851e07
nonamethanks <hellafrickingepic@gmail.com>
2021-11-05 09:40:19 +0100

    Foundation: add support for videos

--- test/unit/sources/foundation_test.rb
+++ test/unit/sources/foundation_test.rb
@@ -5,21 +5,26 @@ module Sources
     context "The source for a Foundation picture" do
       setup do
         @post_url = "https://foundation.app/@dadachyo/~/103724"
+        @post_with_video = "https://foundation.app/@huwari/~/88982"
         @image_url = "https://f8n-ipfs-production.imgix.net/QmPhpz6E9TFRpvdVTviM8Hy9o9rxrnPW5Ywj471NnSNkpi/nft.jpg"
         @image1 = Sources::Strategies.find(@post_url)
         @image2 = Sources::Strategies.find(@image_url)
+        @image3 = Sources::Strategies.find(@post_with_video)
       end
 
       should "get the artist name" do
         assert_equal("dadachyo", @image1.artist_name)
+        assert_equal("huwari", @image3.artist_name)
       end
 
       should "get the artist commentary title" do
         assert_equal("Rose tea", @image1.artist_commentary_title)
+        assert_equal("bus", @image3.artist_commentary_title)
       end
 
       should "get profile url" do
         assert_equal("https://foundation.app/@dadachyo", @image1.profile_url)
+        assert_equal("https://foundation.app/@huwari", @image3.profile_url)
       end
 
       should "get the image url" do
@@ -30,6 +35,7 @@ module Sources
       should "download an image" do
         assert_downloaded(13_908_349, @image1.image_url)
         assert_downloaded(13_908_349, @image2.image_url)
+        assert_downloaded(13_391_766, @image3.image_url)
       end
 
       should "find the correct artist" do
@@ -40,6 +46,7 @@ module Sources
       should "not raise errors" do
         assert_nothing_raised { @image1.to_h }
         assert_nothing_raised { @image2.to_h }
+        assert_nothing_raised { @image3.to_h }
       end
     end
   end
