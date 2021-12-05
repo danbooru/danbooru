danbooru/danbooru

commit 8841de68ac2c9679282379803a5f69210977891a

posts: add adjustable thumbnail sizes (#4932).
Add a menu in the top right of the post index page that lets you select
the thumbnail size.

This menu is currently hidden until the new thumbnails have been generated.

On desktop, there are five thumbnail sizes:

* Small:    150x150 (https://danbooru.donmai.us/posts?size=150)
* Medium:   180x180 (https://danbooru.donmai.us/posts?size=180)
* Large:    225x225 (https://danbooru.donmai.us/posts?size=225)
* Huge:     270x270 (https://danbooru.donmai.us/posts?size=270)
* Gigantic: 360x360 (https://danbooru.donmai.us/posts?size=360)

On mobile, there are four sizes:

* Small:  150x150 / 3 posts per row (https://danbooru.donmai.us/posts?size=150)
* Medium: 180x180 / 2 posts per row (https://danbooru.donmai.us/posts?size=180)
* Large:  225x225 / 2 posts per row (https://danbooru.donmai.us/posts?size=225)
* Huge:   360x360 / 1 posts per row (https://danbooru.donmai.us/posts?size=360)

There are two extra sizes that aren't listed in the menu:

* 225x360 (https://danbooru.donmai.us/posts?size=225w)
* 270x360 (https://danbooru.donmai.us/posts?size=270w)

These sizes are good for tall thumbnails, but not so much for wide
thumbnails. They aren't listed because in practice they're a bit too big.

The 225x225 and 270x270 sizes are really just 360x360 thumbnails scaled
down in HTML. This means 225x225 and 360x360 thumbnails both use the
same amount of bandwidth.

Thumbnail size is currently a per-search option, not a persistent
account-level setting.

This changes the HTML structure of thumbnails somewhat, so this may
break userscripts and custom CSS.

app/components/post_gallery_component.rb
@@ -7,7 +7,7 @@
 # * Inline galleries that fit on a single row, as seen in parent/child post sets.
 #
 class PostGalleryComponent < ApplicationComponent
-  attr_reader :posts, :current_user, :inline, :options
+  attr_reader :posts, :current_user, :inline, :size, :options
 
   delegate :post_preview, :numbered_paginator, to: :helpers
 
@@ -18,14 +18,22 @@ class PostGalleryComponent < ApplicationComponent
   # @param current_user [User] The current user.
   # @param inline [Boolean] If true, the gallery is rendered as a single row with a
   #   horizontal scrollbar. If false, the gallery is rendered as a grid of thumbnails.
+  # @param size [String] The size of thumbnails in the gallery. Can be "150",
+  #   "180", "225", "225w", "270", "270w", or "360".
   # @param options [Hash] A set of options given to the thumbnail in `post_preview`.
-  def initialize(posts:, current_user:, inline: false, **options)
+  def initialize(posts:, current_user:, inline: false, size: PostPreviewComponent::DEFAULT_SIZE, **options)
     super
     @posts = posts
     @posts = @posts.includes(:media_asset) if posts.is_a?(ActiveRecord::Relation)
     @current_user = current_user
     @inline = inline
     @options = options
+
+    if size.to_s.in?(PostPreviewComponent::SIZES)
+      @size = size
+    else
+      @size = PostPreviewComponent::DEFAULT_SIZE
+    end
   end
 
   def gallery_type

app/components/post_gallery_component/post_gallery_component.html.erb
@@ -1,8 +1,8 @@
-<div class="post-gallery post-gallery-<%= gallery_type %>">
+<div class="post-gallery post-gallery-<%= gallery_type %> post-gallery-<%= size %>">
   <% if empty? %>
     <p>No posts found.</p>
   <% else %>
-    <div class="posts-container grid gap-1 grid-cols-2 md:block">
+    <div class="posts-container grid gap-2">
       <% posts.each do |post| -%>
 <%    %><%= post_preview(post, **options) -%>
       <% end -%>

app/components/post_preview_component/post_preview_component.scss
@@ -1,12 +1,11 @@
 @import "../../javascript/src/styles/base/000_vars.scss";
 
 article.post-preview {
-  text-align: center;
   display: inline-block;
   position: relative;
   overflow: hidden;
 
-  a {
+  .post-preview-link {
     display: inline-block;
     position: relative;
 
@@ -30,17 +29,16 @@ article.post-preview {
     font-size: var(--text-sm);
     margin-bottom: 0;
   }
-
-  img {
-    margin: auto;
-  }
 }
 
-/* Avoid dead space around thumbnails in tables. */
-table article.post-preview {
-  height: auto;
+.post-preview-image {
   width: auto;
-  margin: 0;
+  height: auto;
+}
+
+@media screen and (min-width: 660px) {
+  .post-preview-225 .post-preview-image { max-height: 225px; }
+  .post-preview-270 .post-preview-image { max-height: 270px; }
 }
 
 .post-preview {
@@ -117,16 +115,8 @@ body[data-current-user-can-approve-posts="true"] .post-preview {
   }
 }
 
-@media screen and (min-width: 660px) {
-  article.post-preview {
-    width: 154px;
-    margin: 0 10px 10px 0;
-    vertical-align: top;
-  }
-}
-
 @media screen and (max-width: 660px) {
-  article.post-preview img {
+  .post-preview-image {
     border: none !important;
   }
 }

app/views/posts/partials/index/_posts.html.erb
@@ -2,7 +2,7 @@
   <% if post_set.shown_posts.empty? %>
     <%= render "post_sets/blank" %>
   <% else %>
-    <%= render_post_gallery(post_set.posts, show_deleted: post_set.show_deleted?, tags: post_set.tag_string, show_votes: post_set.show_votes?) do |gallery| %>
+    <%= render_post_gallery(post_set.posts, show_deleted: post_set.show_deleted?, tags: post_set.tag_string, show_votes: post_set.show_votes?, size: params[:size]) do |gallery| %>
       <% gallery.footer do %>
         <% if post_set.hidden_posts.present? %>
           <div class="fineprint hidden-posts-notice">

app/components/popup_menu_component.rb
@@ -2,7 +2,14 @@
 
 class PopupMenuComponent < ApplicationComponent
   include ViewComponent::SlotableV2
-  delegate :ellipsis_icon, to: :helpers
 
+  attr_reader :classes
+
+  renders_one :button
   renders_many :items
+
+  # @param classes [String] A list of CSS classes for the root element.
+  def initialize(classes: nil)
+    @classes = classes
+  end
 end

app/components/popup_menu_component/popup_menu_component.html.erb
@@ -1,6 +1,10 @@
-<div class="popup-menu">
+<div class="popup-menu <%= classes %>">
   <a class="popup-menu-button" href="javascript:void(0)">
-    <%= ellipsis_icon %>
+    <% if button.present? %>
+      <%= button %>
+    <% else %>
+      <%= helpers.ellipsis_icon %>
+    <% end %>
   </a>
 
   <ul class="popup-menu-content">

app/components/popup_menu_component/popup_menu_component.scss
@@ -9,7 +9,8 @@ div.popup-menu {
     justify-content: center;
     width: 100%;
     height: 100%;
-    border-radius: 50%;
+    border-radius: 4px;
+    padding: 4px 8px;
     color: var(--muted-text-color);
 
     // the popup menu is open

app/components/post_gallery_component/post_gallery_component.scss
@@ -12,12 +12,30 @@
   }
 }
 
-@media screen and (max-width: 660px) {
-  .post-gallery-full {
-    .posts-container {
-      &.user-disable-cropped-false article.post-preview img.has-cropped-true {
-        object-fit: none;
-      }
-    }
+.post-gallery {
+  .posts-container {
+    place-items: center;
+  }
+
+  .post-preview-image {
+    max-width: 100%;
+  }
+
+  &.post-gallery-150  .posts-container { grid-template-columns: repeat(auto-fill, minmax(150px, 1fr)); }
+  &.post-gallery-180  .posts-container { grid-template-columns: repeat(auto-fill, minmax(180px, 1fr)); }
+  &.post-gallery-225  .posts-container { grid-template-columns: repeat(auto-fill, minmax(225px, 1fr)); }
+  &.post-gallery-225w .posts-container { grid-template-columns: repeat(auto-fill, minmax(225px, 1fr)); }
+  &.post-gallery-270  .posts-container { grid-template-columns: repeat(auto-fill, minmax(270px, 1fr)); }
+  &.post-gallery-270w .posts-container { grid-template-columns: repeat(auto-fill, minmax(270px, 1fr)); }
+  &.post-gallery-360  .posts-container { grid-template-columns: repeat(auto-fill, minmax(360px, 1fr)); }
+
+  @media screen and (max-width: 660px) {
+    &.post-gallery-150  .posts-container { grid-template-columns: repeat(3, auto); }
+    &.post-gallery-180  .posts-container { grid-template-columns: repeat(2, auto); }
+    &.post-gallery-225  .posts-container { grid-template-columns: repeat(2, auto); }
+    &.post-gallery-225w .posts-container { grid-template-columns: repeat(2, auto); }
+    &.post-gallery-270  .posts-container { grid-template-columns: repeat(2, auto); }
+    &.post-gallery-270w .posts-container { grid-template-columns: repeat(2, auto); }
+    &.post-gallery-360  .posts-container { grid-template-columns: repeat(1, auto); }
   }
 }

commit 2e6f480d075bf921edf7a33b579bccb973efc873

pools: fix pool gallery page layout.
Fix the /pools/gallery page layout being broken by 8841de68a.

This required refactoring the PostGalleryComponent to take a set of
PostPreviewComponents instead of a set of Posts.

The upshot is that it's technically possible to have adjustable
thumbnail sizes on the pool gallery page now (although this is not yet
exposed in the UI).

app/components/post_gallery_component.rb
@@ -1,31 +1,27 @@
 # A component that displays a gallery of post thumbnails.
 #
-# There are three types of galleries:
+# There are two types of galleries:
 #
-# * Paginated galleries, as seen on the post index page and pool show page.
-# * Unpaginated galleries, as seen on wiki pages, artist pages, and user profiles.
-# * Inline galleries that fit on a single row, as seen in parent/child post sets.
+# * Grid galleries, where posts are arranged in a fixed grid.
+# * Inline galleries, where posts are arranged on a single scrollable row, as
+#   seen in parent/child post sets.
 #
 class PostGalleryComponent < ApplicationComponent
-  attr_reader :posts, :current_user, :inline, :size, :options
+  attr_reader :inline, :size, :options
 
-  delegate :post_preview, :numbered_paginator, to: :helpers
+  # The list of posts in the gallery.
+  renders_many :posts, PostPreviewComponent
 
-  # A gallery can optionally have a footer that displays between the posts and the paginator.
+  # An optional footer that displays beneath the posts. Usually used for the paginator.
   renders_one :footer
 
-  # @param posts [Array<Post>, ActiveRecord::Relation<Post>] The set of posts to display
-  # @param current_user [User] The current user.
   # @param inline [Boolean] If true, the gallery is rendered as a single row with a
   #   horizontal scrollbar. If false, the gallery is rendered as a grid of thumbnails.
   # @param size [String] The size of thumbnails in the gallery. Can be "150",
   #   "180", "225", "225w", "270", "270w", or "360".
-  # @param options [Hash] A set of options given to the thumbnail in `post_preview`.
-  def initialize(posts:, current_user:, inline: false, size: PostPreviewComponent::DEFAULT_SIZE, **options)
+  # @param options [Hash] A set of options given to the PostPreviewComponent.
+  def initialize(inline: false, size: PostPreviewComponent::DEFAULT_SIZE, **options)
     super
-    @posts = posts
-    @posts = @posts.includes(:media_asset) if posts.is_a?(ActiveRecord::Relation)
-    @current_user = current_user
     @inline = inline
     @options = options
 
@@ -39,26 +35,8 @@ class PostGalleryComponent < ApplicationComponent
   def gallery_type
     if inline
       :inline
-    elsif has_paginator?
-      :paginated
     else
-      :unpaginated
+      :grid
     end
-  end
-
-  def has_paginator?
-    posts.respond_to?(:total_count)
-  end
-
-  def total_count
-    if has_paginator?
-      posts.total_count
-    else
-      posts.length
-    end
-  end
-
-  def empty?
-    total_count == 0
   end
 end

app/components/post_gallery_component/post_gallery_component.html.erb
@@ -1,17 +1,13 @@
 <div class="post-gallery post-gallery-<%= gallery_type %> post-gallery-<%= size %>">
-  <% if empty? %>
+  <% if posts.empty? %>
     <p>No posts found.</p>
   <% else %>
     <div class="posts-container grid gap-2">
       <% posts.each do |post| -%>
-<%    %><%= post_preview(post, **options) -%>
+<%    %><%= post -%>
       <% end -%>
     </div>
   <% end %>
 
   <%= footer %>
-
-  <% if has_paginator? %>
-    <%= numbered_paginator(posts) %>
-  <% end %>
 </div>

app/components/post_preview_component/post_preview_component.scss
@@ -1,7 +1,9 @@
 @import "../../javascript/src/styles/base/000_vars.scss";
 
 article.post-preview {
-  display: inline-block;
+  display: inline-flex;
+  flex-direction: column;
+  place-items: center;
   position: relative;
   overflow: hidden;
 
@@ -28,6 +30,7 @@ article.post-preview {
   .desc {
     font-size: var(--text-sm);
     margin-bottom: 0;
+    text-align: center;
   }
 }
 

app/helpers/components_helper.rb
@@ -3,13 +3,22 @@ module ComponentsHelper
     render PostPreviewComponent.new(post: post, **options)
   end
 
-  def post_previews_html(posts, **options)
+  # Render a set of posts as thumbnail gallery.
+  #
+  # @param posts [ActiveRecord::Relation<Post>, Array<Post>] A set of posts.
+  # @param options [Hash] A hash of options for the PostGalleryComponent and PostPreviewComponent.
+  def render_post_gallery(posts, **options, &block)
     posts = posts.includes(:media_asset) if posts.is_a?(ActiveRecord::Relation)
-    render PostPreviewComponent.with_collection(posts, **options)
-  end
 
-  def render_post_gallery(posts, current_user: CurrentUser.user, **options, &block)
-    render PostGalleryComponent.new(posts: posts, current_user: current_user, **options), &block
+    render(PostGalleryComponent.new(**options)) do |gallery|
+      posts.each do |post|
+        gallery.post(post: post, size: gallery.size, **options)
+      end
+
+      if block_given?
+        yield(gallery, posts)
+      end
+    end
   end
 
   def render_comment(comment, current_user:, **options)

app/views/explore/posts/curated.html.erb
@@ -10,6 +10,10 @@
     <%= render "explore/posts/nav_links", path: :curated_explore_posts_path, date: @date, scale: @scale %>
     <%= render "posts/partials/common/inline_blacklist" %>
 
-    <%= render_post_gallery(@posts) %>
+    <%= render_post_gallery(@posts) do |gallery| %>
+      <% gallery.footer do %>
+        <%= numbered_paginator(@posts) %>
+      <% end %>
+    <% end %>
   </div>
 </div>

app/views/explore/posts/popular.html.erb
@@ -10,6 +10,10 @@
     <%= render "explore/posts/nav_links", path: :popular_explore_posts_path, date: @date, scale: @scale %>
     <%= render "posts/partials/common/inline_blacklist" %>
 
-    <%= render_post_gallery(@posts) %>
+    <%= render_post_gallery(@posts) do |gallery| %>
+      <% gallery.footer do %>
+        <%= numbered_paginator(@posts) %>
+      <% end %>
+    <% end %>
   </div>
 </div>

app/views/favorite_groups/show.html.erb
@@ -15,6 +15,10 @@
 
     <%= render "posts/partials/common/inline_blacklist" %>
 
-    <%= render_post_gallery(@posts, tags: "favgroup:#{@favorite_group.id}", show_deleted: true) %>
+    <%= render_post_gallery(@posts, tags: "favgroup:#{@favorite_group.id}", show_deleted: true) do |gallery| %>
+      <% gallery.footer do %>
+        <%= numbered_paginator(@posts) %>
+      <% end %>
+    <% end %>
   </div>
 </div>

app/views/pools/gallery.html.erb
@@ -9,12 +9,12 @@
 
     <%= render "posts/partials/common/inline_blacklist" %>
 
-    <section>
+    <%= render(PostGalleryComponent.new) do |gallery| %>
       <% @pools.each do |pool| %>
-        <%= post_preview(pool.cover_post, link_target: pool, pool: pool, show_deleted: true) %>
+        <% gallery.post(post: pool.cover_post, link_target: pool, pool: pool, show_deleted: true) %>
       <% end %>
+    <% end %>
 
-      <%= numbered_paginator(@pools) %>
-    </section>
+    <%= numbered_paginator(@pools) %>
   </div>
 </div>

app/views/pools/show.html.erb
@@ -20,6 +20,10 @@
 
     <%= render "posts/partials/common/inline_blacklist" %>
 
-    <%= render_post_gallery(@posts, tags: "pool:#{@pool.id}", show_deleted: @pool.is_series?) %>
+    <%= render_post_gallery(@posts, tags: "pool:#{@pool.id}", show_deleted: @pool.is_series?) do |gallery| %>
+      <% gallery.footer do %>
+        <%= numbered_paginator(@posts) %>
+      <% end %>
+    <% end %>
   </div>
 </div>

app/views/posts/partials/index/_posts.html.erb
@@ -2,7 +2,7 @@
   <% if post_set.shown_posts.empty? %>
     <%= render "post_sets/blank" %>
   <% else %>
-    <%= render_post_gallery(post_set.posts, show_deleted: post_set.show_deleted?, tags: post_set.tag_string, show_votes: post_set.show_votes?, size: params[:size]) do |gallery| %>
+    <%= render_post_gallery(post_set.posts, show_deleted: post_set.show_deleted?, tags: post_set.tag_string, show_votes: post_set.show_votes?, size: params[:size]) do |gallery, posts| %>
       <% gallery.footer do %>
         <% if post_set.hidden_posts.present? %>
           <div class="fineprint hidden-posts-notice">
@@ -32,6 +32,8 @@
             <% end %>
           <% end %>
         <% end %>
+
+        <%= numbered_paginator(posts) %>
       <% end %>
     <% end %>
   <% end %>