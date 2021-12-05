bcc773390b477113340e72786cf9cf80c7f161f9
evazion <noizave@gmail.com>
2021-12-05 16:41:32 -0600

    media assets: add script to backfill new thumbnail sizes.

---
+++ script/fixes/083_generate_media_asset_variants.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require_relative "../../config/environment"
+
+MediaAsset.active.parallel_each do |media_asset|
+  media_file = asset.variant(:original).open_file
+  media_asset.variant("180x180").store_file!(media_file)
+  media_asset.variant("360x360").store_file!(media_file)
+  media_asset.variant("720x720").store_file!(media_file)
+  puts "id=#{media_asset.id}"
+rescue StandardError => e
+  STDERR.puts "id=#{media_asset.id} status=FAILED e='#{e}'"
+ensure
+  media_file&.close
+end
