090125e239e605319e02a608bd7f28279e7278e7
evazion <noizave@gmail.com>
2022-01-04 18:08:54 -0600

    Revert "Temp disable dumping favorites table to BigQuery."
    This reverts commit 788dcbd87b53ad1e5e1dbf5c29d01eb4f23e0bbd.

--- app/logical/bigquery_export_service.rb
+++ app/logical/bigquery_export_service.rb
@@ -41,7 +41,7 @@ class BigqueryExportService
     Rails.application.eager_load!
 
     models = ApplicationRecord.descendants.sort_by(&:name)
-    models -= [Favorite, IpAddress, TagRelationship, ArtistVersion, ArtistCommentaryVersion, NoteVersion, PoolVersion, PostVersion, WikiPageVersion]
+    models -= [IpAddress, TagRelationship, ArtistVersion, ArtistCommentaryVersion, NoteVersion, PoolVersion, PostVersion, WikiPageVersion]
     models
   end
 
