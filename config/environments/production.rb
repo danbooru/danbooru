841990709dc68766230c76850f44f271e03e907e
evazion <noizave@gmail.com>
2022-01-07 20:34:22 -0600

    rails: enable Server-Timing HTTP header in production.
    This returns a Server-Timing header on all HTTP responses, which
    includes details on how long it took the server to render the response.
    Browsers can show this timing information in the devtools. In Chrome, go
    to the Network panel, then click a HTTP request, then click the Timing tab.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Enable server timing
+  config.server_timing = true
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
