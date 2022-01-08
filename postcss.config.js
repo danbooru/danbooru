bb60a5900182690c71166a4ed513241f067d1354
evazion <noizave@gmail.com>
2022-01-08 11:54:45 -0600

    css: remove postcss-flexbugs-fixes plugin.
    Remove a PostCSS plugin used to workaround bugs in the `flex` CSS
    property. Most of these bugs are no longer relevant and only affected
    ancient browsers like IE 10 or 11.
    
    https://github.com/philipwalton/flexbugs

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
