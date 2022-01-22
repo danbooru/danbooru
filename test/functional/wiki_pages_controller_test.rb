danbooru/danbooru

commit 0db20e0cabfffe4d1b0730dd67728bf9dbe617a5

Fix #4591: Wiki pages with filename-like name are broken by default.
Fix wiki pages like this returning 406 errors:

* https://danbooru.donmai.us/wiki_pages/rnd.jpg

Caused by Rails parsing the .jpg part as a file extension and trying to
return a JPEG in response. This happens deep in Rails' MIME negotiation
code, so it's hard to override. The fix is to pass `format: false` in
the route to disable all special handling of file extensions by Rails,
and then handle it ourselves in the controller. Ugly.

This only affected two tags: `rnd.jpg` and `haru.jpg`.

app/controllers/wiki_pages_controller.rb
@@ -30,6 +30,15 @@ class WikiPagesController < ApplicationController
   end
 
   def show
+    if params[:format].present?
+      request.format = params[:format]
+    elsif params[:id].ends_with?(".html", ".json", ".xml")
+      request.format = params[:id].split(".").last
+      params[:id].delete_suffix!(".#{request.format.symbol}")
+    else
+      request.format = "html"
+    end
+
     @wiki_page, found_by = WikiPage.find_by_id_or_title(params[:id])
 
     if request.format.html? && @wiki_page.blank? && found_by == :title

config/routes.rb
@@ -280,7 +280,7 @@ Rails.application.routes.draw do
   resources :webhooks do
     post :receive, on: :collection
   end
-  resources :wiki_pages, id: /.+?(?=\.json|\.xml|\.html)|.+/ do
+  resources :wiki_pages, id: /.+/, format: false do
     put :revert, on: :member
     get :search, on: :collection
     get :show_or_new, on: :collection

test/functional/wiki_pages_controller_test.rb
@@ -122,6 +122,35 @@ class WikiPagesControllerTest < ActionDispatch::IntegrationTest
         assert_response :success
       end
 
+      should "work for a title containing a file extension" do
+        as(@user) { create(:wiki_page, title: "rnd.jpg") }
+        as(@user) { create(:wiki_page, title: "touhou") }
+
+        get wiki_page_path("rnd.jpg")
+        assert_response :success
+        assert_equal("text/html", response.media_type)
+
+        get wiki_page_path("rnd.jpg.json")
+        assert_response :success
+        assert_equal("application/json", response.media_type)
+
+        get wiki_page_path("rnd.jpg.xml")
+        assert_response :success
+        assert_equal("application/xml", response.media_type)
+
+        get wiki_page_path("rnd.jpg.html")
+        assert_response :success
+        assert_equal("text/html", response.media_type)
+
+        get wiki_page_path("rnd.jpg", format: "json")
+        assert_response :success
+        assert_equal("application/json", response.media_type)
+
+        get wiki_page_path("touhou.json")
+        assert_response :success
+        assert_equal("application/json", response.media_type)
+      end
+
       should "mark banned artists as noindex" do
         @artist = create(:artist, name: @wiki_page.title, is_banned: true)
         get wiki_page_path(@wiki_page.title)

commit 90be15e0b5e8270b995e081adf9b16a808d1a6fb

Fix #4973: Wiki pages json index returns 404.
Fix regression introduced in 0db20e0ca. Setting `format: false` on the
wiki pages resource disabled format negotiation on all wiki page routes,
not just the show page, which meant /wiki_pages.json no longer worked.

The fix to monkey patch the internal Rails method that parses the file
extension from the URL, and have it ignore everything but the .html,
.json, .js, and .xml extensions. This is really hacky and may break in
future Rails releases.

app/controllers/wiki_pages_controller.rb
@@ -30,15 +30,6 @@ class WikiPagesController < ApplicationController
   end
 
   def show
-    if params[:format].present?
-      request.format = params[:format]
-    elsif params[:id].ends_with?(".html", ".json", ".xml")
-      request.format = params[:id].split(".").last
-      params[:id].delete_suffix!(".#{request.format.symbol}")
-    else
-      request.format = "html"
-    end
-
     @wiki_page, found_by = WikiPage.find_by_id_or_title(params[:id])
 
     if request.format.html? && @wiki_page.blank? && found_by == :title

config/initializers/core_extensions.rb
@@ -63,6 +63,23 @@ class String
   include Danbooru::Extensions::String
 end
 
+module MimeNegotationExtension
+  # Ignore all file extensions except for .html, .js, .json, and .xml when
+  # parsing the file extension from the URL. Needed for wiki pages (e.g.
+  # /wiki_pages/rnd.jpg).
+  private def format_from_path_extension
+    mime = super
+
+    if mime&.symbol.in?(%i[html js json xml])
+      mime
+    else
+      nil
+    end
+  end
+end
+
+ActionDispatch::Http::MimeNegotiation.prepend(MimeNegotationExtension)
+
 # Make Symbol#to_s return a frozen string. This reduces allocations, but may be
 # incompatible with some libraries.
 #

config/routes.rb
@@ -280,7 +280,7 @@ Rails.application.routes.draw do
   resources :webhooks do
     post :receive, on: :collection
   end
-  resources :wiki_pages, id: /.+/, format: false do
+  resources :wiki_pages, id: /.+?(?=\.json|\.xml|\.html)|.+/ do
     put :revert, on: :member
     get :search, on: :collection
     get :show_or_new, on: :collection

test/functional/wiki_pages_controller_test.rb
@@ -31,6 +31,13 @@ class WikiPagesControllerTest < ActionDispatch::IntegrationTest
         assert_equal(WikiPage.count, response.parsed_body.css("urlset url loc").size)
       end
 
+      should "render for a JSON response" do
+        get wiki_pages_path(format: :json)
+
+        assert_response :success
+        assert_equal("application/json", response.media_type)
+      end
+
       should "redirect the legacy title param to the show page" do
         get wiki_pages_path(title: "tagme")
         assert_redirected_to wiki_pages_path(search: { title_normalize: "tagme" }, redirect: true)