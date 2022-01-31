bb65a95a861c09f669783c46158dda584dd02241
evazion <noizave@gmail.com>
2022-01-31 14:39:30 -0600

    Fix #4981: ruffle broken :(
    * Switch to the official Ruffle NPM package.
    * Fix `core.ruffle.$hash.js` not being imported.

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -1,2 +1,3 @@
-import "../../../node_modules/ruffle-mirror/ruffle.js";
-import "../../../node_modules/ruffle-mirror/1bcd7970dbfb84b93ce4.wasm";
+import "../../../node_modules/@ruffle-rs/ruffle/ruffle.js";
+import "../../../node_modules/@ruffle-rs/ruffle/core.ruffle.25973f3ed2b17396c464.js";
+import "../../../node_modules/@ruffle-rs/ruffle/b2fc0abe3dbbf4e736aa.wasm";
