0f0f7c768b1eb5430c0befe91ded1102f95df7a8
evazion <noizave@gmail.com>
2022-03-06 20:29:56 -0600

    jobs: use GoodJob in development.
    Use the GoodJob job adapter instead of the default Rails async job
    adapter in development mode.
    
    The default async adapter runs jobs in a background thread in the
    `bin/rails server` process, but this sometimes has problems with jobs
    blocking the main server thread. The job queue interface at `/jobs` also
    didn't work with this.
    
    This means that now you have to run `bin/good_job start` in development
    mode in order to work background jobs. This is required for uploads to
    work.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.active_job.queue_adapter = :good_job
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
