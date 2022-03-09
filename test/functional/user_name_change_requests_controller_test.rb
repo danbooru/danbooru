danbooru/danbooru

commit 5ce724f1e2921ed4acc921619af1b4829e37663a

users: remove confirmation when changing username.

app/models/user_name_change_request.rb
@@ -5,7 +5,6 @@ class UserNameChangeRequest < ApplicationRecord
   belongs_to :approver, class_name: "User", optional: true
 
   validate :not_limited, on: :create
-  validates :desired_name, user_name: true, confirmation: true, on: :create
   validates :original_name, presence: true
   validates :desired_name, presence: true
 

test/functional/user_name_change_requests_controller_test.rb
@@ -23,7 +23,7 @@ class UserNameChangeRequestsControllerTest < ActionDispatch::IntegrationTest
 
     context "create action" do
       should "work" do
-        post_auth user_name_change_requests_path, @user, params: { user_name_change_request: { desired_name: "zun", desired_name_confirmation: "zun" }}
+        post_auth user_name_change_requests_path, @user, params: { user_name_change_request: { desired_name: "zun" }}
 
         assert_redirected_to profile_path
         assert_equal("zun", @user.reload.name)

app/policies/user_name_change_request_policy.rb
@@ -10,6 +10,6 @@ class UserNameChangeRequestPolicy < ApplicationPolicy
   end
 
   def permitted_attributes
-    [:desired_name, :desired_name_confirmation]
+    [:desired_name]
   end
 end

app/views/user_name_change_requests/new.html.erb
@@ -27,8 +27,7 @@
 
     <%= edit_form_for(@change_request) do |f| %>
       <%= f.input :desired_name, label: "New name" %>
-      <%= f.input :desired_name_confirmation, label: "Confirm name" %>
-      <%= f.submit "Submit", "data-confirm": "Are you sure you want to change your name?" %>
+      <%= f.submit "Submit" %>
     <% end %>
   </div>
 </div>

commit 80939f5e223f3d7166097076316ed87c08ba74d2

users: fix name changes not being validated.
Fix names not being validated when a user changes their name. Regression in 5ce724f1e.

app/models/user_name_change_request.rb
@@ -6,7 +6,7 @@ class UserNameChangeRequest < ApplicationRecord
 
   validate :not_limited, on: :create
   validates :original_name, presence: true
-  validates :desired_name, presence: true
+  validates :desired_name, user_name: true, presence: true, on: :create
 
   after_create :update_name!
 

test/functional/user_name_change_requests_controller_test.rb
@@ -28,6 +28,14 @@ class UserNameChangeRequestsControllerTest < ActionDispatch::IntegrationTest
         assert_redirected_to profile_path
         assert_equal("zun", @user.reload.name)
       end
+
+      should "fail if the new name is invalid" do
+        assert_no_changes(-> { @user.reload.name }) do
+          post_auth user_name_change_requests_path, @user, params: { user_name_change_request: { desired_name: "foo__bar" }}
+
+          assert_response :success
+        end
+      end
     end
 
     context "show action" do

test/unit/user_name_change_request_test.rb
@@ -15,11 +15,9 @@ class UserNameChangeRequestTest < ActiveSupport::TestCase
 
       should "not validate if the desired name already exists" do
         assert_difference("UserNameChangeRequest.count", 0) do
-          req = UserNameChangeRequest.create(
-            :user_id => @requester.id,
-            :original_name => @requester.name,
-            :desired_name => @requester.name
-          )
+          req = build(:user_name_change_request, user: @requester, original_name: @requester.name, desired_name: @requester.name)
+          req.valid?
+
           assert_equal(["Desired name already exists"], req.errors.full_messages)
         end
       end