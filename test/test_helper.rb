70148366d97c2b1ee464cd1804ce2e696da67881
nonamethanks <hellafrickingepic@gmail.com>
2022-04-12 13:07:34 +0200

    Add source test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,6 +26,7 @@ class ActiveSupport::TestCase
   include DownloadTestHelper
   include IqdbTestHelper
   include UploadTestHelper
+  extend SourceTestHelper
   extend StripeTestHelper
   extend NormalizeAttributeHelper
 
