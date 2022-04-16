c21c25089d9295b8e3230bf193a1911da0eff730
evazion <noizave@gmail.com>
2022-04-16 17:43:17 -0500

    apm: disable Elastic APM initializer.
    This caused problems because it effectively started the APM agent twice,
    causing the configuration to be ignored and duplicate events to be sent.

--- config/initializers/00_elastic_apm.rb
+++ config/initializers/00_elastic_apm.rb
@@ -1,10 +1,11 @@
-if Danbooru.config.elastic_apm_server_url.present?
-  require "elastic_apm"
-
-  # https://www.elastic.co/guide/en/apm/agent/ruby/4.x/api.html#api-agent-start
-  # https://www.elastic.co/guide/en/apm/agent/ruby/4.x/configuration.html
-  ElasticAPM::Rails.start(
-    server_url: Danbooru.config.elastic_apm_server_url,
-    service_name: "Danbooru",
-  )
-end
+#if Danbooru.config.elastic_apm_server_url.present?
+#  require "elastic_apm"
+#
+#  # https://www.elastic.co/guide/en/apm/agent/ruby/4.x/api.html#api-agent-start
+#  # https://www.elastic.co/guide/en/apm/agent/ruby/4.x/configuration.html
+#  ElasticAPM::Rails.start(
+#    server_url: Danbooru.config.elastic_apm_server_url,
+#    service_name: Danbooru.config.app_name,
+#    service_version: Rails.application.config.x.git_hash,
+#  )
+#end
