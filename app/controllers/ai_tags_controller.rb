97db0e6db352cba8ff37dc0392de3cd97dc609e3
evazion <noizave@gmail.com>
2022-06-26 00:36:55 -0500

    ai tags: respect the user's posts-per-page setting on the /ai_tags page.

--- app/controllers/ai_tags_controller.rb
+++ app/controllers/ai_tags_controller.rb
@@ -4,11 +4,12 @@ class AITagsController < ApplicationController
   respond_to :js, :html, :json, :xml
 
   def index
-    @ai_tags = authorize AITag.visible(CurrentUser.user).paginated_search(params, count_pages: false)
-    @ai_tags = @ai_tags.includes(:media_asset, :tag, :post) if request.format.html?
-
+    limit = params[:limit].presence || CurrentUser.user.per_page
     @mode = params.fetch(:mode, "gallery")
     @preview_size = params[:size].presence || cookies[:post_preview_size].presence || MediaAssetGalleryComponent::DEFAULT_SIZE
+
+    @ai_tags = authorize AITag.visible(CurrentUser.user).paginated_search(params, limit: limit, count_pages: false)
+    @ai_tags = @ai_tags.includes(:media_asset, :tag, :post) if request.format.html?
 
     respond_with(@ai_tags)
   end
