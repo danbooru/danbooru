4e7e0e4a74164b9e5c37bec85ef17ca035c81460
Lily <talulah@reunion.is>
2022-11-17 01:54:31 -0400

    posts: Autotag novelai from PNG:Software.

--- app/models/post.rb
+++ app/models/post.rb
@@ -490,6 +490,8 @@ class Post < ApplicationRecord
       tags -= ["sound"] unless is_flash?
       tags << "sound" if media_asset.has_sound?
 
+      tags << "novelai" if media_asset.metadata["PNG:Software"] == "NovelAI"
+
       tags
     end
 
