1a9718250f4909fd29e05fa2a69e2d0765eedf09
evazion <noizave@gmail.com>
2022-11-21 17:41:15 -0600

    replacements: strip spaces from replacement URL.
    Fix a handful of replacements having a replacement URL with a space in front or at the end.
    This caused problems when searching by replacement URL.

--- app/logical/post_replacement_processor.rb
+++ app/logical/post_replacement_processor.rb
@@ -27,7 +27,7 @@ class PostReplacementProcessor
     elsif Source::URL.page_url(image_url).present?
       canonical_url = image_url
     else
-      canonical_url = replacement.replacement_url
+      canonical_url = replacement.replacement_url.strip
     end
 
     replacement.replacement_url = canonical_url
