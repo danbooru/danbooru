a5d4af332d1ed80c29d92261e60ac783a5341fcf
evazion <noizave@gmail.com>
2022-11-27 21:19:05 -0600

    media assets: redesign show page.
    Redesign the media assets show page to:
    
    * Include sidebar with AI tags and image metadata.
    * Include next and previous image buttons.
    * Make the image use 100% of the available screen space and to scroll with the window.

--- app/components/media_asset_component.rb
+++ app/components/media_asset_component.rb
@@ -2,12 +2,19 @@
 
 # A component for showing a full-sized image or video for a media asset.
 class MediaAssetComponent < ApplicationComponent
-  attr_reader :media_asset
+  attr_reader :media_asset, :current_user, :outer_classes, :inner_classes, :dynamic_height
 
   delegate :image_width, :image_height, :variant, :is_image?, :is_video?, :is_ugoira?, :is_flash?, to: :media_asset
 
-  def initialize(media_asset:)
+  renders_one :header
+  renders_one :footer
+
+  def initialize(media_asset:, current_user:, outer_classes: "", inner_classes: "", dynamic_height: false)
     super
     @media_asset = media_asset
+    @current_user = current_user
+    @outer_classes = outer_classes
+    @inner_classes = inner_classes
+    @dynamic_height = dynamic_height
   end
 end
--- app/javascript/src/javascripts/media_asset_component.js
+++ app/javascript/src/javascripts/media_asset_component.js
@@ -1,47 +1,72 @@
 export default class MediaAssetComponent {
   static initialize() {
-    $(".media-asset-component").toArray().forEach(element => {
+    $(".media-asset-container").toArray().forEach(element => {
       new MediaAssetComponent(element);
     });
   }
 
   constructor(element) {
-    this.$component = $(element);
+    this.$container = $(element);
+    this.$component = this.$container.find(".media-asset-component");
+
+    if (this.$container.attr("data-dynamic-height") === "true") {
+      this.updateHeight();
+      $(window).on("scroll.danbooru", element => {
+        this.updateHeight();
+      });
+    }
 
     if (this.$image.length) {
       this.$image.on("click.danbooru", e => this.toggleFit());
+      this.$image.on("load.danbooru", e => this.updateZoom());
+      this.$image.on("load.danbooru", e => this.updateHeight());
       new ResizeObserver(() => this.updateZoom()).observe(this.$image.get(0));
       this.updateZoom();
     }
   }
 
   toggleFit() {
-    this.$component.toggleClass("fit-screen");
+    if (this.canZoom) {
+      this.$container.toggleClass("media-asset-container-fit-height");
+    }
+
     this.updateZoom();
   }
 
   updateZoom() {
     this.$image.removeClass("cursor-zoom-in cursor-zoom-out");
-    this.$zoomLevel.addClass("hidden").text(`${Math.round(100 * this.zoomLevel)}%`);
+    this.$zoomLevel.removeClass("hidden").text(`${Math.round(100 * this.zoomLevel)}%`);
 
-    if (this.isDownscaled) {
-      this.$image.addClass("cursor-zoom-out");
-      this.$zoomLevel.removeClass("hidden");
-    } else if (this.isTooBig) {
+    if (this.canZoomIn) {
       this.$image.addClass("cursor-zoom-in");
+    } else if (this.canZoomOut) {
+      this.$image.addClass("cursor-zoom-out");
     }
+  }
+
+  updateHeight() {
+    // XXX 115 = header height (hardcoded to prevent height glitches as page loads)
+    this.$container.css("--header-visible-height", Math.min(115, Math.max(0, this.$container.offset().top - $(window).scrollTop())) + "px");
   }
 
   get zoomLevel() {
     return this.$image.width() / Number(this.$image.attr("width"));
   }
 
-  get isDownscaled() {
-    return this.$image.width() < Number(this.$image.attr("width"));
+  get canZoom() {
+    return this.canZoomIn || this.canZoomOut;
   }
 
-  get isTooBig() {
-    return this.$image.width() > this.$component.width();
+  get canZoomIn() {
+    return !this.isZoomed && this.$image.height() < this.$image.get(0).naturalHeight && Math.round(this.$image.width()) < Math.round(this.$container.width());
+  }
+
+  get canZoomOut() {
+    return this.isZoomed;
+  }
+
+  get isZoomed() {
+    return !this.$container.is(".media-asset-container-fit-height");
   }
 
   get $image() {
