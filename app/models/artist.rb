danbooru/danbooru

commit e203c6f73013dc4998b94f53c517b051239f4c09

artists: optimize any_name_matches searches.
* Change /artists?search[any_name_matches] to be correctly case-insensitive. Before we lowercased
  the search string, but not the names themselves, so searches could miss results.
* Change /artists?search[any_name_matches] to perform an exact match instead of a substring match by
  default. Before searching for `foo` would actually search for `*foo*`.
* Optimize /artists?search[any_name_matches] by adding an index on `lower(ARRAY[name, group_name] || other_names)`.
  This requires a custom SQL function for lowercasing an array of strings.

The `any_name_matches` search is linked from the artist's other names on the artist page and in the
artist index. Some search crawlers spent a lot of time crawling these links, which were slow because
of the lack of indexes.

app/models/artist.rb
@@ -230,16 +230,18 @@ class Artist < ApplicationRecord
     end
 
     def any_other_name_like(name)
-      where(id: Artist.from("unnest(other_names) AS other_name").where_like("other_name", name))
+      where(id: Artist.from("unnest(other_names) AS other_name").where_ilike("other_name", name))
     end
 
     def any_name_matches(query)
       if query =~ %r{\A/(.*)/\z}
         where_regex(:name, $1).or(any_other_name_matches($1)).or(where_regex(:group_name, $1))
+      elsif query.include?("*")
+        normalized_name = normalize_name(query)
+        where_ilike(:name, normalized_name).or(any_other_name_like(normalized_name)).or(where_ilike(:group_name, normalized_name))
       else
         normalized_name = normalize_name(query)
-        normalized_name = "*#{normalized_name}*" unless normalized_name.include?("*")
-        where_like(:name, normalized_name).or(any_other_name_like(normalized_name)).or(where_like(:group_name, normalized_name))
+        where_array_includes_any("lower(ARRAY[name, group_name]::text[] || other_names)", [normalized_name])
       end
     end
 

db/migrate/20221228232240_add_lower_names_index_to_artists.rb
@@ -1,0 +1,18 @@
+class AddLowerNamesIndexToArtists < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
+  def up
+    execute <<~EOS
+      CREATE OR REPLACE FUNCTION lower(text[]) RETURNS text[] LANGUAGE SQL IMMUTABLE PARALLEL SAFE AS $$
+        SELECT array_agg(lower(value)) FROM unnest($1) value;
+      $$;
+    EOS
+
+    add_index :artists, "lower(ARRAY[name, group_name]::text[] || other_names)", name: "index_artists_on_lower_names", using: :gin, algorithm: :concurrently
+  end
+
+  def down
+    remove_index :artists, "lower(ARRAY[name, group_name]::text[] || other_names)", name: "index_artists_on_lower_names", using: :gin, algorithm: :concurrently
+    execute "DROP FUNCTION IF EXISTS lower(text[])"
+  end
+end

test/unit/artist_test.rb
@@ -496,6 +496,13 @@ class ArtistTest < ActiveSupport::TestCase
       assert_search_equals([yuu, cat_or_fish], any_name_matches: "/cat/")
     end
 
+    should "search case-insensitively on any_name_matches" do
+      artist = create(:artist, name: "foo", other_names: ["BAR"])
+
+      assert_search_equals(artist, any_name_matches: "FOO")
+      assert_search_equals(artist, any_name_matches: "bar")
+    end
+
     should "search on url and return matches" do
       bkub = create(:artist, name: "bkub", url_string: "http://bkub.com")
 

commit 120434bb9793319017011917f8e168a1cc33be4b

Fix #5431: ActiveRecord::StatementInvalid searching artists when ordering by post_count
Broken by e203c6f73013dc4998b94f53c517b051239f4c09.

app/models/artist.rb
@@ -241,7 +241,7 @@ class Artist < ApplicationRecord
         where_ilike(:name, normalized_name).or(any_other_name_like(normalized_name)).or(where_ilike(:group_name, normalized_name))
       else
         normalized_name = normalize_name(query)
-        where_array_includes_any("lower(ARRAY[name, group_name]::text[] || other_names)", [normalized_name])
+        where_array_includes_any("lower(ARRAY[artists.name, artists.group_name]::text[] || artists.other_names)", [normalized_name])
       end
     end
 

test/functional/artists_controller_test.rb
@@ -168,6 +168,7 @@ class ArtistsControllerTest < ActionDispatch::IntegrationTest
         should respond_to_search(name: "masao").with { @masao }
         should respond_to_search(is_banned: "true").with { @banned }
         should respond_to_search(is_deleted: "true").with { @deleted }
+        should respond_to_search(any_name_matches: "masao", order: "post_count").with { @masao }
         should respond_to_search(url_matches: "http://i2.pixiv.net/img04/img/syounen_no_uta/46170939_m.jpg").with { @masao }
         should respond_to_search(url_matches: "http://www.pixiv.net/member_illust.php?mode=medium&illust_id=46170939").with { @masao }
 