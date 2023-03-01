f59768da863711704d0ab6323b9c0d35764525c0
evazion <noizave@gmail.com>
2023-03-01 17:10:52 -0600

    uploads: make the edit panel resizeable.
    Make it so you can resize the edit panel by dragging it.
    
    The size is saved in a cookie, so it's remembered across uploads. This may be undesirable if you
    make the edit panel large for one upload, but want it to be small for the next upload.
    
    The minimum width is constrained to 240px to prevent the panel layout from becoming fucked up if you
    try to make it too small.
    
    The initial width is a fixed 480px. This may be too large for small screens, such as tablets in
    portrait mode.

--- app/javascript/src/javascripts/uploads.js
+++ app/javascript/src/javascripts/uploads.js
@@ -1,4 +1,6 @@
+import Cookie from "./cookie";
 import Utility from "./utility";
+import clamp from "lodash/clamp";
 
 let Upload = {};
 
@@ -9,6 +11,7 @@ Upload.IQDB_HIGH_SIMILARITY = 70;
 Upload.initialize_all = function() {
   if ($("#c-uploads #a-show #p-single-asset-upload").length) {
     this.initialize_similar();
+    this.initialize_draggable_divider();
   }
 
   Upload.loadAssets();
@@ -22,6 +25,26 @@ Upload.loadAssets = async function() {
     await Utility.delay(1000);
   }
 }
+
+Upload.initialize_draggable_divider = function() {
+  Upload.dragStartWidth = 0;
+
+  $(".upload-divider").draggable({ axis: "x" });
+
+  $(".upload-divider").on("dragstart", (event, ui) => {
+    Upload.dragStartWidth = parseInt($(".upload-container").css("--edit-container-width"));
+  });
+
+  $(".upload-divider").on("drag", (event, ui) => {
+    let minWidth = parseInt($(".upload-container").css("--min-edit-container-width"));
+    let maxWidth = $(".upload-container").width() - minWidth;
+    let width = clamp(Upload.dragStartWidth - ui.position.left, minWidth, maxWidth);
+
+    $(".upload-container").css("--edit-container-width", width);
+    Cookie.put("upload_edit_container_width", width);
+    ui.position.left = 0;
+  });
+};
 
 Upload.initialize_similar = function() {
   let media_asset_id = $("input[name='media_asset_id']").val();
