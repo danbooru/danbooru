48ca9b9723ec5219247bf352817f06dd78958667
evazion <noizave@gmail.com>
2023-03-23 12:10:28 -0500

    Update Ruby gems and Yarn packages.

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -1,3 +1,5 @@
 import "../../../node_modules/@ruffle-rs/ruffle/ruffle.js";
-import "../../../node_modules/@ruffle-rs/ruffle/core.ruffle.25973f3ed2b17396c464.js";
-import "../../../node_modules/@ruffle-rs/ruffle/b2fc0abe3dbbf4e736aa.wasm";
+import "../../../node_modules/@ruffle-rs/ruffle/core.ruffle.729f8439a0be4e471cfc.js";
+import "../../../node_modules/@ruffle-rs/ruffle/core.ruffle.fe7a24eb7cc2e69f6060.js";
+import "../../../node_modules/@ruffle-rs/ruffle/d4aab18ba4ef9841e7ed.wasm";
+import "../../../node_modules/@ruffle-rs/ruffle/1d3b09164c8a4cc2905c.wasm";
