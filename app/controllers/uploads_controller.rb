danbooru/danbooru

commit 0ba2a9dda42ff78db71f53afbedfaef66448ac27

Fix #5488: Artist tag re-appended to tags field upon getting an error on upload form
* Change it so that the rating field is highlighted in red if you forget to select the rating.
* Fix it so that the artist tag isn't added twice if you forget the rating.
* Fix it so that if the user submits a post while upload-limited, we only return the upload limit
  error, not other errors such as forgetting the rating.

app/controllers/uploads_controller.rb
@@ -40,6 +40,8 @@ class UploadsController < ApplicationController
     elsif request.format.html? && @upload.media_asset_count > 1
       redirect_to [@upload, UploadMediaAsset]
     else
+      @upload_media_asset = @upload.upload_media_assets.first
+      @post = Post.new_from_upload(@upload_media_asset, add_artist_tag: true, source: @upload_media_asset.canonical_url, **permitted_attributes(Post).to_h.symbolize_keys)
       respond_with(@upload, include: { upload_media_assets: { include: :media_asset }})
     end
   end

app/controllers/upload_media_assets_controller.rb
@@ -18,6 +18,7 @@ class UploadMediaAssetsController < ApplicationController
   def show
     @upload_media_asset = authorize UploadMediaAsset.find(params[:id])
     @media_asset = @upload_media_asset.media_asset
+    @post = Post.new_from_upload(@upload_media_asset, add_artist_tag: true, source: @upload_media_asset.canonical_url, **permitted_attributes(Post).to_h.symbolize_keys)
 
     if request.format.html? && @media_asset&.post&.present?
       flash[:notice] = "Duplicate of post ##{@media_asset.post.id}"

app/helpers/application_helper.rb
@@ -273,16 +273,16 @@ module ApplicationHelper
     end
   end
 
-  def edit_form_for(model, validate: false, **options, &block)
+  def edit_form_for(model, validate: false, error_notice: true, warning_notice: true, **options, &block)
     options[:html] = { autocomplete: "off", novalidate: !validate, **options[:html].to_h }
     options[:authenticity_token] = true if options[:remote] == true
 
     simple_form_for(model, **options) do |form|
-      if model.try(:errors).try(:any?)
+      if error_notice && model.try(:errors).try(:any?)
         concat tag.div(format_text(model.errors.full_messages.join("; ")), class: "notice notice-error notice-small prose")
       end
 
-      if model.try(:warnings).try(:any?)
+      if warning_notice && model.try(:warnings).try(:any?)
         concat tag.div(format_text(model.warnings.full_messages.join("; ")), class: "notice notice-info notice-small prose")
       end
 

app/javascript/src/styles/common/simple_form.scss
@@ -154,6 +154,11 @@ form.simple_form {
           border-top-left-radius: 0;
           border-bottom-left-radius: 0;
         }
+
+        &.field_with_errors .radio label {
+          border-color: var(--chip-red-border-color);
+          background-color: var(--chip-red-background-color);
+        }
       }
     }
   }

app/models/post.rb
@@ -32,12 +32,12 @@ class Post < ApplicationRecord
   before_validation :normalize_tags
   before_validation :blank_out_nonexistent_parents
   before_validation :remove_parent_loops
+  validate :uploader_is_not_limited, on: :create
+  validate :post_is_not_its_own_parent
   validates :md5, uniqueness: { message: ->(post, _data) { "Duplicate of post ##{Post.find_by_md5(post.md5).id}" }}, on: :create
   validates :rating, presence: { message: "not selected" }
   validates :rating, inclusion: { in: RATINGS.keys, message: "must be #{RATINGS.keys.map(&:upcase).to_sentence(last_word_connector: ", or ")}" }, if: -> { rating.present? }
   validates :source, length: { maximum: 1200 }
-  validate :post_is_not_its_own_parent
-  validate :uploader_is_not_limited, on: :create
   before_save :parse_pixiv_id
   before_save :added_tags_are_valid
   before_save :removed_tags_are_valid
@@ -1753,7 +1753,10 @@ class Post < ApplicationRecord
     end
 
     def uploader_is_not_limited
-      errors.add(:uploader, "have reached your upload limit. Please wait for your pending uploads to be approved before uploading more") if uploader.upload_limit.limited?
+      if uploader.upload_limit.limited?
+        errors.add(:uploader, "have reached your upload limit. Please wait for your pending uploads to be approved before uploading more")
+        throw :abort # Don't bother returning other validation errors if we're upload-limited.
+      end
     end
 
     def added_tags_are_valid

app/views/upload_media_assets/show.html.erb
@@ -1,7 +1,7 @@
 <div id="c-uploads" class="h-full">
   <div id="a-show" class="h-full">
     <div id="p-single-asset-upload" class="h-full">
-      <%= render "uploads/single_asset_upload", upload_media_asset: @upload_media_asset %>
+      <%= render "uploads/single_asset_upload", upload_media_asset: @upload_media_asset, post: @post %>
     </div>
   </div>
 </div>

app/views/uploads/_single_asset_upload.html.erb
@@ -117,14 +117,19 @@
     <div class="upload-divider px-4 cursor-col-resize"></div>
 
     <div class="upload-edit-container">
-      <% post = Post.new_from_upload(upload_media_asset, add_artist_tag: true, source: upload_media_asset.canonical_url, **permitted_attributes(Post).to_h.symbolize_keys) %>
-      <%= edit_form_for(post, html: { id: "form", class: "upload-form md:sticky top-4" }) do |f| %>
+      <%= edit_form_for(post, error_notice: false, html: { id: "form", class: "upload-form md:sticky top-4" }) do |f| %>
         <%= hidden_field_tag :media_asset_id, media_asset.id %> <%# used by iqdb javascript %>
         <%= hidden_field_tag :upload_media_asset_id, upload_media_asset.id %>
 
         <%= render(TabPanelComponent.new do |t| %>
           <% t.panel("Tags", active: true) do %>
-            <%= f.input :rating, label: "Rating #{help_tooltip embed_wiki("help:rating_notice", classes: "text-xs"), link_class: "inactive-link" }".html_safe, wrapper_html: { class: "radio-button-group text-xs" }, collection: Post::RATINGS.invert.reverse_each.to_h, as: :radio_buttons, selected: post.rating %>
+            <%= f.input :rating,
+                        label: "Rating #{help_tooltip embed_wiki("help:rating_notice", classes: "text-xs"), link_class: "inactive-link" }".html_safe,
+                        wrapper_html: { class: "radio-button-group text-xs", "x-on:input": "$el.classList.remove('field_with_errors')" },
+                        collection: Post::RATINGS.invert.reverse_each.to_h,
+                        as: :radio_buttons,
+                        selected: post.rating,
+                        error: false %>
 
             <div>
               <div class="flex justify-between">

app/views/uploads/show.html.erb
@@ -10,7 +10,7 @@
       <% end %>
     <% elsif @upload.media_asset_count == 1 %>
       <div id="p-single-asset-upload" class="h-full">
-        <%= render "uploads/single_asset_upload", upload_media_asset: @upload.upload_media_assets.first %>
+        <%= render "uploads/single_asset_upload", upload_media_asset: @upload.upload_media_assets.first, post: @post %>
       </div>
     <% end %>
   </div>

commit 31c6e74a14967c5d0cf9f218df718ea334fc6f6c

uploads: fix show page exception when upload is processing.
Fix an exception on the show page when the upload is still processing and doesn't have an active
media asset yet. Regression in 0ba2a9dda.

app/controllers/uploads_controller.rb
@@ -39,9 +39,11 @@ class UploadsController < ApplicationController
       redirect_to @upload.media_assets.first.post
     elsif request.format.html? && @upload.media_asset_count > 1
       redirect_to [@upload, UploadMediaAsset]
-    else
+    elsif @upload.media_asset_count == 1
       @upload_media_asset = @upload.upload_media_assets.first
       @post = Post.new_from_upload(@upload_media_asset, add_artist_tag: true, source: @upload_media_asset.canonical_url, **permitted_attributes(Post).to_h.symbolize_keys)
+      respond_with(@upload, include: { upload_media_assets: { include: :media_asset }})
+    else
       respond_with(@upload, include: { upload_media_assets: { include: :media_asset }})
     end
   end