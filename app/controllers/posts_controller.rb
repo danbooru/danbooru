76cb75ccb604b7d5c325061bfd5d2b684f9fddd8
evazion <noizave@gmail.com>
2023-04-03 13:43:31 -0500

    Fix #5014: 'Rating not selected' dialogue does not go away if you forget to add a rating on initial upload and add it when prompted.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -93,7 +93,7 @@ class PostsController < ApplicationController
       flash[:notice] = "Duplicate of post ##{@original_post.id}; merging tags"
       redirect_to @original_post
     else
-      flash[:notice] = @post.errors.full_messages.join("; ")
+      flash.now[:notice] = @post.errors.full_messages.join("; ")
       @post.tag_string = params.dig(:post, :tag_string) # Preserve original tag string on validation error
       respond_with(@post, render: { template: "upload_media_assets/show" })
     end
