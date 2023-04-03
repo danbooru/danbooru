danbooru/danbooru

commit e48ceb028e4a69826347143c150156a7efd0ed14

Fix #5484: Error on invalid e-mail during registration
* Fix exception on signup page when email is invalid.
* Add client-side validation to signup page. Validate username and password length and email syntax.

app/helpers/application_helper.rb
@@ -273,8 +273,8 @@ module ApplicationHelper
     end
   end
 
-  def edit_form_for(model, **options, &block)
-    options[:html] = { autocomplete: "off", **options[:html].to_h }
+  def edit_form_for(model, validate: false, **options, &block)
+    options[:html] = { autocomplete: "off", novalidate: !validate, **options[:html].to_h }
     options[:authenticity_token] = true if options[:remote] == true
 
     simple_form_for(model, **options) do |form|

app/controllers/users_controller.rb
@@ -77,18 +77,15 @@ class UsersController < ApplicationController
     user_verifier.log! if user_verifier.requires_verification?
     UserEvent.build_from_request(@user, :user_creation, request)
 
-    if params[:user][:email].present?
-      @user.email_address = EmailAddress.new(address: params[:user][:email])
+    if params[:user][:email_address].present?
+      @user.email_address = EmailAddress.new(address: params[:user][:email_address])
     end
 
     if Danbooru.config.enable_recaptcha? && !verify_recaptcha(model: @user)
-      flash[:notice] = "Sign up failed"
-    elsif @user.email_address&.invalid?(:deliverable)
-      flash[:notice] = "Sign up failed: email address is invalid or doesn't exist"
-      @user.errors.add(:base, @user.email_address.errors.full_messages.join("; "))
-    elsif !@user.save
-      flash[:notice] = "Sign up failed: #{@user.errors.full_messages.join("; ")}"
-    else
+      @user.errors.add(:base, "Sign up failed")
+    elsif @user.email_address&.valid? && @user.email_address&.invalid?(:deliverable)
+      @user.errors.add(:email_address, "is invalid or can't receive mail")
+    elsif @user.save
       session[:user_id] = @user.id
       UserMailer.with_request(request).welcome_user(@user).deliver_later
       set_current_user

app/logical/danbooru/email_address.rb
@@ -12,7 +12,7 @@ module Danbooru
     class Error < StandardError; end
 
     # https://www.regular-expressions.info/email.html
-    EMAIL_REGEX = /\A[a-z0-9._+-]*[a-z0-9_+-]@(?:[a-z0-9][a-z0-9-]{0,61}\.)+[a-z]{2,}\z/i
+    EMAIL_REGEX = /^[a-zA-Z0-9._+-]*[a-zA-Z0-9_+-]@([a-zA-Z0-9][a-zA-Z0-9-]{0,61}\.)+[a-zA-Z]{2,}$/
 
     # Sites that ignore dots in email addresses, e.g. where `foo.bar@gmail.com` is the same as `foobar@gmail.com`.
     IGNORE_DOTS = %w[gmail.com]

app/logical/user_name_validator.rb
@@ -7,6 +7,8 @@
 #
 # @see https://guides.rubyonrails.org/active_record_validations.html#custom-validators
 class UserNameValidator < ActiveModel::EachValidator
+  MIN_LENGTH = 1
+  MAX_LENGTH = 24
   ALLOWED_PUNCTUATION = "_.-" # All other punctuation characters are forbidden
 
   RESERVED_NAMES = [
@@ -22,10 +24,10 @@ class UserNameValidator < ActiveModel::EachValidator
 
     if !options[:skip_uniqueness] && User.without(current_user).find_by_name(name).present?
       rec.errors.add(attr, "already taken")
-    elsif name.length <= 1
-      rec.errors.add(attr, "must be more than 1 character long")
-    elsif name.length >= 25
-      rec.errors.add(attr, "must be less than 25 characters long")
+    elsif name.length <= MIN_LENGTH
+      rec.errors.add(attr, "must be more than #{MIN_LENGTH} character long")
+    elsif name.length > MAX_LENGTH
+      rec.errors.add(attr, "must be less than #{MAX_LENGTH + 1} characters long")
     # \p{di} = default ignorable codepoints. Filters out Hangul filler characters (U+115F, U+1160, U+3164, U+FFA0)
     elsif name =~ /[[:space:]\p{di}]/
       rec.errors.add(attr, "can't contain whitespace")

app/models/email_address.rb
@@ -6,7 +6,7 @@ class EmailAddress < ApplicationRecord
   attribute :address
   attribute :normalized_address
 
-  validates :address, presence: true, format: { message: "is invalid", with: Danbooru::EmailAddress::EMAIL_REGEX }
+  validates :address, presence: true, format: { message: "is invalid", with: Danbooru::EmailAddress::EMAIL_REGEX, multiline: true }
   validates :normalized_address, presence: true, uniqueness: true
   validates :user_id, uniqueness: true
   validate :validate_deliverable, on: :deliverable
@@ -55,7 +55,7 @@ class EmailAddress < ApplicationRecord
   end
 
   def validate_deliverable
-    if Danbooru::EmailAddress.new(address).undeliverable?(allow_smtp: Rails.env.production?)
+    if Danbooru::EmailAddress.parse(address)&.undeliverable?(allow_smtp: Rails.env.production?)
       errors.add(:address, "is invalid or does not exist")
     end
   end

app/views/users/new.html.erb
@@ -12,12 +12,12 @@
       comments. If you want <%= link_to "more features", new_user_upgrade_path %> you can upgrade your account later.
     </p>
 
-    <div id="p3">
-      <%= edit_form_for(@user, html: { id: "signup-form" }) do |f| %>
-        <%= f.input :name, as: :string, input_html: { autocapitalize: "none" } %>
-        <%= f.input :email, label: "Email", required: false, as: :email, input_html: { value: "" }, hint: "Optional" %>
-        <%= f.input :password %>
-        <%= f.input :password_confirmation %>
+    <div>
+      <%= edit_form_for(@user, html: { id: "signup-form" }, validate: true) do |f| %>
+        <%= f.input :name, as: :string, input_html: { autocapitalize: "none", minlength: UserNameValidator::MIN_LENGTH, maxlength: UserNameValidator::MAX_LENGTH }, required: true %>
+        <%= f.input :email_address, label: "Email", required: false, as: :email, input_html: { value: "", pattern: Danbooru::EmailAddress::EMAIL_REGEX, title: "your.name@example.com" }, hint: "Optional" %>
+        <%= f.input :password, input_html: { minlength: 5 }, required: true %>
+        <%= f.input :password_confirmation, input_html: { minlength: 5 }, required: true %>
 
         <% if Danbooru.config.enable_recaptcha? %>
           <%= invisible_recaptcha_tags callback: 'submitInvisibleRecaptchaForm', text: 'Sign up' %>

config/initializers/simple_form.rb
@@ -135,7 +135,7 @@ SimpleForm.setup do |config|
   # in this configuration, which is recommended due to some quirks from different browsers.
   # To stop SimpleForm from generating the novalidate option, enabling the HTML5 validations,
   # change this configuration to true.
-  config.browser_validations = false
+  config.browser_validations = true
 
   # Custom mappings for input types. This should be a hash containing a regexp
   # to match as key, and the input type that will be used when the field name

test/functional/users_controller_test.rb
@@ -353,7 +353,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       end
 
       should "create a user with a valid email" do
-        post users_path, params: { user: { name: "xxx", password: "xxxxx1", password_confirmation: "xxxxx1", email: "webmaster@danbooru.donmai.us" }}
+        post users_path, params: { user: { name: "xxx", password: "xxxxx1", password_confirmation: "xxxxx1", email_address: "webmaster@danbooru.donmai.us" }}
 
         assert_redirected_to User.last
         assert_equal("xxx", User.last.name)
@@ -368,7 +368,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
       should "not create a user with an invalid email" do
         assert_no_difference(["User.count", "EmailAddress.count"]) do
-          post users_path, params: { user: { name: "xxx", password: "xxxxx1", password_confirmation: "xxxxx1", email: "test" }}
+          post users_path, params: { user: { name: "xxx", password: "xxxxx1", password_confirmation: "xxxxx1", email_address: "test" }}
 
           assert_response :success
           assert_no_enqueued_emails
@@ -377,7 +377,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
       should "not create a user with an undeliverable email address" do
         assert_no_difference(["User.count", "EmailAddress.count"]) do
-          post users_path, params: { user: { name: "xxx", password: "xxxxx1", password_confirmation: "xxxxx1", email: "nobody@nothing.donmai.us" } }
+          post users_path, params: { user: { name: "xxx", password: "xxxxx1", password_confirmation: "xxxxx1", email_address: "nobody@nothing.donmai.us" } }
 
           assert_response :success
           assert_no_enqueued_emails

commit 4c9236bda49a0ef712dcabe01e0fcbcbe67fa630

artists: fix not being able to use wildcards in URL search fields.
Fix not being able to use wildcards in URL search fields, such as `/artist_urls?search[url_matches]=*blah*`.

Caused by HTML5 form validation preventing using invalid URLs in search forms. Regression in e48ceb028.

app/helpers/application_helper.rb
@@ -263,7 +263,7 @@ module ApplicationHelper
 
   def search_form_for(url, classes: "inline-form", method: :get, &block)
     defaults = { required: false }
-    html_options = { autocomplete: "off", class: "search-form #{classes}" }
+    html_options = { autocomplete: "off", novalidate: true, class: "search-form #{classes}" }
 
     simple_form_for(:search, method: method, url: url, defaults: defaults, html: html_options) do |f|
       out = "".html_safe