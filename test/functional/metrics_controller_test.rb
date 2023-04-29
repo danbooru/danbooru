3424026b9a197a9009c69e85ccd55c75fad30375
evazion <noizave@gmail.com>
2023-04-29 04:58:20 -0500

    Add Prometheus metrics at /metrics.
    Add basic application-level metrics at /metrics. This mostly consists of the sizes of various tables,
    plus basic application version info.
    
    Possible use cases:
    
    * Monitoring site growth over time (number of users, posts, votes, total size of media assets, etc).
    * Monitoring size of the modqueue (number of pending, flagged, appealed posts).
    * Monitoring background jobs (number of queued, running, failed jobs).
    * Monitoring bulk update requests (number of pending, approved, rejected BURs).
    * Detecting spam or vandalism (look for spikes in new users, etc).
    * Detecting when there are new posts, comments, etc without checking N different endpoints (watch for counters to go up).
    * Uptime monitoring.
    
    Since these metrics are public, it's possible for anyone to build their own dashboards by scraping
    the /metrics endpoint with Promotheus and Grafana.

---
+++ test/functional/metrics_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class MetricsControllerTest < ActionDispatch::IntegrationTest
+  context "The metrics controller" do
+    context "#index action" do
+      should "work for text format" do
+        get metrics_path
+
+        assert_response :success
+      end
+
+      should "work for json format" do
+        get metrics_path(format: :json)
+
+        assert_response :success
+      end
+
+      should "work for xml format" do
+        get metrics_path(format: :json)
+
+        assert_response :success
+      end
+    end
+  end
+end
