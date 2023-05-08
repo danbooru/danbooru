danbooru/danbooru

commit c34d8ca8152f3218b8d45e08592c4a22f2c673c7

sources: simplify parsing of http responses.
Add a `parsed_get` method that does a GET request and returns the parsed JSON/XML/HTML response on success, or nil on error.

This lets us replace code like this:

    def api_response
      return {} if api_url.blank?

      response = http.cache(1.minute).get(api_url)
      return {} unless response.status == 200

      response.parse
    end

with this:

    def api_response
      http.cache(1.minute).parsed_get(api_url) || {}
    end

app/logical/danbooru/http/json_adapter.rb
@@ -1,0 +1,18 @@
+# frozen_string_literal: true
+
+module Danbooru
+  class Http
+    class JsonAdapter < HTTP::MimeType::Adapter
+      HTTP::MimeType.register_adapter "application/json", self
+      HTTP::MimeType.register_adapter "application/ld+json", self
+
+      def decode(str)
+        json = JSON.parse(str)
+        json = json.with_indifferent_access if json.is_a?(Hash)
+        json
+      rescue JSON::ParserError
+        {}
+      end
+    end
+  end
+end

app/logical/danbooru/http.rb
@@ -2,6 +2,7 @@
 
 require "danbooru/http/application_client"
 require "danbooru/http/html_adapter"
+require "danbooru/http/json_adapter"
 require "danbooru/http/xml_adapter"
 require "danbooru/http/cache"
 require "danbooru/http/logger"
@@ -42,7 +43,7 @@ module Danbooru
     attr_accessor :max_size, :http
 
     class << self
-      delegate :get, :head, :put, :post, :delete, :cache, :follow, :max_size, :timeout, :auth, :basic_auth, :headers, :cookies, :use, :proxy, :public_only, :with_legacy_ssl, :download_media, to: :new
+      delegate :get, :head, :put, :post, :delete, :parsed_get, :parsed_post, :cache, :follow, :max_size, :timeout, :auth, :basic_auth, :headers, :cookies, :use, :proxy, :public_only, :with_legacy_ssl, :download_media, to: :new
     end
 
     # The default HTTP client.
@@ -102,6 +103,14 @@ module Danbooru
 
     def post!(url, **options)
       request!(:post, url, **options)
+    end
+
+    def parsed_get(url, **options)
+      parsed_request(:get, url, **options)
+    end
+
+    def parsed_post(url, **options)
+      parsed_request(:post, url, **options)
     end
 
     def follow(*args)
@@ -203,10 +212,19 @@ module Danbooru
     #
     # @param method [String] the HTTP method
     # @param url [String] the URL to request
+    # @param format [Symbol] if present, override the response's content type to be this format (:json, :html, or :xml).
     # @param options [Hash] the URL parameters
     # @return [HTTP::Response] the HTTP response
-    def request(method, url, **options)
-      http.send(method, url, **options)
+    def request(method, url, format: nil, **options)
+      response = http.send(method, url, **options)
+
+      if format
+        mime_type = Mime::Type.lookup_by_extension(format).to_s
+        content_type = HTTP::ContentType.parse(mime_type)
+        response.instance_eval { @content_type = content_type }
+      end
+
+      response
     rescue OpenSSL::SSL::SSLError
       fake_response(590)
     rescue ValidatingSocket::ProhibitedIpError
@@ -237,6 +255,20 @@ module Danbooru
       else
         raise Error, "#{method.upcase} #{url} failed (HTTP #{response.status})"
       end
+    end
+
+    # Perform a HTTP request for the given URL and return the parsed JSON, XML, or HTML response.
+    # Return nil on error, or if the URL was blank.
+    #
+    # @param method [String] The HTTP method.
+    # @param url [String] The URL to request.
+    # @param options [Hash] The URL parameters.
+    # @return [Hash, Array, Nokogiri::HTML::Document, nil] The parsed HTTP response body, or nil on error.
+    def parsed_request(method, url, **options)
+      return nil if url.blank?
+      response = request(method, url, **options)
+      return nil if response.code != 200
+      response.parse
     end
 
     def fake_response(status)

app/logical/pixiv_ajax_client.rb
@@ -396,14 +396,7 @@ class PixivAjaxClient
   # @param url [String] the Pixiv URL
   # @return [Hash] the parsed response, or blank on error
   def get(url)
-    response = client.cache(1.minute).get(url)
-
-    if response.code == 200
-      response.parse["body"]
-    else
-      DanbooruLogger.info("Pixiv API call failed (url=#{url} status=#{response.code} body=#{response.body})")
-      {}
-    end
+    client.cache(1.minute).parsed_get(url)&.dig("body") || {}
   end
 
   # @return [Danbooru::Http] the HTTP client used for Pixiv

app/logical/source/extractor/anifty.rb
@@ -87,24 +87,16 @@ class Source::Extractor
       parsed_url.artist_hash || parsed_referer&.artist_hash
     end
 
-    def api_response
+    memoize def api_response
       return {} if work_id.blank?
 
-      resp = http.cache(1.minute).get("https://asia-northeast1-anifty-59655.cloudfunctions.net/api/v2/creations/#{work_id}")
-      return {} if resp.code != 200
-
-      resp.parse.with_indifferent_access
+      http.cache(1.minute).parsed_get("https://asia-northeast1-anifty-59655.cloudfunctions.net/api/v2/creations/#{work_id}") || {}
     end
-    memoize :api_response
 
-    def artist_api_response
+    memoize def artist_api_response
       return {} if artist_hash.blank?
 
-      resp = http.cache(1.minute).get("https://asia-northeast1-anifty-59655.cloudfunctions.net/api/users/#{artist_hash}")
-      return {} if resp.code != 200
-
-      resp.parse.with_indifferent_access
+      http.cache(1.minute).parsed_get("https://asia-northeast1-anifty-59655.cloudfunctions.net/api/users/#{artist_hash}") || {}
     end
-    memoize :artist_api_response
   end
 end

app/logical/source/extractor/arca_live.rb
@@ -52,15 +52,10 @@ module Source
       end
 
       memoize def page
-        return nil if page_url.blank?
-
         # We need to spoof both the User-Agent (done by default in `Danbooru::Http.external`) and the Accept header,
         # otherwise we start getting hCaptchas if the request rate is too high.
         headers = { "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8" }
-        response = http.cache(1.minute).headers(headers).get(page_url)
-        return nil unless response.status == 200
-
-        response.parse
+        http.cache(1.minute).headers(headers).parsed_get(page_url)
       end
     end
   end

app/logical/source/extractor/art_station.rb
@@ -54,32 +54,27 @@ class Source::Extractor
     end
 
     def image_urls_from_api
-      api_response[:assets].to_a.map do |asset|
+      api_response[:assets].to_a.filter_map do |asset|
         if asset[:asset_type] == "image"
           asset_url(asset[:image_url])
         elsif asset[:asset_type] == "video_clip"
           url = Nokogiri::HTML5.parse(asset[:player_embedded]).at("iframe").attr("src")
-          next if url.nil?
-
-          response = http.cache(1.minute).get(url)
-          next if response.status != 200
-
-          response.parse.at("video source").attr("src")
+          page = http.cache(1.minute).parsed_get(url)
+          page&.at("video source")&.attr("src")
         end
-      end.compact
+      end
     end
 
     def project_id
       parsed_url.work_id || parsed_referer&.work_id
     end
 
+    def api_url
+      "https://www.artstation.com/projects/#{project_id}.json" if project_id.present?
+    end
+
     memoize def api_response
-      return {} if project_id.blank?
-
-      resp = http.cache(1.minute).get("https://www.artstation.com/projects/#{project_id}.json")
-      return {} if resp.code != 200
-
-      resp.parse.with_indifferent_access
+      http.cache(1.minute).parsed_get(api_url) || {}
     end
 
     def asset_url(url)

app/logical/source/extractor/art_street.rb
@@ -80,30 +80,15 @@ class Source::Extractor
     end
 
     memoize def book_api_response
-      return {} if book_api_url.blank?
-
-      response = http.cache(1.minute).get(book_api_url)
-      return {} if response.code != 200
-
-      response.parse
+      http.cache(1.minute).parsed_get(book_api_url) || {}
     end
 
     memoize def page
-      return nil if page_url.blank?
-
-      response = http.cache(1.minute).get(page_url)
-      return nil if response.code != 200
-
-      response.parse
+      http.cache(1.minute).parsed_get(page_url)
     end
 
     memoize def author_page
-      return nil if author_url.blank?
-
-      response = http.cache(1.minute).get(author_url)
-      return nil if response.code != 200
-
-      response.parse
+      http.cache(1.minute).parsed_get(author_url)
     end
 
     def http

app/logical/source/extractor/bilibili.rb
@@ -131,21 +131,15 @@ module Source
         super.use(:spoof_referrer)
       end
 
-      def page
-        return unless page_url.present?
-        response = http.cache(1.minute).get(page_url)
-        return response.parse unless response.status != 200
+      memoize def page
+        http.cache(1.minute).parsed_get(page_url)
       end
 
       def get_json(url)
-        response = http.cache(1.minute).get(url)
-        return {} unless response.status == 200
-        JSON.parse(response).with_indifferent_access
-      rescue JSON::ParserError
-        {}
+        http.cache(1.minute).parsed_get(url) || {}
       end
 
-      def data
+      memoize def data
         if t_work_id.present?
           data = get_json("https://api.bilibili.com/x/polymer/web-dynamic/v1/detail?timezone_offset=-60&id=#{t_work_id}")
           if data.dig("data", "item", "orig", "id_str").present? # it means it's a repost
@@ -160,8 +154,6 @@ module Source
           {}
         end
       end
-
-      memoize :data, :page
     end
   end
 end

commit 2b500b736ff55d6bb98d07daa7f57c7579c54608

http: fix bug when sending POST request with JSON body.
Fixup bug introduced in c34d8ca8. The `json` option didn't work with `Danbooru::Http.post` because
the `encode` method wasn't defined on the JSON adapter.

app/logical/danbooru/http/json_adapter.rb
@@ -6,6 +6,12 @@ module Danbooru
       HTTP::MimeType.register_adapter "application/json", self
       HTTP::MimeType.register_adapter "application/ld+json", self
 
+      def encode(obj)
+        return obj.to_json if obj.respond_to?(:to_json)
+
+        JSON.dump(obj)
+      end
+
       def decode(str)
         json = JSON.parse(str)
         json = json.with_indifferent_access if json.is_a?(Hash)

test/unit/danbooru_http_test.rb
@@ -99,6 +99,11 @@ class DanbooruHttpTest < ActiveSupport::TestCase
         response = Danbooru::Http.get(httpbin_url("redirect-to?url=#{httpbin_url("get")}"))
         assert_equal(200, response.status)
       end
+
+      should "work for POST requests with JSON encoded bodies" do
+        response = Danbooru::Http.post(httpbin_url("/post"), json: { foo: "bar" })
+        assert_equal(200, response.status)
+      end
     end
 
     context "cache feature" do