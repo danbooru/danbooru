0e0524e6373c1c9fe0c1499ac13d6a293f140f74
evazion <noizave@gmail.com>
2023-05-19 17:05:16 -0500

    posts: fix error when doing a search from a ugoira post.
    Fix a NotImplementedError when doing a tag search while viewing a ugoira post:
    
      https://danbooru.donmai.us/posts/5904366?q=bocchi_the_rock%21+minato_aqua&size=original
      -> https://danbooru.donmai.us/posts?tags=bocchi_the_rock%21+minato_aqua&z=5&size=original
    
    Caused by `size=original` being included in the search, which failed because `original` was not a
    valid thumbnail size.

--- app/components/post_preview_component.rb
+++ app/components/post_preview_component.rb
@@ -69,7 +69,7 @@ class PostPreviewComponent < ApplicationComponent
     when "540", "720"
       media_asset.variant("720x720")
     else
-      raise NotImplementedError
+      media_asset.variant("180x180")
     end
   end
 
