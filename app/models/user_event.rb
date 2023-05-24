e5c6dd3d0ac7c5557592296f7a82cd19edc1948e
evazion <noizave@gmail.com>
2023-05-24 18:38:17 -0500

    user events: add data migration script.
    * Start tracking ip_addr, session_id, and user_agent in the user_events table.
    * Add a fix script to migrate data from the user_sessions table to the user_events table.
    
    Downstream users must run this script to migrate their data:
    
        FIX=true ./script/fixes/132_migrate_user_events.rb
    
    This should be done after running `bin/rails db:migrate` from the previous commit.

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -6,10 +6,20 @@
 # with the event.
 
 class UserEvent < ApplicationRecord
-  self.ignored_columns = [:ip_addr, :session_id, :user_agent, :metadata]
+  attribute :id
+  attribute :created_at
+  attribute :updated_at
+  attribute :user_id
+  attribute :user_session_id
+  attribute :category
+  attribute :ip_addr, :ip_address
+  attribute :session_id, :md5
+  attribute :user_agent
+  attribute :metadata
 
   belongs_to :user
   belongs_to :user_session
+  belongs_to :ip_geolocation, foreign_key: :ip_addr, primary_key: :ip_addr, optional: true
 
   enum category: {
     login: 0,
@@ -23,7 +33,6 @@ class UserEvent < ApplicationRecord
     email_change: 600,
   }
 
-  delegate :session_id, :ip_addr, :ip_geolocation, to: :user_session
   delegate :country, :city, :is_proxy?, to: :ip_geolocation, allow_nil: true
 
   def self.visible(user)
@@ -37,7 +46,7 @@ class UserEvent < ApplicationRecord
   end
 
   def self.search(params, current_user)
-    q = search_attributes(params, [:id, :created_at, :updated_at, :category, :user, :user_session], current_user: current_user)
+    q = search_attributes(params, [:id, :created_at, :updated_at, :category, :user, :user_session, :ip_addr, :session_id, :user_agent, :metadata], current_user: current_user)
     q.apply_default_order(params)
   end
 
@@ -53,7 +62,7 @@ class UserEvent < ApplicationRecord
         IpGeolocation.create_or_update!(ip_addr)
         user_session = UserSession.new(session_id: request.session[:session_id], ip_addr: ip_addr, user_agent: request.user_agent)
 
-        user.user_events.build(user: user, category: category, user_session: user_session)
+        user.user_events.build(user: user, category: category, user_session: user_session, ip_addr: ip_addr, session_id: request.session[:session_id], user_agent: request.user_agent)
       end
 
       def create_from_request!(...)
