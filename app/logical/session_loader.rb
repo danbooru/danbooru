740958a7ed01e13200fa5ac222ec28e698c1445d
dstamatis <Dionyssioss@gmail.com>
2023-08-25 20:43:21 +0200

    add end statement

--- app/logical/session_loader.rb
+++ app/logical/session_loader.rb
@@ -183,6 +183,7 @@ class SessionLoader
   def set_safe_mode
     if CurrentUser.user.is_anonymous?
       CurrentUser.safe_mode = safe_mode
+    end
 
     ### No need for special domains, block via role check.
     # safe_mode = request.host.in?(Danbooru.config.safe_mode_hostnames) || params[:safe_mode].to_s.truthy? || CurrentUser.user.enable_safe_mode?
