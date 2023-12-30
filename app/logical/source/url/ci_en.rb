c5c493331e73318624f286cc56de3691512e5f6a
evazion <noizave@gmail.com>
2023-12-30 14:55:48 -0600

    ci-en: parse creator id from media urls.
    Fix it so that we can still find the artist when a post's source is only a raw image URL.

--- app/logical/source/url/ci_en.rb
+++ app/logical/source/url/ci_en.rb
@@ -20,12 +20,23 @@ class Source::URL::CiEn < Source::URL
         @article_id = article_id
 
       # https://ci-en.net/creator/11019
+      # https://ci-en.net/creator/11019/article
       # https://ci-en.dlsite.com/creator/5290
       in "creator", creator_id, *rest
         @creator_id = creator_id
 
       else
         nil
+      end
+
+    in "media.ci-en.jp"
+      case [*path_segments]
+
+      # https://media.ci-en.jp/private/attachment/creator/00011019/62a643d6423c18ec1be16826d687cefb47d8304de928a07c6389f8188dfe6710/image-800.jpg?px-time=1700517240&px-hash=eb626eafb7e5733c96fb0891188848dac10cb84c
+      # https://media.ci-en.jp/public/article_cover/creator/00020980/a65e3c05e2082018f4f28e99e7bc69b67ae96bb6f40b4a4b580ca939f435430d/image-1280-c.jpg
+      in _, _, "creator", creator_id, *rest
+        @creator_id = creator_id.to_i.to_s
+
       end
 
     else
@@ -44,7 +55,7 @@ class Source::URL::CiEn < Source::URL
   def page_url
     if article_id.present?
       "#{profile_url}/article/#{article_id}"
-    else
+    elsif !image_url?
       profile_url
     end
   end
