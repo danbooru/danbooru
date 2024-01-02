danbooru/danbooru

commit 6fb863fd492f16e0789cb08a02bbaed04b29bd41

Fix #5482: Manga from seiga.nicovideo.jp failing to upload properly.
`https://seiga.nicovideo.jp/image/source/12773050` links don't work for some manga. Instead we have
to fall back to `https://deliver.cdn.nicomanga.jp/thumb/12773050p?1655391688`.

We prefer `seiga.nicovideo.jp/image/source/:id` links when possible because they're larger than
`deliver.cdn.nicomanga.jp` links and because they bypass the DRM in `drm.cdn.nicomanga.jp` links
used in some manga (see `https://seiga.nicovideo.jp/watch/mg310193`).

app/logical/source/extractor/nico_seiga.rb
@@ -14,24 +14,27 @@ module Source
 
       def image_urls
         if image_id.present?
-          [image_url_for("https://seiga.nicovideo.jp/image/source/#{image_id}")]
+          [image_url_for("https://seiga.nicovideo.jp/image/source/#{image_id}") || url]
         elsif illust_id.present?
-          [image_url_for("https://seiga.nicovideo.jp/image/source/#{illust_id}")]
-        elsif manga_id.present? && api_client.image_ids.present?
-          api_client.image_ids.map { |id| image_url_for("https://seiga.nicovideo.jp/image/source/#{id}") }
+          [image_url_for("https://seiga.nicovideo.jp/image/source/#{illust_id}") || url]
+        elsif manga_id.present?
+          api_client.manga_api_response.pluck("meta").pluck("source_url").map do |url|
+            image_id = Source::URL.parse(url).image_id
+            image_url_for("https://seiga.nicovideo.jp/image/source/#{image_id}") || url
+          end
         else
-          [image_url_for(url)]
+          [image_url_for(url) || url]
         end
       end
 
       def image_url_for(url)
-        return url if api_client.blank?
+        return nil if api_client.blank?
 
         resp = api_client.head(url)
         if resp.uri.to_s =~ %r{https?://.+/(\w+/\d+/\d+)\z}i
           "https://lohas.nicoseiga.jp/priv/#{$1}"
         else
-          url
+          nil
         end
       end
 

test/unit/sources/nico_seiga_test.rb
@@ -78,6 +78,27 @@ module Sources
       )
     end
 
+    context "A nicoseiga manga url with deliver.cdn.nicomanga.jp images" do
+      strategy_should_work(
+        "https://seiga.nicovideo.jp/watch/mg658198",
+        image_urls: %w[
+          https://deliver.cdn.nicomanga.jp/thumb/12773050p?1655391688
+          https://deliver.cdn.nicomanga.jp/thumb/12773051p?1655391688
+          https://deliver.cdn.nicomanga.jp/thumb/12773052p?1655391688
+          https://deliver.cdn.nicomanga.jp/thumb/12773053p?1655391688
+          https://deliver.cdn.nicomanga.jp/thumb/12773054p?1655391688
+          https://deliver.cdn.nicomanga.jp/thumb/12773055p?1655391688
+          https://deliver.cdn.nicomanga.jp/thumb/12773056p?1655391688
+        ],
+        page_url: "https://seiga.nicovideo.jp/watch/mg658198",
+        artist_name: "あろめみ",
+        profile_url: "https://seiga.nicovideo.jp/user/illust/123720050",
+        tags: %w[DIY 日常],
+        artist_commentary_title: "工作少女 Do It Yourself !!",
+        artist_commentary_desc: "作業中のひとにいきなり話しかけるのはやめましょう"
+      )
+    end
+
     context "A https://lohas.nicoseiga.jp/thumb/${id}i url" do
       strategy_should_work(
         "https://lohas.nicoseiga.jp/thumb/6844226i",

app/logical/nico_seiga_api_client.rb
@@ -14,15 +14,6 @@ class NicoSeigaApiClient
     @http = http
   end
 
-  def image_ids
-    case @work_type
-    when "illust"
-      [api_response["id"]]
-    when "manga"
-      manga_api_response.map { |x| Source::URL.parse(x.dig("meta", "source_url"))&.image_id }.compact
-    end
-  end
-
   def title
     api_response["title"]
   end

commit 0edd5c941f92a96176b170d0f56b02f87a28a560

Fix #5556: The bookmarklet is inconsistent for Nicoseiga manga posts
Fix a bug where we sometimes fetched images belonging to a different post for NicoSeiga manga posts.

Caused by 6fb863fd49, where we tried convert manga sample images to full size images by rewriting
URLs like `https://deliver.cdn.nicomanga.jp/thumb/10543313p?1592370039` to `https://seiga.nicovideo.jp/image/source/10543313`
and following the redirect to the full image. This didn't always work because sometimes it redirected to a different image.

The fix is to check whether `https://seiga.nicovideo.jp/image/source/:id` redirects to an URL like
`https://lohas.nicoseiga.jp/priv/:hash/1704167121/10315315` (in which case it *should* be the right
image), or an URL like `https://lohas.nicoseiga.jp/o/:hash/1704167420/10543313` (in which case it's
a totally different image belonging to a non-manga post).

app/logical/source/extractor/nico_seiga.rb
@@ -19,8 +19,7 @@ module Source
           [image_url_for("https://seiga.nicovideo.jp/image/source/#{illust_id}") || url]
         elsif manga_id.present?
           api_client.manga_api_response.pluck("meta").pluck("source_url").map do |url|
-            image_id = Source::URL.parse(url).image_id
-            image_url_for("https://seiga.nicovideo.jp/image/source/#{image_id}") || url
+            manga_image_url_for(url)
           end
         else
           [image_url_for(url) || url]
@@ -35,6 +34,34 @@ module Source
           "https://lohas.nicoseiga.jp/priv/#{$1}"
         else
           nil
+        end
+      end
+
+      # Try to convert a https://deliver.cdn.nicomanga.jp/thumb/:id URL to the full size image. Not always possible.
+      #
+      # Doesn't work (redirects to a totally different image):
+      #
+      #   https://deliver.cdn.nicomanga.jp/thumb/10543313p?1592370039
+      #   => https://seiga.nicovideo.jp/image/source/10543313
+      #   => https://lohas.nicoseiga.jp/o/a6aaf607d27e9377a62a4353f73671c2138a6190/1704167420/10543313
+      #   => https://lohas.nicoseiga.jp/priv/a6aaf607d27e9377a62a4353f73671c2138a6190/1704167420/10543313
+      #
+      # Works (redirects to the right image):
+      #
+      #   https://deliver.cdn.nicomanga.jp/thumb/10315315p?1586768900
+      #   => https://seiga.nicovideo.jp/image/source/10315315
+      #   => https://lohas.nicoseiga.jp/priv/a9969a0177a30d21aa57720b9afa6b3f0a59dd7e/1704167121/10315315
+      def manga_image_url_for(manga_sample_url)
+        image_id = Source::URL.parse(manga_sample_url).image_id
+        return manga_sample_url if image_id.nil?
+
+        candidate_url = "https://seiga.nicovideo.jp/image/source/#{image_id}"
+        redirected_url = Source::URL.parse(api_client&.head(candidate_url)&.uri.to_s)
+
+        if redirected_url.to_s.match?("/priv/")
+          redirected_url.to_s
+        else
+          manga_sample_url
         end
       end
 

app/logical/source/url/nico_seiga.rb
@@ -88,6 +88,8 @@ module Source
         @image_id = image_id
 
       # https://deliver.cdn.nicomanga.jp/thumb/7891081p?1590171867
+      # https://deliver.cdn.nicomanga.jp/thumb/10543313p?1592370039 (webp: https://deliver.cdn.nicomanga.jp/thumb/aHR0cHM6Ly9zYXBpLm5pY29tYW5nYS5qcC9leHRlcm5hbC90aHVtYj9pZD0xMDU0MzMxMyZyZWY9Z2V0X2V4dGVybmFsX3RodW1iX3VybCZtb2RlPXJlZGlyZWN0JmU9MTcwODMwMzMxMyZoPTU0MGZhNmViZTQ2ZGE3YjUyYjViYmE3M2JiZDcxZTQzODI0NzcxNzI.webp, manga: https://seiga.nicovideo.jp/watch/mg485611)
+      # (note: the id isn't always an image_id. https://seiga.nicovideo.jp/image/source/10543313 redirects to a different image)
       in "deliver.cdn.nicomanga.jp", "thumb", /^(\d+)p$/ => image_id
         @image_id = $1
 

test/unit/sources/nico_seiga_test.rb
@@ -99,6 +99,36 @@ module Sources
       )
     end
 
+    context "A nicoseiga manga url (2)" do
+      strategy_should_work(
+        "https://seiga.nicovideo.jp/watch/mg485611",
+        image_urls: %w[
+          https://deliver.cdn.nicomanga.jp/thumb/10543313p?1592370039
+          https://deliver.cdn.nicomanga.jp/thumb/10543311p?1592262182
+          https://deliver.cdn.nicomanga.jp/thumb/10543312p?1592262182
+          https://deliver.cdn.nicomanga.jp/thumb/10543314p?1610453519
+        ],
+        page_url: "https://seiga.nicovideo.jp/watch/mg485611",
+        artist_name: "まいまい",
+        profile_url: "https://seiga.nicovideo.jp/user/illust/1116797",
+        tags: %w[ドラゴンクエスト 4コマ ムーンブルクの王女 着替え中 マジックミラー],
+        artist_commentary_title: "勇者さまが死んだので帰ります！ No.291 カミカゼアタック",
+        dtext_artist_commentary_desc: <<~EOS.chomp
+          アソビ大全を買おうかどうしようか悩み中
+
+
+          ※ファンティアにはイラストをアップしてるのでフォローよろしくどうぞ
+
+          https://fantia.jp/fanclubs/3263
+
+
+          ※公開中100話以外の回はCG集(描き下ろし多数)に収録、よろしくどうぞ
+
+          https://www.dlsite.com/home/circle/profile/=/maker_id/RG44783.html
+        EOS
+      )
+    end
+
     context "A https://lohas.nicoseiga.jp/thumb/${id}i url" do
       strategy_should_work(
         "https://lohas.nicoseiga.jp/thumb/6844226i",