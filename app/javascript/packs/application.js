danbooru/danbooru

commit 498e85f517ead061db3625ac4ee25d51994d8c44

Update Yarn packages (again).
* stylelint-webpack-plugin 4.1.1 is incompatible with Stylelint 16, so we have to use Stylelint 15.
* @rails/ujs changed something so that `require("@rails/ujs").start()` no longer works.

app/javascript/packs/application.js
@@ -7,7 +7,9 @@ function importAll(r) {
 // everything. See @babel/preset-env.
 import "core-js";
 
-require('@rails/ujs').start();
+import Rails from '@rails/ujs';
+Rails.start();
+
 require('hammerjs');
 require('jquery-hotkeys');
 import morphdom from 'morphdom';

commit 98cb588680759bab35627cea2c337a0ec165a43b

js: fix ajax requests not working.
Broken in 498e85f51. Rails UJS needs to be initialized after loading jQuery so that it can add a
hook that adds the CSRF token to all Ajax requests.

Fixes tag scripts and updating notes not working.

app/javascript/packs/application.js
@@ -7,9 +7,6 @@ function importAll(r) {
 // everything. See @babel/preset-env.
 import "core-js";
 
-import Rails from '@rails/ujs';
-Rails.start();
-
 require('hammerjs');
 require('jquery-hotkeys');
 import Alpine from 'alpinejs';
@@ -30,6 +27,9 @@ require("jquery-ui/themes/base/autocomplete.css");
 require("jquery-ui/themes/base/button.css");
 require("jquery-ui/themes/base/dialog.css");
 require("jquery-ui/themes/base/theme.css");
+
+import Rails from '@rails/ujs';
+Rails.start();
 
 import 'tippy.js/dist/tippy.css';
 