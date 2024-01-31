c658ff89f42250c79ca06e319df6c87a6bfcb779
evazion <noizave@gmail.com>
2024-01-31 05:58:18 +0000

    Update Ruby gems.

--- db/structure.sql
+++ db/structure.sql
@@ -4316,6 +4316,13 @@ CREATE INDEX index_good_job_executions_on_active_job_id_and_created_at ON public
 
 
 --
+-- Name: index_good_job_jobs_for_candidate_lookup; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_good_job_jobs_for_candidate_lookup ON public.good_jobs USING btree (priority, created_at) WHERE (finished_at IS NULL);
+
+
+--
 -- Name: index_good_job_settings_on_key; Type: INDEX; Schema: public; Owner: -
 --
 
