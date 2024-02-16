a2a46655b5ab571e2768b2f6ff3b0735f32ed567
evazion <noizave@gmail.com>
2024-02-16 21:52:08 +0000

    users: allow forgot password page to accept email addresses.
    Allow the forgot password page to accept either the user's name or their email address.
    
    Also fix it so that the forgot password page doesn't leak any information about whether or not an
    account with that email actually exists. Disclosing whether a person with a known email address has
    a Danbooru account could be a privacy violation.
    
    Note that it's still possible to tell whether an email is in use by other means, such as by trying
    to create a new account with that email, or by changing an existing account's email address, or by
    checking the /jobs page to see whether a mail delivery job was created after submitting a password
    reset request.

--- test/functional/password_resets_controller_test.rb
+++ test/functional/password_resets_controller_test.rb
@@ -10,30 +10,77 @@ class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
     end
 
     context "create action" do
-      should "should send a password reset email if the user has a verified email address" do
-        @user = create(:user, email_address: build(:email_address))
-        post password_reset_path, params: { user: { name: @user.name } }
+      context "for an account identified by an email address" do
+        should "send a password reset email if the account exists and has a verified email address" do
+          @user = create(:user, email_address: create(:email_address, address: "Foo.Bar+nospam@Googlemail.com", is_verified: true))
+          post password_reset_path, params: { user: { name: "foobar@gmail.com" } }
 
-        assert_redirected_to new_session_path
-        assert_equal(true, @user.user_events.password_reset.exists?)
+          assert_redirected_to password_reset_path
+          assert_equal(true, @user.user_events.password_reset.exists?)
+          assert_enqueued_email_with UserMailer.with_request(request), :password_reset, args: [@user], queue: "default"
+          perform_enqueued_jobs
+          assert_performed_jobs(1, only: MailDeliveryJob)
+        end
 
-        perform_enqueued_jobs
-        assert_performed_jobs(1, only: MailDeliveryJob)
-        #assert_enqueued_email_with UserMailer.with_request(request), :password_reset, args: [@user], queue: "default"
+        should "send a password reset email if the account exists and has an unverified email address" do
+          @user = create(:user, email_address: create(:email_address, address: "Foo.Bar+nospam@Googlemail.com", is_verified: false))
+          post password_reset_path, params: { user: { name: "foobar@gmail.com" } }
+
+          assert_redirected_to password_reset_path
+          assert_equal(true, @user.user_events.password_reset.exists?)
+          assert_enqueued_email_with UserMailer.with_request(request), :password_reset, args: [@user], queue: "default"
+          perform_enqueued_jobs
+          assert_performed_jobs(1, only: MailDeliveryJob)
+        end
+
+        should "not send an email if an account with the email doesn't exist" do
+          post password_reset_path, params: { user: { name: "foobar@gmail.com" } }
+
+          assert_redirected_to password_reset_path
+          assert_no_enqueued_emails
+          assert_equal(false, UserEvent.password_reset.exists?)
+        end
       end
 
-      should "should fail if the user doesn't have a verified email address" do
-        @user = create(:user)
-        post password_reset_path, params: { user: { name: @user.name } }
+      context "for an account identified by a username" do
+        should "send a password reset email if the account exists and has a verified email address" do
+          @user = create(:user, email_address: create(:email_address, address: "Foo.Bar+nospam@Googlemail.com", is_verified: true))
+          post password_reset_path, params: { user: { name: @user.name } }
 
-        assert_redirected_to @user
-        assert_no_enqueued_emails
-      end
+          assert_redirected_to password_reset_path
+          assert_equal(true, @user.user_events.password_reset.exists?)
+          assert_enqueued_email_with UserMailer.with_request(request), :password_reset, args: [@user], queue: "default"
+          perform_enqueued_jobs
+          assert_performed_jobs(1, only: MailDeliveryJob)
+        end
 
-      should "fail if the user does not exist" do
-        post password_reset_path, params: { user: { name: "qoi23oti" } }
+        should "send a password reset email if the account exists and has an unverified email address" do
+          @user = create(:user, email_address: create(:email_address, address: "Foo.Bar+nospam@Googlemail.com", is_verified: false))
+          post password_reset_path, params: { user: { name: @user.name } }
 
-        assert_redirected_to password_reset_path
+          assert_redirected_to password_reset_path
+          assert_equal(true, @user.user_events.password_reset.exists?)
+          assert_enqueued_email_with UserMailer.with_request(request), :password_reset, args: [@user], queue: "default"
+          perform_enqueued_jobs
+          assert_performed_jobs(1, only: MailDeliveryJob)
+        end
+
+        should "not send an email if the user doesn't have an email address" do
+          @user = create(:user)
+          post password_reset_path, params: { user: { name: @user.name } }
+
+          assert_redirected_to password_reset_path
+          assert_no_enqueued_emails
+          assert_equal(true, @user.user_events.password_reset.exists?)
+        end
+
+        should "not send an email if the user does not exist" do
+          post password_reset_path, params: { user: { name: "qoi23oti" } }
+
+          assert_redirected_to password_reset_path
+          assert_no_enqueued_emails
+          assert_equal(false, UserEvent.password_reset.exists?)
+        end
       end
     end
   end
