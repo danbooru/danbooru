2eace025e6578da3e0df8713327881965a3426fd
evazion <noizave@gmail.com>
2024-02-29 21:32:32 +0000

    sources: fix parsing of domain names in URLs.
    Improve parsing of domain names in URLs:
    
    * Fix it so that invalid domains like `https://blah@gmail.com` aren't allowed in artist URLs.
    * Fix not being able to parse URLs with dotless domains like `https://localhost:3000`.

--- test/unit/source_url_test.rb
+++ test/unit/source_url_test.rb
@@ -7,17 +7,20 @@ class SourceURLTest < ActiveSupport::TestCase
         assert_nil(Source::URL.parse("https://.auone-net.jp/~hakueki/"))
         assert_nil(Source::URL.parse("https://.example.com"))
         assert_nil(Source::URL.parse("https://"))
+        assert_nil(Source::URL.parse("https://./"))
+        assert_nil(Source::URL.parse("https://!!!.com"))
+        # assert_nil(Source::URL.parse("https://foo@gmail.com"))
       end
 
       should "work for valid URLs" do
         assert_not_nil(Source::URL.parse("https://foo.com."))
         assert_not_nil(Source::URL.parse("https://user:pass@foo.com:80"))
-
-        assert_not_nil(Source::URL.parse("https://!!!.com")) # XXX invalid
+        assert_not_nil(Source::URL.parse("https://localhost"))
       end
 
       should "normalize URLs" do
         assert_equal("https://example.com/foo%20%09%0B%0C%0D%0Abar", Source::URL.parse("https://example.com/foo \t\v\f\r\nbar").to_normalized_s)
+        assert_equal("https://example.com", Source::URL.parse("https://EXAMPLE.COM").to_normalized_s)
       end
 
       should "parse URLs containing invalid UTF-8" do
