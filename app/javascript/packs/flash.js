65aeec40775263f6711bd530e0baba2f0c338ffb
Talulah <talulah@reunion.is>
2024-03-03 14:42:24 -0400

    Update gems and npm packages.

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -1,5 +1,5 @@
 import "@ruffle-rs/ruffle/ruffle.js";
-import "@ruffle-rs/ruffle/core.ruffle.e8ce4744b3b34d52408f.js";
-import "@ruffle-rs/ruffle/core.ruffle.fd4be240dd496a0d4626.js";
-import "@ruffle-rs/ruffle/be1b3a98e5ea7abb8714.wasm";
-import "@ruffle-rs/ruffle/c4ef22279940d042f80a.wasm";
+import "@ruffle-rs/ruffle/core.ruffle.57cb6721d3c1f1cfa457.js";
+import "@ruffle-rs/ruffle/core.ruffle.76c006852c6396d9077b.js";
+import "@ruffle-rs/ruffle/2be296c565a710a65d98.wasm";
+import "@ruffle-rs/ruffle/40957fb534f12938007d.wasm";
