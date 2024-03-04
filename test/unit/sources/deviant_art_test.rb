danbooru/danbooru

commit efbb40b3c518b3fa64e03ea7a5718df3dafa5ce2

deviantart: fix breakage caused by DeviantArt changes.
Rewrite the extractor to fix everything being broken by DeviantArt changes.

Before we scraped the deviation UUID from a <meta> element and used that to fetch data from the API.
But DeviantArt removed that element, so now we get all the data from a JSON blob in a <script> tag
on the page. This is better anyway because the JSON contains all that we need so that we don't need
to make additional API calls.

The only thing we use the API for now is getting the download image URL in certain cases. The normal
/download/ endpoint requires a login cookie, but with the API we can just use our API key.

This also fixes it to get the largest sample image whenever possible. This is done by parsing the
JWT token in the URL and using that to determine the maximum width and height for the sample image.

Lastly, we're now able to fetch metadata and larger images for ancient image URLs that didn't
contain an ID. Most old non-wixmp.com image URLs now redirect to wixmp.com, which means we can get
the deviation ID from the wixmp.com URL and use that to get the metadata and the downloadable image
(if available) or a larger sample image.

app/logical/source/extractor/deviant_art.rb
@@ -12,60 +12,82 @@ module Source
       end
 
       def image_urls
-        [image_url]
+        [image_url].compact
       end
 
       def image_url
-        # work is private, deleted, or the url didn't contain a deviation id; use image url as given by user.
-        if api_deviation.blank?
-          if url =~ %r{\Ahttps://images-wixmp-}
-            extract_largest(url)
-          else
-            url
-          end
-        elsif api_deviation[:is_downloadable]
-          api_download[:src]
-        elsif api_deviation[:flash].present?
-          api_deviation.dig(:flash, :src)
-        elsif api_deviation[:videos].present?
-          api_deviation[:videos].max_by { |x| x[:filesize] }[:src]
-        else
-          src = api_deviation.dig(:content, :src)
-          extract_largest(src)
+        if download_url.present?
+          download_url
+        elsif sample_image_url.present?
+          sample_image_url
+        elsif parsed_url.full_image_url.present?
+          parsed_url.full_image_url
+        elsif parsed_url.image_url?
+          # Convert old image URLs to new wixmp.com URLs. Used when the work is deleted and we can't get a better URL.
+          #
+          # http://pre06.deviantart.net/8497/th/pre/f/2009/173/c/c/cc9686111dcffffffb5fcfaf0cf069fb.jpg
+          # => https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8b472d70-a0d6-41b5-9a66-c35687090acc/d23jbr4-8a06af02-70cb-46da-8a96-42a6ba73cdb4.jpg/v1/fill/w_786,h_1017,q_75,strp/cc9686111dcffffffb5fcfaf0cf069fb.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl0sIm9iaiI6W1t7InBhdGgiOiIvZi84YjQ3MmQ3MC1hMGQ2LTQxYjUtOWE2Ni1jMzU2ODcwOTBhY2MvZDIzamJyNC04YTA2YWYwMi03MGNiLTQ2ZGEtOGE5Ni00MmE2YmE3M2NkYjQuanBnIiwid2lkdGgiOiI8PTc4NiIsImhlaWdodCI6Ijw9MTAxNyJ9XV19.EXlDqS_4kMSDO26RTsuqE-H_XI0xSiO3dnAQRV6puqw
+          # => https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/8b472d70-a0d6-41b5-9a66-c35687090acc/d23jbr4-8a06af02-70cb-46da-8a96-42a6ba73cdb4.jpg
+          url = http.cache(1.minute).redirect_url(parsed_url)&.then { |url| Source::URL.parse(url) }
+          url&.full_image_url || url.to_s
         end
       end
 
-      def extract_largest(src)
-        if src =~ %r{\Ahttps://images-wixmp-}
-          sample, separator, * = src.partition("/v1/")
-          if separator.present?
-            src.gsub(/,q_\d+,strp\/([-_a-z0-9]+)\.jpg/, '/\1.png')
-          elsif deviation_id && deviation_id.to_i <= 790_677_560 && src !~ /\.gif\?/
-            src = src.sub(%r{(/f/[a-f0-9-]+/[a-f0-9-]+)}, '/intermediary\1')
-            src.sub(%r{/v1/(fit|fill)/.*\z}i, "")
-          else
-            src
-          end
-        else
-          src = src.sub(%r{\Ahttps?://orig\d+\.deviantart\.net}i, "http://origin-orig.deviantart.net")
-          src.gsub(/q_\d+,strp/, "q_100")
+      # Get the best possible sample URL based on the width/height restrictions in the sample token.
+      def sample_image_url
+        base_url = deviation.dig("media", "baseUri")
+
+        if base_url.present? && sample_token.present?
+          # https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/654817c0-5ba7-4591-9fd7-badae289cf88/d2wq7wl-b7f18546-753e-4d53-8051-ddb1879776c2.jpg/v1/fit/w_700,h_543,q_70,strp/pkmn_king_and_queen_by_mikotoazure_d2wq7wl-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTQzIiwicGF0aCI6IlwvZlwvNjU0ODE3YzAtNWJhNy00NTkxLTlmZDctYmFkYWUyODljZjg4XC9kMndxN3dsLWI3ZjE4NTQ2LTc1M2UtNGQ1My04MDUxLWRkYjE4Nzk3NzZjMi5qcGciLCJ3aWR0aCI6Ijw9NzAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.5YNpOk20V15EU44A7t_q_rQ_azwfLbsC32-hYgho39E
+          # => https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/654817c0-5ba7-4591-9fd7-badae289cf88/d2wq7wl-b7f18546-753e-4d53-8051-ddb1879776c2.jpg/v1/fill/w_700,h_543/pkmn_king_and_queen_by_mikotoazure_d2wq7wl.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTQzIiwicGF0aCI6IlwvZlwvNjU0ODE3YzAtNWJhNy00NTkxLTlmZDctYmFkYWUyODljZjg4XC9kMndxN3dsLWI3ZjE4NTQ2LTc1M2UtNGQ1My04MDUxLWRkYjE4Nzk3NzZjMi5qcGciLCJ3aWR0aCI6Ijw9NzAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.5YNpOk20V15EU44A7t_q_rQ_azwfLbsC32-hYgho39E
+          pretty_name = deviation.dig("media", "prettyName")
+          v1_path = deviation.dig("media", "types").pick("c").gsub(/<prettyName>[^.]*/, pretty_name)
+          url = "#{base_url}#{v1_path}?token=#{sample_token}"
+
+          Source::URL::DeviantArt.parse(url)&.full_image_url
         end
+      end
+
+      # Get the download image URL if the work is downloadable.
+      def download_url
+        # Some deviations have a download token in the metadata. This is better than the download URL from the API
+        # because it doesn't require an extra API call and it never expires.
+        if download_token.present? && deviation.dig("media", "baseUri").present?
+          base_url = deviation.dig("media", "baseUri")
+          "#{base_url}?token=#{download_token}"
+        elsif deviation_extended.dig("download", "url").present?
+          # # XXX The download url can also be obtained this way, but it requires a login and an auth cookie.
+          # url = deviation_extended.dig("download", "url")      # => https://www.deviantart.com/download/1009941765/dgpak1x-43de07ea-842f-4feb-96eb-5fddb8f96c58.png?token=af97e8685c538f7845bd38a0a2e66f7099c4a35b&ts=1709417243
+          # http.cache(1.minute).redirect_url(url).to_s.presence # => https://wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/618b1383-fa36-43cf-a5ef-dbcc45695591/dgpak1x-43de07ea-842f-4feb-96eb-5fddb8f96c58.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImV4cCI6MTcwOTQxNzI2NywiaWF0IjoxNzA5NDE2NjU3LCJqdGkiOiI2NWUzYTBkYjBmM2JmIiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNjE4YjEzODMtZmEzNi00M2NmLWE1ZWYtZGJjYzQ1Njk1NTkxXC9kZ3BhazF4LTQzZGUwN2VhLTg0MmYtNGZlYi05NmViLTVmZGRiOGY5NmM1OC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.yO47UCbiKrSi20vueE0JAXg7ZSBvOGm2wzI5Nz3l9S0&filename=emomei_by_sayohyou_dgpak1x.png
+
+          api_download["src"]
+        elsif videos.present?
+          video = videos.max_by { |video| video["f"] } # max filesize
+          video["b"] # https://wixmp-ed30a86b8c4ca887773594c2.wixmp.com/v/mp4/a31dacef-8ae2-4b0d-bb01-9e7d6911ee00/df87as3-0705c583-cd5e-4b20-bec1-8330a0cf4c25.1080p.b5c48cb0b3064fb18a287df270bb487a.mp4
+        end
+      end
+
+      def videos
+        deviation.dig("media", "types").to_a.select { |type| type["t"] == "video" }
+      end
+
+      # A token that can be used to download the sample image.
+      def sample_token
+        deviation.dig("media", "token").to_a.first
+      end
+
+      # A token that can be used to download the full image. Not always present.
+      def download_token
+        deviation.dig("media", "token").to_a.second
       end
 
       def page_url
-        if stash_page.present?
-          stash_page
-        elsif api_deviation.present?
-          api_deviation[:url]
-        elsif deviation_id.present?
-          page_url_from_image_url
-        else
-          nil
-        end
+        stash_page || deviation["url"].presence || page_url_from_image_url
       end
 
       def page_url_from_image_url
-        stash_page || parsed_url.page_url || parsed_referer&.page_url
+        # Old image URLs redirect to new wixmp.com URLs that can be used to get the page URL.
+        stash_page || parsed_url.page_url_from_redirect(http.cache(1.minute)) || parsed_referer&.page_url
       end
 
       # Sta.sh posts have the same image URLs as DeviantArt but different page URLs. We use the Sta.sh page if we have one.
@@ -73,45 +95,28 @@ module Source
       # Image: https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/83d3eb4d-13e5-4aea-a08f-8d4331d033c4/dcmjs1s-389a7505-142d-4b34-a790-ab4ea1ec9eaa.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzgzZDNlYjRkLTEzZTUtNGFlYS1hMDhmLThkNDMzMWQwMzNjNFwvZGNtanMxcy0zODlhNzUwNS0xNDJkLTRiMzQtYTc5MC1hYjRlYTFlYzllYWEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pIddc32BoLpAJt6D8YcRFonoVy9nC8RgROlYwMp3huo
       # Page: https://sta.sh/01pwva4zzf98
       def stash_page
-        if parsed_url.stash_id.present?
-          parsed_url.page_url
-        elsif parsed_referer&.stash_id.present?
-          parsed_referer.page_url
-        end
+        parsed_url.stash_url || parsed_referer&.stash_url
       end
 
       def profile_url
-        return nil if artist_name.blank?
-        "https://www.deviantart.com/#{artist_name.downcase}"
+        "https://www.deviantart.com/#{artist_name.downcase}" if artist_name.present?
       end
 
-      # Prefer the name from the url because the api metadata won't be present when
-      # the input url doesn't contain a deviation id, or the deviation is private or deleted.
       def artist_name
-        if artist_name_from_url.present?
-          artist_name_from_url
-        elsif api_metadata.present?
-          api_metadata.dig(:author, :username)
-        else
-          nil
-        end
+        user["username"].presence || parsed_url.username || parsed_referer&.username
       end
 
       def artist_commentary_title
-        api_metadata[:title]
+        deviation["title"]
       end
 
       def artist_commentary_desc
-        api_metadata[:description]
+        deviation_extended.dig("descriptionText", "html", "markup")
       end
 
       def tags
-        if api_metadata.blank?
-          return []
-        end
-
-        api_metadata[:tags].map do |tag|
-          [tag[:tag_name], "https://www.deviantart.com/tag/#{tag[:tag_name]}"]
+        deviation_extended["tags"].to_a.map do |tag|
+          [tag["name"], tag["url"]]
         end
       end
 
@@ -149,24 +154,36 @@ module Source
         parsed_url.work_id || parsed_referer&.work_id
       end
 
-      def artist_name_from_url
-        parsed_url.username || parsed_referer&.username
-      end
-
       memoize def page
         http.cache(1.minute).parsed_get(page_url_from_image_url, follow: { max_hops: 1 })
       end
 
-      # Scrape UUID from <meta property="da:appurl" content="DeviantArt://deviation/12F08C5D-A3A4-338C-2F1A-7E4E268C0E8B">
-      # For hidden or deleted works the UUID will be nil.
       memoize def uuid
-        return nil if page.nil?
-        meta = page.at_css('meta[property="da:appurl"]')
-        return nil if meta.nil?
+        deviation_extended["deviationUuid"]
+      end
 
-        appurl = meta["content"]
-        uuid = appurl[%r{\ADeviantArt://deviation/(.*)\z}, 1]
-        uuid
+      memoize def user
+        page_json.dig("@@entities", "user")&.values&.first || {}
+      end
+
+      memoize def deviation
+        page_json.dig("@@entities", "deviation")&.values&.first || {}
+      end
+
+      memoize def deviation_extended
+        page_json.dig("@@entities", "deviationExtended")&.values&.first || {}
+      end
+
+      memoize def page_json
+        return {} if page.nil?
+
+        script = page&.css("body script").to_a.map(&:text).grep(/window.__INITIAL_STATE__/).first.to_s
+        json = script[/window.__INITIAL_STATE__ = JSON.parse\("(.*)"\);/, 1]
+        unescaped_json = json.gsub('\\"', '"').gsub("\\\\", "\\")
+
+        JSON.parse(unescaped_json).with_indifferent_access
+      rescue JSON::ParserError
+        {}
       end
 
       memoize def api_client
@@ -188,7 +205,7 @@ module Source
       end
 
       memoize def api_download
-        return {} unless uuid.present? && api_deviation[:is_downloadable]
+        return {} unless uuid.present?
         api_client.download(uuid)
       end
 
@@ -196,7 +213,8 @@ module Source
         {
           deviation: api_deviation,
           metadata: api_metadata,
-          download: api_download
+          download: api_download,
+          page_json: page_json,
         }
       end
     end

commit 5e2a217e2f20c3349219574ff05d017a57080406

deviantart: fix sta.sh support.
Fix extraction of https://sta.sh/ URLs being broken by previous commit (efbb40b3c).

On sta.sh pages we don't have the __INITIAL_STATE__ JSON blob containing all the deviation metadata,
so we have to use the old method of getting the deviation UUID from the <meta> tag and using it to
make API calls to the old API.

app/logical/source/extractor/deviant_art.rb
@@ -45,6 +45,9 @@ module Source
           url = "#{base_url}#{v1_path}?token=#{sample_token}"
 
           Source::URL::DeviantArt.parse(url)&.full_image_url
+        elsif api_deviation.dig("content", "src").present? # for sta.sh posts
+          url = api_deviation.dig("content", "src")
+          Source::URL::DeviantArt.parse(url)&.full_image_url
         end
       end
 
@@ -60,6 +63,8 @@ module Source
           # url = deviation_extended.dig("download", "url")      # => https://www.deviantart.com/download/1009941765/dgpak1x-43de07ea-842f-4feb-96eb-5fddb8f96c58.png?token=af97e8685c538f7845bd38a0a2e66f7099c4a35b&ts=1709417243
           # http.cache(1.minute).redirect_url(url).to_s.presence # => https://wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/618b1383-fa36-43cf-a5ef-dbcc45695591/dgpak1x-43de07ea-842f-4feb-96eb-5fddb8f96c58.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImV4cCI6MTcwOTQxNzI2NywiaWF0IjoxNzA5NDE2NjU3LCJqdGkiOiI2NWUzYTBkYjBmM2JmIiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNjE4YjEzODMtZmEzNi00M2NmLWE1ZWYtZGJjYzQ1Njk1NTkxXC9kZ3BhazF4LTQzZGUwN2VhLTg0MmYtNGZlYi05NmViLTVmZGRiOGY5NmM1OC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.yO47UCbiKrSi20vueE0JAXg7ZSBvOGm2wzI5Nz3l9S0&filename=emomei_by_sayohyou_dgpak1x.png
 
+          api_download["src"]
+        elsif deviation.blank? && api_deviation["is_downloadable"] # for sta.sh posts
           api_download["src"]
         elsif videos.present?
           video = videos.max_by { |video| video["f"] } # max filesize
@@ -103,20 +108,26 @@ module Source
       end
 
       def artist_name
-        user["username"].presence || parsed_url.username || parsed_referer&.username
+        user["username"].presence || api_metadata.dig("author", "username") || parsed_url.username || parsed_referer&.username
       end
 
       def artist_commentary_title
-        deviation["title"]
+        deviation["title"] || api_metadata["title"]
       end
 
       def artist_commentary_desc
-        deviation_extended.dig("descriptionText", "html", "markup")
+        deviation_extended.dig("descriptionText", "html", "markup") || api_metadata["description"]
       end
 
       def tags
-        deviation_extended["tags"].to_a.map do |tag|
-          [tag["name"], tag["url"]]
+        if deviation_extended.present?
+          deviation_extended["tags"].to_a.map do |tag|
+            [tag["name"], tag["url"]]
+          end
+        else # for sta.sh posts
+          api_metadata["tags"].to_a.map do |tag|
+            [tag["tag_name"], "https://www.deviantart.com/tag/#{Danbooru::URL.escape(tag["tag_name"])}"]
+          end
         end
       end
 
@@ -159,7 +170,11 @@ module Source
       end
 
       memoize def uuid
-        deviation_extended["deviationUuid"]
+        if deviation_extended.present?
+          deviation_extended["deviationUuid"]
+        else # for sta.sh posts
+          page&.at('meta[property="da:appurl"]')&.attr("content")&.delete_prefix("DeviantArt://deviation/")
+        end
       end
 
       memoize def user
@@ -175,11 +190,12 @@ module Source
       end
 
       memoize def page_json
-        return {} if page.nil?
+        return {} if page.nil? || stash_page
 
         script = page&.css("body script").to_a.map(&:text).grep(/window.__INITIAL_STATE__/).first.to_s
         json = script[/window.__INITIAL_STATE__ = JSON.parse\("(.*)"\);/, 1]
-        unescaped_json = json.gsub('\\"', '"').gsub("\\\\", "\\")
+        unescaped_json = json.to_s.gsub('\\"', '"').gsub("\\\\", "\\")
+        return {} if unescaped_json.blank?
 
         JSON.parse(unescaped_json).with_indifferent_access
       rescue JSON::ParserError

test/unit/sources/deviant_art_test.rb
@@ -2,8 +2,7 @@ require "test_helper"
 
 module Sources
   class DeviantArtTest < ActiveSupport::TestCase
-    def setup
-      super
+    setup do
       skip "DeviantArt API keys not set" unless Danbooru.config.deviantart_client_id.present?
     end
 
@@ -325,6 +324,66 @@ module Sources
       )
     end
 
+    context "A deviantart page for a Flash file" do
+      strategy_should_work(
+        "https://www.deviantart.com/midorynn/art/NieR-Automata-Anime-703917761",
+        image_urls: [%r{https://wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e1d5122b-6fee-44df-8b8f-e6e8daa3396d/dbn3ef5-9e051a71-251d-4e0f-b5f1-3beb5e6a8667.swf\?token=}],
+        media_files: [{ size: 4_243_457, width: 700, height: 300 }],
+        artist_name: "midorynn",
+        profile_url: "https://www.deviantart.com/midorynn",
+        tags: %w[animation fanart nier_automata nierautomata nier_automata_2b],
+        artist_commentary_title: "NieR: Automata Anime"
+      )
+    end
+
+    context "For Sta.sh:" do
+      context "A https://sta.sh/:id url" do
+        strategy_should_work(
+          "https://sta.sh/0wxs31o7nn2",
+          image_urls: [%r{\Ahttps://wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/83d3eb4d-13e5-4aea-a08f-8d4331d033c4/dcmga0s-a345a815-2436-4ab5-8941-492011e1bff6.png}],
+          media_files: [{ size: 106_741, width: 750, height: 730 }],
+          page_url: "https://sta.sh/0wxs31o7nn2",
+          artist_name: "noizave",
+          profile_url: "https://www.deviantart.com/noizave",
+          tags: [],
+          artist_commentary_title: "A pepe",
+          artist_commentary_desc: "This is a test."
+        )
+      end
+
+      context "A https://orig00.deviantart.net/* image url with a https://sta.sh/:id referer" do
+        strategy_should_work(
+          "https://orig00.deviantart.net/0fd2/f/2018/252/9/c/a_pepe_by_noizave-dcmga0s.png",
+          image_urls: [%r{\Ahttps://wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/83d3eb4d-13e5-4aea-a08f-8d4331d033c4/dcmga0s-a345a815-2436-4ab5-8941-492011e1bff6.png}],
+          media_files: [{ size: 106_741, width: 750, height: 730 }],
+          referer: "https://sta.sh/0wxs31o7nn2",
+          page_url: "https://sta.sh/0wxs31o7nn2",
+          artist_name: "noizave",
+          profile_url: "https://www.deviantart.com/noizave",
+          tags: [],
+          artist_commentary_title: "A pepe",
+          artist_commentary_desc: "This is a test."
+        )
+      end
+
+      context "A https://orig00.deviantart.net/* image url without the referer" do
+        strategy_should_work(
+          "https://orig00.deviantart.net/0fd2/f/2018/252/9/c/a_pepe_by_noizave-dcmga0s.png",
+          image_urls: ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/83d3eb4d-13e5-4aea-a08f-8d4331d033c4/dcmga0s-a345a815-2436-4ab5-8941-492011e1bff6.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi84M2QzZWI0ZC0xM2U1LTRhZWEtYTA4Zi04ZDQzMzFkMDMzYzQvZGNtZ2Ewcy1hMzQ1YTgxNS0yNDM2LTRhYjUtODk0MS00OTIwMTFlMWJmZjYucG5nIn1dXX0.b29qVmG1on0SIwS0KhZjYM_LowH0A1NvO9cyvHde-mw"],
+          media_files: [{ size: 106_741, width: 750, height: 730 }],
+          # if all we have is the image url, then we can't tell that this is really a sta.sh image.
+          site_name: "Deviant Art",
+          # this is the wrong page, but there's no way to know the correct sta.sh page without the referer.
+          page_url: "https://www.deviantart.com/noizave/art/A-Pepe-763305148",
+          artist_name: "noizave",
+          profile_url: "https://www.deviantart.com/noizave",
+          tags: [],
+          artist_commentary_title: nil,
+          artist_commentary_desc: nil
+        )
+      end
+    end
+
     should "Parse DeviantArt URLs correctly" do
       source1 = "http://fc06.deviantart.net/fs71/f/2013/295/d/7/you_are_already_dead__by_mar11co-d6rgm0e.jpg"
       source2 = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/8b472d70-a0d6-41b5-9a66-c35687090acc/d23jbr4-8a06af02-70cb-46da-8a96-42a6ba73cdb4.jpg/v1/fill/w_786,h_1017,q_70,strp/silverhawks_quicksilver_by_edsfox_d23jbr4-pre.jpg"
@@ -332,6 +391,7 @@ module Sources
       source4 = "http://fc00.deviantart.net/fs71/f/2013/337/3/5/35081351f62b432f84eaeddeb4693caf-d6wlrqs.jpg"
       source5 = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/76098ac8-04ab-4784-b382-88ca082ba9b1/d9x7lmk-595099de-fe8f-48e5-9841-7254f9b2ab8d.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNzYwOThhYzgtMDRhYi00Nzg0LWIzODItODhjYTA4MmJhOWIxXC9kOXg3bG1rLTU5NTA5OWRlLWZlOGYtNDhlNS05ODQxLTcyNTRmOWIyYWI4ZC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.KFOVXAiF8MTlLb3oM-FlD0nnDvODmjqEhFYN5I2X5Bc"
       source6 = "https://fav.me/dbc3a48"
+      source7 = "https://wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e1d5122b-6fee-44df-8b8f-e6e8daa3396d/dbn3ef5-9e051a71-251d-4e0f-b5f1-3beb5e6a8667.swf?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImV4cCI6MTcwOTQ2NTcwNywiaWF0IjoxNzA5NDY1MDk3LCJqdGkiOiI2NWU0NWUxMzAxNTk2Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZTFkNTEyMmItNmZlZS00NGRmLThiOGYtZTZlOGRhYTMzOTZkXC9kYm4zZWY1LTllMDUxYTcxLTI1MWQtNGUwZi1iNWYxLTNiZWI1ZTZhODY2Ny5zd2YifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.6ZShJVsXxVs73R2Akw_9t-BMJKjqPUbNKHkIbzMsqeU"
 
       assert(Source::URL.image_url?(source1))
       assert(Source::URL.image_url?(source2))
@@ -339,6 +399,7 @@ module Sources
       assert(Source::URL.image_url?(source4))
       assert(Source::URL.image_url?(source5))
       assert(Source::URL.page_url?(source6))
+      assert(Source::URL.image_url?(source7))
 
       assert_equal("https://www.deviantart.com/mar11co/art/You-Are-Already-Dead-408921710", Source::URL.page_url(source1))
       assert_equal("https://www.deviantart.com/edsfox/art/Silverhawks-Quicksilver-126872896", Source::URL.page_url(source2))
@@ -346,6 +407,7 @@ module Sources
       assert_equal("https://www.deviantart.com/deviation/417560500", Source::URL.page_url(source4))
       assert_equal("https://www.deviantart.com/deviation/599977532", Source::URL.page_url(source5))
       assert_equal("https://www.deviantart.com/deviation/685436408", Source::URL.page_url(source6))
+      assert_equal("https://www.deviantart.com/deviation/703917761", Source::URL.page_url(source7))
 
       assert_equal("https://www.deviantart.com/noizave", Source::URL.profile_url("https://noizave.daportfolio.com"))
       assert_equal("https://www.deviantart.com/noizave", Source::URL.profile_url("https://noizave.artworkfolio.com"))

test/unit/sources/stash_test.rb
@@ -1,45 +1,0 @@
-require 'test_helper'
-
-module Sources
-  class StashTest < ActiveSupport::TestCase
-    def setup
-      super
-      skip "DeviantArt API keys not set" unless Danbooru.config.deviantart_client_id.present?
-    end
-
-    context "A https://sta.sh/:id url" do
-      strategy_should_work(
-        "https://sta.sh/0wxs31o7nn2",
-        image_urls: [%r{\Ahttps://wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/83d3eb4d-13e5-4aea-a08f-8d4331d033c4/dcmga0s-a345a815-2436-4ab5-8941-492011e1bff6.png}],
-        page_url: "https://sta.sh/0wxs31o7nn2",
-        artist_name: "noizave",
-        profile_url: "https://www.deviantart.com/noizave",
-        artist_commentary_title: "A pepe",
-        artist_commentary_desc: "This is a test."
-      )
-    end
-
-    context "A https://orig00.deviantart.net/* image url with a https://sta.sh/:id referer" do
-      strategy_should_work(
-        "https://orig00.deviantart.net/0fd2/f/2018/252/9/c/a_pepe_by_noizave-dcmga0s.png",
-        image_urls: [%r{\Ahttps://wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/83d3eb4d-13e5-4aea-a08f-8d4331d033c4/dcmga0s-a345a815-2436-4ab5-8941-492011e1bff6.png}],
-        referer: "https://sta.sh/0wxs31o7nn2",
-        page_url: "https://sta.sh/0wxs31o7nn2",
-        artist_name: "noizave",
-        profile_url: "https://www.deviantart.com/noizave",
-        artist_commentary_title: "A pepe",
-        artist_commentary_desc: "This is a test."
-      )
-    end
-
-    context "A https://orig00.deviantart.net/* image url without the referer" do
-      strategy_should_work(
-        "https://orig00.deviantart.net/0fd2/f/2018/252/9/c/a_pepe_by_noizave-dcmga0s.png",
-        # if all we have is the image url, then we can't tell that this is really a sta.sh image.
-        site_name: "Deviant Art",
-        # this is the wrong page, but there's no way to know the correct sta.sh page without the referer.
-        page_url: "https://www.deviantart.com/noizave/art/A-Pepe-763305148"
-      )
-    end
-  end
-end