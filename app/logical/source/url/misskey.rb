77ec0161a6426cb356a57af4eeac5c3a167d5442
evazion <noizave@gmail.com>
2024-03-26 23:22:30 -0500

    misskey: support new misskeyusercontent.jp URLs.
    Misskey switched domains from https://media.misskeyusercontent.com to https://media.misskeyusercontent.jp.

--- app/logical/source/url/misskey.rb
+++ app/logical/source/url/misskey.rb
@@ -4,9 +4,7 @@ class Source::URL::Misskey < Source::URL
   attr_reader :username, :user_id, :note_id
 
   def self.match?(url)
-    url.domain.in?(%w[misskey.io misskey.art misskey.design]) ||
-      (url.host == "media.misskeyusercontent.com" && url.path.starts_with?("/io/")) ||
-      (url.host == "s3.arkjp.net" && url.path.starts_with?("/misskey/"))
+    url.domain.in?(%w[misskey.io misskey.art misskey.design misskeyusercontent.com misskeyusercontent.jp]) || url.host.match?(/\A(s3|nos3)\.arkjp\.net\z/)
   end
 
   def parse
@@ -26,13 +24,26 @@ class Source::URL::Misskey < Source::URL
     in _, _, "notes", note_id
       @note_id = note_id
 
+    # https://media.misskeyusercontent.jp/io/dfca7bd4-c073-4ea0-991f-313ab3a77847.png
+    # https://media.misskeyusercontent.com/io/thumbnail-e9f307e4-3fad-435f-91b6-3768d688491d.webp | https://misskey.io/notes/9hfx0ezipu
+    # https://media.misskeyusercontent.com/io/webpublic-a2cdd9c7-0449-4a61-b453-b5c7b2134677.png
+    # https://proxy.misskeyusercontent.com/image.webp?url=https%3A%2F%2Fimg.pawoo.net%2Fmedia_attachments%2Ffiles%2F111%2F232%2F575%2F490%2F284%2F147%2Foriginal%2F9aaf0c71a41b5647.jpeg | https://misskey.io/notes/9ktdpaq840
+    # https://media.misskeyusercontent.com/misskey/7d2adf4a-b2dd-40b4-ba27-916e44f7bd48.png | https://misskey.io/notes/9bxaf592x6
+    # https://nos3.arkjp.net/image.webp?url=https%3A%2F%2Fimg.pawoo.net%2Fmedia_attachments%2Ffiles%2F110%2F314%2F466%2F230%2F358%2F806%2Foriginal%2F6fbcc38659d3cb97.jpeg | https://misskey.io/notes/9edoxxq8h8
+    # https://s3.arkjp.net/misskey/930fe4fb-c07b-4439-804e-06fb472d698f.gif | https://misskey.io/notes/9dd5xo5zda
+    # https://files.misskey.art//webpublic-94d9354f-ddba-406b-b878-4ce02ccfa505.webp | https://misskey.art/notes/9pjpq1zrcy
+    # https://file.misskey.design/post/webpublic-ac7072e9-812f-460b-ad24-1f303a62f0b4.webp | https://misskey.design/notes/9r8c6x1n1p
+    in _, _, *path if file_ext.in?(%w[jpg jpeg png gif webp webm mp4])
+      @image_url = true
+
     else
       nil
     end
   end
 
   def site_name
-    if host.in?(%w[s3.arkjp.net media.misskeyusercontent.com])
+    case domain
+    in "arkjp.net", "misskeyusercontent.com", "misskeyusercontent.jp"
       "Misskey.io"
     else
       domain.capitalize
@@ -40,22 +51,20 @@ class Source::URL::Misskey < Source::URL
   end
 
   def image_url?
-    host.in?(%w[s3.arkjp.net media.misskeyusercontent.com]) ||
-      (host == "misskey.art" && path.starts_with?("/files/")) ||
-      (host == "misskey.design" && path.starts_with?("/post/"))
+    @image_url
   end
 
   def page_url
     if note_id.present?
-      "https://#{domain}/notes/#{note_id}"
+      "https://#{host}/notes/#{note_id}"
     end
   end
 
   def profile_url
     if username.present?
-      "https://#{domain}/@#{username}"
+      "https://#{host}/@#{username}"
     elsif user_id.present?
-      "https://#{domain}/users/#{user_id}"
+      "https://#{host}/users/#{user_id}"
     end
   end
 end
--- test/unit/sources/misskey_test.rb
+++ test/unit/sources/misskey_test.rb
@@ -57,7 +57,7 @@ module Sources
       )
     end
 
-    context "A direct image url" do
+    context "A s3.arkjp.net direct image url" do
       strategy_should_work(
         "https://s3.arkjp.net/misskey/99ae6116-2896-4cf3-9abc-e9746cd2408e.jpg",
         image_urls: ["https://s3.arkjp.net/misskey/99ae6116-2896-4cf3-9abc-e9746cd2408e.jpg"],
@@ -66,9 +66,42 @@ module Sources
       )
     end
 
+    context "A media.misskeyusercontent.jp direct image url" do
+      strategy_should_work(
+        "https://media.misskeyusercontent.jp/io/webpublic-806fd8e2-3425-486f-975e-2fb57d8e651a.png",
+        image_urls: ["https://media.misskeyusercontent.jp/io/webpublic-806fd8e2-3425-486f-975e-2fb57d8e651a.png"],
+        media_files: [{ file_size: 386_451 }]
+      )
+    end
+
+    context "A files.misskey.art direct image url" do
+      strategy_should_work(
+        "https://files.misskey.art//webpublic-94d9354f-ddba-406b-b878-4ce02ccfa505.webp",
+        image_urls: ["https://files.misskey.art//webpublic-94d9354f-ddba-406b-b878-4ce02ccfa505.webp"],
+        media_files: [{ file_size: 35_338 }]
+      )
+    end
+
+    context "A file.misskey.design direct image url" do
+      strategy_should_work(
+        "https://file.misskey.design/post/webpublic-ac7072e9-812f-460b-ad24-1f303a62f0b4.webp",
+        image_urls: ["https://file.misskey.design/post/webpublic-ac7072e9-812f-460b-ad24-1f303a62f0b4.webp"],
+        media_files: [{ file_size: 188_294 }]
+      )
+    end
+
     should "Parse Misskey URLs correctly" do
       assert(Source::URL.image_url?("https://s3.arkjp.net/misskey/thumbnail-10c4379a-b999-4148-9d32-7bb6f22453bf.webp"))
       assert(Source::URL.image_url?("https://s3.arkjp.net/misskey/7d2adf4a-b2dd-40b4-ba27-916e44f7bd48.png"))
+      assert(Source::URL.image_url?("https://media.misskeyusercontent.jp/io/dfca7bd4-c073-4ea0-991f-313ab3a77847.png"))
+      assert(Source::URL.image_url?("https://media.misskeyusercontent.com/io/thumbnail-e9f307e4-3fad-435f-91b6-3768d688491d.webp"))
+      assert(Source::URL.image_url?("https://media.misskeyusercontent.com/io/webpublic-a2cdd9c7-0449-4a61-b453-b5c7b2134677.png"))
+      assert(Source::URL.image_url?("https://proxy.misskeyusercontent.com/image.webp?url=https%3A%2F%2Fimg.pawoo.net%2Fmedia_attachments%2Ffiles%2F111%2F232%2F575%2F490%2F284%2F147%2Foriginal%2F9aaf0c71a41b5647.jpeg"))
+      assert(Source::URL.image_url?("https://media.misskeyusercontent.com/misskey/7d2adf4a-b2dd-40b4-ba27-916e44f7bd48.png"))
+      assert(Source::URL.image_url?("https://nos3.arkjp.net/image.webp?url=https%3A%2F%2Fimg.pawoo.net%2Fmedia_attachments%2Ffiles%2F110%2F314%2F466%2F230%2F358%2F806%2Foriginal%2F6fbcc38659d3cb97.jpeg"))
+      assert(Source::URL.image_url?("https://s3.arkjp.net/misskey/930fe4fb-c07b-4439-804e-06fb472d698f.gif"))
+      assert(Source::URL.image_url?("https://files.misskey.art//webpublic-94d9354f-ddba-406b-b878-4ce02ccfa505.webp"))
+      assert(Source::URL.image_url?("https://file.misskey.design/post/webpublic-ac7072e9-812f-460b-ad24-1f303a62f0b4.webp"))
 
       assert(Source::URL.page_url?("https://misskey.io/notes/9bxaf592x6"))
       assert_equal("https://misskey.io/notes/9bxaf592x6", Source::URL.page_url("https://misskey.io/notes/9bxaf592x6#pswp"))
