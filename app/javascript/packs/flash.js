9c5ba1b91853f04791e2843ac12052d337b43784
Talulah <talulah@reunion.is>
2024-03-27 10:23:41 -0300

    Fix Ruffle packs.

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -1,5 +1,5 @@
 import "@ruffle-rs/ruffle/ruffle.js";
-import "@ruffle-rs/ruffle/core.ruffle.57cb6721d3c1f1cfa457.js";
-import "@ruffle-rs/ruffle/core.ruffle.76c006852c6396d9077b.js";
-import "@ruffle-rs/ruffle/2be296c565a710a65d98.wasm";
-import "@ruffle-rs/ruffle/40957fb534f12938007d.wasm";
+import "@ruffle-rs/ruffle/core.ruffle.acef68c032af24bfd8c8.js";
+import "@ruffle-rs/ruffle/core.ruffle.bb26a71ccb0aa747c8a7.js";
+import "@ruffle-rs/ruffle/ca182dee324f8e0b099a.wasm";
+import "@ruffle-rs/ruffle/e4ac35563b7246e7d3c0.wasm";
