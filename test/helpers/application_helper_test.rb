ff8a4d4fbde1862c4378119982e87e7054b4b717
DCrow <kuroimist@gmail.com>
2024-03-27 12:48:02 +0300

    Change media asset links to use the same format

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -10,5 +10,28 @@ class ApplicationHelperTest < ActionView::TestCase
         assert_equal("", format_text(dtext))
       end
     end
+
+    context "link_to_media_asset method" do
+      should "render link" do
+        media_asset = create(:media_asset, file_ext: "jpg")
+        link = link_to_media_asset(media_asset)
+        text = "#{ActiveSupport::NumberHelper.number_to_human_size(media_asset.file_size)} .#{media_asset.file_ext}, " \
+               "#{media_asset.image_width}x#{media_asset.image_height}"
+
+        assert_match(/#{Regexp.quote(media_asset_path(media_asset))}/, link)
+        assert_match(/#{Regexp.quote(text)}/, link)
+      end
+
+      should "render link with duration" do
+        media_asset = create(:media_asset, file_ext: "mp4", duration: 30)
+        link = link_to_media_asset(media_asset)
+        text = "#{ActiveSupport::NumberHelper.number_to_human_size(media_asset.file_size)} .#{media_asset.file_ext}, " \
+               "#{media_asset.image_width}x#{media_asset.image_height} " \
+               "(#{Danbooru::Helpers.duration_to_hhmmss(media_asset.duration)})"
+
+        assert_match(/#{Regexp.quote(media_asset_path(media_asset))}/, link)
+        assert_match(/#{Regexp.quote(text)}/, link)
+      end
+    end
   end
 end
