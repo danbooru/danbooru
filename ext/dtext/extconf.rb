b25d12811e7629107ad77f780f05612d8210acc7
evazion <noizave@gmail.com>
2024-03-29 22:56:52 -0500

    Fix -flto compilation warning.
    Fix this compilation warning:
    
        lto-wrapper: warning: using serial compilation of 5 LTRANS jobs
        lto-wrapper: note: see the ‘-flto’ option documentation for more information

--- ext/dtext/extconf.rb
+++ ext/dtext/extconf.rb
@@ -5,9 +5,9 @@ CONFIG["MKMF_VERBOSE"] = "1"
 $warnflags = "-Wall -Wextra -Werror -Wno-unused-parameter -Wuninitialized -Wnull-dereference -Wformat=2 -Wformat-overflow=2 -Wstrict-overflow=5"
 
 if ENV["DTEXT_DEBUG"]
-  $CXXFLAGS << " -std=c++20 -O2 -g3 -pipe -flto -fno-strict-aliasing -fsanitize=undefined,leak -D_GNU_SOURCE -DDEBUG -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_GLIBCXX_SANITIZE_VECTOR -D_FORTIFY_SOURCE=2"
+  $CXXFLAGS << " -std=c++20 -O2 -g3 -pipe -flto=auto -fno-strict-aliasing -fsanitize=undefined,leak -D_GNU_SOURCE -DDEBUG -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_GLIBCXX_SANITIZE_VECTOR -D_FORTIFY_SOURCE=2"
 else
-  $CXXFLAGS << " -std=c++20 -O2 -pipe -flto -fno-strict-aliasing -D_GNU_SOURCE -DNDEBUG"
+  $CXXFLAGS << " -std=c++20 -O2 -pipe -flto=auto -fno-strict-aliasing -D_GNU_SOURCE -DNDEBUG"
 end
 
 create_makefile "dtext/dtext"
