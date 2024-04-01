2228b39c363fd3f78377380bcd203dce1c773466
evazion <noizave@gmail.com>
2024-04-01 02:21:26 -0500

    config: fixup mail delivery settings.
    Fixup bug in 10b754d71 where mail settings didn't take effect correctly because they were in an
    after_initialize hook.

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -20,26 +20,24 @@ else
 end
 
 if mail_delivery_url.present?
-  Rails.application.config.after_initialize do
-    url = Danbooru::URL.parse!(mail_delivery_url, schemes: %w[smtp smtps])
-    params = url.params.dup.symbolize_keys
-    default_port = (url.scheme == "smtp") ? 587 : 465
+  url = Danbooru::URL.parse!(mail_delivery_url, schemes: %w[smtp smtps])
+  params = url.params.dup.symbolize_keys
+  default_port = (url.scheme == "smtp") ? 587 : 465
 
-    Rails.application.config.action_mailer.raise_delivery_errors = true
-    Rails.application.config.action_mailer.delivery_method = :smtp
-    Rails.application.config.action_mailer.smtp_settings = {
-      address: url.host,
-      port: url.port || default_port,
-      user_name: Addressable::URI.unencode(url.user),
-      password: Addressable::URI.unencode(url.password),
-      enable_starttls: params.delete(:enable_starttls)&.truthy?,
-      enable_starttls_auto: params.delete(:enable_starttls_auto)&.truthy?,
-      openssl_verify_mode: params.delete(:openssl_verify_mode)&.to_sym,
-      authentication: params.delete(:authentication)&.to_sym,
-      tls: params.delete(:tls) || url.scheme == "smtps",
-      open_timeout: params.delete(:open_timeout)&.to_i || 5,
-      read_timeout: params.delete(:read_timeout)&.to_i || 5,
-      **params,
-    }.compact
-  end
+  Rails.application.config.action_mailer.raise_delivery_errors = true
+  Rails.application.config.action_mailer.delivery_method = :smtp
+  Rails.application.config.action_mailer.smtp_settings = {
+    address: url.host,
+    port: url.port || default_port,
+    user_name: Addressable::URI.unencode(url.user),
+    password: Addressable::URI.unencode(url.password),
+    enable_starttls: params.delete(:enable_starttls)&.truthy?,
+    enable_starttls_auto: params.delete(:enable_starttls_auto)&.truthy?,
+    openssl_verify_mode: params.delete(:openssl_verify_mode)&.to_sym,
+    authentication: params.delete(:authentication)&.to_sym,
+    tls: params.delete(:tls) || url.scheme == "smtps",
+    open_timeout: params.delete(:open_timeout)&.to_i || 5,
+    read_timeout: params.delete(:read_timeout)&.to_i || 5,
+    **params,
+  }.compact
 end
