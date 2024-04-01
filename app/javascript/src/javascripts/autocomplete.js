danbooru/danbooru

commit 91871d8b24ece53f89659811e492cecb53793f8f

autocomplete: add autocompletion for :emoji: syntax.
Make it so that typing :blah: shows an autocomplete menu for emoji containing the word "blah".

Also fix some bugs in @-mention autocomplete. Fix it not working when the @ sign occurred after
punctuation (e.g. `<@blah>`), and fix the menu not closing after you typed a space at the end of the
username.

app/javascript/src/javascripts/autocomplete.js
@@ -25,7 +25,7 @@ Autocomplete.initialize_all = function() {
   });
 
   this.initialize_tag_autocomplete();
-  this.initialize_mention_autocomplete($("form div.input.dtext textarea"));
+  this.initialize_dtext_autocomplete($("form div.input.dtext textarea"));
   this.initialize_fields($('[data-autocomplete="tag"]'), "tag");
   this.initialize_fields($('[data-autocomplete="artist"]'), "artist");
   this.initialize_fields($('[data-autocomplete="pool"]'), "pool");
@@ -48,34 +48,31 @@ Autocomplete.initialize_fields = function($fields, type) {
   });
 };
 
-Autocomplete.initialize_mention_autocomplete = function($fields) {
+// Autocomplete @-mentions and :emoji: references in DText.
+Autocomplete.initialize_dtext_autocomplete = function($fields) {
   $fields.autocomplete({
     select: function(event, ui) {
       Autocomplete.insert_completion(this, ui.item.value);
       return false;
     },
+    position: {
+      at: "left top",
+      my: "left bottom"
+    },
     source: async function(req, resp) {
       var cursor = this.element.get(0).selectionStart;
-      var name = null;
+      let match = req.term.substring(0, cursor).match(/(?<=[ \r\n/"\\()[\]{}<>]|^)([@:])(\S*)$/);
+      let type = match?.[1];
+      let name = match?.[2];
 
-      for (var i = cursor; i >= 1; --i) {
-        if (req.term[i - 1] === " ") {
-          return;
-        }
-
-        if (req.term[i - 1] === "@") {
-          if (i === 1 || /[ \r\n]/.test(req.term[i - 2])) {
-            name = req.term.substring(i, cursor);
-            break;
-          } else {
-            return;
-          }
-        }
-      }
-
-      if (name) {
+      if (type === "@") {
         let results = await Autocomplete.autocomplete_source(name, "mention");
         resp(results);
+      } else if (type === ":") {
+        let results = await Autocomplete.autocomplete_source(name, "emoji", { limit: 50, allowEmpty: true });
+        resp(results);
+      } else {
+        resp([]);
       }
     }
   });
@@ -149,8 +146,8 @@ Autocomplete.render_item = function(list, item) {
   return list.append(item.html);
 };
 
-Autocomplete.autocomplete_source = async function(query, type) {
-  if (query === "") {
+Autocomplete.autocomplete_source = async function (query, type, { allowEmpty = false, limit = Autocomplete.MAX_RESULTS } = {}) {
+  if (query === "" && !allowEmpty) {
     return [];
   }
 
@@ -158,7 +155,7 @@ Autocomplete.autocomplete_source = async function(query, type) {
     "search[query]": query,
     "search[type]": type,
     "version": Autocomplete.VERSION,
-    "limit": Autocomplete.MAX_RESULTS
+    "limit": limit,
   });
 
   let items = $(html).find("li").toArray().map(item => {
@@ -178,4 +175,3 @@ $(document).ready(function() {
 });
 
 export default Autocomplete;
-

app/components/autocomplete_component.rb
@@ -16,6 +16,8 @@ class AutocompleteComponent < ApplicationComponent
       link_to user_path(result.id), class: "user-#{result.level}", "@click.prevent": "", &block
     when "pool"
       link_to pool_path(result.id), class: "pool-category-#{result.category}", "@click.prevent": "", &block
+    when "emoji"
+      link_to "javascript:void(0)", class: "tag-type-#{Tag.categories.general}", "@click.prevent": "", &block
     else
       link_to posts_path(tags: result.value), class: "tag-type-#{result.category}", "@click.prevent": "", &block
     end
@@ -44,6 +46,8 @@ class AutocompleteComponent < ApplicationComponent
       highlight_matching_words(result.value, query)
     elsif result.type == "mention"
       highlight_wildcard_match(result.label, query + "*")
+    elsif result.type == "emoji"
+      highlight_wildcard_match(result.label, "*" + query + "*")
     elsif query.include?("*")
       highlight_wildcard_match(result.value, query)
     else

app/components/autocomplete_component/autocomplete_component.html.erb
@@ -3,7 +3,10 @@
     <%= tag.li class: "ui-menu-item", "data-autocomplete-type": result.type, "data-autocomplete-value": result.value do %>
       <div class="ui-menu-item-wrapper" tabindex="-1">
         <%= link_to_result result do %>
-          <% if result.antecedent.present? %>
+          <% if result.type == "emoji" %>
+            <div class="inline-block prose"><%= DText.new(result.value, inline: true).format_text %></div>
+            <span class="autocomplete-text"><%= highlight_result(result) %></span>
+          <% elsif result.antecedent.present? %>
             <span class="autocomplete-antecedent"><%= highlight_antecedent(result) %></span>
             <span class="autocomplete-arrow">→</span>
             <%= result.label %>

app/javascript/src/styles/common/autocomplete.scss
@@ -1,8 +1,14 @@
+@import "./utilities";
+
 .ui-autocomplete.ui-widget {
   font-size: var(--text-sm);
   border: 1px solid var(--autocomplete-border-color);
   box-shadow: var(--shadow-lg);
   max-width: 480px;
+  max-height: 480px;
+
+  @extend %thin-scrollbar;
+  padding-right: 0;
 
   div.ui-menu-item-wrapper {
     padding: 0.25em 0.4em;

app/javascript/src/styles/common/dtext.scss
@@ -194,13 +194,20 @@ div.prose {
     }
   }
 
-  emoji[data-mode="inline"] img { max-height: 24px; }
-  emoji[data-mode="block"]  img { max-height: 128px; }
-
   emoji[data-mode="block"] {
     display: inline-block;
     font-size: var(--text-xxl);
     line-height: 1.25;
+  }
+
+  emoji[data-mode="inline"] img {
+    max-height: 24px;
+    max-width: 24px;
+  }
+
+  emoji[data-mode="block"] img {
+    max-height: 128px;
+    max-width: 128px;
   }
 
   details {

app/logical/autocomplete_service.rb
@@ -74,6 +74,8 @@ class AutocompleteService
       autocomplete_user(query)
     when :mention
       autocomplete_mention(query)
+    when :emoji
+      autocomplete_emoji(query)
     when :pool
       autocomplete_pool(query)
     when :favorite_group
@@ -371,6 +373,27 @@ class AutocompleteService
     end
   end
 
+  # Complete an emoji reference (e.g. `:smile:`).
+
+  # @param string [String] The name of the emoji (e.g. `smile`, not `:smile:`)
+  # @return [Array<Hash>] the autocomplete results
+  def autocomplete_emoji(emoji)
+    normalized_emoji = emoji.downcase.chomp(":")
+    emojis = Danbooru.config.dtext_emojis.keys
+
+    results = emojis.grep(/#{normalized_emoji}/i).sort_by do |match|
+      exact = match.casecmp?(normalized_emoji) ? 1 : 0
+      prefix = match.downcase.starts_with?(normalized_emoji) ? 1 : 0
+
+      # Sort exact name matches first, then prefix matches, then sort by name.
+      [-exact, -prefix, match]
+    end.take(limit)
+
+    results.map do |v|
+      { type: "emoji", label: ":#{v}:", value: ":#{v}:" }
+    end
+  end
+
   # Complete a search typed in the browser address bar.
   # @param string [String] the name of the tag
   # @return [Array<(String, [Array<String>])>] the autocomplete results
@@ -384,7 +407,9 @@ class AutocompleteService
   # How long autocomplete results can be cached. Cache short result lists (<10
   # results) for less time because they're more likely to change.
   def cache_duration
-    if autocomplete_results.size == limit
+    if type == :emoji
+      5.minutes
+    elsif autocomplete_results.size == limit
       24.hours
     else
       1.hour
@@ -396,7 +421,7 @@ class AutocompleteService
   def cache_publicly?
     if type == :tag_query && parsed_query.tag_names.one?
       true
-    elsif type.in?(%i[tag artist wiki_page pool opensearch])
+    elsif type.in?(%i[tag artist wiki_page pool emoji opensearch])
       true
     else
       false

app/logical/d_text.rb
@@ -245,9 +245,14 @@ class DText
     node.replace(html)
   end
 
+  # Insert the emoji value into an <emoji> node.
   def replace_emoji!(node)
-    name = node["data-name"]
-    node.inner_html = emojis[name]
+    lowercase_name = node["data-name"]
+    proper_name, value = emojis.find { |name, _| name.casecmp?(lowercase_name) }
+
+    node["title"] = ":#{proper_name}:"
+    node["data-mode"] = "inline" if inline
+    node.inner_html = value
   end
 
   # Return the DText parsed to HTML. This is before the HTML is rewritten to colorize wiki links and replace

commit fb13ffdd9092f358ce6ebe5e897bcf21b9286afc

autocomplete: fix Javascript breakage in Safari on iOS <16.4.
Fixup Javascript breakage introduced in 91871d8b2. Safari on iOS <16.4 doesn't supported positive
lookbehind assertions in regexes. This caused a syntax error that broke all the site's Javascript.

We don't actually need a lookbehind assertion here, so we can just get rid of it.

app/javascript/src/javascripts/autocomplete.js
@@ -61,9 +61,11 @@ Autocomplete.initialize_dtext_autocomplete = function($fields) {
     },
     source: async function(req, resp) {
       var cursor = this.element.get(0).selectionStart;
-      let match = req.term.substring(0, cursor).match(/(?<=[ \r\n/"\\()[\]{}<>]|^)([@:])(\S*)$/);
-      let type = match?.[1];
-      let name = match?.[2];
+      let match = req.term.substring(0, cursor).match(/([ \r\n/"\\()[\]{}<>]|^)([@:])(\S*)$/);
+
+      let prefix = match?.[1];
+      let type = match?.[2];
+      let name = match?.[3];
 
       if (type === "@") {
         let results = await Autocomplete.autocomplete_source(name, "mention");