a9f35ed019a1edc720dffc530026a15598a527b3
evazion <noizave@gmail.com>
2024-04-06 15:04:52 -0500

    hentai foundry: fix parsing of non-www URLs.

--- app/logical/source/url/hentai_foundry.rb
+++ app/logical/source/url/hentai_foundry.rb
@@ -8,30 +8,30 @@ class Source::URL::HentaiFoundry < Source::URL
   end
 
   def parse
-    case [host, *path_segments]
+    case [subdomain, domain, *path_segments]
 
     # https://pictures.hentai-foundry.com/a/Afrobull/795025/Afrobull-795025-kuroeda.png
     # https://pictures.hentai-foundry.com/_/-MadKaiser-/532792/-MadKaiser--532792-FFXIV_Miqote.png
-    in "pictures.hentai-foundry.com", _, username, /^\d+$/ => work_id, file
+    in "pictures", "hentai-foundry.com", _, username, /^\d+$/ => work_id, file
       @file = file
       @username = username
       @work_id = work_id
 
     # http://pictures.hentai-foundry.com//s/soranamae/363663.jpg
-    in "pictures.hentai-foundry.com", _, username, /^(\d+)\.\w+$/ => file
+    in "pictures", "hentai-foundry.com", _, username, /^(\d+)\.\w+$/ => file
       @file = file
       @username = username
       @work_id = $1
 
     # http://www.hentai-foundry.com/piccies/d/dmitrys/1183.jpg
-    in "www.hentai-foundry.com", "piccies", _, username, /^(\d+)\.\w+$/ => file
+    in _, "hentai-foundry.com", "piccies", _, username, /^(\d+)\.\w+$/ => file
       @file = file
       @username = username
       @work_id = $1
 
     # https://www.hentai-foundry.com/pictures/user/Afrobull/795025
     # https://www.hentai-foundry.com/pictures/user/Afrobull/795025/kuroeda
-    in "www.hentai-foundry.com", "pictures", "user", username, /^\d+$/ => work_id, *slug
+    in _, "hentai-foundry.com", "pictures", "user", username, /^\d+$/ => work_id, *slug
       @username = username
       @work_id = work_id
 
@@ -39,27 +39,27 @@ class Source::URL::HentaiFoundry < Source::URL
     # http://www.hentai-foundry.com/pic-149160.html
     # http://www.hentai-foundry.com/pic-149160.php
     # http://www.hentai-foundry.com/pic_full-66045.php
-    in "www.hentai-foundry.com", /^pic\w*-(\d+)/
+    in _, "hentai-foundry.com", /^pic\w*-(\d+)/
       @work_id = $1
 
     # https://thumbs.hentai-foundry.com/thumb.php?pid=795025&size=350
-    in "thumbs.hentai-foundry.com", "thumb.php" if params[:pid].present?
+    in "thumbs", "hentai-foundry.com", "thumb.php" if params[:pid].present?
       @work_id = params[:pid]
 
     # https://www.hentai-foundry.com/user/kajinman
     # https://www.hentai-foundry.com/user/kajinman/profile
     # https://www.hentai-foundry.com/user/J-likes-to-draw/profile
-    in "www.hentai-foundry.com", "user", username, *slug
+    in _, "hentai-foundry.com", "user", username, *slug
       @username = username
 
     # https://www.hentai-foundry.com/pictures/user/kajinman
     # https://www.hentai-foundry.com/pictures/user/kajinman/scraps
-    in "www.hentai-foundry.com", "pictures", "user", username, *slug
+    in _, "hentai-foundry.com", "pictures", "user", username, *slug
       @username = username
 
     # http://www.hentai-foundry.com/user-RockCandy.php
     # http://www.hentai-foundry.com/profile-sawao.php
-    in "www.hentai-foundry.com", /^(?:user|profile)-([^.]+)\.php$/
+    in _, "hentai-foundry.com", /^(?:user|profile)-([^.]+)\.php$/
       @username = $1
 
     else
