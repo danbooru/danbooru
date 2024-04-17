8beba491c63e8bd4cf128ff6d8495a8b0a8babb3
evazion <noizave@gmail.com>
2024-04-17 13:31:10 -0500

    Fix #5613: instagram: /user/p/id links tagged bad_source

--- test/unit/sources/instagram_test.rb
+++ test/unit/sources/instagram_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 module Sources
-  class Instagram < ActiveSupport::TestCase
+  class InstagramTest < ActiveSupport::TestCase
     context "Instagram:" do
       should "Parse Instagram URLs correctly" do
         assert(Source::URL.image_url?("https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/202831473_394388808595845_6890631933098833028_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=109&_nc_ohc=Fcle68OyC80AX8VTGxs&edm=AABBvjUBAAAA&ccb=7-4&oh=00_AT_DYX0zhyNR9vo6ZFKfXjzEWqwFLLfEd3qcpAds5KIvnA&oe=6216DDB5&_nc_sid=83d603"))
@@ -10,6 +10,7 @@ module Sources
         assert(Source::URL.page_url?("https://www.instagram.com/p/CbDW9mVuEnn/"))
         assert(Source::URL.page_url?("https://www.instagram.com/reel/CV7mHEwgbeF/?utm_medium=copy_link"))
         assert(Source::URL.page_url?("https://www.instagram.com/tv/CMjUD1epVWW/"))
+        assert(Source::URL.page_url?("https://www.instagram.com/peachmomoko60/p/CyyRYaBxp25/"))
 
         assert(Source::URL.profile_url?("https://www.instagram.com/itomugi/"))
         assert(Source::URL.profile_url?("https://www.instagram.com/stories/itomugi/"))
