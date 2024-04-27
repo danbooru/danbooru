6acbf30f5cab6bcf2836aeea0c02a34666542d03
evazion <noizave@gmail.com>
2024-04-27 16:55:12 -0500

    Fix #5351: Add opensea.io upload support.

---
+++ app/logical/source/url/opensea.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+# @see Source::Extractor::Opensea
+class Source::URL::Opensea < Source::URL
+  RESERVED_USERNAMES = %w[about account activity assets blog careers category collection drops learn partners privacy studio tos rankings]
+
+  attr_reader :username, :user_id, :chain, :contract_id, :token_id, :full_image_url
+
+  def self.match?(url)
+    url.domain.in?(%w[opensea.io openseauserdata.com seadn.io])
+  end
+
+  def parse
+    case [subdomain, domain, *path_segments]
+
+    # https://i.seadn.io/s/raw/files/473d8a4978c86ede320b8372dfe2a8b3.png?auto=format&dpr=1&w=384 (sample)
+    # https://i.seadn.io/s/raw/files/473d8a4978c86ede320b8372dfe2a8b3.png (full)
+    in "i", "seadn.io", "s", "raw", "files", _
+      @full_image_url = url.omit(:query).to_s
+
+    # https://i.seadn.io/gae/CnA27YghZgRXfI35roMJts6x43S6xwjkBqXF2ujywUl5ibx9Gd16TKsPwVBEyYyszO96XbWx85HzoGxQ6JI6FHQpjZ5YvEZo1CHxVA?auto=format&dpr=1&w=1000 (sample)
+    # https://i.seadn.io/gae/CnA27YghZgRXfI35roMJts6x43S6xwjkBqXF2ujywUl5ibx9Gd16TKsPwVBEyYyszO96XbWx85HzoGxQ6JI6FHQpjZ5YvEZo1CHxVA?w=99999 (full)
+    in "i", "seadn.io", "gae", _
+      @full_image_url = "#{url.omit(:query)}?w=99999"
+
+    # https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/73367181727578658379392940909024713110943326450271164125938382654208802291713
+    # https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/47707087614834185592401815072389651465878170492683018350293856127512379129861
+    in _, "opensea.io", "assets", chain, contract_id, token_id
+      @chain = chain
+      @contract_id = contract_id
+      @token_id = token_id
+
+    # https://opensea.io/0x7C01A933e8761DDf96C2322c772FbD2527ded439
+    in _, "opensea.io", /^0x\h{39}/ => user_id
+      @user_id = user_id
+
+    # https://opensea.io/accounts/0xff605910dc69999dca1fe2fa289a43cc2d51f0fc
+    in _, "opensea.io", "accounts", /^0x\h{39}/ => user_id
+      @user_id = user_id
+
+    # https://opensea.io/tororotororo
+    # https://opensea.io/tororotororo/created
+    # https://opensea.io/McArtCollection?tab=created
+    # https://opensea.io/tama5.eth (Ethereum Name Service (ENS) name)
+    in _, "opensea.io", username, *rest unless username.in?(RESERVED_USERNAMES)
+      @username = username
+
+    # https://i.seadn.io/s/primary-drops/0xc274a97f1691ef390f662067e95a6eff1f99b504/31341974:about:media:98e2f8a2-a8aa-46d9-9267-87108353c759.jpeg?auto=format&dpr=1&w=1920 (profile banner)
+    # https://opensea.io/collection/illustration-ainousoko
+    else
+      nil
+    end
+  end
+
+  def image_url?
+    domain.in?(%w[openseauserdata.com seadn.io])
+  end
+
+  def page_url
+    "https://opensea.io/assets/#{chain}/#{contract_id}/#{token_id}" if chain.present? && contract_id.present? && token_id.present?
+  end
+
+  def profile_url
+    if username.present?
+      "https://opensea.io/#{username}"
+    elsif user_id.present?
+      "https://opensea.io/#{user_id}"
+    end
+  end
+end
---
+++ test/unit/sources/opensea_test.rb
@@ -0,0 +1,127 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Sources
+  class OpenseaTest < ActiveSupport::TestCase
+    context "Opensea:" do
+      context "An Opensea /raw/ sample image URL" do
+        strategy_should_work(
+          "https://i.seadn.io/s/raw/files/473d8a4978c86ede320b8372dfe2a8b3.png?auto=format&dpr=1&w=384",
+          image_urls: %w[https://i.seadn.io/s/raw/files/473d8a4978c86ede320b8372dfe2a8b3.png],
+          media_files: [{ file_size: 52_000 }],
+          page_url: nil
+        )
+      end
+
+      context "An Opensea /gae/ sample image URL" do
+        strategy_should_work(
+          "https://i.seadn.io/gae/CnA27YghZgRXfI35roMJts6x43S6xwjkBqXF2ujywUl5ibx9Gd16TKsPwVBEyYyszO96XbWx85HzoGxQ6JI6FHQpjZ5YvEZo1CHxVA?auto=format&dpr=1&w=1000 ",
+          image_urls: %w[https://i.seadn.io/gae/CnA27YghZgRXfI35roMJts6x43S6xwjkBqXF2ujywUl5ibx9Gd16TKsPwVBEyYyszO96XbWx85HzoGxQ6JI6FHQpjZ5YvEZo1CHxVA?w=99999],
+          media_files: [{ file_size: 1_186_411 }],
+          page_url: nil
+        )
+      end
+
+      context "An Opensea ethereum post" do
+        strategy_should_work(
+          "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/47707087614834185592401815072389651465878170492683018350293856127512379129861",
+          image_urls: %w[https://openseauserdata.com/files/c6783eecd8fe6016d909324b48df5c9b.jpg],
+          media_files: [{ file_size: 2_965_837 }],
+          page_url: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/47707087614834185592401815072389651465878170492683018350293856127512379129861",
+          profile_url: "https://opensea.io/Lusan666",
+          profile_urls: %w[https://opensea.io/Lusan666 https://opensea.io/0x697941341f8e6729da58b5f0c3447e142d970922],
+          artist_name: "Lusan666",
+          tag_name: "lusan666",
+          other_names: ["Lusan666"],
+          dtext_artist_commentary_title: "Black cat - Levana",
+          dtext_artist_commentary_desc: <<~EOS.chomp
+            Happy Black Cat Appreciation Day!!
+
+            Here's your black cat. She's a little fierce, be careful
+          EOS
+        )
+      end
+
+      context "An Opensea matic post" do
+        strategy_should_work(
+          "https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/73367181727578658379392940909024713110943326450271164125938382654208802291713",
+          image_urls: %w[https://openseauserdata.com/files/070922161d3c4deab9f76263b3ce6018.png],
+          media_files: [{ file_size: 5_623_570 }],
+          page_url: "https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/73367181727578658379392940909024713110943326450271164125938382654208802291713",
+          profile_url: "https://opensea.io/hebitsukai",
+          profile_urls: %w[https://opensea.io/hebitsukai https://opensea.io/0xa2345a7139b18e57f1fd5ea27c87b51e09af241f],
+          artist_name: "hebitsukai",
+          tag_name: "hebitsukai",
+          other_names: ["hebitsukai"],
+          dtext_artist_commentary_title: "mode",
+          dtext_artist_commentary_desc: <<~EOS.chomp
+            1/1 NFT 10000 x 7000 px PNG
+          EOS
+        )
+      end
+
+      context "An Opensea video post" do
+        strategy_should_work(
+          "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/36019834214304435122883776814468417387629241948867352800925243912165007556609",
+          image_urls: %w[https://openseauserdata.com/files/c8e1e33ca7ec81c4c49be690c96e29da.mp4],
+          media_files: [{ file_size: 43_195_672 }],
+          page_url: "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/36019834214304435122883776814468417387629241948867352800925243912165007556609",
+          profile_url: "https://opensea.io/Bear_witch",
+          profile_urls: %w[https://opensea.io/Bear_witch https://opensea.io/0x4fa280f224f6e5528ecae02f979cee2aad949d73],
+          artist_name: "Bear_witch",
+          tag_name: "bear_witch",
+          other_names: ["Bear_witch"],
+          dtext_artist_commentary_title: "HanumanWarrior02",
+          dtext_artist_commentary_desc: "HanumanWarrior02"
+        )
+      end
+
+      context "An Opensea post without an image" do
+        strategy_should_work(
+          "https://opensea.io/assets/ethereum/0xe07b8409130c8ca1548c16cf43d612c3a099e1f7/8",
+          image_urls: [],
+          page_url: "https://opensea.io/assets/ethereum/0xe07b8409130c8ca1548c16cf43d612c3a099e1f7/8",
+          profile_url: "https://opensea.io/Elina_E2n04n",
+          profile_urls: %w[https://opensea.io/Elina_E2n04n https://opensea.io/0x7c01a933e8761ddf96c2322c772fbd2527ded439],
+          artist_name: "Elina_E2n04n",
+          tag_name: "elina_e2n04n",
+          other_names: ["Elina_E2n04n"],
+          dtext_artist_commentary_title: "願いを叶える使者",
+          dtext_artist_commentary_desc: <<~EOS.chomp
+            モチーフ：七夕（主に短冊）
+            上着は織姫のストールを意識してデザインしました。
+          EOS
+        )
+      end
+
+      context "A deleted or nonexistent Opensea post" do
+        strategy_should_work(
+          "https://opensea.io/assets/ethereum/0xe07b8409130c8ca1548c16cf43d612c3a099e1f7/999999",
+          image_urls: [],
+          media_files: [],
+          page_url: "https://opensea.io/assets/ethereum/0xe07b8409130c8ca1548c16cf43d612c3a099e1f7/999999",
+          profile_url: nil,
+          profile_urls: %w[],
+          artist_name: nil,
+          tag_name: nil,
+          other_names: [],
+          dtext_artist_commentary_title: "",
+          dtext_artist_commentary_desc: ""
+        )
+      end
+
+      should "parse Opensea URLs correctly" do
+        assert(Source::URL.image_url?("https://i.seadn.io/s/raw/files/473d8a4978c86ede320b8372dfe2a8b3.png?auto=format&dpr=1&w=384"))
+        assert(Source::URL.image_url?("https://i.seadn.io/gae/CnA27YghZgRXfI35roMJts6x43S6xwjkBqXF2ujywUl5ibx9Gd16TKsPwVBEyYyszO96XbWx85HzoGxQ6JI6FHQpjZ5YvEZo1CHxVA?auto=format&dpr=1&w=1000"))
+
+        assert(Source::URL.page_url?("https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/73367181727578658379392940909024713110943326450271164125938382654208802291713"))
+        assert(Source::URL.page_url?("https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/47707087614834185592401815072389651465878170492683018350293856127512379129861"))
+
+        assert(Source::URL.profile_url?("https://opensea.io/0x7C01A933e8761DDf96C2322c772FbD2527ded439"))
+        assert(Source::URL.profile_url?("https://opensea.io/accounts/0xff605910dc69999dca1fe2fa289a43cc2d51f0fc"))
+        assert(Source::URL.profile_url?("https://opensea.io/tororotororo"))
+      end
+    end
+  end
+end
