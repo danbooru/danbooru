808f000f4bea27796f6d63cb78412bdbe1d282a8
evazion <noizave@gmail.com>
2024-04-29 16:44:55 -0500

    Fix #5332: Add fc2 upload support.
    Add partial support for uploading blog posts from fc2.com.
    
    URLs like this are potentially supported (not all will work):
    
    * http://niyamalog.blog.fc2.com/blog-entry-3.html
    
    In general, FC2 hosts both blogs and personal websites. Personal websites use completely custom HTML
    and don't have any common structure that can be scraped. Blogs use custom HTML templates and most
    don't have anything in common that can be scraped either. FC2 says there are thousands of different
    blog templates available and most have totally different HTML structures.
    
    There is one trick: blogs have a mobile version, and if the blog uses the default mobile template,
    then it can be scraped. You can get the mobile version by adding the `?sp` param or by spoofing a
    mobile user agent:
    
        curl http://niyamalog.blog.fc2.com/blog-entry-3.html?sp
        curl -H "User-Agent: Android Mobile" http://niyamalog.blog.fc2.com/blog-entry-3.html
    
    However, not all blogs have a mobile version, and not all that do use the default mobile template:
    
        https://killfuckdie.blog.fc2.com/blog-entry-207.html (no mobile version)
        http://oekakigakusyuu.blog97.fc2.com/blog-entry-320.html (custom mobile template)
    
    The only way to scrape these blogs would be to use a bunch of heuristics to try to grab the body of
    the blog post while avoiding the rest of the page (the header, footer, sidebar, comments, etc).
    Firefox Reader mode (cf. https://github.com/mozilla/readability) can do a pretty good job of this,
    but it relies on tons of complex heuristics.

--- app/logical/source/url/fc2.rb
+++ app/logical/source/url/fc2.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Source::URL::Fc2 < Source::URL
-  attr_reader :username, :profile_url, :page_url, :file
+  attr_reader :username, :subsite, :blog_entry, :album_filename, :candidate_full_image_url, :candidate_page_urls
 
   def self.match?(url)
-    url.domain.in?(%w[fc2.com fc2blog.net fc2blog.us])
+    url.domain.in?(%w[fc2.com fc2web.com fc2blog.net fc2blog.us])
   end
 
   def site_name
@@ -15,68 +15,171 @@ class Source::URL::Fc2 < Source::URL
     case [*host.split("."), *path_segments]
 
     # http://hosystem.blog36.fc2.com/blog-entry-37.html
-    in username, /^blog\d*$/, "fc2", "com", /^blog-entry-(\d+)\.html$/ => blog_path
+    # http://swordsouls.blog131.fc2blog.us/blog-entry-376.html
+    # http://swordsouls.blog131.fc2blog.net/blog-entry-376.html
+    in username, /^blog\d*$/, ("fc2" | "fc2blog"), _, /^blog-entry-(\d+)\.html$/
       @username = username
-      @profile_url = "http://#{username}.blog.fc2.com"
-      @page_url = "http://#{username}.blog.fc2.com/#{blog_path}"
+      @subsite = "blog"
+      @blog_entry = $1
+
+    # http://oekakigakusyuu.blog97.fc2.com/?m&no=320
+    # http://abk00.blog71.fc2.com/?no=3052
+    in username, /^blog\d*$/, ("fc2" | "fc2blog"), _, *rest if params[:no]&.match?(/^\d+$/)
+      @username = username
+      @subsite = "blog"
+      @blog_entry = params[:no]
+
+    # http://niyamalog.blog.fc2.com/img/20170330Xray6z7P/
+    # http://niyamalog.blog.fc2.com/img/bokuomapop.jpg/
+    # http://swordsouls.blog131.fc2blog.us/img/20141009132121fb7.jpg/
+    in username, /^blog\d*$/, ("fc2" | "fc2blog"), _, "img", file
+      @username = username
+      @subsite = "blog"
+      @album_filename = file
+
+    # http://alternatif.blog26.fc2.com/?mode=image&filename=rakugaki10.jpg
+    # http://swordsouls.blog131.fc2blog.us/?mode=image&filename=20141009132121fb7.jpg
+    in username, /^blog\d*$/, ("fc2" | "fc2blog"), _, *rest if params[:mode] == "image" && params[:filename].present?
+      @username = username
+      @subsite = "blog"
+      @album_filename = params[:filename]
 
     # http://silencexs.blog.fc2.com
     # http://silencexs.blog106.fc2.com
+    # http://swordsouls.blog131.fc2blog.net
+    # http://swordsouls.blog131.fc2blog.us
+    # http://niyamalog.blog.fc2.com/imgs/
+    # http://orihc.blog13.fc2.com/page-3.html
     # http://onidocoro.blog14.fc2.com/file/20071003061150.png
-    in username, /^blog\d*$/, "fc2", "com", *rest
+    # http://mtunk.blog49.fc2.com/blog-date-20101218.html
+    # http://mtunk.blog49.fc2.com/?date=20111230
+    # http://yoruichi3.blog5.fc2.com/file/neru.jpg
+    in username, /^blog\d*$/, ("fc2" | "fc2blog"), _, *rest
       @username = username
-      @profile_url = "http://#{username}.blog.fc2.com"
+      @subsite = "blog"
 
     # http://794ancientkyoto.web.fc2.com
     # http://yorokobi.x.fc2.com
     # https://lilish28.bbs.fc2.com
     # http://jpmaid.h.fc2.com
     # http://toritokaizoku.web.fc2.com/tori.html (404: http://toritokaizoku.web.fc2.com)
+    # http://oriongarlic.web.fc2.com/share/person/4000/O4284.jpg?2047604
     in username, ("bbs" | "web" | "h" | "x") => subsite, "fc2", "com", *rest
       @username = username
       @subsite = subsite
-      @profile_url = ["http://#{username}.#{subsite}.fc2.com", *rest].join("/")
-
-    # http://swordsouls.blog131.fc2blog.net
-    # http://swordsouls.blog131.fc2blog.us
-    in username, /^blog\d*$/, "fc2blog", ("net" | "us") => tld, *rest
-      @username = username
-      @profile_url = "http://#{username}.blog.fc2blog.#{tld}"
 
     # http://blog23.fc2.com/m/mosha2/file/uru.jpg
     # http://blog.fc2.com/g/genshi/file/20070612a.jpg
-    in /^blog\d*$/, "fc2", "com", /^\w$/, username, "file", file
-      @file = file
+    in /^blog\d*$/, "fc2", "com", /^\w$/, username, "file", _
       @username = username
-      @profile_url = "http://#{username}.blog.fc2.com"
+      @subsite = "blog"
 
+    # http://xkilikox.fc2web.com
+    # http://xkilikox.fc2web.com/image/haguruma.html
+    # http://xkilikox.fc2web.com/image/haguruma00.jpg
+    # http://yappaga.fc2web.com/gallery.html
+    in username, "fc2web", "com", *rest
+      @username = username
+
+    # https://blog-imgs-119.fc2.com/n/i/y/niyamalog/bokuomapops.jpg (sample)
+    # https://blog-imgs-119-origin.fc2.com/n/i/y/niyamalog/bokuomapop.jpg (full)
+    # http://blog-imgs-103.fc2.com/k/u/s/kusarenk/kanade_s.jpg (full)
+    # http://blog-imgs-99-origin.fc2.com/h/7/2/h723/idkdldss.jpg (full)
+    #
     # http://blog-imgs-63-origin.fc2.com/y/u/u/yuukyuukikansya/140817hijiri02.jpg
     # http://blog-imgs-61.fc2.com/o/m/o/omochi6262/20130402080220583.jpg
     # http://blog.fc2.com/g/b/o/gbot/20071023195141.jpg
-    in (/^blog-imgs-\d+(-origin)?$/ | "blog"), "fc2", "com", /^\w$/, /^\w$/, /^\w$/, username, file
-      @file = file
+
+    # http://blog-imgs-57.fc2blog.us/s/w/o/swordsouls/20141009132121fb7.jpg
+    in (/^blog-imgs-\d+(-origin)?$/ | "blog"), ("fc2" | "fc2blog"), _, /^\w$/, /^\w$/, /^\w$/, username, _
       @username = username
-      @page_url = "http://#{username}.blog.fc2.com/img/#{file}"
-      @profile_url = "http://#{username}.blog.fc2.com"
+      @subsite = "blog"
+
+      if original_url.match?(/s\.jpg$/)
+        @candidate_full_image_url = original_url.gsub(/s\.jpg$/, ".jpg")
+        @candidate_page_urls = %W[
+          http://#{username}.blog.#{domain}/img/#{basename.gsub(/s\.jpg$/, ".jpg")}/
+          http://#{username}.blog.#{domain}/img/#{basename}/
+        ]
+      else
+        @candidate_page_urls = "http://#{username}.blog.#{domain}/img/#{basename}/"
+      end
 
     # http://diary.fc2.com/user/yuuri/img/2005_12/26.jpg
     # http://diary1.fc2.com/user/kou_48/img/2006_8/14.jpg
     # http://diary.fc2.com/user/kazuharoom/img/2015_5/22.jpg
-    in /diary\d*$/, "fc2", "com", "user", username, "img", date, file
-      @file = file
+    in /diary\d*$/, "fc2", "com", "user", username, "img", date, _
       @username = username
+      @subsite = "diary"
       @year, @month = date.split("_")
       @day = filename
       @page_url = "http://#{host}/cgi-sys/ed.cgi/#{username}?Y=#{@year}&M=#{@month}&D=#{@day}"
-      @profile_url = "http://diary.fc2.com/cgi-sys/ed.cgi/#{username}"
 
     # http://diary.fc2.com/cgi-sys/ed.cgi/kazuharoom/?Y=2012&M=10&D=22
     in /diary\d*$/, "fc2", "com", "cgi-sys", "ed.cgi", username
       @username = username
-      @profile_url = "http://diary.fc2.com/cgi-sys/ed.cgi/#{username}"
+      @subsite = "diary"
 
+    # https://clap.fc2.com/uploads/h/o/hoge/hacs.jpg
     else
       nil
+    end
+  end
+
+  def image_url?
+    # http://niyamalog.blog.fc2.com/img/bokuomapop.jpg/ (a page url, not an image url)
+    album_filename.present? ? false : super
+  end
+
+  def page_url
+    # http://hosystem.blog36.fc2.com/blog-entry-37.html
+    # http://swordsouls.blog131.fc2blog.us/blog-entry-376.html
+    if username.present? && blog_entry.present?
+      "http://#{username}.blog.#{domain}/blog-entry-#{blog_entry}.html"
+
+    # http://niyamalog.blog.fc2.com/img/20170330Xray6z7P/
+    # http://niyamalog.blog.fc2.com/img/bokuomapop.jpg/
+    # http://swordsouls.blog131.fc2blog.us/img/20141009132121fb7.jpg/
+    # http://swordsouls.blog131.fc2blog.us/?mode=image&filename=20141009132121fb7.jpg
+    elsif username.present? && album_filename.present?
+      "http://#{username}.blog.#{domain}/img/#{album_filename}/"
+    end
+  end
+
+  def profile_url
+    # http://hosystem.blog36.fc2.com/blog-entry-37.html
+    # http://swordsouls.blog131.fc2blog.us/blog-entry-376.html
+    # http://onidocoro.blog14.fc2.com/file/20071003061150.png
+    # http://blog.fc2.com/g/genshi/file/20070612a.jpg
+    if subsite == "blog" && username.present?
+      "http://#{username}.blog.#{domain}"
+
+    # http://diary.fc2.com/user/yuuri/img/2005_12/26.jpg
+    # http://diary.fc2.com/cgi-sys/ed.cgi/kazuharoom/?Y=2012&M=10&D=22
+    elsif subsite == "diary" && username.present?
+      "http://diary.fc2.com/cgi-sys/ed.cgi/#{username}"
+
+    # http://794ancientkyoto.web.fc2.com
+    # http://yorokobi.x.fc2.com
+    # https://lilish28.bbs.fc2.com
+    # http://jpmaid.h.fc2.com
+    # http://fromh.web.fc2.com/codefromh/top.html
+    # http://toritokaizoku.web.fc2.com/tori.html (404: http://toritokaizoku.web.fc2.com)
+    # http://tritre.web.fc2.com (404: http://tritre.web.fc2.com/index.html)
+    #
+    # http://xkilikox.fc2web.com
+    # http://yappaga.fc2web.com/gallery.html (404: http://yappaga.fc2web.com)
+    # http://naokimk2.fc2web.com/HP2/TOP.html (404: http://naokimk2.fc2web.com)
+    #
+    # http://xkilikox.fc2web.com/image/haguruma.html (XXX: should be page url, not a profile url)
+    elsif (subsite.in?(%w[bbs web h x]) || domain == "fc2web.com") && !image_url?
+      URI.join("http://#{host}", path).to_s.chomp("/")
+
+    # http://rxsdm.h.fc2.com/f/061201_2.jpg
+    # http://xkilikox.fc2web.com/image/haguruma00.jpg
+    elsif image_url? && username.present?
+      "http://#{host}"
+
     end
   end
 end
--- test/unit/sources/fc2_test.rb
+++ test/unit/sources/fc2_test.rb
@@ -3,6 +3,239 @@ require 'test_helper'
 module Sources
   class Fc2Test < ActiveSupport::TestCase
     context "FC2:" do
+      context "A blog sample image URL" do
+        strategy_should_work(
+          "https://blog-imgs-119.fc2.com/n/i/y/niyamalog/bokuomapops.jpg",
+          image_urls: %w[https://blog-imgs-119.fc2.com/n/i/y/niyamalog/bokuomapop.jpg],
+          media_files: [{ file_size: 556_347 }],
+          page_url: "http://niyamalog.blog.fc2.com/img/bokuomapop.jpg/",
+          profile_urls: %w[http://niyamalog.blog.fc2.com],
+          artist_name: "niyamalog",
+          tag_name: "niyamalog",
+          other_names: ["niyamalog"],
+          tags: [],
+          dtext_artist_commentary_title: "",
+          dtext_artist_commentary_desc: ""
+        )
+      end
+
+      context "A blog full image URL" do
+        strategy_should_work(
+          "http://blog-imgs-99-origin.fc2.com/h/7/2/h723/idkdldss.jpg",
+          image_urls: %w[http://blog-imgs-99-origin.fc2.com/h/7/2/h723/idkdldss.jpg],
+          media_files: [{ file_size: 848_735 }],
+          page_url: "http://h723.blog.fc2.com/img/idkdldss.jpg/",
+          profile_url: "http://h723.blog.fc2.com",
+          profile_urls: %w[http://h723.blog.fc2.com],
+          artist_name: "h723",
+          tag_name: "h723",
+          other_names: ["h723"],
+          tags: [],
+          dtext_artist_commentary_title: "",
+          dtext_artist_commentary_desc: ""
+        )
+      end
+
+      context "A blog post with a mobile version" do
+        strategy_should_work(
+          "https://niyamalog.blog.fc2.com/blog-entry-3.html",
+          image_urls: %w[
+            https://blog-imgs-119.fc2.com/n/i/y/niyamalog/bokuomapop.jpg
+            https://blog-imgs-119.fc2.com/n/i/y/niyamalog/20180824171514638.jpg
+            https://blog-imgs-119.fc2.com/n/i/y/niyamalog/20180824171512954.jpg
+            https://blog-imgs-119.fc2.com/n/i/y/niyamalog/20180824171513acd.jpg
+            https://blog-imgs-119.fc2.com/n/i/y/niyamalog/201808241715095fb.jpg
+            https://blog-imgs-119.fc2.com/n/i/y/niyamalog/20180824171510941.jpg
+            https://blog-imgs-119.fc2.com/n/i/y/niyamalog/20180824171507829.jpg
+          ],
+          media_files: [
+            { file_size: 556_347 },
+            { file_size: 369_435 },
+            { file_size: 770_209 },
+            { file_size: 305_289 },
+            { file_size: 886_461 },
+            { file_size: 351_217 },
+            { file_size: 245_744 },
+          ],
+          page_url: "http://niyamalog.blog.fc2.com/blog-entry-3.html",
+          profile_url: "http://niyamalog.blog.fc2.com",
+          profile_urls: %w[http://niyamalog.blog.fc2.com],
+          artist_name: "niyamalog",
+          tag_name: "niyamalog",
+          other_names: ["niyamalog", "おやま"],
+          tags: [],
+          dtext_artist_commentary_title: "僕のおまわりさん販促用ペーパーなど",
+          dtext_artist_commentary_desc: <<~EOS.chomp
+            僕のおまわりさんの発売日から1年経過しましたので前回同様
+            電子版のおまけ以外の特典を公開いたします～。
+
+            紙ペーパーはなくなり次第配布終了なのですが電子ですと配信が続く限り
+            特典はずっとつきますので、申し訳ありませんがその辺はご理解いただければと思います。
+            割引キャンペーンの時とかに気が向いたら電子版もよろしくお願いいたします・・・！
+
+            "[image]":[http://niyamalog.blog.fc2.com/img/bokuomapop.jpg/]
+            書店用ＰＯＰ
+
+            "[image]":[http://niyamalog.blog.fc2.com/img/20180824171514638.jpg/]
+            ▲特約店さま用ペーパー
+
+            "[image]":[http://niyamalog.blog.fc2.com/img/20180824171512954.jpg/]
+            "[image]":[http://niyamalog.blog.fc2.com/img/20180824171513acd.jpg/]
+            ▲コミコミさま両面カード
+
+            "[image]":[http://niyamalog.blog.fc2.com/img/201808241715095fb.jpg/]
+
+            "[image]":[http://niyamalog.blog.fc2.com/img/20180824171510941.jpg/]
+            ▲とらのあなさま両面カード
+
+            "[image]":[http://niyamalog.blog.fc2.com/img/20180824171507829.jpg/]
+            ▲アニメイトさま用イラストカード
+
+            ※個人的にプリントアウトしてお手元にお持ち頂くのは全くかまいませんが
+            プリントアウトしたものをネットオークションやフリマアプリなどに出品するのは
+            やめてくださいね～～
+
+            現在momentで連載中の僕のおまわりさん続編もあと2話ほどで最終回をむかえますが（全5話です）
+            誠治と晋とチコたんを最後まで見守ってくださるとうれしいです
+
+            発売中のドラマCDも宜しくお願いたします！！
+          EOS
+        )
+      end
+
+      context "A blog post without a mobile version" do
+        strategy_should_work(
+          "https://killfuckdie.blog.fc2.com/blog-entry-207.html",
+          image_urls: [],
+          page_url: "http://killfuckdie.blog.fc2.com/blog-entry-207.html",
+          profile_url: "http://killfuckdie.blog.fc2.com",
+          profile_urls: %w[http://killfuckdie.blog.fc2.com],
+          artist_name: "killfuckdie",
+          tag_name: "killfuckdie",
+          other_names: ["killfuckdie"],
+          tags: [],
+          dtext_artist_commentary_title: "",
+          dtext_artist_commentary_desc: ""
+        )
+      end
+
+      context "A blog post with a nonstandard mobile version" do
+        strategy_should_work(
+          "http://oekakigakusyuu.blog97.fc2.com/blog-entry-320.html",
+          image_urls: [],
+          page_url: "http://oekakigakusyuu.blog.fc2.com/blog-entry-320.html",
+          profile_url: "http://oekakigakusyuu.blog.fc2.com",
+          profile_urls: %w[http://oekakigakusyuu.blog.fc2.com],
+          artist_name: "oekakigakusyuu",
+          tag_name: "oekakigakusyuu",
+          other_names: ["oekakigakusyuu"],
+          tags: [],
+          dtext_artist_commentary_title: "",
+          dtext_artist_commentary_desc: ""
+        )
+      end
+
+      context "A fc2blog.us blog post" do
+        strategy_should_work(
+          "http://swordsouls.blog131.fc2blog.us/blog-entry-376.html",
+          image_urls: %w[http://blog-imgs-57.fc2blog.us/s/w/o/swordsouls/20141009132121fb7.jpg],
+          media_files: [{ file_size: 233_495 }],
+          page_url: "http://swordsouls.blog.fc2blog.us/blog-entry-376.html",
+          profile_url: "http://swordsouls.blog.fc2blog.us",
+          profile_urls: %w[http://swordsouls.blog.fc2blog.us],
+          artist_name: "swordsouls",
+          tag_name: "swordsouls",
+          other_names: ["swordsouls", "深藍侵襲"],
+          tags: [],
+          dtext_artist_commentary_title: "看板姫♡",
+          dtext_artist_commentary_desc: <<~EOS.chomp
+            "[image]":[http://s20.postimg.org/8szg5q37f/image.png]
+
+            【七つの大罪】
+
+            收看纪念~动画相当不错~好久不见的超级王道展开让我也好好的燃了一把！~自己一直都很喜欢这样的王道热血动画，比如【妖精的尾巴】【海贼王】等等这些包括漫画在内我都是一直在追的~
+
+            很久以前在看【妖尾】的时候就有过一起2个作者一起联合画了一个短篇，自己是在那个时候知道这个作品的，不过并没有看漫画，不过这次看完动画之后完全燃起来了~目前在忍住没去看漫画~
+
+            主角的设定以及世界的设定也是自己很喜欢的类型的~这种中欧世界观的魔幻世界一直也是自己理想的异次元世界之一~然后最最最重要的是女主角非常非常可爱~从设定上就是直击我的爱好了~
+
+            那坚强而纯真的性格，那完美的身材，完全是理想的女性角色了呢~虽然有点爱哭，不过在自己的信念上非常鉴定，而且同时也拥有不想牵扯无辜的他人的善良的心灵~自己目前对公主酱的好感非常高~超级期待之后的表现~
+
+            然后就是霍克酱www好久没看到这么萌的宠物角色了~吐槽也很犀利~存在感也超强~就算旁边站着一个几乎完美无瑕的公主酱也无法掩盖它的存在感wwww
+
+            目前虽然才一集，不过就音乐，设定，战斗，画面等等所有因素综合起来这个作品也是目前新番已出所有作品里面首屈一指的~强烈推荐没看过的朋友去好好的燃一下~
+
+            PS：这个季度的作品目前给自己的感觉很不错~有好几部的第一集都非常有好感~强烈期待后续发展中(｀・ω・´)！
+          EOS
+        )
+      end
+
+      context "A blog post with a ?no=:id param" do
+        strategy_should_work(
+          "http://abk00.blog71.fc2.com/?no=3052",
+          image_urls: %w[https://blog-imgs-83-origin.fc2.com/a/b/k/abk00/20150930193800.jpg],
+          media_files: [{ file_size: 141_637 }],
+          page_url: "http://abk00.blog.fc2.com/blog-entry-3052.html",
+          profile_url: "http://abk00.blog.fc2.com",
+          profile_urls: %w[http://abk00.blog.fc2.com],
+          artist_name: "abk00",
+          tag_name: "abk00",
+          other_names: ["abk00", "名無しの落書き所"],
+          tags: [],
+          dtext_artist_commentary_title: "GOD EATER EXTRA[アリサ・イリーニチナ・アミエーラ]",
+          dtext_artist_commentary_desc: <<~EOS.chomp
+            "[image]":[https://blog-imgs-83-origin.fc2.com/a/b/k/abk00/20150930193800.jpg]
+            [b]2015/09/30(水) ゴッドイーター エクストラ[アリサ・イリーニチナ・アミエーラ][/b]
+
+            [b]いやぁ…イイ下乳でしたね……(遠い目[/b]
+
+            今期は二人もイイ下乳キャラがいたのに
+            どうしてこうなったｘ２になって残念というか無念というか。
+            [b]なぜ下乳に一切触れぬ･･･触れぬのか･･･ッ！？[/b]
+            いやまあ必要不必要以前に毎回3回以上触れてる健全な性少年育成漫画のアニメ化主人公さんもいたりするわけですが
+
+            [b]やはり時代はエロによって作られる。[/b](今週の格言)  関連記事  * " ルパン三世[レベッカ・ロッセリーニ] (2015/10/04) ":[http://abk00.blog.fc2.com/blog-entry-3055.html?sp]
+            * " ランスアンドマスクス[鬼堂院真緒] (2015/10/02) ":[http://abk00.blog.fc2.com/blog-entry-3054.html?sp]
+            * GOD EATER EXTRA[アリサ・イリーニチナ・アミエーラ] (2015/09/30)
+            * " GOD EATER EXTRA[雨宮ツバキ] (2015/09/29) ":[http://abk00.blog.fc2.com/blog-entry-3051.html?sp]
+            * " わかば＊ガール[真柴直/小橋若葉] (2015/09/28) ":[http://abk00.blog.fc2.com/blog-entry-3050.html?sp]
+          EOS
+        )
+      end
+
+      context "A deleted or nonexistent blog post" do
+        strategy_should_work(
+          "http://oekakigakusyuu.blog97.fc2.com/blog-entry-999999.html",
+          image_urls: [],
+          page_url: "http://oekakigakusyuu.blog.fc2.com/blog-entry-999999.html",
+          profile_url: "http://oekakigakusyuu.blog.fc2.com",
+          profile_urls: %w[http://oekakigakusyuu.blog.fc2.com],
+          artist_name: "oekakigakusyuu",
+          tag_name: "oekakigakusyuu",
+          other_names: ["oekakigakusyuu"],
+          tags: [],
+          dtext_artist_commentary_title: "",
+          dtext_artist_commentary_desc: ""
+        )
+      end
+
+      context "A fc2web.com image" do
+        strategy_should_work(
+          "http://xkilikox.fc2web.com/image/haguruma00.jpg",
+          image_urls: %w[http://xkilikox.fc2web.com/image/haguruma00.jpg],
+          media_files: [{ file_size: 34_232 }],
+          page_url: nil,
+          profile_url: "http://xkilikox.fc2web.com",
+          profile_urls: %w[http://xkilikox.fc2web.com],
+          artist_name: "xkilikox",
+          tag_name: "xkilikox",
+          other_names: ["xkilikox"],
+          tags: [],
+          dtext_artist_commentary_title: "",
+          dtext_artist_commentary_desc: ""
+        )
+      end
+
       should "Parse FC2 URLs correctly" do
         assert(Source::URL.image_url?("http://onidocoro.blog14.fc2.com/file/20071003061150.png"))
         assert(Source::URL.image_url?("http://blog23.fc2.com/m/mosha2/file/uru.jpg"))
@@ -12,6 +245,17 @@ module Sources
         assert(Source::URL.image_url?("http://blog.fc2.com/g/b/o/gbot/20071023195141.jpg"))
         assert(Source::URL.image_url?("http://diary.fc2.com/user/yuuri/img/2005_12/26.jpg"))
 
+        assert(Source::URL.page_url?("http://hosystem.blog36.fc2.com/blog-entry-37.html"))
+        assert(Source::URL.page_url?("http://swordsouls.blog131.fc2blog.us/blog-entry-376.html"))
+        assert(Source::URL.page_url?("http://oekakigakusyuu.blog97.fc2.com/?m&no=320"))
+        assert(Source::URL.page_url?("http://abk00.blog71.fc2.com/?no=3052"))
+
+        assert(Source::URL.page_url?("http://niyamalog.blog.fc2.com/img/20170330Xray6z7P/"))
+        assert(Source::URL.page_url?("http://niyamalog.blog.fc2.com/img/bokuomapop.jpg/"))
+        assert(Source::URL.page_url?("http://swordsouls.blog131.fc2blog.us/img/20141009132121fb7.jpg/"))
+        assert(Source::URL.page_url?("http://alternatif.blog26.fc2.com/?mode=image&filename=rakugaki10.jpg"))
+        assert(Source::URL.page_url?("http://swordsouls.blog131.fc2blog.us/?mode=image&filename=20141009132121fb7.jpg"))
+
         assert(Source::URL.profile_url?("http://silencexs.blog.fc2.com"))
         assert(Source::URL.profile_url?("http://794ancientkyoto.web.fc2.com"))
         assert(Source::URL.profile_url?("http://yorokobi.x.fc2.com"))
@@ -19,6 +263,12 @@ module Sources
         assert(Source::URL.profile_url?("http://jpmaid.h.fc2.com"))
         assert(Source::URL.profile_url?("http://swordsouls.blog131.fc2blog.net"))
         assert(Source::URL.profile_url?("http://swordsouls.blog131.fc2blog.us"))
+        assert(Source::URL.profile_url?("http://xkilikox.fc2web.com/image/haguruma.html"))
+
+        assert_equal("http://diary.fc2.com/cgi-sys/ed.cgi/kazuharoom", Source::URL.profile_url("http://diary.fc2.com/cgi-sys/ed.cgi/kazuharoom/?Y=2012&M=10&D=22"))
+        assert_equal("http://toritokaizoku.web.fc2.com/tori.html", Source::URL.profile_url("http://toritokaizoku.web.fc2.com/tori.html"))
+        assert_equal("http://xkilikox.fc2web.com/image/haguruma.html", Source::URL.profile_url("http://xkilikox.fc2web.com/image/haguruma.html"))
+        assert_equal("http://xkilikox.fc2web.com", Source::URL.profile_url("http://xkilikox.fc2web.com/image/haguruma00.jpg"))
       end
     end
   end
