942224f5c93b8bb5a7c9883925369d0ec73bb4c4
evazion <noizave@gmail.com>
2024-05-10 23:02:54 -0500

    furaffinity: add support for fxfuraffinity.net domains.
    Add support for fxraffinity.net, fxfuraffinity.net, vxfuraffinity.net, and xfuraffinity.net domains.

--- app/logical/source/url/furaffinity.rb
+++ app/logical/source/url/furaffinity.rb
@@ -4,22 +4,22 @@ class Source::URL::Furaffinity < Source::URL
   attr_reader :work_id, :username, :filename
 
   def self.match?(url)
-    url.domain == "furaffinity.net"
+    url.domain.in?(%w[furaffinity.net fxraffinity.net fxfuraffinity.net vxfuraffinity.net xfuraffinity.net])
   end
 
   def parse
-    case [host, *path_segments]
+    case [subdomain, domain, *path_segments]
 
     # https://www.furaffinity.net/view/46821705/
     # https://www.furaffinity.net/view/46802202/ (scrap)
     # https://www.furaffinity.net/full/46821705/
-    in _, ("view" | "full"), /^\d+$/ => work_id
+    in _, _, ("view" | "full"), /^\d+$/ => work_id
       @work_id = work_id
 
     # https://d.furaffinity.net/art/iwbitu/1650222955/1650222955.iwbitu_yubi.jpg
     # https://t.furaffinity.net/46821705@800-1650222955.jpg
     # https://a.furaffinity.net/1550854991/iwbitu.gif
-    in _, "art", username, subdir, filename
+    in _, "furaffinity.net", "art", username, subdir, filename
       @username = username
       @filename = filename
 
@@ -28,7 +28,7 @@ class Source::URL::Furaffinity < Source::URL
     # https://www.furaffinity.net/gallery/iwbitu/folder/133763/Regular-commissions
     # https://www.furaffinity.net/user/lottieloveart/user?user_id=1021820442510802945
     # https://www.furaffinity.net/stats/duskmoor/submissions/
-    in _, ("gallery" | "user" | "favorites" | "scraps" | "journals" | "stats"), username, *pages
+    in _, "furaffinity.net", ("gallery" | "user" | "favorites" | "scraps" | "journals" | "stats"), username, *pages
       @username = username
 
     else
