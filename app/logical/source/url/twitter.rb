68b6fc12a6962927968a2b97d55491cd1a068d3a
evazion <noizave@gmail.com>
2024-05-11 00:49:39 -0500

    twitter: support rewriting profile pictures to full size.
    Add support for uploading Twitter profile pictures and rewriting them to the full size.

--- app/logical/source/url/twitter.rb
+++ app/logical/source/url/twitter.rb
@@ -28,7 +28,7 @@ class Source::URL::Twitter < Source::URL
     # https://pbs.twimg.com/tweet_video_thumb/ETkN_L3X0AMy1aT.jpg
     # https://pbs.twimg.com/ext_tw_video_thumb/1243725361986375680/pu/img/JDA7g7lcw7wK-PIv.jpg
     # https://pbs.twimg.com/amplify_video_thumb/1215590775364259840/img/lolCkEEioFZTb5dl.jpg
-    in _, "twimg.com", ("media" | "tweet_video_thumb" | "ext_tw_video_thumb" | "amplify_video_thumb") => media_type, *subdirs, file
+    in "pbs", "twimg.com", ("media" | "tweet_video_thumb" | "ext_tw_video_thumb" | "amplify_video_thumb") => media_type, *subdirs, file
       # EBGbJe_U8AA4Ekb.jpg:small
       @file, @file_size = file.split(":")
       @file, @file_ext = @file.split(".")
@@ -43,13 +43,22 @@ class Source::URL::Twitter < Source::URL
 
     # https://pbs.twimg.com/profile_banners/780804311529906176/1475001696
     # https://pbs.twimg.com/profile_banners/780804311529906176/1475001696/600x200
-    in _, "twimg.com", "profile_banners" => media_type, /^\d+$/ => user_id, /^\d+$/ => file_id, *dimensions
+    in "pbs", "twimg.com", "profile_banners" => media_type, /^\d+$/ => user_id, /^\d+$/ => file_id, *dimensions
       @user_id = user_id
       @profile_banner = true
       @full_image_url = "#{site}/#{media_type}/#{user_id}/#{file_id}/1500x500"
 
+    # https://pbs.twimg.com/profile_images/1425792004877733891/UM8s9d2x_400x400.png (sample)
+    # https://pbs.twimg.com/profile_images/417182061145780225/ttN6_CSs_normal.jpeg (sample)
+    # https://pbs.twimg.com/profile_images/417182061145780225/ttN6_CSs_400x400.jpeg (sample)
+    # https://pbs.twimg.com/profile_images/417182061145780225/ttN6_CSs.jpeg (full; 1252x1252)
+    # https://pbs.twimg.com/profile_images/378800000094437961/99ac06f2873f8597fa5961a404769066.gif (obsolete)
+    in "pbs", "twimg.com", "profile_images", /^\d+$/ => user_id, /^(\w{8})(_\w+)?\.\w+$/
+      @user_id = user_id
+      @full_image_url = "https://pbs.twimg.com/profile_images/#{user_id}/#{$1}.#{file_ext}"
+
     # https://pbs.twimg.com/ad_img/1415875929608396801/pklSzcPz?format=jpg&name=small
-    in _, "twimg.com", "ad_img" => media_type, media_id, file if params[:format].present?
+    in "pbs", "twimg.com", "ad_img" => media_type, media_id, file if params[:format].present?
       @full_image_url = "#{site}/#{media_type}/#{media_id}/#{file}?format=#{params[:format]}&name=orig"
 
     # https://video.twimg.com/tweet_video/E_8lAMJUYAIyenr.mp4
@@ -57,6 +66,11 @@ class Source::URL::Twitter < Source::URL
     # https://video.twimg.com/ext_tw_video/1496554514312269828/pu/vid/360x270/SygSrUcDpCr1AnOf.mp4?tag=12
     # https://video.twimg.com/ext_tw_video/1496554514312269828/pu/vid/960x720/wiC1XIw8QehhL5JL.mp4?tag=12
     # https://video.twimg.com/ext_tw_video/1496554514312269828/pu/vid/480x360/amWjOw0MmLdnPMPB.mp4?tag=12
+    # https://si0.twimg.com/profile_background_images/378800000179574457/3UC-Xcnj.jpeg
+    # https://si0.twimg.com/profile_images/2375708513/luajz7c7uredbbp4v9kr.jpeg
+    # https://a2.twimg.com/profile_images/1210943186/KABABABABA.jpg
+    # https://pbs-0.twimg.com/media/C9xkZf7UMAEbsf7.jpg
+    # https://p.twimg.com/A1zDhzxCMAA0vtj.jpg:large
     in _, "twimg.com", *rest
       nil
 
