f6a765eaa086c4304bbdca8322595c4933186d53
evazion <noizave@gmail.com>
2024-05-11 22:06:29 -0500

    sources: add 6 new URL shorteners.
    Add the following URL shorteners:
    
    * https://0rz.tw
    * https://dlvr.it
    * https://naver.me
    * https://pse.is
    * https://reurl.cc
    * https://x.gd
    
    These are sometimes used in commentaries.

--- test/unit/sources/url_shortener_test.rb
+++ test/unit/sources/url_shortener_test.rb
@@ -6,7 +6,7 @@ module Sources
   class URLShortenerTest < ActiveSupport::TestCase
     def self.assert_redirects_to(source_url, destination_url)
       context "redirecting #{source_url} to #{destination_url}" do
-        strategy_should_work(source_url, redirect_url: Source::URL.parse(destination_url))
+        strategy_should_work(source_url, redirect_url: destination_url)
       end
     end
 
@@ -42,40 +42,52 @@ module Sources
       end
 
       context "A valid shortened URL" do
-        assert_redirects_to("https://amzn.to/4afHvyL", "https://www.amazon.com/Portable-Shortwave-Operated-Reception-Earphone/dp/B0BPKJ1XP1")
+        assert_redirects_to("http://0rz.tw/Gwl0j", "https://www.pixiv.net/fanbox/creator/660275?utm_campaign=www_work_list&utm_medium=site_flow&utm_source=pixiv")
+        assert_redirects_to("http://amzn.to/4afHvyL", "https://www.amazon.com/Portable-Shortwave-Operated-Reception-Earphone/dp/B0BPKJ1XP1")
         assert_redirects_to("http://bit.ly/4aAVa4y", "https://fantia.jp/posts/2679869?utm_source=pixiv&utm_medium=referral")
         assert_redirects_to("http://j.mp/cKV0uf", "http://blog-imgs-32-origin.fc2.com/c/o/n/connyac/20100314032806e94.jpg")
         assert_redirects_to("http://cutt.ly/GfQ2szk", "https://www.youtube.com/channel/UCTeu1oeRYy8Kz_tpv0mASYg?view_as=subscriber")
+        assert_redirects_to("http://dlvr.it/SWKqJ0", "https://gizmodo.com/kamen-rider-zero-one-comic-ragnarok-villain-titan-1849389764?utm_source=twitter&utm_medium=SocialMarketing&utm_campaign=dlvrit&utm_content=io9")
         assert_redirects_to("http://goo.gl/wVjj5", "http://seiga.nicovideo.jp/clip/864621")
         assert_redirects_to("http://goo.gl/forms/mxcIFGtbmrVVxheW2", "https://docs.google.com/forms/d/e/1FAIpQLScyOgVE2yTG_vSpPebT7qRUwY_a2qoNzwO56IY3tOcn0g8mDg/viewform?c=0&w=1&usp=send_form")
         assert_redirects_to("http://images.app.goo.gl/5uBga7TuPKHxyyR1A", "https://www.google.com/imgres?imgurl=https://pbs.twimg.com/media/D7VjgrTUcAEdvFf.png&imgrefurl=https://twitter.com/nejimaki_neji&docid=IXigeWQkHlv6fM&tbnid=-DQ5gjY1vFJK0M:&vet=1&w=900&h=810&hl=en-us&source=sh/x/im")
         assert_redirects_to("http://photos.app.goo.gl/eHfTwV866X4Vf7Zt5", "https://photos.google.com/share/AF1QipPMW7tBlPkOmK86r7Gwq_sJVXN3RLecedSgeUKX4mGv98naklY16X4e-cY1O5V_4g?key=aEcyMno2VmFSZGMxYkRZb0x0aWllUXB6b3RMS3pn")
         assert_redirects_to("http://is.gd/UeUnvf", "https://www.pixiv.net/artworks/62476859")
         assert_redirects_to("http://ow.ly/WmrYu", "https://twitter.com/dankanemitsu/status/680792282887409664")
+        assert_redirects_to("http://naver.me/FABhCw8Z", "https://m.comic.naver.com/bestChallenge/list?titleId=696711")
         assert_redirects_to("http://pin.it/4A1N0Rd5W", "https://www.pinterest.com/pin/580612576989556785/sent/?invite_code=9e94baa7faae405d84a7787593fa46fd&sender=580612714368486682&sfo=1")
+        assert_redirects_to("http://pse.is/4b4tda", "https://twitter.com/d_art_taipei/status/1542795420132204547?s=20&t=5KZ9b0gnQro7txOAfaZB3g")
+        assert_redirects_to("http://reurl.cc/E2zlnA", "https://www.youtube.com/channel/UCM1nL55m_QImE0ZRqvGmWWQ")
         assert_redirects_to("http://t.ly/x8f4j", "https://docs.google.com/document/d/166zHw2WwtJufey71cDjfhL_1Vvga9AWbL4BtHMcJu9I/edit")
         assert_redirects_to("http://tiny.cc/6ut5vz", "https://drive.google.com/drive/folders/1SMBFYwAOq3h6rhWS5rLQdxDqLGq5OwY2")
         assert_redirects_to("http://tinyurl.com/3avx9w4r", "https://spell-breakers.blogspot.com/2023/07/schools-out-for-summer.html")
         assert_redirects_to("http://t.co/Dxn7CuVErW", "https://twitter.com/Kekeflipnote/status/1496555599718498319/video/1")
         assert_redirects_to("http://pic.twitter.com/Dxn7CuVErW", "https://twitter.com/Kekeflipnote/status/1496555599718498319/video/1")
         assert_redirects_to("http://wp.me/p32Sjo-oJ", "http://xn--t8jf3evasg9m.com/?p=1533")
+        assert_redirects_to("http://x.gd/uysub", "https://nohito.fanbox.cc/posts/7903482")
       end
 
       context "A deleted or nonexistent shortened URL" do
+        assert_redirects_to("https://0rz.tw/bad", nil)
         assert_redirects_to("https://amzn.to/bad", nil)
         assert_redirects_to("https://bit.ly/qwo9iqwe9ogqerg", nil)
         assert_redirects_to("https://cutt.ly/qwoifjqwio", nil)
+        assert_redirects_to("https://dlvr.it/bad", nil)
         assert_redirects_to("https://goo.gl/bad", nil)
         assert_redirects_to("https://goo.gl/forms/bad", nil)
         assert_redirects_to("https://photos.app.goo.gl/bad", nil)
         assert_redirects_to("https://is.gd/qwoifjqwio", nil)
         assert_redirects_to("https://ow.ly/qwoifjqwio", nil)
+        assert_redirects_to("https://naver.me/bad", nil)
         assert_redirects_to("https://pin.it/bad", nil)
+        assert_redirects_to("https://pse.is/bad", nil)
+        assert_redirects_to("https://reurl.cc/bad", nil)
         assert_redirects_to("https://t.ly/bad", nil)
         assert_redirects_to("https://tiny.cc/qwoifjqwio", nil)
         assert_redirects_to("https://tinyurl.com/qwoifjqwio", nil)
         assert_redirects_to("https://t.co/bad", nil)
         assert_redirects_to("https://wp.me/qwoifjqwio", nil)
+        assert_redirects_to("https://x.gd/bad", nil)
       end
 
       should "parse URLs correctly" do
