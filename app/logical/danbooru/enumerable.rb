db58dfca9c44750b21b49ae36aaea2c5270c80bb
hdk5 <hdk5@hdk5.xyz>
2024-08-03 01:10:48 +0300

    parallel_each: provide promises a way to cancel themselves earlier

--- app/logical/danbooru/enumerable.rb
+++ app/logical/danbooru/enumerable.rb
@@ -6,16 +6,43 @@
 # @see https://ruby-concurrency.github.io/concurrent-ruby/master/file.promises.out.html
 module Danbooru
   module Enumerable
+    # Like `#map`, but perform the block on each item in parallel.
+    def parallel_map(executor = :io, &block)
+      return enum_for(:parallel_map) unless block_given?
+
+      promises = map do |*item|
+        Concurrent::Promises.delay_on(executor, *item, &block)
+      end
+
+      Concurrent::Promises.zip_futures_on(:immediate, *promises).value!
+    end
+
     # Like `#each`, but perform the block on each item in parallel.
     def parallel_each(executor = :io, &block)
       return enum_for(:parallel_each) unless block_given?
 
-      promises = map do |*item|
-        Concurrent::Promises.future_on(executor, *item, &block)
-      end
-
-      Concurrent::Promises.zip(*promises).wait!
+      parallel_map(executor, &block)
       self
+    end
+
+    # Iterates the given block for each element with `Concurrent::AtomicBoolean` cancellation flag.
+    # If an exception occurs, the flag is set to true and pending elements yield `nil`.
+    # The cancellation flag should be checked in each iteration cycle of a loop performing significant work.
+    #
+    # @see https://ruby-concurrency.github.io/concurrent-ruby/master/Concurrent/Cancellation.html
+    def cancellable(&block)
+      return enum_for(:cancellable) unless block_given?
+
+      cancelled = Concurrent::AtomicBoolean.new false
+
+      each do |*item|
+        begin
+          yield *item, cancelled unless cancelled.true?
+        rescue
+          cancelled.make_true
+          raise
+        end
+      end
     end
   end
 end
