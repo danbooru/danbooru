928ddb863d60f524148331926960227ea831887a
hdk5 <hdk5@hdk5.xyz>
2024-09-15 22:35:18 +0300

    bluesky extractor tweaks

--- test/unit/sources/bluesky_test.rb
+++ test/unit/sources/bluesky_test.rb
@@ -44,7 +44,13 @@ module Sources
         display_name: "Baguette",
         username: "yourbaguette",
         tags: ["Art", "FanArt", "Digimon", "SteinsGate", "Omori", "FFXIV"],
-        dtext_artist_commentary_desc: "Thanks for the opportunity Bison ! \n\nI'm Baguette, and I mostly draw fanarts of whatever obsession I have ! I will move in Sweden in a week, work on my art and aim to open a little shop this year while working part time ! \n\n#Art #FanArt #Digimon #SteinsGate #Omori #FFXIV",
+        dtext_artist_commentary_desc: <<~EOS.chomp
+          Thanks for the opportunity Bison !
+
+          I'm Baguette, and I mostly draw fanarts of whatever obsession I have ! I will move in Sweden in a week, work on my art and aim to open a little shop this year while working part time !
+
+          "#Art":[https://bsky.app/hashtag/Art] "#FanArt":[https://bsky.app/hashtag/FanArt] "#Digimon":[https://bsky.app/hashtag/Digimon] "#SteinsGate":[https://bsky.app/hashtag/SteinsGate] "#Omori":[https://bsky.app/hashtag/Omori] "#FFXIV":[https://bsky.app/hashtag/FFXIV]
+        EOS
       )
     end
 
@@ -66,6 +72,20 @@ module Sources
       )
     end
 
+    context "A post with a video" do
+      strategy_should_work(
+        "https://bsky.app/profile/tuyoki.bsky.social/post/3l47ij5osb32u",
+        image_urls: %w[https://bsky.social/xrpc/com.atproto.sync.getBlob?did=did:plc:mymwxdm4zedrqufkotuxn72k&cid=bafkreih2h3eretfhugjzao5af3jc5zxfydsdyvyijhfi7ij4et55jqrqfi],
+        media_files: [{ file_size: 1_172_768 }],
+        page_url: "https://bsky.app/profile/did:plc:mymwxdm4zedrqufkotuxn72k/post/3l47ij5osb32u",
+        profile_urls: %w[https://bsky.app/profile/tuyoki.bsky.social https://bsky.app/profile/did:plc:mymwxdm4zedrqufkotuxn72k],
+        display_name: "temmie",
+        username: "tuyoki",
+        tags: [],
+        dtext_artist_commentary_desc: "victory pose"
+      )
+    end
+
     context "A post url with DID as user id" do
       strategy_should_work(
         "https://bsky.app/profile/did:plc:3jogsxcisdcdzwjobhxbav2w/post/3kkvo4d4jd32g",
@@ -81,6 +101,29 @@ module Sources
         username: "ixy",
         tags: [],
         dtext_artist_commentary_desc: "らき☆すた原作２０周年おめでとうございます",
+      )
+    end
+
+    context "A post with Unicode tags" do
+      strategy_should_work(
+        "https://bsky.app/profile/mzmanjo.bsky.social/post/3l46kshfnjt2t",
+        image_urls: %w[https://bsky.social/xrpc/com.atproto.sync.getBlob?did=did:plc:4ix5icku4nehfgpkyhtrpto6&cid=bafkreiblyhv7hrjeaft44lx2l2kdtqcru5uzqumimnlhqopjh2gvyvm7ca],
+        media_files: [{ file_size: 475_001 }],
+        page_url: "https://bsky.app/profile/did:plc:4ix5icku4nehfgpkyhtrpto6/post/3l46kshfnjt2t",
+        profile_urls: %w[https://bsky.app/profile/mzmanjo.bsky.social https://bsky.app/profile/did:plc:4ix5icku4nehfgpkyhtrpto6],
+        display_name: "アンジョー",
+        username: "mzmanjo",
+        tags: [
+          ["100日チャレンジ", "https://bsky.app/hashtag/100日チャレンジ"],
+          ["逃げ若", "https://bsky.app/hashtag/逃げ若"],
+          ["逃げ上手の若君", "https://bsky.app/hashtag/逃げ上手の若君"],
+        ],
+        dtext_artist_commentary_title: "",
+        dtext_artist_commentary_desc: <<~EOS.chomp
+          8日目 北条時行
+          "#100日チャレンジ":[https://bsky.app/hashtag/100日チャレンジ]
+          "#逃げ若":[https://bsky.app/hashtag/逃げ若] "#逃げ上手の若君":[https://bsky.app/hashtag/逃げ上手の若君]
+        EOS
       )
     end
 
