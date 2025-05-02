5c01f64b82e844322dd6ff2343f3d8a2ca1e101e
Talulah <talulah@reunion.is>
2025-05-02 06:48:58 +0000

    Fix ruffle imports.

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -1,5 +1,5 @@
 import "@ruffle-rs/ruffle/ruffle.js";
-import "@ruffle-rs/ruffle/core.ruffle.5010a85e56f5e95d73a4.js";
-import "@ruffle-rs/ruffle/core.ruffle.14bc4cd87652213df02f.js";
-import "@ruffle-rs/ruffle/72478294b75d4a239072.wasm";
-import "@ruffle-rs/ruffle/fd97f55bfd1ccd82a8d8.wasm";
+import "@ruffle-rs/ruffle/core.ruffle.d7b4e9136951d7ba899e.js";
+import "@ruffle-rs/ruffle/core.ruffle.58241492c8289478c844.js";
+import "@ruffle-rs/ruffle/4316626e20ec3b22d6c1.wasm";
+import "@ruffle-rs/ruffle/9287ce844015bfaf85c8.wasm";
