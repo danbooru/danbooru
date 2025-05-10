6b09a8b0bb7b13c8452b7e0036f6d4cb26bbed50
evazion <noizave@gmail.com>
2025-05-09 22:39:26 -0500

    bcy: remove extractor for dead site.
    BCY shut down in 2023 but image URLs still worked, so we still had support for rewriting sample
    image URLs to full size URLs. But now these images don't work any more, so we can get rid of this.

--- test/unit/sources/bcy_test.rb
+++ test/unit/sources/bcy_test.rb
@@ -5,23 +5,6 @@ require "test_helper"
 module Sources
   class BcyTest < ActiveSupport::TestCase
     context "BCY:" do
-      context "A sample image URL" do
-        strategy_should_work(
-          "https://img5.bcyimg.com/drawer/103785/post/178q3/88fdb790392d11e7b58d17da09c22716.jpg/w650",
-          image_urls: %w[http://img5.bcyimg.com/drawer/103785/post/178q3/88fdb790392d11e7b58d17da09c22716.jpg],
-          media_files: [{ file_size: 651_142 }],
-          page_url: nil,
-          profile_url: nil,
-          profile_urls: %w[],
-          artist_name: nil,
-          tag_name: nil,
-          other_names: [],
-          tags: [],
-          dtext_artist_commentary_title: "",
-          dtext_artist_commentary_desc: ""
-        )
-      end
-
       should "Parse URLs correctly" do
         assert(Source::URL.image_url?("https://img5.bcyimg.com/drawer/103785/post/178q3/88fdb790392d11e7b58d17da09c22716.jpg/w650"))
         assert(Source::URL.image_url?("https://img9.bcyimg.com/drawer/32360/post/178vu/46229ec06e8111e79558c1b725ebc9e6.jpg"))
