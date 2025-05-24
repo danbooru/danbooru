76328ea4775307ee47903cc77d2ac37288d7666e
Tom Hanks Noname <nonamethanks@users.noreply.github.com>
2025-05-24 11:54:26 +0000

    Reduce test metaprogramming

--- test/unit/sources/parser/arca_live_parser_test.rb
+++ test/unit/sources/parser/arca_live_parser_test.rb
@@ -2,19 +2,27 @@ require "test_helper"
 
 module Source::URL::Tests
   class ArcaLiveParserTest < ActiveSupport::TestCase
-    should_recognize_image_urls(
-      "https://ac2.namu.la/20221225sac2/e06dcf8edd29c597240898a6752c74dbdd0680fc932cfd0ecc898795f1db34b5.jpg?type=orig",
-    )
+    context "for image urls" do
+      should_recognize_image_urls(
+        "https://ac2.namu.la/20221225sac2/e06dcf8edd29c597240898a6752c74dbdd0680fc932cfd0ecc898795f1db34b5.jpg?type=orig",
+      )
+    end
 
-    should_recognize_page_urls(
-      "https://arca.live/b/arknights/66031722?p=1",
-    )
+    context "for page urls" do
+      should_recognize_page_urls(
+        "https://arca.live/b/arknights/66031722?p=1",
+      )
+    end
 
-    should_recognize_profile_urls(
-      "https://arca.live/u/@Si리링",
-      "https://arca.live/u/@Nauju/45320365",
-    )
+    context "for profile urls" do
+      should_recognize_profile_urls(
+        "https://arca.live/u/@Si리링",
+        "https://arca.live/u/@Nauju/45320365",
+      )
+    end
 
-    url_parser_should_work("https://arca.live/u/@%EC%9C%BE%ED%8C%8C", username: "윾파")
+    context "when parsing" do
+      url_parser_should_work("https://arca.live/u/@%EC%9C%BE%ED%8C%8C", username: "윾파")
+    end
   end
 end
--- test/unit/sources/parser/art_station_parser_test.rb
+++ test/unit/sources/parser/art_station_parser_test.rb
@@ -2,24 +2,32 @@ require "test_helper"
 
 module Source::URL::Tests
   class ArtStationParserTest < ActiveSupport::TestCase
-    should_recognize_image_urls(
-      "http://cdna.artstation.com/p/assets/images/images/005/804/224/large/titapa-khemakavat-sa-dui-srevere.jpg?1493887236",
-      "https://cdn-animation.artstation.com/p/video_sources/000/466/622/workout.mp4",
-    )
+    context "for image urls" do
+      should_recognize_image_urls(
+        "http://cdna.artstation.com/p/assets/images/images/005/804/224/large/titapa-khemakavat-sa-dui-srevere.jpg?1493887236",
+        "https://cdn-animation.artstation.com/p/video_sources/000/466/622/workout.mp4",
+      )
+    end
 
-    should_recognize_page_urls(
-      "https://www.artstation.com/artwork/ghost-in-the-shell-fandom",
-      "https://artstation.com/artwork/04XA4",
-    )
+    context "for page urls" do
+      should_recognize_page_urls(
+        "https://www.artstation.com/artwork/ghost-in-the-shell-fandom",
+        "https://artstation.com/artwork/04XA4",
+      )
+    end
 
-    should_recognize_profile_urls(
-      "https://www.artstation.com/sa-dui",
-      "https://artstation.com/artist/sa-dui",
-      "https://anubis1982918.artstation.com",
-    )
+    context "for profile urls" do
+      should_recognize_profile_urls(
+        "https://www.artstation.com/sa-dui",
+        "https://artstation.com/artist/sa-dui",
+        "https://anubis1982918.artstation.com",
+      )
+    end
 
-    url_parser_should_work("https://dudeunderscore.artstation.com/projects/NoNmD?album_id=23041", page_url: "https://www.artstation.com/artwork/NoNmD", username: "dudeunderscore")
-    url_parser_should_work("https://anubis1982918.artstation.com/projects/qPVGP/", page_url: "https://www.artstation.com/artwork/qPVGP", username: "anubis1982918")
-    url_parser_should_work("https://www.artstation.com/artwork/ghost-in-the-shell-fandom", page_url: "https://www.artstation.com/artwork/ghost-in-the-shell-fandom", username: nil)
+    context "when parsing" do
+      url_parser_should_work("https://dudeunderscore.artstation.com/projects/NoNmD?album_id=23041", page_url: "https://www.artstation.com/artwork/NoNmD", username: "dudeunderscore")
+      url_parser_should_work("https://anubis1982918.artstation.com/projects/qPVGP/", page_url: "https://www.artstation.com/artwork/qPVGP", username: "anubis1982918")
+      url_parser_should_work("https://www.artstation.com/artwork/ghost-in-the-shell-fandom", page_url: "https://www.artstation.com/artwork/ghost-in-the-shell-fandom", username: nil)
+    end
   end
 end
