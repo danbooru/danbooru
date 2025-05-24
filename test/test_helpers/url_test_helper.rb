49f0906064c48a40603fc9ab72b72d3ac911dd4d
Tom Hanks Noname <nonamethanks@users.noreply.github.com>
2025-05-24 12:52:29 +0000

    fixup

--- test/test_helpers/url_test_helper.rb
+++ test/test_helpers/url_test_helper.rb
@@ -27,6 +27,17 @@ module UrlTestHelper
       end
     end
 
+    def should_not_find_false_positives(arguments = {})
+      arguments.each do |url_type, false_positives|
+        url_type = url_type.to_s.singularize
+        false_positives.each do |false_positive|
+          should "not wrongly identify #{false_positive} as a #{url_type.tr("_", " ")}" do
+            assert_not Source::URL.public_send("#{url_type}?", false_positive)
+          end
+        end
+      end
+    end
+
     def url_parser_should_work(url, attributes = {})
       url = Source::URL.parse(url)
 
