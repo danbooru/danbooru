6e26afee6385a8abfefc6e33ecd84d4055c62edd
Tom Hanks Noname <nonamethanks@users.noreply.github.com>
2025-05-24 11:54:26 +0000

    Reduce test metaprogramming

--- test/test_helpers/url_test_helper.rb
+++ test/test_helpers/url_test_helper.rb
@@ -5,38 +5,30 @@ module UrlTestHelper
   class_methods do
     def should_recognize_image_urls(*image_urls)
       image_urls.each do |image_url|
-        context image_url do
-          should "be parsed as an image url" do
-            assert(Source::URL.image_url?(image_url))
-          end
+        should "identify #{image_url} as an image url" do
+          assert(Source::URL.image_url?(image_url))
         end
       end
     end
 
     def should_recognize_profile_urls(*profile_urls)
       profile_urls.each do |profile_url|
-        context profile_url do
-          should "be parsed as a profile url" do
-            assert(Source::URL.profile_url?(profile_url))
-          end
+        should "identify #{profile_url} as a profile url" do
+          assert(Source::URL.profile_url?(profile_url))
         end
       end
     end
 
     def should_recognize_page_urls(*page_urls)
       page_urls.each do |page_url|
-        context page_url do
-          should "be parsed as a page url" do
-            assert(Source::URL.page_url?(page_url))
-          end
+        should "identify #{page_url} as a page url" do
+          assert(Source::URL.page_url?(page_url))
         end
       end
     end
 
-    def url_parser_should_work(url, **attributes)
-      setup do
-        url = Source::URL.parse(url)
-      end
+    def url_parser_should_work(url, attributes = {})
+      url = Source::URL.parse(url)
 
       context url do
         attributes.each do |attribute, expected_value|
