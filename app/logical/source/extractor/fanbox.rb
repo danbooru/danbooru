c135a6a0ae11648936e2488d322847eb7191352a
hdk5 <hdk5@hdk5.xyz>
2025-05-28 16:32:03 +0300

    fanbox: fix sample to full image url conversion

--- app/logical/source/extractor/fanbox.rb
+++ app/logical/source/extractor/fanbox.rb
@@ -5,8 +5,10 @@ module Source
   class Extractor
     class Fanbox < Source::Extractor
       def image_urls
-        if parsed_url.image_url?
+        if parsed_url.full_image_url.present?
           [parsed_url.full_image_url]
+        elsif parsed_url.candidate_full_image_urls.present?
+          [parsed_url.candidate_full_image_urls.find { |url| http_exists?(url) } || url.to_s]
         elsif api_response.present?
           file_list
         else
