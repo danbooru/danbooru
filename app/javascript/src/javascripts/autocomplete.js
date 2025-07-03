08e08eebf06565d524c86b6b8cf036aae6bddcb4
Toks <tttoksss@gmail.com>
2025-07-03 06:03:27 +0000

    Tag autocomplete: Treat the caret as a wildcard in the middle of a term
    Previously, when autocompleting with the caret in the middle of a tag, we assumed that the user had already manually typed the whole replacement word.
    
    With this change, it allows the user to correct a word in the middle of a tag by starting to type only the first few characters of it and then get useful autocomplete results for the entire tag, including both the first few characters of the new word as well as the finished words that come after the caret.
    
    e.g. `hand_on_a|hip` would previously search for `hand_on_ahip*` and suggest nothing, but now it will search for `hand_on_a*_hip*` and suggest `hand_on_another's_hip`.

--- app/javascript/src/javascripts/autocomplete.js
+++ app/javascript/src/javascripts/autocomplete.js
@@ -148,10 +148,20 @@ Autocomplete.initialize_tag_autocomplete = function() {
 
 Autocomplete.current_term = function($input, caret = $input.get(0).selectionStart) {
   let query = $input.get(0).value;
+  let term_before_caret = query.substring(0, caret);
   let term_after_caret = query.substring(caret).match(/\S*/)[0];
-  caret += term_after_caret.length;
+  let term = term_before_caret;
+  if (term_after_caret) {
+    // If the caret is in the middle of tag, treat it as a wildcard asterisk.
+    // This allows the user to get useful autocomplete results by only typing the first few characters of a word between two other words.
+    term += "*" + term_after_caret;
+    if (!term_before_caret.includes("*") && !term_after_caret.includes("*")) {
+      // If the user did not manually type an asterisk then we need to add one at the end to simulate the normal prefix search behavior.
+      term += "*";
+    }
+  }
   let regexp = new RegExp(`^[-~(]*(${Autocomplete.tag_prefixes().join("|")})?`);
-  let match = query.substring(0, caret).match(/\S*$/)[0].replace(regexp, "").toLowerCase();
+  let match = term.match(/\S*$/)[0].replace(regexp, "").toLowerCase();
   return match;
 };
 
