danbooru/danbooru

commit 82f131b1da090b316a348633652989eb2c47c07a

blacklists: fix blacklisted posts being briefly visible.
Fix the long-standing bug where a blacklisted post would be visible for a split-second before being
hidden.

This makes all images hidden by default and then unhidden if they're not blacklisted. This may lead
to a different kind of loading jank where, instead of posts being visible for a split-second,
they're first hidden then they appear a split-second later. This will be true even if nothing is
blacklisted. If you have a very long blacklist that takes a long time to process, this will be
especially noticeable as images are only unhidden after your blacklist is processed.

This also means that if Javascript is broken, usually because the user is using a very old browser,
then images won't be visible at all.

app/components/blacklist_component/blacklist_component.scss
@@ -11,3 +11,7 @@
     filter: blur(var(--blacklist-blur-amount, 40px)) !important;
   }
 }
+
+.post-preview:not(.blacklisted), .image-container:not(.blacklisted) {
+  visibility: hidden;
+}

commit 638611eec0d252a27b7b9d2b752a0a0f3744caef

posts: fix posts being hidden after tag scripting.
Fix a regression caused by 82f131b1d where posts were hidden after being updated by a tag script.

Caused by posts not being given the `blacklisted` class to mark them as being initialized by blacklists.

Also rename the `blacklisted` CSS class to `blacklist-initialized` for clarity. This will be a
breaking change for any custom CSS or userscripts that rely on this class.

app/components/blacklist_component/blacklist_component.scss
@@ -12,6 +12,6 @@
   }
 }
 
-.post-preview:not(.blacklisted), .image-container:not(.blacklisted) {
+.post-preview:not(.blacklist-initialized), .image-container:not(.blacklist-initialized) {
   visibility: hidden;
 }

app/javascript/src/javascripts/blacklists.js
@@ -116,7 +116,7 @@ class Post {
     this.blacklist = blacklist;
     this.rules = new Set();
 
-    this.post.classList.add("blacklisted");
+    this.post.classList.add("blacklist-initialized");
     this.post.post = this; // Attach the post object to the DOM element for access with `$("#post_123").get(0).post`
   }
 
@@ -154,7 +154,7 @@ class Post {
   // Hide the post when it's blacklisted.
   hide() {
     this.post.classList.remove("blacklisted-hidden", "blacklisted-blurred");
-    this.post.classList.add("blacklisted-active", this.blacklistClass);
+    this.post.classList.add("blacklist-initialized", "blacklisted-active", this.blacklistClass);
 
     let video = this.post.querySelector("video#image");
     if (video) {
@@ -166,6 +166,7 @@ class Post {
   // Unhide the post when it's not blacklisted.
   show() {
     this.post.classList.remove("blacklisted-active", "blacklisted-hidden", "blacklisted-blurred");
+    this.post.classList.add("blacklist-initialized");
 
     let video = this.post.querySelector("video#image");
     if (video) {