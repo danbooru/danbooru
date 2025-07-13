502d2d9e1248f634d8009d7336b55019a90221cb
evazion <noizave@gmail.com>
2025-07-13 16:49:51 -0500

    bans: update UI.
    * On the manage user page, show the current ban length and allow editing the ban.
    * On the edit ban page, add an Unban button for removing the ban.
    * Style the new ban and edit ban pages similarly to the other pages on the manage user page.
    * After banning or banning a user, redirect to the user's profile instead of the ban listing.
    * Replace the show ban page with a redirect to the ban listing.

--- test/functional/bans_controller_test.rb
+++ test/functional/bans_controller_test.rb
@@ -3,30 +3,39 @@ require 'test_helper'
 class BansControllerTest < ActionDispatch::IntegrationTest
   context "A bans controller" do
     context "new action" do
-      should "render" do
-        @mod = create(:mod_user)
+      should "render for a mod" do
+        get_auth new_ban_path, create(:moderator_user)
 
-        get_auth new_ban_path, @mod
         assert_response :success
+      end
+
+      should "not render for a regular user" do
+        get_auth new_ban_path, create(:user)
+
+        assert_response 403
       end
     end
 
     context "edit action" do
-      should "render" do
-        @mod = create(:mod_user)
-        @ban = create(:ban)
+      should "render for a mod" do
+        get_auth edit_ban_path(create(:ban)), create(:moderator_user)
 
-        get_auth edit_ban_path(@ban.id), @mod
         assert_response :success
+      end
+
+      should "not render for a regular user" do
+        get_auth edit_ban_path(create(:ban)), create(:user)
+
+        assert_response 403
       end
     end
 
     context "show action" do
       should "render" do
         @ban = create(:ban)
-
         get ban_path(@ban)
-        assert_response :success
+
+        assert_redirected_to bans_path(search: { id: @ban.id })
       end
     end
 
@@ -58,7 +67,7 @@ class BansControllerTest < ActionDispatch::IntegrationTest
           @mod = create(:mod_user)
           post_auth bans_path, @mod, params: { ban: { duration: 1.day.iso8601, reason: "xxx", user_id: @user.id }}
 
-          assert_redirected_to bans_path
+          assert_redirected_to @user
           assert_equal(true, @user.reload.is_banned?)
           assert_match(/banned <@#{@user.name}> 1 day: xxx/, ModAction.last.description)
           assert_equal(@user, ModAction.last.subject)
@@ -117,29 +126,45 @@ class BansControllerTest < ActionDispatch::IntegrationTest
     end
 
     context "update action" do
-      should "update a ban" do
+      should "allow mods to update a ban" do
         @ban = create(:ban)
         @mod = create(:mod_user)
         put_auth ban_path(@ban.id), @mod, params: {ban: {reason: "xxx", duration: 1.day.iso8601}}
 
+        assert_redirected_to @ban.user
         assert_equal("xxx", @ban.reload.reason)
-        assert_redirected_to(ban_path(@ban))
+      end
+
+      should "not allow regular users to update a ban" do
+        @ban = create(:ban)
+        put_auth ban_path(@ban.id), create(:user), params: { ban: { reason: "xxx", duration: 1.day.iso8601 }}
+
+        assert_response 403
+        assert_not_equal("xxx", @ban.reload.reason)
       end
     end
 
     context "destroy action" do
-      should "destroy a ban" do
+      should "allow mods to destroy a ban" do
         @ban = create(:ban)
         @mod = create(:mod_user)
 
         assert_difference("Ban.count", -1) do
           delete_auth ban_path(@ban.id), @mod
 
-          assert_redirected_to bans_path
+          assert_redirected_to @ban.user
           assert_match(/unbanned <@#{@ban.user.name}>/, ModAction.last.description)
           assert_equal(@ban.user, ModAction.last.subject)
           assert_equal(@mod, ModAction.last.creator)
         end
+      end
+
+      should "not allow regular users to destroy a ban" do
+        @ban = create(:ban)
+        delete_auth ban_path(@ban.id), create(:user)
+
+        assert_response 403
+        assert_equal(true, @ban.reload.persisted?)
       end
     end
   end
