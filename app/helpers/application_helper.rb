ecb6f236075dace743719aa81b0c6615834ca34f
NekoAria <NekoAria@users.noreply.github.com>
2025-08-18 12:57:55 +0800

    feat(views/artist): replace wiki page links with bulk update request links for tag aliases

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -224,6 +224,10 @@ module ApplicationHelper
     link_to text, wiki_page_path(title), class: "wiki-link #{classes}", **options
   end
 
+  def link_to_bulk_update_requests(text, tag_name = text, classes: nil, **options)
+    link_to text, bulk_update_requests_path(commit: "Search", search: { status: "approved", tags_include_any: tag_name }), class: "bulk-update-link #{classes}", **options
+  end
+
   def link_to_wikis(*wiki_titles, **options)
     links = wiki_titles.map do |title|
       link_to_wiki title.tr("_", " "), title
