2676ff28cec64419c40cc15450640227ba0f7de0
Talulah <talulah@reunion.is>
2026-03-09 20:50:17 +0200

    Use upload_media_assets.user_id for indices and lookups.

--- app/models/user.rb
+++ app/models/user.rb
@@ -171,7 +171,7 @@ class User < ApplicationRecord
   has_many :tag_aliases, foreign_key: :creator_id
   has_many :tag_implications, foreign_key: :creator_id
   has_many :uploads, foreign_key: :uploader_id, dependent: :destroy
-  has_many :upload_media_assets, through: :uploads, dependent: :destroy
+  has_many :upload_media_assets, dependent: :destroy
   has_many :mod_actions, as: :subject, dependent: :destroy
   has_many :reactions, as: :model, dependent: :destroy
   has_many :site_credentials, foreign_key: :creator_id, dependent: :destroy
