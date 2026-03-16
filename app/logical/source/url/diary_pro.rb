1b0e094b0c95f49cc6e46048eab5cfeb324241e3
evazion <noizave@gmail.com>
2026-03-15 23:08:15 -0500

    diarypro: fix exception on unmatched urls.
    Fix an exception on URLs like https://johnmung.info/diarypro/diary.cgi due to a missing `else`
    clause in the `case` statement.

--- app/logical/source/url/diary_pro.rb
+++ app/logical/source/url/diary_pro.rb
@@ -5,33 +5,42 @@
 class Source::URL::DiaryPro < Source::URL
   site "Diary Pro", url: "https://www.web-liberty.net/download/diarypro/index.html"
 
-  attr_reader :work_id, :base_url
+  attr_reader :work_id, :subdirs
 
   def self.match?(url)
     # http://webknight0.sakura.ne.jp/cgi-bin/diarypro/data/upfile/9-1.jpg
     # http://www.yanbow.com/~myanie/diarypro/diary.cgi?mode=image&upfile=279-1.jpg
-    [url.subdomain, url.domain, *url.path_segments] in *, "diarypro", ("data" | "diary.cgi"), *
+    url.path_segments in *, "diarypro", ("data" | "diary.cgi"), *
   end
 
   def parse
-    case [subdomain, domain, *path_segments]
+    case path_segments
     # http://nekomataya.net/diarypro/data/upfile/66-1.jpg
     # http://www117.sakura.ne.jp/~cat_rice/diarypro/data/upfile/31-1.jpg
     # http://webknight0.sakura.ne.jp/cgi-bin/diarypro/data/upfile/9-1.jpg
-    in _, _, *subdirs, "diarypro", "data", "upfile", /^(\d+)-\d+\./
+    in *subdirs, "diarypro", "data", "upfile", /^(\d+)-\d+\./
       @work_id = $1
-      @base_url = [site, *subdirs].join("/")
+      @subdirs = subdirs
 
     # http://akimbo.sakura.ne.jp/diarypro/diary.cgi?mode=image&upfile=723-4.jpg
     # http://www.danshaku.sakura.ne.jp/cgi-bin/diarypro/diary.cgi?mode=image&upfile=56-1.jpg
     # http://www.yanbow.com/~myanie/diarypro/diary.cgi?mode=image&upfile=279-1.jpg
-    in _, _, *subdirs, "diarypro", "diary.cgi" if params[:mode] == "image" && params[:upfile].present?
+    in *subdirs, "diarypro", "diary.cgi" if params[:mode] == "image" && params[:upfile].present?
       @work_id = params[:upfile][/^\d+/]
-      @base_url = [site, *subdirs].join("/")
+      @subdirs = subdirs
+
+    # https://johnmung.info/diarypro/diary.cgi?no=31
+    in *subdirs, "diarypro", "diary.cgi" if params[:no].present?
+      @work_id = params[:no]
+      @subdirs = subdirs
+
+    # https://johnmung.info/diarypro/diary.cgi
+    else
+      nil
     end
   end
 
   def page_url
-    "#{base_url}/diarypro/diary.cgi?no=#{@work_id}" if base_url.present? && work_id.present?
+    "#{[site, *subdirs.to_a].join("/")}/diarypro/diary.cgi?no=#{@work_id}" if work_id.present?
   end
 end
--- test/unit/source/url/diary_pro_url_test.rb
+++ test/unit/source/url/diary_pro_url_test.rb
@@ -3,8 +3,21 @@ require "test_helper"
 module Source::Tests::URL
   class DiaryProUrlTest < ActiveSupport::TestCase
     context "Diary Pro URLs" do
-      should parse_url("http://nekomataya.net/diarypro/data/upfile/216-1.jpg").into(page_url: "http://nekomataya.net/diarypro/diary.cgi?no=216")
+      should be_image_url(
+        "http://nekomataya.net/diarypro/data/upfile/66-1.jpg",
+        "http://www117.sakura.ne.jp/~cat_rice/diarypro/data/upfile/31-1.jpg",
+      )
 
+      should be_page_url(
+        "https://www.johnmung.info/diarypro/diary.cgi?mode=image&upfile=32-1.jpg",
+        "https://johnmung.info/diarypro/diary.cgi?no=31",
+      )
+
+      should_not be_page_url(
+        "https://johnmung.info/diarypro/diary.cgi",
+      )
+
+      should parse_url("http://nekomataya.net/diarypro/data/upfile/216-1.jpg").into(page_url: "http://nekomataya.net/diarypro/diary.cgi?no=216")
       should parse_url("http://akimbo.sakura.ne.jp/diarypro/diary.cgi?mode=image&upfile=716-3.jpg").into(page_url: "http://akimbo.sakura.ne.jp/diarypro/diary.cgi?no=716")
     end
   end
