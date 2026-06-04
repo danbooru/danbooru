ec9a6a5cff26e2de2ed4b408a752720c4a25be27
Talulah <talulah@reunion.is>
2026-06-04 17:18:44 +0300

    Fix #6033: Add a link to the search cheatsheet near the search bar

--- app/components/help_tooltip_component.rb
+++ app/components/help_tooltip_component.rb
@@ -4,9 +4,14 @@
 class HelpTooltipComponent < ApplicationComponent
   attr_reader :icon, :tooltip_content, :link_class
 
-  def initialize(icon, tooltip_content, link_class: nil)
+  def initialize(icon, tooltip_content, link_class: nil, tooltip_options: {})
     @icon = icon
     @tooltip_content = tooltip_content
     @link_class = link_class
+    @tooltip_options = tooltip_options
+  end
+
+  def tooltip_options
+    helpers.data_attributes_for(@tooltip_options, "data", @tooltip_options)
   end
 end
