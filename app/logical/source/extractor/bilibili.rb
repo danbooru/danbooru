fe4f798404989beacb3e65b7a98d53d39f4db44b
asgembed <211016138+asgembed@users.noreply.github.com>
2026-06-20 01:01:01 +0800

    bilibili: fix `image_urls` empty bug
    Probably a recent change in Bilibili API that returns `"module_content":null`, causing this `image_urls` empty bug.

--- app/logical/source/extractor/bilibili.rb
+++ app/logical/source/extractor/bilibili.rb
@@ -344,7 +344,7 @@ module Source
 
         modules = data["modules"]
         if modules.present?
-          data["modules"] = modules.each { |mod| mod.delete("module_type") }.reduce({}, :merge)
+          data["modules"] = modules.each { |mod| mod.delete("module_type") }.reduce({}) { |mod, hash| mod.merge(hash) { |_key, v1, v2| v1 || v2 } }
         end
 
         data
