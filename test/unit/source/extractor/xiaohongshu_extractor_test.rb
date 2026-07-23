d58931e0c0f48fb7f275d32516844fa1235c0048
Tom Hanks Noname <nonamethanks@users.noreply.github.com>
2026-07-23 10:19:00 +0000

    Improve xiaohongshu tests

--- test/unit/source/extractor/xiaohongshu_extractor_test.rb
+++ test/unit/source/extractor/xiaohongshu_extractor_test.rb
@@ -48,13 +48,12 @@ module Source::Tests::Extractor
     end
 
     context "A source with no page URL" do
-      should "not raise when fetching the page" do
-        source = Source::Extractor.find("https://ci.xiaohongshu.com/1000g00828idf6nofk05g5ohki5uk137o8beqcv8")
-
-        assert_nil(source.page_url)
-        assert_nil(source.send(:page))
-        assert_nothing_raised { source.profile_urls }
-      end
+      strategy_should_work(
+        "https://ci.xiaohongshu.com/1000g00828idf6nofk05g5ohki5uk137o8beqcv8",
+        image_urls: %w[https://ci.xiaohongshu.com/1000g00828idf6nofk05g5ohki5uk137o8beqcv8],
+        media_files: [{ file_size: 561_862 }],
+        page_url: nil,
+      )
     end
 
     context "A post with multiple images" do
@@ -253,7 +252,7 @@ module Source::Tests::Extractor
 
     context "A post on rednote.com" do
       setup { skip "Xiaohongshu extractor requires credentials" unless Source::Extractor::Xiaohongshu.enabled? }
-    
+
       strategy_should_work(
         "https://www.rednote.com/explore/69d503c7000000001f007995?xsec_token=ABR7xc7xnydtN9Cqof7XXZDNQnk_bgI9LF753p_b_5IZg=",
         image_urls: %w[
