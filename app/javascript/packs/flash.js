7f99ab63ffb32eb0963880af8d2fb9605706782f
Tom Hanks Noname <nonamethanks@users.noreply.github.com>
2026-08-16 16:42:03 +0000

    Update ruffle pack references

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -1,5 +1,5 @@
 import "@ruffle-rs/ruffle/ruffle.js";
-import "@ruffle-rs/ruffle/core.ruffle.5010a85e56f5e95d73a4.js";
-import "@ruffle-rs/ruffle/core.ruffle.14bc4cd87652213df02f.js";
-import "@ruffle-rs/ruffle/72478294b75d4a239072.wasm";
-import "@ruffle-rs/ruffle/fd97f55bfd1ccd82a8d8.wasm";
+import "@ruffle-rs/ruffle/core.ruffle.0875e44536e955474b0c.js";
+import "@ruffle-rs/ruffle/core.ruffle.831c4f4a93befb9e84af.js";
+import "@ruffle-rs/ruffle/1ef41ff58c9763bed027.wasm";
+import "@ruffle-rs/ruffle/63468f5322aed2e768a8.wasm";
