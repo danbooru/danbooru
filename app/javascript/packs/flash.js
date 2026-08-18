f9ecfb672f805750d8d159e41f4eb92c1ab7db58
Tom Hanks Noname <nonamethanks@users.noreply.github.com>
2026-08-18 20:16:37 +0000

    Ruffle: dynamically discover chunk files instead of hardcoding them

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -1,5 +1,5 @@
 import "@ruffle-rs/ruffle/ruffle.js";
-import "@ruffle-rs/ruffle/core.ruffle.0875e44536e955474b0c.js";
-import "@ruffle-rs/ruffle/core.ruffle.831c4f4a93befb9e84af.js";
-import "@ruffle-rs/ruffle/1ef41ff58c9763bed027.wasm";
-import "@ruffle-rs/ruffle/63468f5322aed2e768a8.wasm";
+
+// Discover whatever chunk files ruffle ships, instead of hardcoding their hashes here.
+const ruffleChunks = require.context("@ruffle-rs/ruffle", false, /^\.\/(core\.ruffle\..*\.js|.*\.wasm)$/);
+ruffleChunks.keys().forEach(ruffleChunks);
