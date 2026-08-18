c6720ed4dfe1a777f84eec1b603fb6f28cff0691
Tom Hanks Noname <nonamethanks@users.noreply.github.com>
2026-08-18 22:24:01 +0000

    Fix #6549: Banning a post deletes the query string

--- app/controllers/moderator/post/posts_controller.rb
+++ app/controllers/moderator/post/posts_controller.rb
@@ -25,15 +25,11 @@ module Moderator
       def ban
         @post = authorize ::Post.find(params[:id])
         @post.ban!(CurrentUser.user)
-
-        respond_with(@post, notice: "Post was banned")
       end
 
       def unban
         @post = authorize ::Post.find(params[:id])
         @post.unban!(CurrentUser.user)
-
-        respond_with(@post, notice: "Post was unbanned")
       end
     end
   end
