ff14eb361ffc3a36088ea321483d6746fc3242cf
nonamethanks <hellafrickingepic@gmail.com>
2026-08-28 10:18:56 +0000

    Post Disapprovals: fix detailed rejection form not populating existing message

--- app/javascript/src/javascripts/mod_queue.js
+++ app/javascript/src/javascripts/mod_queue.js
@@ -3,8 +3,13 @@ import Utility from './utility'
 let ModQueue = {};
 
 ModQueue.detailed_rejection_dialog = function() {
-  $("#post_disapproval_post_id").val($(this).data("post-id"));
-  $("#detailed-rejection-dialog").find("form")[0].reset();
+  let $link = $(this);
+  let $form = $("#detailed-rejection-dialog").find("form");
+  $form[0].reset();
+
+  $("#post_disapproval_post_id").val($link.data("post-id"));
+  $("#post_disapproval_reason").val($link.data("reason") || "disinterest");
+  $("#post_disapproval_message").val($link.data("message") || "");
 
   Utility.dialog("Detailed Rejection", "#detailed-rejection-dialog");
   return false;
