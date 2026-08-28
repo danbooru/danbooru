921105bf0a68c27972d999936857913ca2129dde
nonamethanks <hellafrickingepic@gmail.com>
2026-08-28 11:14:51 +0000

    Fix #5435: Approver pruner doesn't send warning dmails properly
    * make sure monthly jobs run on the first of the month: before, they ran every 1 month after the cron container's restart, which could trigger at any day
    * make sure approvers get at least one week's notice before they are demoted for inactivity

--- app/logical/approver_pruner.rb
+++ app/logical/approver_pruner.rb
@@ -3,7 +3,8 @@
 # Demote all approvers who haven't approved at least 30 posts in the last 45
 # days. Moderators and recently promoted approvers are exempt. Runs as a monthly
 # maintenance task. Approvers who are facing demotion are sent a weekly warning
-# dmail first.
+# dmail first, and are only actually demoted once they've had at least a week's
+# notice.
 #
 # @see DanbooruMaintenance#monthly
 module ApproverPruner
@@ -11,6 +12,11 @@ module ApproverPruner
 
   APPROVAL_PERIOD = 45.days
   MINIMUM_APPROVALS = 30
+
+  WARNING_PERIOD = 21.days
+  MINIMUM_WARNING_PERIOD = 7.days
+
+  WARNING_TITLE = "You will lose approval privileges soon"
 
   def enabled?
     Danbooru.config.approver_pruning_enabled?.to_s.truthy?
@@ -27,11 +33,20 @@ module ApproverPruner
     end
   end
 
-  # Demote all inactive approvers
+  # Get the list of inactive approvers who received a warning dmail at least a
+  # week ago (and not so long ago that it's stale).
+  # @return [Array<User>] the list of inactive approvers due for demotion
+  def warned_inactive_approvers
+    inactive_approvers.select do |approver|
+      approver.dmails.received.exists?(title: WARNING_TITLE, created_at: WARNING_PERIOD.ago..MINIMUM_WARNING_PERIOD.ago)
+    end
+  end
+
+  # Demote all inactive approvers who have already received a warning dmail
   def prune!
     return unless enabled?
 
-    inactive_approvers.each do |user|
+    warned_inactive_approvers.each do |user|
       CurrentUser.scoped(User.system) do
         user.update!(level: User::Levels::CONTRIBUTOR)
         user.feedback.create!(category: "neutral", body: "Lost approval privileges", creator: User.system, disable_dmail_notification: true)
@@ -48,14 +63,14 @@ module ApproverPruner
   # Send a warning dmail to approvers who are pending demotion.
   def dmail_inactive_approvers!
     return unless enabled?
+    return unless Date.current >= Date.current.next_month.beginning_of_month - WARNING_PERIOD
 
     days_until_next_month = (Date.current.next_month.beginning_of_month - Date.current).to_i
-    return unless days_until_next_month <= 21
 
     inactive_approvers.each do |user|
       Dmail.create_automated(
         to: user,
-        title: "You will lose approval privileges soon",
+        title: WARNING_TITLE,
         body: <<~BODY)
           You've approved fewer than #{MINIMUM_APPROVALS} posts in the past #{APPROVAL_PERIOD.inspect}.
 
--- test/unit/approver_pruner_test.rb
+++ test/unit/approver_pruner_test.rb
@@ -7,12 +7,75 @@ class ApproverPrunerTest < ActiveSupport::TestCase
       @approver = create(:approver)
     end
 
-    should "demote inactive approvers" do
-      assert_equal([@approver.id], ApproverPruner.inactive_approvers.map(&:id))
-      assert_nothing_raised { ApproverPruner.prune! }
+    should "demote inactive approvers who have received a warning dmail" do
+      travel_to(Date.parse("2020-01-20")) { ApproverPruner.dmail_inactive_approvers! }
+
+      travel_to(Date.parse("2020-02-01")) do
+        assert_equal([@approver.id], ApproverPruner.warned_inactive_approvers.map(&:id))
+        assert_nothing_raised { ApproverPruner.prune! }
+      end
+
       assert_equal(User::Levels::CONTRIBUTOR, @approver.reload.level)
+      assert_equal(2, @approver.dmails.received.count)
+      assert_equal("Approver inactivity", @approver.dmails.received.first.title)
+    end
+
+    # @see https://github.com/danbooru/danbooru/issues/5435
+    should "not demote inactive approvers who have not received a warning dmail" do
+      assert_equal([@approver.id], ApproverPruner.inactive_approvers.map(&:id))
+      assert_equal([], ApproverPruner.warned_inactive_approvers.map(&:id))
+
+      assert_nothing_raised { ApproverPruner.prune! }
+      assert_equal(User::Levels::APPROVER, @approver.reload.level)
+      assert_equal(0, @approver.dmails.received.count)
+    end
+
+    should "not demote inactive approvers who were warned less than a week ago" do
+      travel_to(Date.parse("2020-01-20")) { ApproverPruner.dmail_inactive_approvers! }
+
+      travel_to(Date.parse("2020-01-24")) do # only 4 days after the warning
+        assert_equal([], ApproverPruner.warned_inactive_approvers.map(&:id))
+        assert_nothing_raised { ApproverPruner.prune! }
+      end
+
+      assert_equal(User::Levels::APPROVER, @approver.reload.level)
       assert_equal(1, @approver.dmails.received.count)
-      assert_equal("Approver inactivity", @approver.dmails.received.last.title)
+    end
+
+    should "demote inactive approvers who were warned a week or more ago" do
+      travel_to(Date.parse("2020-01-20")) { ApproverPruner.dmail_inactive_approvers! }
+
+      travel_to(Date.parse("2020-01-27")) do # exactly a week after the warning
+        assert_equal([@approver.id], ApproverPruner.warned_inactive_approvers.map(&:id))
+        assert_nothing_raised { ApproverPruner.prune! }
+      end
+
+      assert_equal(User::Levels::CONTRIBUTOR, @approver.reload.level)
+    end
+
+    should "not demote inactive approvers based on a warning dmail from a past cycle" do
+      # The approver went inactive and was warned once, 2 years before the
+      # current cycle below, but became active again afterwards and was never
+      # demoted. Now they've gone inactive a second time; the old warning
+      # shouldn't count towards this new cycle.
+      travel_to(Date.parse("2018-01-20")) { ApproverPruner.dmail_inactive_approvers! }
+
+      travel_to(Date.parse("2020-01-15")) do
+        assert_equal([@approver.id], ApproverPruner.inactive_approvers.map(&:id))
+        assert_equal([], ApproverPruner.warned_inactive_approvers.map(&:id))
+        assert_nothing_raised { ApproverPruner.prune! }
+      end
+      assert_equal(User::Levels::APPROVER, @approver.reload.level)
+
+      # Once they receive a fresh warning for the current cycle, demotion proceeds as normal.
+      travel_to(Date.parse("2020-01-20")) { ApproverPruner.dmail_inactive_approvers! }
+
+      travel_to(Date.parse("2020-01-27")) do
+        assert_equal([@approver.id], ApproverPruner.warned_inactive_approvers.map(&:id))
+        assert_nothing_raised { ApproverPruner.prune! }
+      end
+
+      assert_equal(User::Levels::CONTRIBUTOR, @approver.reload.level)
     end
 
     should "not demote active approvers" do
@@ -46,6 +109,33 @@ class ApproverPrunerTest < ActiveSupport::TestCase
       assert_nothing_raised { ApproverPruner.prune! }
       assert_equal(User::Levels::APPROVER, @approver.reload.level)
       assert_equal(0, @approver.dmails.received.count)
+    end
+
+    # @see https://github.com/danbooru/danbooru/issues/5435
+    should "not demote inactive approvers before the first of the month" do
+      monthly_event = Clockwork.manager.instance_variable_get(:@events).find { |event| event.to_s == "monthly" }
+
+      # Simulate the cron container restarting on some day in the middle of the
+      # month, which resets Clockwork's in-memory record of when the job last ran.
+      monthly_event.instance_variable_set(:@last, nil)
+
+      travel_to(Date.parse("2020-01-20")) { ApproverPruner.dmail_inactive_approvers! }
+
+      (Date.parse("2020-01-15")...Date.parse("2020-02-01")).each do |date|
+        travel_to(date) do
+          monthly_event.run(Time.current) if monthly_event.run_now?(Time.current)
+          perform_enqueued_jobs
+        end
+      end
+
+      assert_equal(User::Levels::APPROVER, @approver.reload.level)
+
+      travel_to(Date.parse("2020-02-01")) do
+        monthly_event.run(Time.current) if monthly_event.run_now?(Time.current)
+        perform_enqueued_jobs
+      end
+
+      assert_equal(User::Levels::CONTRIBUTOR, @approver.reload.level)
     end
   end
 end
