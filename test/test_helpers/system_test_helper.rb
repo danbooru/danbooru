730d6990b6338205b1d1a669b0a1389a46fb3048
nonamethanks <hellafrickingepic@gmail.com>
2026-08-31 13:00:26 +0000

    Expand e2e blacklist tests

--- test/test_helpers/system_test_helper.rb
+++ test/test_helpers/system_test_helper.rb
@@ -13,4 +13,16 @@ module SystemTestHelper
     fill_in "Password", with: user.password
     click_button "Login"
   end
+
+  def assert_visible(selector, **options)
+    assert_selector selector, visible: :visible, **options
+  end
+
+  def assert_hidden(selector, **options)
+    assert_selector selector, visible: :hidden, **options
+  end
+
+  def post_selector(post)
+    ".post-preview[data-id='#{post.id}']"
+  end
 end
